cb4a8070f19f64d6a80816a91db5db5368edbc01
Samuli Heljo <samuli@samuliheljo.com>
2015-03-26 11:18:02 +0000

    Improves front page

--- app/asset/js/main.js
+++ app/asset/js/main.js
@@ -1,5 +1,6 @@
 var FastClick = require('fastclick')
 require('../../ui-component/offcanvas/offcanvas')
+require('../../ui-component/tabs/tabs')
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
--- app/server/server.js
+++ app/server/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 var path = require('path')
 
 /********** Global **********/
-var port = 8080
+var port = process.env.PORT || 8080 
 var app = express()
 
 /********** Routes **********/
