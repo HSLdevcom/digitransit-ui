8ed71bb9a308445c434da7488f39689841ed301c
Hannes Junnila <hannes.junnila@gmail.com>
2015-05-03 00:30:02 +0300

    Fix masonry

--- app/component/stop-cards/MasonryComponent.js
+++ app/component/stop-cards/MasonryComponent.js
@@ -7,34 +7,74 @@ module.exports = React.createClass({
 
     masonry: false,
 
-    options: {transitionDuration: 0},
+    domChildren: [],
+
+    options: {transitionDuration: 1},
 
     reference: "masonryContainer",
 
     initializeMasonry: function(force) {
         if (!this.masonry || force) {
-            if (this.refs[this.reference]) {
-                this.masonry = new Masonry(this.refs[this.reference].getDOMNode(), this.options);
-            }
+            this.masonry = new Masonry(this.refs[this.reference].getDOMNode(), this.options);
+            this.domChildren = this.getNewDomChildren();
         }
     },
 
     getNewDomChildren: function () {
-        if (this.refs[this.reference]) {
-            var node = this.refs[this.reference].getDOMNode();
-            var children = this.options.itemSelector ? node.querySelectorAll(this.options.itemSelector) : node.children;
+        var node = this.refs[this.reference].getDOMNode();
+        var children = this.options.itemSelector ? node.querySelectorAll(this.options.itemSelector) : node.children;
 
-            return Array.prototype.slice.call(children);
-        } else {
-            return [];
+        return Array.prototype.slice.call(children);
+    },
+
+    diffDomChildren: function() {
+        var oldChildren = this.domChildren;
+        var newChildren = this.getNewDomChildren();
+
+        var removed = oldChildren.filter(function(oldChild) {
+            //return !~newChildren.indexOf(oldChild);
+        });
+
+        var added = newChildren.filter(function(newChild) {
+            return !~oldChildren.indexOf(newChild);
+        });
+
+        var moved = [];
+
+        if (removed.length === 0) {
+            moved = oldChildren.filter(function(child, index) {
+                return index !== newChildren.indexOf(child);
+            });
         }
+
+        this.domChildren = newChildren;
+
+        return {
+            old: oldChildren,
+            'new': newChildren, // fix for ie8
+            removed: removed,
+            added: added,
+            moved: moved
+        };
     },
 
     performLayout: function() {
-        if (this.masonry) {
+        var diff = this.diffDomChildren();
+
+        if (diff.removed.length > 0) {
+            this.masonry.remove(diff.removed);
             this.masonry.reloadItems();
-            this.masonry.layout();
         }
+
+        if (diff.added.length > 0) {
+            this.masonry.appended(diff.added);
+        }
+
+        if (diff.moved.length > 0) {
+            this.masonry.reloadItems();
+        }
+
+        this.masonry.layout();
     },
 
     componentDidMount: function() {
@@ -47,15 +87,18 @@ module.exports = React.createClass({
     componentDidUpdate: function() {
         if (!isBrowser) return;
 
-        this.initializeMasonry();
         this.performLayout();
     },
 
     componentWillReceiveProps: function() {
-        setTimeout(function() {
+        this._timer = setTimeout(function() {
             this.masonry.reloadItems();
             this.forceUpdate();
         }.bind(this), 0);
+    },
+
+    componentWillUnmount: function() {
+        clearTimeout(this._timer);
     },
 
     render: function () {
