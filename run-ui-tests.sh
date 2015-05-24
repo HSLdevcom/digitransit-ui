feb10c6ebc24d6c528ea8537678dddd5dbf05eb0
Hannes Junnila <hannes.junnila@gmail.com>
2015-05-24 23:30:08 +0300

    Really kill the children

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -7,15 +7,29 @@ echo "***************************"
 MOCHA_CMD=./node_modules/mocha/bin/mocha
 PHANTOMJS=./node_modules/phantomjs/bin/phantomjs
 
+killtree() {
+    local _pid=$1
+    kill -stop ${_pid} # needed to stop quickly forking parent from producing children between child killing and parent killing
+    for _child in $(ps -o pid --no-headers --ppid ${_pid}); do
+        killtree ${_child}
+    done
+    kill -TERM ${_pid}
+}
+
 if [ "$1" == "local" ]; then
   npm run dev &
+  NODE_PID=$!
   $PHANTOMJS --webdriver=4444 &
+  PHANTOMJS_PID=$!
   sleep 10 # Wait for the server to start
   TIMEOUT=10000 \
   DRIVER='local' \
   BASE_URL='http://localhost:8080' \
   $MOCHA_CMD -t 0 -R spec --compilers coffee:coffee-script/register test/*/*-spec.coffee
-  trap 'jobs -p | xargs kill' EXIT
+  EXIT=$?
+  killtree $NODE_PID
+  killtree $PHANTOMJS_PID
+  exit $EXIT
 elif [ "$1" == "saucelabs" ]; then
 
   if [ ! -f ./test/credentials.json ]; then
