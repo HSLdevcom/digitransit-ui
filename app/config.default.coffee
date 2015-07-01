a660e9d9485e192ec8e64424e0df532b2809b5f5
Hannes Junnila <hannes.junnila@gmail.com>
2015-07-01 15:02:33 +0300

    Enable themes via configuration
    Also simplify webpack config

--- app/config.default.coffee
+++ app/config.default.coffee
@@ -4,7 +4,6 @@ module.exports =
     GEOCODER: 'http://matka.hsl.fi/geocoder/'
     MAP: 'http://matka.hsl.fi/hsl-map/'
     MQTT: 'ws://213.138.147.225:1883'
-    FONT: 'http://fonts.googleapis.com/css?family=Nunito:300,400,700%7COpen+Sans+Condensed:300,700'
+    FONT: 'http://fonts.googleapis.com/css?family=Lato:300,400,700%7CPT+Sans+Narrow:400,700'
     REALTIME: 'http://matka.hsl.fi/navigator-server'
   title: 'Digitransit demo'
-
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,82 @@ var csswring = require('csswring');
 
 var port = process.env.HOT_LOAD_PORT || 9000;
 
+function getLoadersConfig(env) {
+  if (env === "development") {
+    return([
+      { test: /\.css$/, loaders: ['style', 'css', 'postcss']},
+      { test: /\.cjsx$/, loaders: ['react-hot', 'coffee', 'cjsx']},
+      { test: /\.coffee$/, loader: 'coffee' },
+      { test: /\.jsx$/, loaders: ['react-hot', 'jsx']},
+      { test: /\.scss$/,
+        loaders: [
+          'style', 'css', 'postcss',
+          'sass?includePaths[]=' + (path.resolve(__dirname, "./sass/themes", process.env.CONFIG ? process.env.CONFIG : 'default'))
+        ]
+      },
+      { test: /\.(eot|png|ttf|woff)$/, loader: 'file'}
+    ])
+  } else {
+    return([
+      //{ test: /\/app\/page\/.*\.cjsx$/, loader: 'react-router-proxy' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css!postcss")},
+      { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
+      { test: /\.coffee$/, loader: 'coffee' },
+      { test: /\.jsx$/, loader: 'jsx'},
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", 'css!postcss!sass!includePaths[]=' +
+          (path.resolve(__dirname, "./sass/themes", process.env.CONFIG ? process.env.CONFIG : 'default')))
+      },
+      { test: /\.(eot|png|ttf|woff)$/, loader: 'file'}
+    ])
+  }
+}
 
+function getPluginsConfig(env) {
+  if (env === "development") {
+    return([
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fi|sv|en/),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify("development"),
+          ROOT_PATH: JSON.stringify(process.env.ROOT_PATH ? process.env.ROOT_PATH : '/'),
+          CONFIG: JSON.stringify(process.env.CONFIG ? process.env.CONFIG : 'default')
+        }
+      }),
+      new webpack.NoErrorsPlugin()
+    ])
+  } else {
+    return([
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fi|sv|en/),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify("production"),
+          ROOT_PATH: JSON.stringify(process.env.ROOT_PATH ? process.env.ROOT_PATH : '/'),
+          CONFIG: JSON.stringify(process.env.CONFIG ? process.env.CONFIG : 'default')
+        }
+      }),
+      new webpack.PrefetchPlugin('react'),
+      new webpack.PrefetchPlugin('react-router'),
+      new webpack.PrefetchPlugin('fluxible'),
+      new webpack.PrefetchPlugin('leaflet'),
+      new webpack.optimize.OccurenceOrderPlugin(true),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        mangle: {
+          except: ['$super', '$', 'exports', 'require']
+        }
+      }),
+      new ExtractTextPlugin("css/bundle.css", {
+        allChunks: true
+      }),
+      new webpack.NoErrorsPlugin()
+    ])
+  }
+}
 
 module.exports = {
   devtool: (process.env.NODE_ENV === "development") ? 'eval' : undefined, // This is not as dirty as it looks. It just generates source maps without being crazy slow.
@@ -23,65 +98,24 @@ module.exports = {
     path: path.join(__dirname, "_static"),
     filename: 'js/bundle.js',
     chunkFilename: 'js/[id].js',
-    publicPath: (process.env.NODE_ENV === "development") ? 'http://localhost:' + port + '/' : (process.env.ROOT_PATH != undefined) ? process.env.ROOT_PATH : '/'
+    publicPath: (process.env.NODE_ENV === "development") ?
+      'http://localhost:' + port + '/' : (process.env.ROOT_PATH != undefined) ? process.env.ROOT_PATH :
+      '/'
   },
   resolveLoader: {
     modulesDirectories: ['node_modules']
   },
-  plugins: (process.env.NODE_ENV === "development") ? [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fi|sv|en/),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify("development"),
-        ROOT_PATH: JSON.stringify(process.env.ROOT_PATH ? process.env.ROOT_PATH : '/'),
-        CONFIG: JSON.stringify(process.env.CONFIG ? process.env.CONFIG : 'default')
-      }
-    }),
-    new webpack.NoErrorsPlugin()
-  ] : [
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fi|sv|en/),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify("production"),
-        ROOT_PATH: JSON.stringify(process.env.ROOT_PATH ? process.env.ROOT_PATH : '/'),
-        CONFIG: JSON.stringify(process.env.CONFIG ? process.env.CONFIG : 'default')
-      }
-    }),
-    new webpack.PrefetchPlugin('react'),
-    new webpack.PrefetchPlugin('react-router'),
-    new webpack.PrefetchPlugin('fluxible'),
-    new webpack.PrefetchPlugin('leaflet'),
-    new webpack.optimize.OccurenceOrderPlugin(true),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      mangle: {
-          except: ['$super', '$', 'exports', 'require']
-        }
-      }),
-    new ExtractTextPlugin("css/bundle.css", {
-            allChunks: true
-        }),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: getPluginsConfig(process.env.NODE_ENV),
   resolve: {
     extensions: ['', '.js', '.cjsx', '.jsx', '.coffee']
   },
   module: {
-    loaders: [
-      //{ test: /\/app\/page\/.*\.cjsx$/, loader: 'react-router-proxy' },
-      (process.env.NODE_ENV === "development") ? { test: /\.css$/, loaders: ['style', 'css', 'postcss']} : { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css!postcss")},
-      (process.env.NODE_ENV === "development") ? { test: /\.cjsx$/, loaders: ['react-hot', 'coffee', 'cjsx']} : { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
-      { test: /\.coffee$/, loader: 'coffee' },
-      (process.env.NODE_ENV === "development") ? { test: /\.jsx$/, loaders: ['react-hot', 'jsx']} : { test: /\.jsx$/, loader: 'jsx'},
-      (process.env.NODE_ENV === "development") ? { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass']} : { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass")},
-      { test: /\.(eot|png|ttf|woff)$/, loader: 'file'}
-    ]
+    loaders: getLoadersConfig(process.env.NODE_ENV)
   },
-  postcss: (process.env.NODE_ENV === "development") ? [ autoprefixer({ browsers: ['last 2 version', '> 1%', 'IE 9'] })] : [ autoprefixer({ browsers: ['last 2 version', '> 1%', 'IE 9'] }), csswring],
+  postcss: (process.env.NODE_ENV === "development") ?
+    [ autoprefixer({ browsers: ['last 2 version', '> 1%', 'IE 9'] })] :
+    [ autoprefixer({ browsers: ['last 2 version', '> 1%', 'IE 9'] }), csswring]
+  ,
   node: {
     net: "empty",
     tls: "empty"
