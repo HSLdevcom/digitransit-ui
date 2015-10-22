5ce032cf673eceb8a58358af7b7ccb9833054db3
Markku Rontu <markku.rontu@iki.fi>
2015-10-22 13:55:06 +0300

    Remove extra blank

--- test/frontpage2.js
+++ test/frontpage2.js
@@ -1,16 +1,16 @@
 module.exports = {
-  'Frontpage map loads' : function (browser) {
     browser.init();
+  'Frontpage map loads': function (browser) {
     browser.expect.element('div.leaflet-map-pane').to.be.visible;
     browser.expect.element('span.title').text.to.contain('Digitransit');
   },
 
-  'Stops menu opens' : function (browser) {
+  'Stops menu opens': function (browser) {
     browser.click('.tabs-row li:nth-child(2)');
     browser.expect.element('.frontpage-panel-wrapper').to.be.visible;
   },
 
-  'Stops menu closes' : function (browser) {
+  'Stops menu closes': function (browser) {
     browser.click('.tabs-row li:nth-child(2)');
     browser.expect.element('.frontpage-panel-wrapper').not.to.be.present;
     browser.end();
