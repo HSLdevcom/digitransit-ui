d0f25634d6d90c1fcca0f86da16fdcfb505cc4c5
Markku Rontu <markku.rontu@iki.fi>
2015-10-22 13:55:39 +0300

    Move init and end to before and after

--- test/frontpage2.js
+++ test/frontpage2.js
@@ -1,5 +1,12 @@
 module.exports = {
+  before: function (browser) {
     browser.init();
+  },
+
+  after: function (browser) {
+    browser.end();
+  },
+
   'Frontpage map loads': function (browser) {
     browser.expect.element('div.leaflet-map-pane').to.be.visible;
     browser.expect.element('span.title').text.to.contain('Digitransit');
@@ -13,5 +20,5 @@ module.exports = {
   'Stops menu closes': function (browser) {
     browser.click('.tabs-row li:nth-child(2)');
     browser.expect.element('.frontpage-panel-wrapper').not.to.be.present;
-    browser.end();
-  }};
+  }
+};
