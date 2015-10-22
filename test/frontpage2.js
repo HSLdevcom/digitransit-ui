176969f313e1a43ed2d342a426148fa5167cb38d
Markku Rontu <markku.rontu@iki.fi>
2015-10-22 14:24:31 +0300

    Check that first stop information loads and is correct

--- test/frontpage2.js
+++ test/frontpage2.js
@@ -15,6 +15,8 @@ module.exports = {
   'Stops menu opens': function (browser) {
     browser.click('.tabs-row li:nth-child(2)');
     browser.expect.element('.frontpage-panel-wrapper').to.be.visible;
+    browser.expect.element('.cards').to.be.present.before(1000);
+    browser.expect.element('.cards .card:first-child .h4').text.to.contain('MÄKELÄNRINNE');
   },
 
   'Stops menu closes': function (browser) {
