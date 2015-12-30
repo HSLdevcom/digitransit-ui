8c0fb2f572efaa4b9e184ac2714727873e29a8ba
Sami Siren <siren@foofactory.fi>
2015-12-30 13:54:00 +0200

    do not start watch when mock is to be used. handle geolocation errors

--- static/geolocation.js
+++ static/geolocation.js
@@ -1,24 +1,16 @@
-
-console.log("starting initial js positioning", new Date().getTime());
-
-
 window.retrieveGeolocation = function retrievePos(pos) {
-  console.log("initial handler", pos);
   window.digiTransitPosition = pos;
 }
 
-//todo check mock
-window.geoWatchId = navigator.geolocation.watchPosition(function geoPosition(position) {
-    console.log("Got pos:", position);
-    window.retrieveGeolocation(position);
-  }, function handleError(error) {
-    if (error.code === 1)
-      console.log("geocoder denied");
-    else if (error.code === 2) {
-      console.log("geolocation not supported")
+window.retrieveError = function retrieveError(error) {
+  window.digiTransitPositionError = error;
+}
+
+if(window.location.search.indexOf('mock') === -1){
+  window.geoWatchId = navigator.geolocation.watchPosition(function geoPosition(position) {
+      window.retrieveGeolocation(position);
+    }, function handleError(error) {
+      retrieveError(error);
     }
-    else if (error.code === 3) {
-      console.log("geolocation timeout")
-    }
-  }
-  , {enableHighAccuracy: true, timeout: 60000, maximumAge: 0});
+    , {enableHighAccuracy: true, timeout: 60000, maximumAge: 60000});
+}
