e66c010ce3aa0be0cf6efe58ea8d808a9bdb49b7
Markku Rontu <markku.rontu@iki.fi>
2016-01-12 14:25:13 +0200

    refactor: rename for consistency

--- test/api/browser-upgrade.js
+++ test/api/browser-upgrade.js
@@ -39,7 +39,7 @@ module.exports = function (browser) {
       done = url;
     }
 
-    console.log('commit=' + process.env.SNAP_COMMIT_SHORT);
+    console.log('snap_commit_short=' + process.env.SNAP_COMMIT_SHORT);
     console.log('launch_url=' + (launch_url || 'default'));
 
     browser.timeouts('script', GLOBAL_TIMEOUT_MS, function () {
