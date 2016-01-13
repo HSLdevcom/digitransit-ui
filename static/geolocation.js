e4fc313994121ce653ad644eb9fccf054928d335
Samuli Heljo <samuli@samuliheljo.com>
2016-01-13 16:39:48 +0200

    Async geolocation watchposition

--- static/geolocation.js
+++ static/geolocation.js
@@ -11,11 +11,13 @@ window.retrieveError = function retrieveError(error) {
   window.position.error = error;
 }
 
-if(window.location.search.indexOf('mock') === -1){
-  window.geoWatchId = navigator.geolocation.watchPosition(function geoPosition(position) {
-      window.retrieveGeolocation(position);
-    }, function handleError(error) {
-      window.retrieveError(error);
-    }
-    , {enableHighAccuracy: true, timeout: 60000, maximumAge: 60000});
-}
+setTimeout(function() {
+  if(window.location.search.indexOf('mock') === -1){
+    window.geoWatchId = navigator.geolocation.watchPosition(function geoPosition(position) {
+        window.retrieveGeolocation(position);
+      }, function handleError(error) {
+        window.retrieveError(error);
+      }
+      , {enableHighAccuracy: true, timeout: 60000, maximumAge: 60000});
+  }
+}, 1)
