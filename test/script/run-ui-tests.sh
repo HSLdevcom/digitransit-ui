a5f2ec62ae4a7063c8135db6dad1c25152e0eb2e
Sami Siren <siren@foofactory.fi>
2016-01-18 11:55:20 +0200

    allow running tests also without starting the server

--- test/script/run-ui-tests.sh
+++ test/script/run-ui-tests.sh
@@ -73,19 +73,37 @@ if [ "$1" == "local" ]; then
   killtree $NODE_PID
   exit $TESTSTATUS
 elif [ "$1" == "browserstack" ]; then
-  if [ "$#" -ne 3 ]; then
+  if [ "$#" -lt 3 ]; then
     echo "ERROR: You need to use BrowserStack Username and API key as parameters"
-    echo "usage: npm run test-browserstack -- BROWSERSTACK_USERNAME BROWSERSTACK_KEY"
+    echo "usage: npm run test-browserstack -- BROWSERSTACK_USERNAME BROWSERSTACK_KEY [noserver]"
     exit
   fi
+
+  if [ "$4" == "noserver" ]; then
+    echo "Not starting local server."
+    START_SERVER=0
+  else
+    echo "Starting local server."
+    START_SERVER=1
+    npm run build; CONFIG=hsl PORT=8000 npm run start &
+    NODE_PID=$!
+  fi
+
   $BROWSERSTACK_LOCAL_BINARY $3 &
   BROWSERSTACK_PID=$!
+
   # Wait for the server to start
   sleep 10
   # Then run tests
   env BROWSERSTACK_USER=$2 BROWSERSTACK_KEY=$3 $NIGHTWATCH_BINARY -c ./test/config/nightwatch.json -e bs-fx,bs-chrome,bs-iphone,bs-ie
   TESTSTATUS=$?
   # Kill Node and Browserstack tunnel
+  if [ "$START_SERVER" == "1" ]; then
+    echo "Shutting down local server"
+    killtree $NODE_PID
+  fi
+
+  echo "Shutting down Browserstack tunnel"
   killtree $BROWSERSTACK_PID
   exit $TESTSTATUS
 else
