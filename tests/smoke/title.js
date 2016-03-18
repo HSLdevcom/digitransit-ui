41eada4e33638591c1dc00e4ec0b939084c98ba9
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-03-18 13:43:29 +0100

    NRP-162 test failure

--- tests/smoke/title.js
+++ tests/smoke/title.js
@@ -2,12 +2,6 @@ module.exports = {
   'title' : function (browser) {
     browser
       .url(browser.launch_url)
-      .assert.title("Rutebanken");
-  }
-
-  'Simple itinerary search' : function(browser) {
-    browser
-      .url(browser.launch_url)
-      .assert.title("Failure");
+      .assert.title("Rutebanken  failure");
   }
 };
