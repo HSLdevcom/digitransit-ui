3aa78d4b33e18c2d97156757ba83166193d5c5c6
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-03-18 13:44:58 +0100

    Revert "NRP-162 test failurep"
    This reverts commit f2cb76852a31dbd9ab017bcb087c69f403c6c420.

--- tests/smoke/title.js
+++ tests/smoke/title.js
@@ -4,10 +4,4 @@ module.exports = {
       .url(browser.launch_url)
       .assert.title("Rutebanken");
   }
-
-  'Simple itinerary search' : function(browser) {
-    browser
-      .url(browser.launch_url)
-      .assert.title("Failure");
-  }
 };
