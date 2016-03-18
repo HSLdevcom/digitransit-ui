77508034b990a557353f5e60ce6fadf2e61fba12
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-03-18 13:30:45 +0100

    NRP-162 Fix the title test

--- tests/general/title.js
+++ tests/general/title.js
@@ -2,13 +2,6 @@ module.exports = {
   'title' : function (browser) {
     browser
       .url('http://test.rutebanken.org')
-      .waitForElementVisible('body', 1000)
-      .getTitle(function(result) {
-        console.log("Hei");
-        this.assert.equal(typeof result, 'string');
-        this.assert.equal(result.value, 'Rutebanken');
-      })
-      .end();
-
+      .assert.title("Rutebanken");
   }
 };
