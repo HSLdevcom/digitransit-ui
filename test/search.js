b9e7dbd2fdfdd5265ab039d92a3ce9c563568fca
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2016-04-14 16:27:41 +0300

    Add missing semicolons

--- test/search.js
+++ test/search.js
@@ -31,14 +31,14 @@ suite('Search', function () {
 
     it('Destination should be set to kamppi when kamppi is entered to destination, also a route search should be performed', function (browser) {
       browser.url("/");
-      setDestination(browser, "kamppi")
+      setDestination(browser, "kamppi");
       //TODO: create destination tests when summary page is ready
     });
 
     it('Route search should be run when both source and destination are set', function (browser) {
       browser.url("/");
-      setOrigin(browser, "kamppi")
-      setDestination(browser, "sampsantie 40")
+      setOrigin(browser, "kamppi");
+      setDestination(browser, "sampsantie 40");
       browser.expect.element('.itinerary-summary-row').to.be.visible.before(browser.ELEMENT_VISIBLE_TIMEOUT);
     });
 
