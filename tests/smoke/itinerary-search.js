cf73f9d5e1644a3a11d8799c92165ea303384132
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-04-20 09:36:38 +0200

    NRP-236 Update itinerary search test to match the new fake single search field. Replace som pause calls with waitForElementVisible (no reason to wait more than necesarry)

--- tests/smoke/itinerary-search.js
+++ tests/smoke/itinerary-search.js
@@ -1,23 +1,30 @@
+
+
 module.exports = {
 
     'Verify that we get a result from itinerary search' : function (browser) {
       browser
         .url(browser.launch_url)
+        .waitForElementVisible('#front-page-search-bar', browser.globals.elementVisibleTimeout)
+        .click('#front-page-search-bar')
 
         .waitForElementVisible('#origin', browser.globals.elementVisibleTimeout)
         .click('#origin')
-        .pause(100)
-        .setValue('#search-origin', "Hausmanns gate, Oslo")
+        .waitForElementVisible('#search-origin', browser.globals.elementVisibleTimeout)
+        .setValue('#search-origin', "Hausmanns gate")
         .pause(1000) //wait for suggestions
         .setValue('#search-origin', browser.Keys.ENTER)
-        .pause(100) //wait for dialog to vanish
+
+        .waitForElementVisible('#front-page-search-bar', browser.globals.elementVisibleTimeout)
+        .click('#front-page-search-bar')
+
         .waitForElementVisible('#destination', browser.globals.elementVisibleTimeout)
         .click("#destination")
-        .pause(100)
+        .waitForElementVisible('#search-destination', browser.globals.elementVisibleTimeout)
         .setValue('#search-destination', "Malerhaugveien 28, Oslo")
         .pause(1000) //wait for suggestions
         .setValue('#search-destination', browser.Keys.ENTER)
-        .pause(100) //wait for dialog to vanish
+        
         .waitForElementVisible(".itinerary-summary-row", browser.globals.itinerarySearchTimeout)
         .end();
     }
