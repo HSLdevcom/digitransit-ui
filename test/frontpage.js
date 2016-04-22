89f25530b6a6047887b5e6dafdec448c4ae0a694
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2016-04-22 16:02:55 +0300

    Port tests to ES6

--- test/frontpage.js
+++ test/frontpage.js
@@ -1,6 +1,8 @@
-var suite = require('./api/suite.js').suite;
+/* eslint no-unused-expressions: "off", prefer-arrow-callback: "off" */
+// The test framework abuses property getters
+const suite = require('./api/suite.js').suite;
 
-suite('Frontpage', function () {
+suite('Frontpage', () => {
   it('should have title', function (browser) {
     browser.expect.element('span.title').text.to.contain('Reittiopas.fi');
   });
