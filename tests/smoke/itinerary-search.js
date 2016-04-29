18f0c7f5651ef08dbb50bf7b207820496b8cd05d
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-04-29 12:21:44 +0200

    NRP-244 Remove redundant naming. Add smoke tag.

--- tests/smoke/itinerary-search.js
+++ tests/smoke/itinerary-search.js
@@ -3,22 +3,23 @@
 var searchHelper = require("../../helpers/search-helper");
 
 module.exports = {
-    'Itinerary search from Hausmanns gate to Malerhaugveien 28' : function (browser) {
+    '@tags': ['smoke'],
+    'From Hausmanns gate to Malerhaugveien 28' : function (browser) {
       var browser = browser.url(browser.launch_url);
       searchHelper.waitingItinerarySearch(browser, "Hausmanns gate", "Malerhaugveien 28, Oslo");
       browser.end();
     },
-    'Itinerary search from Hausmanns gate to Ula nord' : function (browser) {
+    'From Hausmanns gate to Ula nord' : function (browser) {
       var browser = browser.url(browser.launch_url);
       searchHelper.waitingItinerarySearch(browser, "Hausmanns gate", "Ula nord");
       browser.end();
     },
-    'Itinerary search from Røros skole to Festplassen, Bergen' : function (browser) {
+    'From Røros skole to Festplassen, Bergen' : function (browser) {
       var browser = browser.url(browser.launch_url);
       searchHelper.waitingItinerarySearch(browser, "Røros skole", "Festplassen");
       browser.end();
     },
-    'Itinerary search from Festplassen, Bergen to Scandic Alta' : function (browser) {
+    'From Festplassen, Bergen to Scandic Alta' : function (browser) {
       var browser = browser.url(browser.launch_url);
       searchHelper.waitingItinerarySearch(browser, "Festplassen", "Scandic Alta");
       browser.end();
