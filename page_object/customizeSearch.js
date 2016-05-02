f97067b51108f46e2bb9530a264abb741bddccae
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-05-02 16:46:38 +0200

    NRP-244 Make it possible to enable modality. Add test for walking only and test for airplane only.

--- page_object/customizeSearch.js
+++ page_object/customizeSearch.js
@@ -1,25 +1,41 @@
 'use strict'
 
-var modalities = ["tram", "rail", "bus", "air", "citybike", "ferry", "subway"];
+var modalities = ["bus", "tram", "rail", "subway", "ferry", "citybike", "air"];
 
 var commands = {
   clickCanvasToggle: function() {
     return this.click("@canvasToggle");
   },
   enableModality: function(modality) {
-    this.click(".btn-bar > .btn:nth-of-type(6)");
+    var index;
+    for(var i in modalities) {
+      if(modalities[i] === modality) {
+        i++;
+        var client = this;
+        this.exists(".btn-bar > ." + modality, function(selector, found) {
+          if(!found) {
+            console.log("activating modality " + modality);
+            client.click(".btn-bar > .btn:nth-of-type(" + i + ")");
+            return;
+          } else {
+            console.log("modality "+ modality + " is already activated");
+          }
+        });
+      }
+    }
   },
-  disableAllModalitiesExcept: function(exception) {
-    var modality = "tram";
+  disableAllModalitiesExcept: function(except) {
     for (var i in modalities) {
       var modality = modalities[i];
       var selector = ".btn-bar > ." + modality;
       var client = this;
 
-      if(exception !== modality) {
-        this.exists(selector, function(selector) {
-          console.log("Clicking "+selector);
-          client.click(selector);
+      if(except !== modality) {
+        this.exists(selector, function(selector, found) {
+          if(found) {
+            console.log("Clicking "+selector);
+            client.click(selector);
+          }
         });
       }
     }
@@ -27,7 +43,9 @@ var commands = {
   exists: function(selector, callback) {
     this.api.elements("css selector", selector, function(result) {
       if(result.value && result.value.length > 0 && result.value[0].ELEMENT) {
-        callback(selector);
+        callback(selector, true);
+      } else {
+        callback(selector, false);
       }
     });
   }
