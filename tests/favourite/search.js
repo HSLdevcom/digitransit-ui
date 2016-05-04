08f7f6e6527b99518414cb87621b671edbc2be0f
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-05-04 13:35:28 +0200

    NRP-244 Missing ;

--- tests/favourite/search.js
+++ tests/favourite/search.js
@@ -6,7 +6,7 @@ module.exports = {
         var browser = browser.url(browser.launch_url);
 
         var myFavourites = browser.page.myFavourites();
-        var favouriteName = "A favourite that should show up in search results"
+        var favouriteName = "A favourite that should show up in search results";
         myFavourites.saveHomeFavourite("Vestre vei 21, Asker", favouriteName);
 
         var searchFields = browser.page.searchFields();
