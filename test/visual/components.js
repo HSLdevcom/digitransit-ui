599c4809ecfe6850d796377b855ce9ef85b627c2
Hannes Junnila <hannes.junnila@gmail.com>
2016-05-13 00:04:02 +0300

    fix lint issues

--- test/visual/components.js
+++ test/visual/components.js
@@ -1,61 +1,63 @@
+/* global gemini */
+/* eslint prefer-arrow-callback: 0 */
 
-gemini.suite('components', function(parent) {
+gemini.suite('components', function (parent) {
   parent.setUrl('/styleguidelines');
 
-  gemini.suite('Departure', function(component) {
-    gemini.suite('normal', function(suite) {
+  gemini.suite('Departure', function () {
+    gemini.suite('normal', function (suite) {
       suite.setCaptureElements('#Departure .component-example:nth-of-type(1) .component')
       .ignoreElements('.realtime-icon')
-      .capture('normal')
+      .capture('normal');
     });
-    gemini.suite('added-padding', function(suite) {
+    gemini.suite('added-padding', function (suite) {
       suite.setCaptureElements('#Departure .component-example:nth-of-type(2) .component')
-      .capture('normal')
+      .capture('normal');
     });
-    gemini.suite('with-stop', function(suite) {
+    gemini.suite('with-stop', function (suite) {
       suite.setCaptureElements('#Departure .component-example:nth-of-type(3) .component')
       .capture('normal');
     });
   });
 
-  gemini.suite('DepartureTime', function(component) {
-    gemini.suite('normal', function(suite) {
+  gemini.suite('DepartureTime', function () {
+    gemini.suite('normal', function (suite) {
       suite.setCaptureElements('#DepartureTime .component-example:nth-of-type(2) .component')
-      .capture('normal')
+      .capture('normal');
     });
   });
 
-  gemini.suite('RouteNumber', function(component) {
-    gemini.suite('normal', function(suite) {
+  gemini.suite('RouteNumber', function () {
+    gemini.suite('normal', function (suite) {
       suite.setCaptureElements('#RouteNumber .component-example:nth-of-type(1) .component')
-      .capture('normal')
+      .capture('normal');
     });
-    gemini.suite('vertical', function(suite) {
+    gemini.suite('vertical', function (suite) {
       suite.setCaptureElements('#RouteNumber .component-example:nth-of-type(3) .component')
       .capture('normal');
     });
   });
 
-  gemini.suite('RouteDestination', function(component) {
+  gemini.suite('RouteDestination', function (component) {
     component.setCaptureElements('#RouteDestination .component-example:nth-of-type(1) .component')
-    .capture('normal')
+    .capture('normal');
   });
 
-  gemini.suite('StopReference', function(component) {
+  gemini.suite('StopReference', function (component) {
     component.setCaptureElements('#StopReference .component-example:nth-of-type(1) .component')
-    .capture('normal')
+    .capture('normal');
   });
 
-  gemini.suite('Distance', function(component) {
-    gemini.suite('zero', function(suite) {
+  gemini.suite('Distance', function () {
+    gemini.suite('zero', function (suite) {
       suite.setCaptureElements('#Distance .component-example:nth-of-type(1) .component')
-      .capture('normal')
+      .capture('normal');
     });
-    gemini.suite('meters', function(suite) {
+    gemini.suite('meters', function (suite) {
       suite.setCaptureElements('#Distance .component-example:nth-of-type(2) .component')
-      .capture('normal')
+      .capture('normal');
     });
-    gemini.suite('km', function(suite) {
+    gemini.suite('km', function (suite) {
       suite.setCaptureElements('#Distance .component-example:nth-of-type(3) .component')
       .capture('normal');
     });
