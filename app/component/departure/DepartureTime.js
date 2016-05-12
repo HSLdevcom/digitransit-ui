28661cad54587146510d8b9e3164b83d986025ba
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2016-05-12 15:34:02 +0300

    ES6fy DepartureTime

---
+++ app/component/departure/DepartureTime.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import cx from 'classnames';
+import Icon from '../icon/icon';
+import ComponentUsageExample from '../documentation/component-usage-example';
+import Example from '../documentation/example-data';
+import moment from 'moment';
+import intl from 'react-intl';
+const { FormattedMessage } = intl;
+
+const DepartureTime = (props, context) => {
+  let realtime;
+  if (props.realtime && !props.canceled) {
+    realtime = <Icon img="icon-icon_realtime" className="realtime-icon realtime" />;
+  }
+
+  let canceled;
+  if (props.canceled) {
+    canceled = <Icon img="icon-icon_caution" className="icon cancelation-info" />;
+  }
+
+  let shownTime;
+  const departureTime = moment(props.departureTime * 1000);
+  const currentTime = moment(props.currentTime * 1000);
+  if (departureTime.isBefore(currentTime) ||
+      departureTime.isAfter(currentTime.clone().add(20, 'minutes'))) {
+    shownTime = departureTime.format('HH:mm');
+  } else if (currentTime.diff(departureTime, 'minutes') === 0) {
+    shownTime = <FormattedMessage id="arriving-soon" defaultMessage="Now" />;
+  } else {
+    shownTime = `${departureTime.diff(currentTime, 'minutes')}
+      ${context.intl.formatMessage({ id: 'minute-short', defaultMessage: 'min' })}`;
+  }
+
+  return (
+    <span
+      style={props.style}
+      className={cx('time', {
+        realtime: props.realtime,
+        canceled: props.canceled,
+      },
+      props.className)}
+    >
+      {realtime}
+      {canceled}
+      {shownTime}
+    </span>);
+};
+
+DepartureTime.contextTypes = {
+  intl: intl.intlShape.isRequired,
+};
+
+DepartureTime.description = (
+  <div>
+    <p>
+      Display time in correct format. Displays minutes for 20 minutes,
+      otherwise in HH:mm format.
+      Also, it takes into account if the time is realtime.
+    </p>
+    <ComponentUsageExample
+      description="real time"
+    >
+      <DepartureTime
+        departureTime={Example.realtimeDeparture.stoptime}
+        realtime={Example.realtimeDeparture.realtime}
+        currentTime={Example.currentTime}
+      />
+    </ComponentUsageExample>
+    <ComponentUsageExample description="not real time" >
+      <DepartureTime
+        departureTime={Example.departure.stoptime}
+        realtime={Example.departure.realtime}
+        currentTime={Example.currentTime}
+      />
+    </ComponentUsageExample>
+  </div>);
+
+DepartureTime.propTypes = {
+  departureTime: React.PropTypes.number.isRequired,
+  realtime: React.PropTypes.bool,
+  canceled: React.PropTypes.bool,
+  currentTime: React.PropTypes.number.isRequired,
+  style: React.PropTypes.object,
+  className: React.PropTypes.string,
+};
+
+DepartureTime.displayName = 'DepartureTime';
+export default DepartureTime;
