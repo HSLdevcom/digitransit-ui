703458aa3476aef605580943419c12521fa6716e
Hannes Junnila <hannes.junnila@gmail.com>
2016-05-17 19:23:16 +0300

    Use roar features of modern JS

--- app/component/summary/TimeSelectorContainer.js
+++ app/component/summary/TimeSelectorContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import TimeActions from '../../action/time-action';
 import moment from 'moment';
 import TimeSelectors from './TimeSelectors';
@@ -7,44 +7,28 @@ import { intlShape } from 'react-intl';
 
 import debounce from 'lodash/debounce';
 
-class TimeSelectorContainer extends React.Component {
+class TimeSelectorContainer extends Component {
   static contextTypes = {
-    getStore: React.PropTypes.func.isRequired,
-    executeAction: React.PropTypes.func.isRequired,
+    getStore: PropTypes.func.isRequired,
+    executeAction: PropTypes.func.isRequired,
     intl: intlShape.isRequired,
   };
 
-  constructor(props, context) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-    this.changeTime = this.changeTime.bind(this);
-    this.changeDate = this.changeDate.bind(this);
-    this.setArriveBy = this.setArriveBy.bind(this);
-    this.state = {
-      time: context.getStore('TimeStore').getSelectedTime(),
-    };
-    this.dispatchChangedtime = debounce(
-      () => this.context.executeAction(
-        TimeActions.setSelectedTime,
-        this.state.time,
-      ), 500);
-  }
+  state = { time: this.context.getStore('TimeStore').getSelectedTime() };
 
   componentDidMount() {
-    return this.context.getStore('TimeStore').addChangeListener(this.onChange);
+    this.context.getStore('TimeStore').addChangeListener(this.onChange);
   }
 
   componentWillUnmount() {
-    return this.context.getStore('TimeStore').removeChangeListener(this.onChange);
+    this.context.getStore('TimeStore').removeChangeListener(this.onChange);
   }
 
-  onChange({ selectedTime }) {
-    return this.setState({ time: selectedTime });
-  }
+  onChange = ({ selectedTime, currentTime }) =>
+    this.setState({ time: selectedTime || currentTime });
 
-  setArriveBy(event) {
-    return this.context.executeAction(TimeActions.setArriveBy, event.target.value === 'true');
-  }
+  setArriveBy = ({ target }) =>
+    this.context.executeAction(TimeActions.setArriveBy, target.value === 'true');
 
   getDates() {
     const dates = [];
@@ -73,29 +57,21 @@ class TimeSelectorContainer extends React.Component {
     return dates;
   }
 
-  changeTime(event) {
-    this.setState(
-      {
-        time: moment(
-          `${event.target.value} ${this.state.time.format('YYYY-MM-DD')}`,
-          'H:m YYYY-MM-DD'
-        ),
-      },
-      this.dispatchChangedtime
-    );
-  }
+  dispatchChangedtime = debounce(
+    () => this.context.executeAction(
+      TimeActions.setSelectedTime,
+      this.state.time,
+    ), 500);
 
-  changeDate(event) {
-    this.setState(
-      {
-        time: moment(
-          `${this.state.time.format('H:m')} ${event.target.value}`,
-          'H:m YYYY-MM-DD'
-        ),
-      },
-      this.dispatchChangedtime
-    );
-  }
+  changeTime = ({ target }) => this.setState(
+    { time: moment(`${target.value} ${this.state.time.format('YYYY-MM-DD')}`, 'H:m YYYY-MM-DD') },
+    this.dispatchChangedtime
+  );
+
+  changeDate = ({ target }) => this.setState(
+    { time: moment(`${this.state.time.format('H:m')} ${target.value}`, 'H:m YYYY-MM-DD') },
+    this.dispatchChangedtime
+  );
 
   render() {
     const arriveBy = this.context.getStore('TimeStore').getArriveBy();
