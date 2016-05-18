047f7d5108a88aca238dd068984bcb2e15118b57
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2016-05-18 14:27:29 +0300

    Re-enable piwik for disruption info

--- app/action/DisruptionInfoAction.js
+++ app/action/DisruptionInfoAction.js
@@ -1,7 +1,15 @@
+function sendPiwik(context, status) {
+  if (context.piwik != null) {
+    context.piwik.trackEvent('Modal', 'Disruption', status);
+  }
+}
+
 export function open(actionContext) {
+  sendPiwik(actionContext, 'open');
   return actionContext.dispatch('openDisruptionInfo');
 }
 
 export function close(actionContext) {
+  sendPiwik(actionContext, 'close');
   return actionContext.dispatch('closeDisruptionInfo');
 }
