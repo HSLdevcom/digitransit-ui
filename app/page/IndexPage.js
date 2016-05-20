d72526e7c4c96f838e883379f8aaff83b3d39ea8
Markku Rontu <markku.rontu@iki.fi>
2016-05-20 15:47:40 +0300

    feat: convert front page panel to ES6

--- app/page/IndexPage.js
+++ app/page/IndexPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Config from '../config';
 import DefaultNavigation from '../component/navigation/DefaultNavigation';
-import FrontPagePanel from '../component/front-page/front-page-panel';
+import FrontPagePanel from '../component/front-page/FrontPagePanel';
 import SearchMainContainer from '../component/search/search-main-container';
 import MapWithTracking from '../component/map/MapWithTracking';
 import FeedbackPanel from '../component/feedback/feedback-panel';
@@ -22,23 +22,21 @@ class IndexPage extends React.Component {
   }
 
   componentWillMount() {
-    return this.resetToCleanState();
+    this.resetToCleanState();
   }
 
   componentDidMount() {
-    const ref = this.context.location.search;
+    const search = this.context.location.search;
 
-    if (ref != null && ref.indexOf('citybikes') >= -1) {
-      return this.context.executeAction(forceCitybikeState);
+    if (search && search.indexOf('citybikes') >= -1) {
+      this.context.executeAction(forceCitybikeState);
     }
-
-    return undefined;
   }
 
   resetToCleanState() {
     this.context.executeAction(clearDestination);
     this.context.executeAction(unsetSelectedTime);
-    return this.context.executeAction(reset);
+    this.context.executeAction(reset);
   }
 
   render() {
