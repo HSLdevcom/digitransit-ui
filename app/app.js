16cb9702ea3191b7e1d689c52ed8030f3f2e1278
Hannes Junnila <hannes.junnila@gmail.com>
2016-05-23 11:06:06 +0300

    Convert PositonStore

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ import ItinerarySearchStore from './store/itinerary-search-store';
 import ModeStore from './store/mode-store';
 import NotImplementedStore from './store/not-implemented-store';
 import OldSearchesStore from './store/old-searches-store';
-import PositionStore from './store/position-store';
+import PositionStore from './store/PositionStore';
 import PreferencesStore from './store/preferences-store';
 import RealTimeInformationStore from './store/real-time-information-store';
 import SearchStore from './store/search-store';
