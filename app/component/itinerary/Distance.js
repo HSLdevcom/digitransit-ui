4c4c9e8368a9446a670d6d4eeaea5d51270f5daf
Markku Rontu <markku.rontu@iki.fi>
2016-05-24 14:07:50 +0300

    feat: convert itinerary distance to ES6

---
+++ app/component/itinerary/Distance.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+const Distance = (props) => {
+  let approxDistance;
+
+  if (this.props.distance > 0) {
+    approxDistance = Math.round(props.distance / 50) * 50;
+
+    if (approxDistance > 50) {
+      return (
+        <FormattedMessage
+          id="approx-meters"
+          values={{ approxDistance }}
+          defaultMessage="About {approxDistance} meters"
+        />
+      );
+    }
+  }
+
+  return null;
+};
+
+Distance.propTypes = {
+  distance: PropTypes.number.isRequired,
+};
+
+export default Distance;
