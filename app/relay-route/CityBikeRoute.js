3a9acfafd1466abfc8501ee76c3dc2a742d7469b
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2016-05-27 14:02:53 +0300

    Define parameters

--- app/relay-route/CityBikeRoute.js
+++ app/relay-route/CityBikeRoute.js
@@ -1,15 +1,13 @@
 import Relay from 'react-relay';
 
 export default class CityBikeRoute extends Relay.Route {
-  /* eslint-disable graphql/template-strings */
   static queries = {
     station: () => Relay.QL`
-      query {
+      query ($stationId: String!) {
         bikeRentalStation(id: $stationId)
       }
     `,
   };
-  /* eslint-enable graphql/template-strings */
   static paramDefinitions = {
     stationId: { required: true },
   };
