f207c8ee82a76817f2509e1acfee2efd4536f7be
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-05-27 11:23:44 +0200

    NRP-380 Move itinerary searches from tag @smoke, leaving only search for Hausmanns gate to Malerhaugveien as smoke test

--- tests/itinerary-search/itinerary-search.js
+++ tests/itinerary-search/itinerary-search.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+    "@tags": ["itinerary"],
     'Origin and destination exists in instructions if suggestion is chosen': function(browser) {
         var browser = browser.url(browser.launch_url);
 
@@ -15,6 +16,27 @@ module.exports = {
         itineraryInstructions.waitForFirstItineraryInstructionColumn();
         itineraryInstructions.verifyOrigin("Hausmanns gate, Oslo");
         itineraryInstructions.verifyDestination("Malerhaugveien 28, Oslo");
+        browser.end();
+    },
+    'From Hausmanns gate to Ula nord': function(browser) {
+        var browser = browser.url(browser.launch_url);
+        var searchFields = browser.page.searchFields();
+        searchFields.itinerarySearch("Hausmanns gate", "Ula nord");
+        browser.page.itinerarySummary().waitForFirstItineraryRow();
+        browser.end();
+    },
+    'From Røros skole to Festplassen, Bergen': function(browser) {
+        var browser = browser.url(browser.launch_url);
+        var searchFields = browser.page.searchFields();
+        searchFields.itinerarySearch("Røros skole", "Festplassen");
+        browser.page.itinerarySummary().waitForFirstItineraryRow();
+        browser.end();
+    },
+    'From Festplassen, Bergen to Scandic Alta': function(browser) {
+        var browser = browser.url(browser.launch_url);
+        var searchFields = browser.page.searchFields();
+        searchFields.itinerarySearch("Festplassen", "Scandic Alta");
+        browser.page.itinerarySummary().waitForFirstItineraryRow();
         browser.end();
     }
 }
