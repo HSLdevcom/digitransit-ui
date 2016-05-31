f212a3892b92e001e071073e03338608e3869217
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-05-31 17:34:52 +0200

    NRP-301 Change to only textual feedback. Hide input when submitted.

--- app/component/itinerary-feedback/itinerary-feedback.js
+++ app/component/itinerary-feedback/itinerary-feedback.js
@@ -1,34 +1,49 @@
-import React from 'react'
-import Icon from '../icon/icon'
-import intl from 'react-intl'
-
-const FormattedMessage = intl.FormattedMessage
+import React from 'react';
+import intl from 'react-intl';
+import cx from 'classnames';
 
 export default class ItineraryFeedback extends React.Component {
   static contextTypes = {
     getStore: React.PropTypes.func.isRequired,
     executeAction: React.PropTypes.func.isRequired,
     intl: intl.intlShape.isRequired,
-    piwik: React.PropTypes.object
+    piwik: React.PropTypes.object,
   };
 
   constructor(args) {
     super(...args);
     this.state = {
-      voted: false
+      submitted: false,
+      feedbackText: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.sendFeedback = this.sendFeedback.bind(this);
   }
 
-  rate(rating) {
-    this.context.piwik.trackEvent("Feedback", "Itinerary", "Rating", rating)
-    this.state.voted = true;
+  sendFeedback() {
+    // this.context.piwik.trackEvent("Feedback", "Itinerary", "Feedback", this.state.feedbackText);
+    this.setState({submitted: true});
+  }
+
+  handleChange(event) {
+    this.state.feedbackText = event.target.value;
   }
 
   render() {
     return (
-      <div className="itinerary-feedback">
-      <div className="option" onClick={() => rate(10)}><Icon img="icon-icon_thumb-up"/></div>
-      <div className="option" onClick={() => rate(1)}><Icon img="icon-icon_thumb-down"/></div>
-    </div>);
+      <div className='itinerary-feedback'>
+          <div className={cx('form', {'submitted': this.state.submitted})}>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+             />
+            <input type="button" value="Send" onClick={this.sendFeedback}/>
+          </div>
+          <div className={cx('done', {'submitted': this.state.submitted})}>
+            Thanks!
+          </div>
+      </div>
+    );
   }
 }
