f0a209c30d6b9f31944a891f90eab90f10170a24
rrudd <raoul.udd@netlight.com>
2016-06-06 14:17:10 +0300

    Add input type browser support detection util

---
+++ app/component/util/inputTypeCheck.js
@@ -0,0 +1,38 @@
+function supportsType(inputType) {
+  if (typeof document !== 'undefined') {
+    const input = document.createElement('input');
+    input.setAttribute('type', inputType);
+    const desiredType = input.getAttribute('type');
+    let supported = false;
+    if (input.type === desiredType) {
+      supported = true;
+    }
+    input.value = 'Hello world';
+    const helloWorldAccepted = (input.value === 'Hello world');
+    switch (desiredType) {
+      case 'email':
+      case 'url':
+        if (!input.validationMessage) {
+          supported = false;
+        }
+        break;
+      case 'color':
+      case 'date':
+      case 'datetime':
+      case 'month':
+      case 'number':
+      case 'time':
+      case 'week':
+        if (helloWorldAccepted) {
+          supported = false;
+        }
+        break;
+      default:
+        break;
+    }
+    return supported;
+  }
+  return true;
+}
+
+module.exports = supportsType;
