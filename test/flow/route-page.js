9b30b0cbfcf576d13072713b61985add5f1d04f2
Hannes Junnila <hannes.junnila@gmail.com>
2016-06-09 23:57:44 +0300

    Rewrite tests to use chained syntax

--- test/flow/route-page.js
+++ test/flow/route-page.js
@@ -1,13 +1,10 @@
 const suite = require('./api/suite.js').suite;
 
 suite('Route Page', () => {
-  before((browser, done) => {
-    browser.init('/linjat/HSL:1006:1:01', done);
-  });
-
   describe('when location is known', () => {
     before((browser, done) => {
-      browser.setCurrentPosition(60.2, 24.95, 0, done);
+      browser.url('/linjat/HSL:1006:1:01')
+        .setCurrentPosition(60.2, 24.95, 0, done);
     });
 
     it('should contain walk distance to nearest stop', (browser) => {
--- test/flow/search.js
+++ test/flow/search.js
@@ -1,38 +1,38 @@
 const suite = require('./api/suite.js').suite;
 
 
-function setOrigin(browser, src) {
-  browser.fakeSearch.openSearch();
-  browser.origin.selectOrigin();
-  browser.origin.enterText(src);
-}
+const setOrigin = (browser, src) => 
+  browser.fakeSearch.openSearch()
+    .origin.selectOrigin()
+    .origin.enterText(src)
 
-function setDestination(browser, src) {
-  browser.fakeSearch.openSearch();
-  browser.destination.selectDestination();
-  browser.destination.enterText(src);
-}
+const setDestination = (browser, src) =>
+  browser.fakeSearch.openSearch()
+    .destination.selectDestination()
+    .destination.enterText(src)
 
 suite('Search', () => {
-  describe('When Origin is manually set to other than Kammpi', () => {
+  describe('When Origin is manually set to other than Kamppi', () => {
     before((browser, done) => {
       browser.setCurrentPosition(60.2, 24.95, 0, done);
     });
 
+    it('Should show show origin popup after source is entered', (browser) =>
+      setOrigin(browser, 'kamppi')
+        .fakeSearch.openSearch()
+        .origin.selectOrigin().expect.element('#search-origin').to.be.visible.before(browser.ELEMENT_VISIBLE_TIMEOUT)
+    );
+
     it('Source should be set to kamppi when kamppi is entered to source', (browser) => {
-      setOrigin(browser, 'kamppi');
-      browser.fakeSearch.openSearch();
-      browser.origin.selectOrigin();
-      browser.expect.element('#search-origin').to.be.visible
-        .before(browser.ELEMENT_VISIBLE_TIMEOUT);
-      browser.expect.element('#search-origin').value.to
-        .contain('Kamppi, long distance traffic, Helsinki');
+      browser
+        .expect.element('#search-origin').value
+        .to.contain('Kamppi, long distance traffic, Helsinki')
     });
 
     describe('After route search', () => {
       before((browser, done) => {
-        browser.url('/');
-        setOrigin(browser, 'kamppi');
+        browser.url('/')
+        setOrigin(browser, 'kamppi')
         setDestination(browser, 'sampsantie 40');
         done();
       });
@@ -42,34 +42,24 @@ suite('Search', () => {
           .before(browser.ELEMENT_VISIBLE_TIMEOUT);
       });
 
-      describe('When returning to front-page and changing origin', () => {
-        before((browser, done) => {
-          browser.back.click(() => {
-            browser.origin.popup.click(() => {
-              browser.origin.clear(() => {
-                browser.setValue('#search-origin', 'aurinkolahti');
-                browser.expect.element('#react-autowhatever-suggest--item-0').text.to.
-                  contain('Aurinkolahti, Helsinki').before(browser.ELEMENT_VISIBLE_TIMEOUT);
-                browser.click('#react-autowhatever-suggest--item-0');
-                done();
-              });
-            });
-          });
-        });
+      it('should go back to front page when clicking back', (browser) => {
+        browser.back.click().pause(500).assert.title('Reittiopas.fi');
+      });
 
-        it('Search is not done because destination is cleared', (browser) => {
+      it('should not search again when changig origin', (browser) => {
+        setOrigin(browser, 'aurinkolahti')
           // wait for dialog to vanish and possible changes to occur
-          browser.pause(500, () => browser.assert.title('Reittiopas.fi'));
-        });
+          .pause(500).assert.title('Reittiopas.fi');
       });
     });
   });
 
   describe('When position is geocoded to Lidl Kamppi', () => {
     before((browser, done) => {
-      browser.url('/?mock');
-      browser.pause(500, () =>
-        browser.setCurrentPosition(60.168201, 24.93079, 0, () => browser.pause(500, done)));
+      browser.url('/?mock')
+        .pause(500)
+        .setCurrentPosition(60.168201, 24.93079, 0)
+        .pause(500, done)
     });
 
     it('Should automatically route after position is set', (browser) => {
@@ -79,8 +69,8 @@ suite('Search', () => {
     });
 
     it('Should route to details page when first itinerary is clicked', (browser) => {
-      browser.click('.itinerary-summary-row:first-of-type');
-      browser.expect.element('.itinerary-leg-first-row').to.be.visible
+      browser.click('.itinerary-summary-row:first-of-type')
+        .expect.element('.itinerary-leg-first-row').to.be.visible
         .before(browser.ELEMENT_VISIBLE_TIMEOUT);
     });
 
