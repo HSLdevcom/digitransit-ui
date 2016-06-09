06da343d1cd2bd182b2a315d91db8c816b53f930
Hannes Junnila <hannes.junnila@gmail.com>
2016-06-10 00:09:41 +0300

    remove unneeded diff

--- test/flow/itinerary.js
+++ test/flow/itinerary.js
@@ -1,15 +1,15 @@
 const suite = require('./api/suite.js').suite;
 
 suite('Itinerary', () => {
+  before((browser, done) => {
+    browser.init(
+      '/reitti/Ratamestarinkatu%206%2C%20Helsinki%3A%3A60.1992385%2C24.9397229/' +
+      'Majurinkulma%202%2C%20Espoo%3A%3A60.211233%2C24.825391'
+      , done
+    );
+  });
+  
   describe('When we go to summary page', () => {
-    before((browser, done) => {
-      browser.init(
-        '/reitti/Ratamestarinkatu%206%2C%20Helsinki%3A%3A60.1992385%2C24.9397229/' +
-        'Majurinkulma%202%2C%20Espoo%3A%3A60.211233%2C24.825391'
-        , done
-      );
-    });
-
     it('finds a route', (browser) =>
       browser
         .expect.element('.itinerary-summary-row').to.be.visible
--- test/flow/route-page.js
+++ test/flow/route-page.js
@@ -1,10 +1,13 @@
 const suite = require('./api/suite.js').suite;
 
 suite('Route Page', () => {
+  before((browser, done) => {
+    browser.init('/linjat/HSL:1006:1:01', done);
+  });
+
   describe('when location is known', () => {
     before((browser, done) => {
-      browser.url('/linjat/HSL:1006:1:01')
-        .setCurrentPosition(60.2, 24.95, 0, done);
+      browser.setCurrentPosition(60.2, 24.95, 0, done);
     });
 
     it('should contain walk distance to nearest stop', (browser) => {
