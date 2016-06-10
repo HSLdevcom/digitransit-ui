09e26b170022c012eab0c309d8059807a4dbe42e
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-06-10 09:29:52 +0200

    Update repository url and homepage.

--- custom_commands/setGeolocation.js
+++ custom_commands/setGeolocation.js
@@ -1,6 +1,6 @@
 exports.command = function(latitude, longitude, callback) {
     this.execute(function(latitude2, longitude2) {
-        // Requires usnig digitransit-ui with the following in the URL: ?mock
+        // Requires using digitransit-ui with the following in the URL: ?mock
         window.mock.geolocation.setCurrentPosition(latitude2, longitude2);
     }, [latitude, longitude]);
     return this;
