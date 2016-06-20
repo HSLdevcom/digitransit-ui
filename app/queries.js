3c9c87528b3aeb19059315d44bf16d8fe42dd623
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2016-06-20 13:21:14 +0300

    Transform RouteMapContainer

--- app/queries.js
+++ app/queries.js
@@ -91,7 +91,7 @@ export const RoutePageFragments = {
         longName
       }
       ${require('./component/route/route-header-container').getFragment('pattern')}
-      ${require('./component/route/route-map-container').getFragment('pattern')}
+      ${require('./component/route/RouteMapContainer').default.getFragment('pattern')}
       ${require('./component/route/RouteScheduleContainer').default.getFragment('pattern')}
       ${require('./component/route/RouteStopListContainer').default.getFragment('pattern')}
     }
@@ -115,22 +115,6 @@ export const RouteHeaderFragments = {
       stops {
         name
       }
-    }
-  `,
-};
-
-export const RouteMapFragments = {
-  pattern: () => Relay.QL`
-    fragment on Pattern {
-      code
-      stops {
-        lat
-        lon
-        name
-        gtfsId
-        ${require('./component/stop-cards/stop-card-header').getFragment('stop')}
-      }
-      ${require('./component/map/route/route-line').getFragment('pattern')}
     }
   `,
 };
@@ -457,7 +441,7 @@ export const TripPageFragments = {
           longName
         }
         ${require('./component/route/route-header-container').getFragment('pattern')}
-        ${require('./component/route/route-map-container').getFragment('pattern')}
+        ${require('./component/route/RouteMapContainer').default.getFragment('pattern')}
       }
       stoptimes {
         scheduledDeparture
