6b175b9963eeb75e0f0cd0250e417969895e28d0
rrudd <raoul.udd@netlight.com>
2016-06-29 14:26:47 +0300

    refactor route alert container

--- app/component/route/RouteAlertsContainer.js
+++ app/component/route/RouteAlertsContainer.js
@@ -3,74 +3,89 @@ import Relay from 'react-relay';
 import { FormattedMessage, intlShape } from 'react-intl';
 import moment from 'moment';
 import find from 'lodash/find';
+import connectToStores from 'fluxible-addons-react/connectToStores';
 
 import RouteAlertsRow from './RouteAlertsRow';
 
-class RouteAlertsContainer extends React.Component {
 
-  getAlerts = (alerts) => {
-    const routeMode = this.props.route.type.toLowerCase();
-    const routeLine = this.props.route.shortName;
+const getAlerts = (route, currentTime, intl) => {
+  const routeMode = route.type.toLowerCase();
+  const routeLine = route.shortName;
 
-    return alerts.map(alert => {
-      const { id } = alert;
-      const startTime = moment(alert.effectiveStartDate * 1000);
-      const endTime = moment(alert.effectiveEndDate * 1000);
-      const header = find(alert.alertHeaderTextTranslations,
-                                 ['language', this.context.intl.locale]);
-      const description = find(alert.alertDescriptionTextTranslations,
-                                 ['language', this.context.intl.locale]);
-      const currentTime = moment();
-      const alertDate = endTime.format('DD.MM.YYYY');
-      const currentDate = currentTime.format('DD.MM.YYYY');
-      const day = currentDate === alertDate ?
-        <FormattedMessage id="today" defaultMessage="Today" /> : alertDate;
-      const active = endTime > currentTime;
+  return route.alerts.map(alert => {
+    const { id } = alert;
+    const header = find(alert.alertHeaderTextTranslations,
+                               ['language', intl.locale]);
+    const description = find(alert.alertDescriptionTextTranslations,
+                               ['language', intl.locale]);
+    const startTime = moment(alert.effectiveStartDate * 1000);
+    const endTime = moment(alert.effectiveEndDate * 1000);
+    const alertDate = endTime.format('DD.MM.YYYY');
+    const expired = endTime < currentTime;
 
-      return (
-        <RouteAlertsRow
-          key={id}
-          startTime={startTime}
-          endTime={endTime}
-          header={header ? header.text : null}
-          description={description ? description.text : null}
-          day={day}
-          routeMode={routeMode}
-          routeLine={routeLine}
-          active={active}
-        />);
-    });
-  }
-
-  render() {
-    const alerts = this.props.route.alerts;
-
-    if (alerts.length === 0) {
-      return (
-        <div className="no-alerts-message">
-          <FormattedMessage
-            id="disruption-info-no-alerts"
-            defaultMessage="No disruption info."
-          />
-        </div>);
+    let day;
+    switch (currentTime.diff(endTime, 'days')) {
+      case 0:
+        day = <FormattedMessage id="today" defaultMessage="Today" />;
+        break;
+      case 1:
+        day = <FormattedMessage id="yesterday" defaultMessage="Yesterday" />;
+        break;
+      default:
+        day = alertDate;
     }
 
     return (
-      <div className="route-alerts-list momentum-scroll">
-        {this.getAlerts(alerts)}
+      <RouteAlertsRow
+        key={id}
+        startTime={startTime.format('HH:mm')}
+        endTime={endTime.format('HH:mm')}
+        header={header ? header.text : null}
+        description={description ? description.text : null}
+        day={day}
+        routeMode={routeMode}
+        routeLine={routeLine}
+        expired={expired}
+      />);
+  });
+};
+
+function RouteAlertsContainer({ route, currentTime }, { intl }) {
+  if (route.alerts.length === 0) {
+    return (
+      <div className="no-alerts-message">
+        <FormattedMessage
+          id="disruption-info-no-alerts"
+          defaultMessage="No disruption info."
+        />
       </div>);
   }
+
+  return (
+    <div className="route-alerts-list momentum-scroll">
+      {getAlerts(route, currentTime, intl)}
+    </div>);
 }
 
 RouteAlertsContainer.propTypes = {
   route: React.PropTypes.object.isRequired,
+  currentTime: React.PropTypes.object,
 };
 
 RouteAlertsContainer.contextTypes = {
   intl: intlShape,
 };
 
-export default Relay.createContainer(RouteAlertsContainer,
+const RouteAlertsContainerWithTime = connectToStores(
+  RouteAlertsContainer,
+  ['TimeStore'],
+  context => ({
+    currentTime: context.getStore('TimeStore').getCurrentTime(),
+  })
+);
+
+
+export default Relay.createContainer(RouteAlertsContainerWithTime,
   {
     fragments: {
       route: () => Relay.QL`
