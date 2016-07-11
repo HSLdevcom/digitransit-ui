ab23ba5876139acb75dbcef584af4570360d62b5
Hannes Junnila <hannes.junnila@gmail.com>
2016-07-11 14:33:55 +0300

    Use proper SVG attributes

--- app/component/util/DesktopWrapper.js
+++ app/component/util/DesktopWrapper.js
@@ -20,8 +20,8 @@ function DesktopWrapper({ children, containerWidth }) {
         <div className="desktop-wrapper--child-content">
           <svg viewBox="0 0 500 1000" className="desktop-wrapper--phone">
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               fill="#222"
               d="M11 137h3v39h-3v-39zm0 84h3v72h-3v-72zm475 0h3v72h-3v-72zM11 311h3v72h-3v-72zM431
                 13.8H69c-30.3 0-55 24.6-55 55v862.4c0 30.4 24.7 55 55 55h362c30.3 0 55-24.6
@@ -33,8 +33,8 @@ function DesktopWrapper({ children, containerWidth }) {
                 33.7-15.2 33.7-33.8s-15-33.7-33.4-33.7z"
             />
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M174.6 68.7c4.3 0 7.8 3.5 7.8 7.8s-3.5 7.8-7.8 7.8-7.8-3.5-7.8-7.8 3.5-7.8
                 7.8-7.8zm75.4-30c3.2 0 6 2.7 6 6 0 3-2.8 5.8-6 5.8s-5.8-2.6-5.8-5.8c0-3.3 2.6-6
                 5.8-6zm-34 34h68c1.7 0 3 1.3 3 3v1.7c0 1.7-1.3 3-3 3h-68c-1.7 0-3-1.3-3-3v-1.8c0-1.6
