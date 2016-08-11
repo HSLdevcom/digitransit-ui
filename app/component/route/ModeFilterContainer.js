f01f3e6863acd72c638fe969fc8ce0782b21ab15
Sami Siren <siren@foofactory.fi>
2016-08-11 13:09:47 +0300

    cleanup

--- app/component/route/ModeFilterContainer.js
+++ app/component/route/ModeFilterContainer.js
@@ -1,20 +1,6 @@
-import React from 'react';
 import ModeFilter from '../util/ModeFilter';
 import ModeSelectedAction from '../../action/mode-selected-action';
 import connectToStores from 'fluxible-addons-react/connectToStores';
-
-const ModeFilterContainer = ({ modes, action }) => (
-  <ModeFilter
-    modes={modes}
-    action={action}
-    buttonClass="btn mode-nearby"
-  />
-);
-
-ModeFilterContainer.propTypes = {
-  modes: React.PropTypes.object.isRequired,
-  action: React.PropTypes.object.isRequired,
-};
 
 export default connectToStores(
   ModeFilter,
