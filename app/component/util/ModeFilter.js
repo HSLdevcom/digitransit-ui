e2e5f7b4dacf7a88152f90a70ecf5f091f16f594
Sami Siren <siren@foofactory.fi>
2016-08-11 11:34:27 +0300

    refactor

--- app/component/util/ModeFilter.js
+++ app/component/util/ModeFilter.js
@@ -13,39 +13,34 @@ class ModeFilter extends React.Component {
     executeAction: React.PropTypes.func.isRequired,
   };
 
-  getToggleButton = (type, style) => {
-    if (config.transportModes[type].availableForSelection) {
-      const icon = `${type}-withoutBox`;
-      const camelCaseType = type.charAt(0).toUpperCase() + type.slice(1);
-      const upperCaseType = type.toUpperCase();
-      const actionName = `toggle${camelCaseType}State`;
-      return (<ToggleButton
-        icon={icon}
-        onBtnClick={() => {
-          this.context.executeAction(this.props.action[actionName]);
-        }}
-        state={this.props.modes.indexOf(upperCaseType) !== -1}
-        checkedClass={type}
-        style={style}
-        className={this.props.buttonClass}
-      />);
-    }
-    return null;
-  };
+  availableModes = () => Object.keys(config.transportModes).filter(
+    (mode) => (config.transportModes[mode].availableForSelection))
 
   calcWidth = () => {
-    let numberOfModes = 0;
-
-    for (const key of Object.keys(config.transportModes)) {
-      if (config.transportModes[key].availableForSelection) {
-        numberOfModes++;
-      }
-    }
-
+    const numberOfModes = this.availableModes().length;
     return 100 / numberOfModes;
   }
 
   render = () => {
+    const ModeToggleButton = ({ type, style }) => {
+      if (config.transportModes[type].availableForSelection) {
+        const icon = `${type}-withoutBox`;
+        const camelCaseType = type.charAt(0).toUpperCase() + type.slice(1);
+        const upperCaseType = type.toUpperCase();
+        const actionName = `toggle${camelCaseType}State`;
+        return (<ToggleButton
+          icon={icon}
+          onBtnClick={() => {
+            this.context.executeAction(this.props.action[actionName]);
+          }}
+          state={this.props.modes.indexOf(upperCaseType) !== -1}
+          checkedClass={type}
+          style={style}
+          className={this.props.buttonClass}
+        />);
+      }
+      return null;
+    };
     const widthPercentage = this.calcWidth();
 
     const style = {
@@ -54,12 +49,12 @@ class ModeFilter extends React.Component {
 
     // TODO we could build the filter strictly based on config
     return (<div className="btn-bar mode-filter no-select">
-      {this.getToggleButton('bus', style)}
-      {this.getToggleButton('tram', style)}
-      {this.getToggleButton('rail', style)}
-      {this.getToggleButton('subway', style)}
-      {this.getToggleButton('ferry', style)}
-      {this.getToggleButton('citybike', style)}
+      <ModeToggleButton type="bus" style={style} />
+      <ModeToggleButton type="tram" style={style} />
+      <ModeToggleButton type="rail" style={style} />
+      <ModeToggleButton type="subway" style={style} />
+      <ModeToggleButton type="ferry" style={style} />
+      <ModeToggleButton type="citybike" style={style} />
     </div>);
   }
 }
