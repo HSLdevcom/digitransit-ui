7ee3ef2793fc3a540c793e8ae15ab9651c32ef33
Sami Siren <siren@foofactory.fi>
2016-08-18 13:33:33 +0300

    make layout changes, add doc

--- app/component/route/RouteStop.js
+++ app/component/route/RouteStop.js
@@ -6,6 +6,7 @@ import TripLink from '../trip/TripLink';
 import WalkDistance from '../itinerary/walk-distance';
 import StopCode from '../itinerary/StopCode';
 import { fromStopTime } from '../departure/DepartureTime';
+import ComponentUsageExample from '../documentation/ComponentUsageExample';
 
 const routeStopSvg = (
   <svg style={{ position: 'absolute', width: 12, height: 65, left: -14 }} >
@@ -20,8 +21,8 @@ const lastRouteStopSvg = (
   </svg>
 );
 
-const RouteStop = (props) => {
-  const vehicles = props.vehicles && props.vehicles.map((vehicle) =>
+const RouteStop = ({ vehicles, stop, mode, distance, last, currentTime }) => {
+  const vehicleTripLinks = vehicles && vehicles.map((vehicle) =>
       (<Relay.RootContainer
         key={vehicle.id}
         Component={TripLink}
@@ -42,29 +43,35 @@ const RouteStop = (props) => {
 
   return (
     <div className="route-stop row">
-      <div className="columns small-3 route-stop-now">{vehicles}</div>
-      <Link to={`/pysakit/${props.stop.gtfsId}`}>
-        <div className={`columns small-5 route-stop-name ${props.mode}`}>
-          {props.last ? lastRouteStopSvg : routeStopSvg}
-          {props.stop.name}&nbsp;
-          {props.distance &&
-            <WalkDistance
-              className="nearest-route-stop"
-              icon="icon_location-with-user"
-              walkDistance={props.distance}
-            />
-          }
-          <br />
-          <StopCode code={props.stop.code} />
-          <span className="route-stop-address">{props.stop.desc}</span>
-        </div>
-        {(
-          props.stop.stopTimesForPattern && props.stop.stopTimesForPattern.length > 0 &&
-          props.stop.stopTimesForPattern.map((stopTime) => (
-            <div key={stopTime.scheduledDeparture} className="columns small-2 route-stop-time">
-              {fromStopTime(stopTime, props.currentTime)}
+      <div className="columns small-3 route-stop-now">{vehicleTripLinks}</div>
+      <Link to={`/pysakit/${stop.gtfsId}`}>
+        <div className={`columns small-9 route-stop-name ${mode}`}>
+          <div className="row">
+            <div className="columns small-8">
+              {last ? lastRouteStopSvg : routeStopSvg}
+              {stop.name}
             </div>
-          )))}
+            {(stop.stopTimesForPattern && stop.stopTimesForPattern.length > 0 &&
+              stop.stopTimesForPattern.map((stopTime) => (
+                <div key={stopTime.scheduledDeparture} className="columns small-2 route-stop-time">
+                  {fromStopTime(stopTime, currentTime)}
+                </div>
+              )))}
+          </div>
+          <div className="row">
+            <div className="columns small-8">
+              <StopCode code={stop.code} />
+              <span className="route-stop-address">{stop.desc}</span>
+            </div>
+            <div className="columns small-2 route-stop-time">
+              {distance && <WalkDistance
+                className="nearest-route-stop"
+                icon="icon_location-with-user"
+                walkDistance={distance}
+              />}
+            </div><div className="columns small-2"></div>
+          </div>
+        </div>
       </Link>
     </div>);
 };
@@ -78,4 +85,17 @@ RouteStop.propTypes = {
   last: React.PropTypes.bool,
 };
 
+RouteStop.description = (
+  <ComponentUsageExample description="basic">
+    <RouteStop
+      stop={{ stopTimesForPattern: [{ realtime: true, realtimeState: 'UPDATED',
+      realtimeDeparture: 48796, serviceDay: 1471467600, scheduledDeparture: 48780 },
+     { realtime: false, realtimeState: 'SCHEDULED', realtimeDeparture: 49980,
+       serviceDay: 1471467600, scheduledDeparture: 49980 }],
+      gtfsId: 'HSL:1173101', lat: 60.198185699999726, lon: 24.940634400000118,
+      name: 'Asemapäällikönkatu', desc: 'Ratamestarinkatu', code: '0663' }}
+      mode="bus" distance={200} last={false} currentTime={1471515614}
+    />
+  </ComponentUsageExample>
+);
 export default RouteStop;
