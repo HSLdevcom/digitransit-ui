5c7f820967f0eecf481bfe2cf1b6597dda4ed726
Hannes Junnila <hannes.junnila@gmail.com>
2016-08-23 16:24:18 +0300

    Simplify

--- app/component/map/popups/ParkAndRideHubPopup.js
+++ app/component/map/popups/ParkAndRideHubPopup.js
@@ -1,14 +1,16 @@
 import Relay from 'react-relay';
-import ParkAndRidePopup from './ParkAndRidePopup';
 import withProps from 'recompose/withProps';
+import compact from 'lodash/compact';
+import every from 'lodash/every';
+import sumBy from 'lodash/sumBy';
+import ParkAndRidePopup from './ParkAndRidePopup';
 
-export default Relay.createContainer(withProps(props => ({
-  realtime:
-    props.facilities.reduce((prev, current) => prev && (!current || current.realtime), true),
-  maxCapacity:
-    props.facilities.reduce((prev, current) => prev + (current ? current.maxCapacity : 0), 0),
-  spacesAvailable:
-    props.facilities.reduce((prev, current) => prev + (current ? current.spacesAvailable : 0), 0),
+export default Relay.createContainer(withProps(({ facilities }) => ({
+  // compact removes any falseisch values from the array
+  // (bike parks are included in the hub but return null from graphQL carParks)
+  realtime: every(compact(facilities), 'realtime'),
+  maxCapacity: sumBy(compact(facilities), 'maxCapacity'),
+  spacesAvailable: sumBy(compact(facilities), 'spacesAvailable'),
 }))(ParkAndRidePopup), {
   fragments: {
     facilities: () => Relay.QL`
