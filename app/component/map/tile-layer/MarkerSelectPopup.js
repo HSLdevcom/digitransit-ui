7991a13e1bae7b4e16e666d5192ef1134d44430e
rrudd <raoul.udd@netlight.com>
2016-08-24 09:58:48 +0300

    add momentum scroll to cluster popup and disable x-axis scrolling

--- app/component/map/tile-layer/MarkerSelectPopup.js
+++ app/component/map/tile-layer/MarkerSelectPopup.js
@@ -43,7 +43,10 @@ function MarkerSelectPopup(props) {
         <FormattedMessage id="choose-stop" defaultMessage="Choose stop" />
       </h3>
       <hr className="no-margin gray" />
-      <div style={{ maxHeight: 176, overflow: 'scroll' }}>
+      <div
+        className="scrollable momentum-scroll"
+        style={{ maxHeight: 176, overflowY: 'scroll' }}
+      >
         {rows}
       </div>
     </div>
