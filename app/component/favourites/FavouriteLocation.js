7bb95a6f74655491338573f559360660b22e744e
Sami Siren <siren@foofactory.fi>
2016-08-31 09:16:19 +0300

    make visual changes

--- app/component/favourites/FavouriteLocation.js
+++ app/component/favourites/FavouriteLocation.js
@@ -10,6 +10,7 @@ import RouteNumber from '../departure/RouteNumber';
 import { favouriteLocation as favouriteLocationExample } from '../documentation/ExampleData';
 
 const FavouriteLocation = (props) => {
+  console.log('props', props);
   if (!props.locationName) {
     return (
       <Link
@@ -26,21 +27,10 @@ const FavouriteLocation = (props) => {
     );
   }
 
-  let arrivalTime;
   let departureTime;
   let firstTransitLeg;
   const timeIsNotPast = props.currentTime < props.departureTime;
-  if (props.arrivalTime && timeIsNotPast) {
-    arrivalTime = (
-      <DepartureTime
-        departureTime={props.arrivalTime}
-        realtime={false}
-        currentTime={props.currentTime}
-      />
-    );
-  } else {
-    arrivalTime = <div className="favourite-location-content-placeholder">--:--</div>;
-  }
+
   if (props.departureTime && timeIsNotPast) {
     departureTime = (
       <DepartureTime
@@ -63,29 +53,22 @@ const FavouriteLocation = (props) => {
       />
     );
   }
-  return (
+  return (<div style={{ position: 'relative' }}>
     <div
-      className={cx('favourite-location-content', props.className)}
       onClick={() => props.clickFavourite(props.locationName, props.lat, props.lon)}
+      className={cx('favourite-location-content', props.className)}
     >
-      <div className="favourite-location-header">{props.locationName}</div>
       <div className="favourite-location-arrival">
-        <nobr>
-          <span className="favourite-location-icon">
-            <Icon img={props.favouriteLocationIconId} />
-          </span>
-          <span className="favourite-location-arrival-time">{arrivalTime}</span>
-        </nobr>
+        <Icon className="favourite-location-icon" img={props.favouriteLocationIconId} />
+        <div className="favourite-location-name">{props.locationName}</div>
       </div>
-      <div className="favourite-location-departure">
-        {departureTime}
-        <nobr>
-          <Icon img="icon-icon_arrow-right" className="favourite-location-arrow" />
-          <Icon img="icon-icon_walk" className="favourite-location-departure-icon" />
-        </nobr>
-        {firstTransitLeg}
+      <div className="favourite-location-departure">{firstTransitLeg}&nbsp;{departureTime}
       </div>
     </div>
+    <div className="favourite-edit-icon-click-area">
+      <Icon className="favourite-edit-icon" img="icon-icon_edit" />
+    </div>
+  </div>
   );
 };
 
@@ -97,7 +80,6 @@ FavouriteLocation.description = (
         clickFavourite={() => {}}
         locationName={favouriteLocationExample.locationName}
         favouriteLocationIconId="icon-icon_place"
-        arrivalTime={favouriteLocationExample.arrivalTime}
         departureTime={favouriteLocationExample.departureTime}
         currentTime={favouriteLocationExample.currentTime}
         firstTransitLeg={favouriteLocationExample.firstTransitLeg}
@@ -112,7 +94,6 @@ FavouriteLocation.propTypes = {
   className: React.PropTypes.string,
   locationName: React.PropTypes.string,
   favouriteLocationIconId: React.PropTypes.string,
-  arrivalTime: React.PropTypes.number,
   departureTime: React.PropTypes.number,
   currentTime: React.PropTypes.number,
   firstTransitLeg: React.PropTypes.object,
--- app/component/favourites/FavouriteLocationContainer.js
+++ app/component/favourites/FavouriteLocationContainer.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import Relay from 'react-relay';
-import head from 'lodash/head';
 import find from 'lodash/find';
 import FavouriteLocation from './FavouriteLocation';
 import config from '../../config';
@@ -8,14 +7,14 @@ import config from '../../config';
 
 const FavouriteLocationContainer = ({ currentTime, onClickFavourite, plan, favourite }) => {
   const itinerary = (plan && plan.plan.itineraries[0]) || {};
-  const firstTransitLeg = head(find(itinerary.legs, leg => leg.transitLeg));
+  const firstTransitLeg = find(itinerary.legs, leg => (leg.transitLeg));
 
   return (<FavouriteLocation
     locationName={favourite.locationName}
     favouriteLocationIconId={favourite.selectedIconId}
     lat={favourite.lat} lon={favourite.lon}
     clickFavourite={onClickFavourite}
-    departureTime={itinerary.startTime / 1000} arrivalTime={itinerary.endTime / 1000}
+    departureTime={firstTransitLeg.startTime / 1000}
     currentTime={currentTime} firstTransitLeg={firstTransitLeg}
   />);
 };
@@ -54,6 +53,7 @@ export default Relay.createContainer(FavouriteLocationContainer, {
               realTime
               transitLeg
               mode
+              startTime
               route {
                 shortName
               }
