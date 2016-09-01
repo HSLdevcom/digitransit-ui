a75a1e4dedc7eeb7c8d83ad676eabda181b07842
Samuli Heljo <samuli@samuliheljo.com>
2016-09-01 11:49:39 +0300

    Add eslint workaround comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
       'env': 'relay',
       'schemaJson': require('./build/schema.json'),
     }],
+    // Temporary fix for: https://github.com/babel/babel-eslint/issues/316
     'generator-star-spacing': 0,
   },
   settings: {
