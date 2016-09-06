39904f43c29228d961b700c47e0e69153d37186b
Sami Siren <siren@foofactory.fi>
2016-09-06 13:12:36 +0300

    cleanup

--- app/component/favourites/FavouriteLocation.js
+++ app/component/favourites/FavouriteLocation.js
@@ -50,7 +50,7 @@ const FavouriteLocation = ({ favourite, className, currentTime, departureTime,
       <div className="favourite-location-departure">{transitLeg}&nbsp;{departureTimeComponent}
       </div>
       <Link
-        onClick={(e) => { e.stopPropagation(); console.log('clicked'); return false; }}
+        onClick={(e) => { e.stopPropagation(); }}
         to={`/suosikki/muokkaa/${id}`}
         className="cursor-pointer no-decoration"
       ><div className="favourite-edit-icon-click-area" >
