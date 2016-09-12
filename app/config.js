c48f348f16d3e77a2c3fc8ce0feced13b2729b9d
Hannes Junnila <hannes.junnila@gmail.com>
2016-09-12 15:44:36 +0300

    Merge config files

--- app/config.js
+++ app/config.js
@@ -1,2 +1,17 @@
 // This is server render. Return correct config file.
-export default require(`./config.${process.env.CONFIG || 'default'}`);
+import mergeWith from 'lodash/mergeWith';
+import defaultConfig from './config.default';
+
+let additionalConfig;
+
+if (process.env.CONFIG && process.env.CONFIG !== 'default') {
+  // eslint-disable-next-line global-require
+  additionalConfig = require(`./config.${process.env.CONFIG}`);
+}
+
+function customizer(objValue, srcValue) {
+  if (Array.isArray(objValue)) { return srcValue; } // Return only latest if array
+  return undefined; // Otherwise use default customizer
+}
+
+export default mergeWith({}, defaultConfig, additionalConfig, customizer);
