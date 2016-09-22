65910fe78380cdb07f73c10f9daf38db8b7a6f80
Markku Rontu <markku.rontu@iki.fi>
2016-09-22 14:20:07 +0300

    feat: change test addresses
    Previous addresses were in English and don't work with the current dev
    Pelias. These should be more stable addresses.

--- test/flow/tests/itinerary-search/walk.js
+++ test/flow/tests/itinerary-search/walk.js
@@ -3,7 +3,7 @@ module.exports = {
   'Walk in the park': (browser) => {
     browser.url(browser.launch_url);
 
-    browser.page.searchFields().itinerarySearch('Helsinki central railway', 'Kaisaniemen puisto');
+    browser.page.searchFields().itinerarySearch('Helsingin rautatieasema', 'Kaisaniemen puisto');
 
     const customizeSearch = browser.page.customizeSearch();
     customizeSearch.clickCanvasToggle();
