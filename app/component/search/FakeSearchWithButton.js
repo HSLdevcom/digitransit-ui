1c682c88f0a23b8a82b4e0e14e92925294008d9b
Sami Siren <siren@foofactory.fi>
2016-09-27 09:43:09 +0300

    destructure

--- app/component/search/FakeSearchWithButton.js
+++ app/component/search/FakeSearchWithButton.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import pure from 'recompose/pure';
 import Icon from '../icon/icon';
 import ComponentUsageExample from '../documentation/ComponentUsageExample';
 import FakeSearchBar from './FakeSearchBar';
 
-
-const FakeSearchWithButton = pure((props) => (
+const FakeSearchWithButton = ({ fakeSearchBar, onClick }) => (
   <div className="row search-form">
     <div className="small-12 columns search-form-map-overlay">
       <div className="row collapse postfix-radius">
         <div className="small-11 columns">
-          {props.fakeSearchBar}
+          {fakeSearchBar}
         </div>
-        <div className="small-1 columns" onClick={props.onClick}>
+        <div className="small-1 columns" onClick={onClick}>
           <span className="postfix search cursor-pointer button-icon">
             <Icon img="icon-icon_search" />
           </span>
@@ -20,23 +18,24 @@ const FakeSearchWithButton = pure((props) => (
       </div>
     </div>
   </div>
-));
+);
 
 FakeSearchWithButton.propTypes = {
   fakeSearchBar: React.PropTypes.object.isRequired,
   onClick: React.PropTypes.func,
 };
 
+FakeSearchWithButton.displayName = 'FakeSearchWithButton';
+
 FakeSearchWithButton.description = () => (
-
-
   <div>
+    <p>
+      Visual search component that acts as a link to search dialog.
+    </p>
     <ComponentUsageExample description="Centered fake search field with search icon button">
-      <div>
-        <FakeSearchWithButton fakeSearchBar={<FakeSearchBar placeholder="Enter address" />} />
-      </div>
+      <FakeSearchWithButton fakeSearchBar={<FakeSearchBar placeholder="Enter address" />} />
     </ComponentUsageExample>
   </div>);
 
-FakeSearchWithButton.displayName = 'FakeSearchWithButton';
+
 export default FakeSearchWithButton;
