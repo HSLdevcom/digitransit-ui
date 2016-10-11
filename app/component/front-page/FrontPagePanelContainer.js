9868c9d1b4bcbd1be592d15da9ea2462fd608eb2
Sami Siren <siren@foofactory.fi>
2016-10-11 15:31:04 +0300

    add large version of search dialog

--- app/component/front-page/FrontPagePanelContainer.js
+++ app/component/front-page/FrontPagePanelContainer.js
@@ -54,7 +54,7 @@ export default class FrontPagePanelContainer extends React.Component {
     }
   }
 
-  componentWillReceiveProps = (nextProps, b) => {
+  componentWillReceiveProps = (nextProps) => {
     const frombp = this.props.breakpoint;
     const tobp = nextProps.breakpoint;
 
