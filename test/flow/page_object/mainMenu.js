22e421fa1fbd619d948a71ad9023209b6186e3de
Sami Siren <siren@foofactory.fi>
2016-10-25 12:03:36 +0300

    make test pass on non english system

--- test/flow/page_object/mainMenu.js
+++ test/flow/page_object/mainMenu.js
@@ -1,17 +1,32 @@
 function clickMenuToggle() {
-  return this.click('@mainMenuToggle');
+  this.api.debug('clicking menu toggle');
+  return this.click('@mainMenuToggle', result => {
+    this.assert.equal(result.status, 0);
+  });
+}
+
+function clickSelectEnglish() {
+  this.api.debug('selecting english language');
+  return this.click('@langEn', result => {
+    this.assert.equal(result.status, 0);
+  });
 }
 
 function openAbout() {
-  this.click('#about');
+  this.api.debug('clicking about');
+  this.click('#about', result => {
+    this.assert.equal(result.status, 0);
+  });
 }
 
 module.exports = {
   commands: [{
     clickMenuToggle,
+    clickSelectEnglish,
     openAbout,
   }],
   elements: {
     mainMenuToggle: '.main-menu-toggle',
+    langEn: '#lang-en',
   },
 };
