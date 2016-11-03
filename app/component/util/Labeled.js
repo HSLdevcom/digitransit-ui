67ce4f773140112cddcaab64d8d733bf1de81a7b
Markku Rontu <markku.rontu@iki.fi>
2016-11-03 15:09:00 +0200

    feat: document Labeled component

--- app/component/util/Labeled.js
+++ app/component/util/Labeled.js
@@ -22,15 +22,15 @@ Labeled.propTypes = {
   className: React.PropTypes.string,
 };
 
-// TODO Add example once ComponentUsageExample is fixed
-
-// const exampleLabel = <span>'Example label'</span>;
+const exampleLabel = <span>Example label</span>;
 
 Labeled.description = (
   <div>
     <p>
       This component wraps other components into a labeled component.
-      <ComponentUsageExample />
+      <ComponentUsageExample>
+        <Labeled label={exampleLabel} showLabel>Example content</Labeled>
+      </ComponentUsageExample>
     </p>
   </div>
 );
