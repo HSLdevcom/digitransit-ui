ee2ffdf5568ee72fa3cc86797246d232e3720caa
Hannes Junnila <hannes.junnila@gmail.com>
2016-11-14 09:55:31 +0200

    Use a real place in tests

--- test/flow/tests/smoke/itinerary-search.js
+++ test/flow/tests/smoke/itinerary-search.js
@@ -2,7 +2,7 @@ module.exports = {
   '@tags': ['smoke'],
   'From Hausmanns gate to Munkkirinne': (browser) => {
     browser.url(browser.launch_url);
-    browser.page.searchFields().itinerarySearch('Hausmanns gate', 'Munkkirinne');
+    browser.page.searchFields().itinerarySearch('Mannerheim museo', 'Munkkirinne');
     browser.page.itinerarySummary().waitForFirstItineraryRow();
     browser.end();
   },
