e23ac61300872f06de8fedfbe7e934a188c3e8ac
Hannes Junnila <hannes.junnila@gmail.com>
2016-11-14 11:12:30 +0200

    Update test description

--- test/flow/tests/smoke/itinerary-search.js
+++ test/flow/tests/smoke/itinerary-search.js
@@ -1,6 +1,6 @@
 module.exports = {
   '@tags': ['smoke'],
-  'From Hausmanns gate to Munkkirinne': (browser) => {
+  'From Mannerheim museo to Munkkirinne': (browser) => {
     browser.url(browser.launch_url);
     browser.page.searchFields().itinerarySearch('Mannerheim museo', 'Munkkirinne');
     browser.page.itinerarySummary().waitForFirstItineraryRow();
