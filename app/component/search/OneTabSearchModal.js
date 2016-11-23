59a95551dcbe1f37739c14bd1ad74af63b01d420
Vesa Meskanen <vesa.meskanen@logica.com>
2016-11-23 12:02:29 +0200

    Option to render a responsive version of the component
    Search modal is now responsive when props.responsive is set true.
    The option may get removed (=always responsive) when favorite places
    get a desktop version.
    
    Other changes include conditional use of placeholder text instead of
    editing existing value. This is probably a temporary implementation.
    All search modal use cases should follow same conventions. Now each
    of the 3 cases (favorites, main  page search, summary page search)
    has an unique behavior.

--- app/component/search/OneTabSearchModal.js
+++ app/component/search/OneTabSearchModal.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Tab from 'material-ui/Tabs/Tab';
 import { intlShape } from 'react-intl';
+import cx from 'classnames';
 
 import GeolocationOrInput from './GeolocationOrInput';
 import { setEndpoint, setUseCurrent } from '../../action/EndpointActions';
 import SearchModal from './SearchModal';
-
+import SearchModalLarge from './SearchModalLarge';
 
 class OneTabSearchModal extends React.Component {
   static contextTypes = {
@@ -14,6 +15,7 @@ class OneTabSearchModal extends React.Component {
     intl: intlShape.isRequired,
     router: React.PropTypes.object,
     location: React.PropTypes.object,
+    breakpoint: React.PropTypes.string.isRequired,
   };
 
   static propTypes = {
@@ -25,7 +27,8 @@ class OneTabSearchModal extends React.Component {
     modalIsOpen: React.PropTypes.oneOfType(
       [React.PropTypes.bool, React.PropTypes.string]).isRequired,
     target: React.PropTypes.oneOfType([React.PropTypes.bool, React.PropTypes.string]),
-  };
+    responsive: React.PropTypes.bool, // a switch to force use of fullscreen modal, e.g. from favourite places
+ };
 
   componentDidUpdate() {
     if (this.props.modalIsOpen) {
@@ -57,31 +60,54 @@ class OneTabSearchModal extends React.Component {
   };
 
   render() {
-    const searchTabLabel = (this.props.customTabLabel ?
+    if (!this.props.modalIsOpen) {
+      return false;
+    }
+
+    let label = (this.props.customTabLabel ?
       this.props.customTabLabel :
       this.context.intl.formatMessage({
         id: this.props.target || 'origin',
         defaultMessage: this.props.target || 'origin',
       }));
 
+
+    let searchTabLabel;
+    let Component;
+    let cName = 'onetab-search-modal-container';
+    let placeholder;
+
+    if(this.context.breakpoint === 'large' && this.props.responsive) {
+      Component = SearchModalLarge;
+      cName += ' bp-large';
+      searchTabLabel = '';
+      placeholder = label;
+    } else {
+      Component = SearchModal;
+      searchTabLabel = label;
+    }
+/*              initialValue={this.props.initialValue} */
     return (
-      <SearchModal
-        selectedTab="tab"
-        modalIsOpen={this.props.modalIsOpen}
-        closeModal={this.props.closeModal}
-      >
-        <Tab className="search-header__button--selected" label={searchTabLabel} value="tab">
-          <GeolocationOrInput
-            ref={(c) => { this.geolocationOrInput = c; }}
-            useCurrentPosition={this.props.endpoint && this.props.endpoint.useCurrentPosition}
-            initialValue={this.props.initialValue}
-            type="endpoint"
-            onSuggestionSelected={
-              this.props.customOnSuggestionSelected || this.onSuggestionSelected}
-            close={this.props.closeModal}
-          />
-        </Tab>
-      </SearchModal>);
+      <div className={cName}>
+        <Component
+          selectedTab="tab"
+          modalIsOpen={this.props.modalIsOpen}
+          closeModal={this.props.closeModal}
+        >
+          <Tab className="search-header__button--selected" label={searchTabLabel} value="tab">
+            <GeolocationOrInput
+              ref={(c) => { this.geolocationOrInput = c; }}
+              useCurrentPosition={this.props.endpoint && this.props.endpoint.useCurrentPosition}
+              initialValue={placeholder ? '' : this.props.initialValue}
+              placeholder={placeholder}
+              type="endpoint"
+              onSuggestionSelected={
+                this.props.customOnSuggestionSelected || this.onSuggestionSelected}
+              close={this.props.closeModal}
+            />
+          </Tab>
+        </Component>
+      </div>);
   }
 }
 
