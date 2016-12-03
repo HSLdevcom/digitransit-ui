a4cc3235e34887491ad319d15c30bf924018927e
Hannes Junnila <hannes.junnila@gmail.com>
2016-12-03 20:48:17 +0200

    Always show into on initial visit

--- app/component/SplashOrChildren.js
+++ app/component/SplashOrChildren.js
@@ -2,6 +2,9 @@ import React from 'react';
 import connectToStores from 'fluxible-addons-react/connectToStores';
 import Splash from './Splash';
 
+import config from '../config';
+import { getIntroShown } from '../store/localStorage';
+
 
 const SplashOrComponent = ({ displaySplash, children }) => {
   if (!displaySplash) {
@@ -21,6 +24,7 @@ export default connectToStores(SplashOrComponent, ['PositionStore', 'EndpointSto
 
     return {
       displaySplash: (
+        (config.shouldShowIntro && getIntroShown() !== true) ||
         (origin.useCurrentPosition &&
             !context.getStore('PositionStore').getLocationState().hasLocation) ||
         (!origin.useCurrentPosition && (!origin.lat || !origin.lon))), // selected location
