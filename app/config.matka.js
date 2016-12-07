8dde7159ecc854e9da4183da67c781c6cf2c62a7
Samuli Heljo <samuli@samuliheljo.com>
2016-12-07 14:31:16 +0200

    matka.fi config

--- app/config.matka.js
+++ app/config.matka.js
@@ -6,7 +6,9 @@ const PIWIK_ADDRESS = process.env.PIWIK_ADDRESS || '';
 const PIWIK_ID = process.env.PIWIK_ID || '';
 const SENTRY_DSN = process.env.SENTRY_DSN || '';
 const PORT = process.env.PORT || 8080;
-const APP_DESCRIPTION = 'Digitransit journey planning UI';
+const APP_DESCRIPTION =
+  'Liikenneviraston Matka.fi uudistuu. Apuasi kaivataan kehitystyössä. ' +
+  'Tule palvelun testaajaksi tai tee siitä saman tien parempi.';
 
 export default {
   PIWIK_ADDRESS,
@@ -30,12 +32,12 @@ export default {
   },
 
   APP_PATH: `${APP_PATH}`,
-  title: 'Reittihaku',
+  title: 'Reittiopas',
 
   contactName: {
-    sv: 'Digitransit',
-    fi: 'Digitransit',
-    default: "Digitransit's",
+    sv: 'Livin',
+    fi: 'Livin',
+    default: "FTA's",
   },
 
   searchParams: {},
@@ -53,7 +55,7 @@ export default {
 
   maxWalkDistance: 10000,
   maxBikingDistance: 100000,
-  availableLanguages: ['fi', 'sv', 'en', 'fr', 'nb'],
+  availableLanguages: ['fi', 'sv', 'en'],
   defaultLanguage: 'en',
   // This timezone data will expire on 31.12.2020
   timezoneData: 'Europe/Helsinki|EET EEST|-20 -30|01010101010101010101010|1BWp0 1qM0 WM0 1qM0 ' +
@@ -141,8 +143,9 @@ export default {
     locationAware: true,
   },
 
+  // TOOD: Switch back in april
   cityBike: {
-    showCityBikes: true,
+    showCityBikes: false,
 
     useUrl: {
       fi: 'https://www.hsl.fi/citybike',
@@ -170,7 +173,7 @@ export default {
   terminalStopsMinZoom: 12,
   terminalNamesZoom: 16,
 
-  appBarLink: { name: 'Digitransit', href: 'https://www.digitransit.fi/' },
+  appBarLink: { name: 'Liikennevirasto', href: 'http://www.liikennevirasto.fi/' },
 
   colors: {
     primary: '#00AFFF',
@@ -181,9 +184,9 @@ export default {
   },
 
   socialMedia: {
-    title: 'Digitransit',
+    title: 'Uusi Matka.fi',
     description: APP_DESCRIPTION,
-    locale: 'en_US',
+    locale: 'fi_FI',
 
     twitter: {
       site: '@hsldevcom',
@@ -192,7 +195,7 @@ export default {
 
   meta: {
     description: APP_DESCRIPTION,
-    keywords: 'digitransit',
+    keywords: 'reitti,reitit,opas,reittiopas,joukkoliikenne',
   },
   // Ticket information feature toggle
   showTicketInformation: false,
@@ -233,9 +236,10 @@ export default {
       defaultValue: true,
     },
 
+    // TOOD: Switch back in april
     citybike: {
-      availableForSelection: true,
-      defaultValue: true,
+      availableForSelection: false,
+      defaultValue: false,
     },
 
     airplane: {
@@ -376,9 +380,9 @@ export default {
 
   footer: {
     content: [
-      { label: (function () { return `© HSL, Liikennevirasto ${(1900 + new Date().getYear())}`; }()) },
+      { label: (function () { return `© Liikennevirasto ${(1900 + new Date().getYear())}`; }()) },
       {},
-      { name: 'footer-feedback', nameEn: 'Send feedback', href: 'https://github.com/HSLdevcom/digitransit-ui/issues', icon: 'icon-icon_speech-bubble' },
+      { name: 'footer-feedback', nameEn: 'Send feedback', href: 'http://www.liikennevirasto.fi/yhteystiedot/palaute', icon: 'icon-icon_speech-bubble' },
       { name: 'about-this-service', nameEn: 'About this service', route: '/tietoja-palvelusta', icon: 'icon-icon_info' },
     ],
   },
@@ -398,19 +402,19 @@ export default {
   /* eslint-disable max-len*/
   aboutThisService: {
     fi: {
-      about: 'Palvelu kattaa joukkoliikenteen, kävelyn, pyöräilyn ja yksityisautoilun rajatuilta osin. Palvelu perustuu Digitransit palvelualustaan.',
+      about: 'Tämän palvelun tarjoaa Liikennevirasto joukkoliikenteen reittisuunnittelua varten koko Suomen alueella. Palvelu kattaa joukkoliikenteen, kävelyn, pyöräilyn ja yksityisautoilun rajatuilta osin. Palvelu perustuu Digitransit palvelualustaan.',
       digitransit: 'Digitransit palvelualusta on HSL:n ja Liikenneviraston kehittämä avoimen lähdekoodin ratkaisu, jonka taustalla toimii mm. OpenTripPlanner. Lähdekoodi tarjotaan EUPL v1.2 ja AGPLv3 lisensseillä.',
       datasources: 'Kartat, kadut, rakennukset, pysäkkisijainnit ym. tiedot tarjoaa © OpenStreetMap contributors ja ne ladataan Geofabrik palvelusta. Osoitetiedot tuodaan VRK:n rakennustietorekisteristä ja ne ladataan OpenAddresses-palvelusta. Joukkoliikenteen reitit ja aikataulut ladataan mm. Liikenneviraston valtakunnallisesta joukkoliikenteen tietokannasta.',
     },
 
     sv: {
-      about: 'Reseplaneraren täcker med vissa begränsningar kollektivtrafik, promenad, cykling samt privatbilism. Tjänsten baserar sig på Digitransit-plattformen.',
+      about: 'Den här tjänsten erbjuds av Trafikverket för reseplanering inom hela Finland. Reseplaneraren täcker med vissa begränsningar kollektivtrafik, promenad, cykling samt privatbilism. Tjänsten baserar sig på Digitransit-plattformen.',
       digitransit: 'Digitransit-plattformen är en öppen programvara utvecklad av HRT och Trafikverket, som bl.a. stödjer sig på OpenTripPlanner. Källkoden distribueras under EUPL v1.2 och AGPLv3 licenserna.',
       datasources: 'Kartor, gator, byggnader, hållplatser och dylik information erbjuds av © OpenStreetMap contributors och laddas ned från Geofabrik-tjänsten. Addressinformation hämtas från BRC:s byggnadsinformationsregister och laddas ned från OpenAddresses-tjänsten. Kollektivtrafikens rutter och tidtabeller hämtas bl.a. från Trafikverkets landsomfattande kollektivtrafiksdatabas.',
     },
 
     en: {
-      about: 'The service covers public transport, walking, cycling, and some private car use. Service is built on Digitransit platform.',
+      about: 'This service is provided by Finnish Transport Agency for journey planning and information in Finland. The service covers public transport, walking, cycling, and some private car use. Service is built on Digitransit platform.',
       digitransit: 'Digitransit service platform is created by HSL and Finnish Transport Agency, built on top of e.g. OpenTripPlanner. The source code of the platform is dual-licensed under the EUPL v1.2 and AGPLv3 licenses.',
       datasources: "Maps, streets, buildings, stop locations etc. from © OpenStreetMap contributors downloaded from Geofabrik. Additional address data from Finland's Population Register Centre downloaded from OpenAddresses Public transport routes and timetables from Finnish Transport Agency's national public transit database.",
     },
@@ -426,18 +430,18 @@ export default {
 
     content: {
       fi: {
-        title: 'Tämä on Digitransitin kehitysversio',
-        content: 'Käytät Digitransitin kehitysversiota.',
+        title: 'Tämä on Matka.fi:n kehitysversio',
+        content: 'Käytät Matka.fi-palvelun kehitysversiota. Kokeile uusia ominaisuuksia ja lähetä meille palautetta. Päivitämme palvelua jatkuvasti. Lisätietoa projektista löydät osoitteesta digitransit.fi.',
       },
 
       sv: {
-        title: 'Det här är utvecklingsversionen av Digitransit',
-        content: 'Du använder utvecklingsversionen av Digitransit.',
+        title: 'Det här är utvecklingsversionen av Resa.fi',
+        content: 'Du använder utvecklingsversionen av Resa.fi. Prova de nya egenskaperna och ge oss feedback. Vi uppdaterar tjänsten kontinuerligt. Mer information om projektet hittar du på addressen digitransit.fi.',
       },
 
       en: {
-        title: 'This version of Digitransit is under development',
-        content: 'You are using the development version of Digitransit.',
+        title: 'This version of Journey.fi is under development',
+        content: 'You are using the development version of Journey.fi. Try the new features and send us feedback. We are updating the service constantly. More information about the project can be found at digitransit.fi.',
       },
     },
   }],
