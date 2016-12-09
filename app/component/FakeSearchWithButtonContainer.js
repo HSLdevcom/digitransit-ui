7a29e1b7d1203fe3357c7b86e5a37f2e7e1d6384
Sami Siren <siren@foofactory.fi>
2016-12-09 15:40:22 +0200

    comvert to button

--- app/component/FakeSearchWithButtonContainer.js
+++ app/component/FakeSearchWithButtonContainer.js
@@ -4,21 +4,26 @@ import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 import FakeSearchBar from './FakeSearchBar';
 
+
 export const FakeSearchWithButton = ({ fakeSearchBar, onClick, breakpoint }) => (
   <div className={`row search-form bp-${breakpoint}`}>
-    <div className="small-12 columns search-form-map-overlay">
-      <div className="row collapse postfix-radius">
-        <div className="small-11 columns">
-          {fakeSearchBar}
-        </div>
-        <div className="small-1 columns" onClick={onClick}>
-          <span className="postfix search cursor-pointer button-icon">
-            <Icon img="icon-icon_search" />
-          </span>
+    <button tabIndex="0" onClick={onClick} className="modest">
+      <div className="small-12 columns search-form-map-overlay">
+        <div className="row collapse postfix-radius">
+          <div className="small-11 columns">
+            {fakeSearchBar}
+          </div>
+          <div className="small-1 columns" onClick={onClick}>
+            <span className="postfix search cursor-pointer button-icon">
+              <Icon img="icon-icon_search" />
+            </span>
+          </div>
+
         </div>
       </div>
-    </div>
+    </button>
   </div>
+
 );
 
 FakeSearchWithButton.propTypes = {
@@ -36,8 +41,8 @@ FakeSearchWithButton.displayName = 'FakeSearchWithButton';
 FakeSearchWithButton.description = () => (
   <div>
     <p>
-      Visual search component that acts as a link to search dialog.
-    </p>
+    Visual search component that acts as a link to search dialog.
+  </p>
     <ComponentUsageExample description="Centered fake search field with search icon button">
       <FakeSearchWithButton fakeSearchBar={<FakeSearchBar placeholder="Enter address" />} />
     </ComponentUsageExample>
