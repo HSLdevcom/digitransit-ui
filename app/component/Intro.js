7cc4de2a7fb4f233d5e96dfd0b680b610bbd3065
Samuli Heljo <samuli@samuliheljo.com>
2016-12-09 13:52:14 +0200

    refactor HSL slides to separate file

--- app/component/Intro.js
+++ app/component/Intro.js
@@ -11,47 +11,9 @@ const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 let slides = {};
 
 if (typeof window !== 'undefined') {
-  /* eslint-disable global-require */
-  slides = { hsl: [
-    {
-      image: require('../../static/img/hsl-intro-pic.png'),
-      header: {
-        fi: 'Tervetuloa käyttämään uutta reittiopasta',
-        en: 'Tervetuloa käyttämään uutta reittiopasta',
-        sv: 'Välkommen till den nya reseplaneraren',
-      },
-      text: {
-        fi: 'Nykyinen sijaintisi on avain uusiin ominaisuuksiin. Sallithan paikannuksen!',
-        en: 'Nykyinen sijaintisi on avain uusiin ominaisuuksiin. Sallithan paikannuksen!',
-        sv: 'Din position är nyckeln till nya egenskaper. Tillåt gärna att dela din plats!',
-      },
-    }, {
-      image: require('../../static/img/hsl-origin.png'),
-      header: {
-        fi: 'Missä oletkin',
-        en: 'Missä oletkin',
-        sv: 'Där du är',
-      },
-      text: {
-        fi: 'Nykyinen sijaintisi toimii lähtöpaikkana. Saat reitityksen kertomalla määränpään.',
-        en: 'Nykyinen sijaintisi toimii lähtöpaikkana. Saat reitityksen kertomalla määränpään.',
-        sv: 'Din position fungerar som avfärdsplats. Resan planerar du genom att berätta vart du är på väg.',
-      },
-    }, {
-      image: require('../../static/img/hsl-nearyou.png'),
-      header: {
-        fi: 'Milloin lähden?',
-        en: 'Milloin lähden?',
-        sv: 'När åker jag?',
-      },
-      text: {
-        fi: 'Tarkista nopeasti, mitä lähtee seuraavaksi lähipysäkeiltäsi.',
-        en: 'Tarkista nopeasti, mitä lähtee seuraavaksi lähipysäkeiltäsi.',
-        sv: 'Kolla snabbt och enkelt vilka förbindelser som är nära dig.',
-      },
-    },
-  ] };
-  /* eslint-enable global-require */
+  slides = {
+    hsl: require('./IntroHsl').default, // eslint-disable-line global-require
+  };
 }
 
 const themeSlides = slides[config.CONFIG] || [];
