da2e1c7aed3c88fcfd250100c9c620375e4d1a96
rrudd <raoul.udd@netlight.com>
2016-12-15 14:08:06 +0200

    remove icons from route from / to links

--- app/component/map/MarkerPopupBottom.js
+++ app/component/map/MarkerPopupBottom.js
@@ -38,11 +38,9 @@ class MarkerPopupBottom extends React.Component {
     return (
       <div className="bottom location">
         <div onClick={() => this.routeFrom()} className="route cursor-pointer">
-          <Icon img="icon-icon_route" />
           <FormattedMessage id="route-from-here" defaultMessage="Route from here" />
         </div>
         <div onClick={() => this.routeTo()} className="route cursor-pointer">
-          <Icon img="icon-icon_route" />
           <FormattedMessage id="route-here" defaultMessage="Route to here" />
         </div>
       </div>);
