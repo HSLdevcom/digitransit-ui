6cebd94902d6f86e1eaf3ee7d1f4990f347f7935
Hannes Junnila <hannes.junnila@gmail.com>
2016-12-15 16:36:38 +0200

    remove unnecessary template string

--- app/component/MainMenuContainer.js
+++ app/component/MainMenuContainer.js
@@ -79,19 +79,20 @@ class MainMenuContainer extends Component {
             </Drawer>
           )}
         </LazilyLoad>
-        {config.mainMenu.show ?
+        {config.mainMenu.show ? (
           <div className="icon-holder cursor-pointer main-menu-toggle">
             <button
-              aria-label={`${this.context.intl.formatMessage({
+              aria-label={this.context.intl.formatMessage({
                 id: 'main-menu-label-open',
                 defaultMessage: 'Open main menu',
-              })}`}
+              })}
               onClick={this.toggleOffcanvas}
               className="noborder cursor-pointer"
             >
               <Icon img={'icon-icon_menu'} className="icon" />
-            </button></div> :
-          null}
+            </button>
+          </div>
+        ) : null}
       </div>);
   }
 }
