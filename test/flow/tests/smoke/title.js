3a9d36d87323900247f99df6d6477a48e864fa06
Hannes Junnila <hannes.junnila@gmail.com>
2016-12-15 15:03:14 +0200

    Add missing timeout

--- test/flow/tests/smoke/title.js
+++ test/flow/tests/smoke/title.js
@@ -2,7 +2,7 @@ module.exports = {
   '@tags': ['smoke'],
   'Page should have title logo': (browser) => {
     browser.url(browser.launch_url);
-    browser.waitForElementVisible('.navi-logo, .logo');
+    browser.waitForElementVisible('.navi-logo, .logo', browser.globals.elementVisibleTimeout);
     browser.end();
   },
 };
