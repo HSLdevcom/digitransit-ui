d7c0ccfde8c670f340e2ad40ea93ae0fe50f5eca
Sami Siren <siren@foofactory.fi>
2016-12-20 09:25:13 +0200

    use fewer suites, add new & moved images

--- test/visual/components.js
+++ test/visual/components.js
@@ -3,66 +3,75 @@
 
 /**
  * Customizable test function
- * @componentName component name as specified in styleguide to test
+ * @theSuite the suite to use
+ * @variationName variation name
  * @aptureOrExampleNumber example number (index starts from 1) or array of
  *   selectors for capture-elements
  * @ignoreElements ignore ignoreElements selector (string) or selectors (Array of string)
- * @fn
+ * @fn callback(actions, find) –  Optional callback describes a sequence of actions to bring the
+ * page to this state, starting from a previous state of the suite.
 **/
-function test(componentName, captureOrExampleNumber, ignoreElements, fn = () => {}) {
-  return function (suite) {
-    try {
-      let capture = `#${componentName} .component-example:nth-of-type(${captureOrExampleNumber
-         || 1}) .component`;
-      if (captureOrExampleNumber instanceof Array) {
-        capture = captureOrExampleNumber;
-      }
-      suite
-      .setUrl(`/styleguide/component/${componentName}`)
+function testVariation(theSuite, variationName, captureOrExampleNumber = 1,
+  ignoreElements, fn = () => {}) {
+  try {
+    let capture = `.component-example:nth-of-type(${captureOrExampleNumber}) .component`;
+    if (captureOrExampleNumber instanceof Array) {
+      capture = captureOrExampleNumber;
+    }
+    theSuite
       .setCaptureElements(capture)
       .ignoreElements(ignoreElements || [])
-      .capture('normal', {}, fn);
-    } catch (error) {
-      console.error('oooo', error);
-    }
-  };
+      .capture(variationName, {}, fn);
+  } catch (error) {
+    console.error('Error occurred while testing variation', variationName);
+    throw error;
+  }
+}
+
+function test(componentName, suiteFn) {
+  gemini.suite(componentName, (suite) => {
+    suite.setUrl(`/styleguide/component/${componentName}`);
+    suiteFn(suite);
+  });
 }
 
 /**
  * The most basic test that compares the first example of a component.
  * Every component should be tested atleast this much.
  **/
-function basicTest(name) {
-  gemini.suite(name, test(name));
+function basicTest(componentName) {
+  test(componentName, (suite) => {
+    testVariation(suite, 'normal');
+  });
 }
 
 gemini.suite('components', () => {
-  gemini.suite('Departure', () => {
-    gemini.suite('normal', test('Departure', 1,
-      '#Departure .component-example:nth-of-type(1) .component .realtime-icon'));
-    gemini.suite('added-padding', test('Departure', 2));
-    gemini.suite('with-stop', test('Departure', 3));
-    gemini.suite('isArrival', test('Departure', 4));
+  test('Departure', (suite) => {
+    testVariation(suite, 'normal', 1, '#Departure .component-example:nth-of-type(1) .component .realtime-icon');
+    testVariation(suite, 'added-padding', 2);
+    testVariation(suite, 'with-stop', 3);
+    testVariation(suite, 'isArrival', 4);
   });
 
-  gemini.suite('DepartureTime', () => {
-    gemini.suite('normal', test('DepartureTime', 2));
-    gemini.suite('canceled', test('DepartureTime', 3));
+  test('DepartureTime', (suite) => {
+    testVariation(suite, 'normal', 2);
+    testVariation(suite, 'canceled', 3);
   });
 
-  gemini.suite('RouteNumber', () => {
-    gemini.suite('normal', test('RouteNumber'));
-    gemini.suite('vertical', test('RouteNumber', 2));
-  });
-  gemini.suite('RouteDestination', () => {
-    gemini.suite('normal', test('RouteDestination'));
-    gemini.suite('isArrival', test('RouteDestination', 2));
+  test('RouteNumber', (suite) => {
+    testVariation(suite, 'normal');
+    testVariation(suite, 'vertical', 2);
   });
 
-  gemini.suite('Distance', () => {
-    gemini.suite('zero', test('Distance'));
-    gemini.suite('meters', test('Distance', 2));
-    gemini.suite('km', test('Distance', 3));
+  test('RouteDestination', (suite) => {
+    testVariation(suite, 'normal');
+    testVariation(suite, 'isArrival', 2);
+  });
+
+  test('Distance', (suite) => {
+    testVariation(suite, 'zero');
+    testVariation(suite, 'meters', 2);
+    testVariation(suite, 'km', 2);
   });
 
   basicTest('PlatformNumber');
@@ -76,60 +85,61 @@ gemini.suite('components', () => {
 
   basicTest('Availability');
 
-  gemini.suite('ParkAndRideAvailability', () => {
-    gemini.suite('non-realtime', test('ParkAndRideAvailability'));
-    gemini.suite('realtime', test('ParkAndRideAvailability'));
+  test('ParkAndRideAvailability', (suite) => {
+    testVariation(suite, 'non-realtime');
+    testVariation(suite, 'realtime', 2);
   });
 
-  gemini.suite('FavouriteLocation', test('FavouriteLocation', 1,
-    '#FavouriteLocation .component-example:nth-of-type(1) .component .realtime-icon'));
+  test('FavouriteLocation', (suite) => {
+    testVariation(suite, 'normal', 1,
+      '#FavouriteLocation .component-example:nth-of-type(1) .component .realtime-icon');
+  });
 
   basicTest('EmptyFavouriteLocationSlot');
 
-  gemini.suite('TripRouteStop', () => {
-    gemini.suite('non-realtime', test('TripRouteStop'));
-    gemini.suite('realtime',
-      test('TripRouteStop',
-        [
-          '#TripRouteStop .component-example:nth-of-type(2) .component',
-          '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime',
-        ], '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime'));
+  test('TripRouteStop', (suite) => {
+    testVariation(suite, 'non-realtime');
+    testVariation(suite, 'realtime', [
+      '#TripRouteStop .component-example:nth-of-type(2) .component',
+      '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime',
+    ], '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime');
   });
-  gemini.suite('Favourite', () => {
-    gemini.suite('favourite', test('Favourite'));
-    gemini.suite('favourite-hovered', test('Favourite', 1, [], actions => actions.mouseMove(
-      '#Favourite .component-example:nth-of-type(1) .component svg')));
-    gemini.suite('not-favourite', test('Favourite', 2));
-    gemini.suite('not-favourite-hovered', test('Favourite', 2, [], actions => actions.mouseMove(
-      '#Favourite .component-example:nth-of-type(2) .component svg')));
+  test('Favourite', (suite) => {
+    testVariation(suite, 'normal');
+    testVariation(suite, 'hovered', 1, [], actions => actions.mouseMove(
+      '#Favourite .component-example:nth-of-type(1) .component svg'));
+    testVariation(suite, 'not-favourite', 2);
+    testVariation(suite, 'not-favourite-hovered', 2, [], actions => actions.mouseMove(
+      '#Favourite .component-example:nth-of-type(2) .component svg'));
   });
 
-  gemini.suite('IconWithTail', () => {
-    gemini.suite('normal', test('IconWithTail', 1));
-    gemini.suite('rotate', test('IconWithTail', 2));
-    gemini.suite('class', test('IconWithTail', 3));
-    gemini.suite('notail', test('IconWithTail', 4));
+  test('IconWithTail', (suite) => {
+    testVariation(suite, 'normal');
+    testVariation(suite, 'rotate', 2);
+    testVariation(suite, 'class', 3);
+    testVariation(suite, 'notail', 4);
   });
 
   basicTest('SelectedIconWithTail');
   basicTest('IconWithCaution');
   basicTest('IconWithBigCaution');
 
-  gemini.suite('TimeNavigationButtons', () => {
-    gemini.suite('normal', test('TimeNavigationButtons'));
-    gemini.suite('hovered', test('TimeNavigationButtons', 1, [], (actions) => {
+
+  test('TimeNavigationButtons', (suite) => {
+    testVariation(suite, 'normal');
+    testVariation(suite, 'hovered', 1, [], (actions) => {
       actions.mouseMove(
          // eslint-disable-next-line comma-dangle
         '#TimeNavigationButtons .component-example:nth-of-type(1) .component button:first-of-type'
       ).wait(400); // Wait for animation to happen
-    }));
+    });
   });
 
   basicTest('TimeSelectors');
 
-  gemini.suite('RightOffcanvasToggle', () => {
-    gemini.suite('default', test('RightOffcanvasToggle'));
-    gemini.suite('adjusted', test('RightOffcanvasToggle', 2));
+  test('RightOffcanvasToggle', (suite) => {
+    testVariation(suite, 'default');
+    testVariation(suite, 'adjusted', 2);
   });
 
   basicTest('MarkerSelectPopup');
@@ -143,17 +153,17 @@ gemini.suite('components', () => {
   basicTest('RouteScheduleStopSelect');
   basicTest('RouteScheduleTripRow');
 
-  gemini.suite('AppBarSmall', () => {
-    gemini.suite('with-back-arrow', test('AppBarSmall'));
-    gemini.suite('without-back-arrow', test('AppBarSmall', 2));
-    gemini.suite('with-logo', test('AppBarSmall', 3));
+  test('AppBarSmall', (suite) => {
+    testVariation(suite, 'with-back-arrow');
+    testVariation(suite, 'without-back-arrow', 2);
+    testVariation(suite, 'with-logo', 3);
   });
 
   basicTest('AppBarLarge');
 
-  gemini.suite('FakeSearchWithButton', () => {
-    gemini.suite('basic', test('FakeSearchWithButton'));
-    gemini.suite('large', test('FakeSearchWithButton', 2));
+  test('FakeSearchWithButton', (suite) => {
+    testVariation(suite, 'basic');
+    testVariation(suite, 'large', 2);
   });
 
   basicTest('FrontPagePanelLarge');
@@ -161,30 +171,30 @@ gemini.suite('components', () => {
   basicTest('ExternalLink');
   basicTest('LangSelect');
 
-  gemini.suite('ModeFilter', () => {
-    gemini.suite('Grey buttons', test('ModeFilter'));
-    gemini.suite('White buttons', test('ModeFilter',
-      ['#ModeFilter .nearby-routes .component-example:nth-of-type(1) .component']));
+  test('ModeFilter', (suite) => {
+    testVariation(suite, 'grey-buttons');
+    testVariation(suite, 'white-buttons', ['.nearby-routes .component-example:nth-of-type(1) .component']);
   });
 
-  gemini.suite('RouteStop',
-    test('RouteStop',
-      [
-        '#RouteStop .component-example:nth-of-type(1) .component',
-        '#RouteStop .component-example:nth-of-type(1) .component svg.realtime',
-      ], '#RouteStop .component-example:nth-of-type(1) .component svg.realtime'));
+  test('RouteStop', (suite) => {
+    testVariation(suite, 'normal', [
+      '#RouteStop .component-example:nth-of-type(1) .component',
+      '#RouteStop .component-example:nth-of-type(1) .component svg.realtime',
+    ], '#RouteStop .component-example:nth-of-type(1) .component svg.realtime');
+  });
 
-  gemini.suite('DepartureRow', () => {
-    gemini.suite('Normal', test('DepartureRow', 1, [
+  test('DepartureRow', (suite) => {
+    testVariation(suite, 'normal', 1, [
       '#DepartureRow .component-example:nth-of-type(1) .component .realtime-icon',
-    ]));
-    gemini.suite('With cancellation', test('DepartureRow', 2));
+    ]);
+
+    testVariation(suite, 'with-cancelation', 2);
   });
 
-  gemini.suite('BicycleRentalStationRow', () => {
-    gemini.suite('Plenty of bikes', test('BicycleRentalStationRow'));
-    gemini.suite('Few bikes', test('BicycleRentalStationRow', 2));
-    gemini.suite('No bikes', test('BicycleRentalStationRow', 3));
+  test('BicycleRentalStationRow', (suite) => {
+    testVariation(suite, 'plenty-of-bikes');
+    testVariation(suite, 'few-bikes', 2);
+    testVariation(suite, 'no-bikes', 3);
   });
 
   basicTest('StopPageHeader');
@@ -198,26 +208,26 @@ gemini.suite('components', () => {
   basicTest('SelectedStopPopupContent');
   basicTest('PageFooter');
 
-  gemini.suite('FooterItem', () => {
-    gemini.suite('basic', test('FooterItem'));
-    gemini.suite('with-icon', test('FooterItem', 2));
+  test('FooterItem', (suite) => {
+    testVariation(suite, 'basic');
+    testVariation(suite, 'with-icon', 2);
   });
 
-  gemini.suite('SummaryRow', () => {
-    gemini.suite('passive', test('SummaryRow'));
-    gemini.suite('active', test('SummaryRow', 2));
-    gemini.suite('open', test('SummaryRow', 3));
+  test('SummaryRow', (suite) => {
+    testVariation(suite, 'passive');
+    testVariation(suite, 'active', 2);
+    testVariation(suite, 'open', 3);
   });
 
-  gemini.suite('CurrentPositionSuggestionItem', () => {
-    gemini.suite('With position', test('CurrentPositionSuggestionItem'));
-    gemini.suite('No position', test('CurrentPositionSuggestionItem', 2));
+  test('CurrentPositionSuggestionItem', (suite) => {
+    testVariation(suite, 'with-position');
+    testVariation(suite, 'no-position', 2);
   });
 
-  gemini.suite('SuggestionItem', () => {
-    gemini.suite('Favourite', test('SuggestionItem'));
-    gemini.suite('Address', test('SuggestionItem', 2));
-    gemini.suite('Route', test('SuggestionItem', 3));
-    gemini.suite('Stop', test('SuggestionItem', 4));
+  test('SuggestionItem', (suite) => {
+    testVariation(suite, 'Favourite');
+    testVariation(suite, 'Address', 2);
+    testVariation(suite, 'Route', 3);
+    testVariation(suite, 'Stop', 4);
   });
 });
