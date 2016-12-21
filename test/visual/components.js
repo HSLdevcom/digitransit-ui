dfcd3b2d3d153462ac7fcbe8452a66e8bf61cf8e
Sami Siren <siren@foofactory.fi>
2016-12-21 21:27:07 +0200

    flatten, allow skipping tests per prowser

--- test/visual/components.js
+++ test/visual/components.js
@@ -3,7 +3,8 @@
 
 /**
  * Customizable test function
- * @theSuite the suite to use
+ * @skip browsers to skip
+ * @componentName the component name to test
  * @variationName variation name
  * @aptureOrExampleNumber example number (index starts from 1) or array of
  *   selectors for capture-elements
@@ -11,223 +12,177 @@
  * @fn callback(actions, find) –  Optional callback describes a sequence of actions to bring the
  * page to this state, starting from a previous state of the suite.
 **/
-function testVariation(theSuite, variationName, captureOrExampleNumber = 1,
+function testVariation(skip, componentName, variationName = 'normal', captureOrExampleNumber = 1,
   ignoreElements, fn = () => {}) {
-  try {
-    let capture = `.component-example:nth-of-type(${captureOrExampleNumber}) .component`;
-    if (captureOrExampleNumber instanceof Array) {
-      capture = captureOrExampleNumber;
+  gemini.suite(`${componentName}_${variationName}`, (suite) => {
+    if (skip) {
+      suite.skip(skip);
     }
-    theSuite
+
+    try {
+      let capture = `.component-example:nth-of-type(${captureOrExampleNumber}) .component`;
+      if (captureOrExampleNumber instanceof Array) {
+        capture = captureOrExampleNumber;
+      }
+      suite
+      .setUrl(`/styleguide/component/${componentName}`)
       .setCaptureElements(capture)
       .ignoreElements(ignoreElements || [])
       .capture(variationName, {}, fn);
-  } catch (error) {
-    console.error('Error occurred while testing variation', variationName);
-    throw error;
-  }
-}
-
-function test(componentName, suiteFn) {
-  gemini.suite(componentName, (suite) => {
-    suite.setUrl(`/styleguide/component/${componentName}`);
-    suiteFn(suite);
+    } catch (error) {
+      console.error('Error occurred while testing variation', variationName);
+      throw error;
+    }
   });
 }
 
-/**
- * The most basic test that compares the first example of a component.
- * Every component should be tested atleast this much.
- **/
-function basicTest(componentName) {
-  test(componentName, (suite) => {
-    testVariation(suite, 'normal');
-  });
-}
+// tests//
+testVariation('ie11', 'Departure', 'normal', 1, '#Departure .component-example:nth-of-type(1) .component .realtime-icon');
+testVariation(undefined, 'Departure', 'added-padding', 2);
+testVariation(undefined, 'Departure', 'with-stop', 3);
+testVariation(undefined, 'Departure', 'isArrival', 4);
 
-gemini.suite('components', () => {
-  test('Departure', (suite) => {
-    testVariation(suite, 'normal', 1, '#Departure .component-example:nth-of-type(1) .component .realtime-icon');
-    testVariation(suite, 'added-padding', 2);
-    testVariation(suite, 'with-stop', 3);
-    testVariation(suite, 'isArrival', 4);
-  });
+testVariation(undefined, 'DepartureTime', 'normal', 2);
+testVariation(undefined, 'DepartureTime', 'canceled', 3);
 
-  test('DepartureTime', (suite) => {
-    testVariation(suite, 'normal', 2);
-    testVariation(suite, 'canceled', 3);
-  });
 
-  test('RouteNumber', (suite) => {
-    testVariation(suite, 'normal');
-    testVariation(suite, 'vertical', 2);
-  });
+testVariation(undefined, 'RouteNumber', 'normal');
+testVariation(undefined, 'RouteNumber', 'vertical', 2);
 
-  test('RouteDestination', (suite) => {
-    testVariation(suite, 'normal');
-    testVariation(suite, 'isArrival', 2);
-  });
+testVariation(undefined, 'RouteDestination', 'normal');
+testVariation(undefined, 'RouteDestination', 'isArrival', 2);
 
-  test('Distance', (suite) => {
-    testVariation(suite, 'zero');
-    testVariation(suite, 'meters', 2);
-    testVariation(suite, 'km', 2);
-  });
 
-  basicTest('PlatformNumber');
+testVariation(undefined, 'Distance', 'zero');
+testVariation(undefined, 'Distance', 'meters', 2);
+testVariation(undefined, 'Distance', 'km', 2);
 
-  basicTest('CardHeader');
-  basicTest('Card');
-  basicTest('CityBikeCard');
-  basicTest('CityBikeContent');
-  basicTest('CityBikeAvailability');
-  basicTest('CityBikeUse');
+testVariation(undefined, 'PlatformNumber');
 
-  basicTest('Availability');
+testVariation(undefined, 'CardHeader');
+testVariation(undefined, 'Card');
+testVariation(undefined, 'CityBikeCard');
+testVariation(undefined, 'CityBikeContent');
+testVariation(undefined, 'CityBikeAvailability');
+testVariation(undefined, 'CityBikeUse');
 
-  test('ParkAndRideAvailability', (suite) => {
-    testVariation(suite, 'non-realtime');
-    testVariation(suite, 'realtime', 2);
-  });
+testVariation(undefined, 'Availability');
 
-  test('FavouriteLocation', (suite) => {
-    testVariation(suite, 'normal', 1,
+testVariation(undefined, 'ParkAndRideAvailability', 'non-realtime');
+testVariation(undefined, 'ParkAndRideAvailability', 'realtime', 2);
+
+testVariation('ie11', 'FavouriteLocation', 'normal', 1,
       '#FavouriteLocation .component-example:nth-of-type(1) .component .realtime-icon');
-  });
 
-  basicTest('EmptyFavouriteLocationSlot');
+testVariation(undefined, 'EmptyFavouriteLocationSlot');
 
-  test('TripRouteStop', (suite) => {
-    testVariation(suite, 'non-realtime');
-    testVariation(suite, 'realtime', [
-      '#TripRouteStop .component-example:nth-of-type(2) .component',
-      '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime',
-    ], '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime');
-  });
-  test('Favourite', (suite) => {
-    testVariation(suite, 'normal');
-    testVariation(suite, 'hovered', 1, [], actions => actions.mouseMove(
+testVariation(undefined, 'TripRouteStop', 'non-realtime');
+testVariation('ie11', 'TripRouteStop', 'realtime', [
+  '#TripRouteStop .component-example:nth-of-type(2) .component',
+], '#TripRouteStop .component-example:nth-of-type(2) .component svg.realtime');
+
+testVariation(undefined, 'Favourite', 'normal');
+testVariation(undefined, 'Favourite', 'hovered', 1, [], actions => actions.mouseMove(
       '#Favourite .component-example:nth-of-type(1) .component svg'));
-    testVariation(suite, 'not-favourite', 2);
-    testVariation(suite, 'not-favourite-hovered', 2, [], actions => actions.mouseMove(
+testVariation(undefined, 'Favourite', 'not-favourite', 2);
+testVariation(undefined, 'Favourite', 'not-favourite-hovered', 2, [], actions => actions.mouseMove(
       '#Favourite .component-example:nth-of-type(2) .component svg'));
-  });
-
-  test('IconWithTail', (suite) => {
-    testVariation(suite, 'normal');
-    testVariation(suite, 'rotate', 2);
-    testVariation(suite, 'class', 3);
-    testVariation(suite, 'notail', 4);
-  });
-
-  basicTest('SelectedIconWithTail');
-  basicTest('IconWithCaution');
-  basicTest('IconWithBigCaution');
 
 
-  test('TimeNavigationButtons', (suite) => {
-    testVariation(suite, 'normal');
-    testVariation(suite, 'hovered', 1, [], (actions) => {
-      actions.mouseMove(
+testVariation(undefined, 'IconWithTail', 'normal');
+testVariation(undefined, 'IconWithTail', 'rotate', 2);
+testVariation(undefined, 'IconWithTail', 'class', 3);
+testVariation(undefined, 'IconWithTail', 'notail', 4);
+
+testVariation(undefined, 'SelectedIconWithTail');
+testVariation(undefined, 'IconWithCaution');
+testVariation(undefined, 'IconWithBigCaution');
+
+
+testVariation(undefined, 'TimeNavigationButtons', 'normal');
+testVariation(undefined, 'TimeNavigationButtons', 'hovered', 1, [], (actions) => {
+  actions.mouseMove(
          // eslint-disable-next-line comma-dangle
         '#TimeNavigationButtons .component-example:nth-of-type(1) .component button:first-of-type'
       ).wait(400); // Wait for animation to happen
-    });
-  });
-
-  basicTest('TimeSelectors');
-
-  test('RightOffcanvasToggle', (suite) => {
-    testVariation(suite, 'default');
-    testVariation(suite, 'adjusted', 2);
-  });
-
-  basicTest('MarkerSelectPopup');
-  basicTest('SelectStopRow');
-  basicTest('SelectTerminalRow');
-  basicTest('SelectCityBikeRow');
-  basicTest('SelectParkAndRideRow');
-  basicTest('TicketInformation');
-  basicTest('RouteScheduleDateSelect');
-  basicTest('RouteScheduleHeader');
-  basicTest('RouteScheduleStopSelect');
-  basicTest('RouteScheduleTripRow');
-
-  test('AppBarSmall', (suite) => {
-    testVariation(suite, 'with-back-arrow');
-    testVariation(suite, 'without-back-arrow', 2);
-    testVariation(suite, 'with-logo', 3);
-  });
-
-  basicTest('AppBarLarge');
-
-  test('FakeSearchWithButton', (suite) => {
-    testVariation(suite, 'basic');
-    testVariation(suite, 'large', 2);
-  });
-
-  basicTest('FrontPagePanelLarge');
-  basicTest('FrontPagePanelSmall');
-  basicTest('ExternalLink');
-  basicTest('LangSelect');
-
-  test('ModeFilter', (suite) => {
-    testVariation(suite, 'grey-buttons');
-    testVariation(suite, 'white-buttons', ['.nearby-routes .component-example:nth-of-type(1) .component']);
-  });
-
-  test('RouteStop', (suite) => {
-    testVariation(suite, 'normal', [
-      '#RouteStop .component-example:nth-of-type(1) .component',
-      '#RouteStop .component-example:nth-of-type(1) .component svg.realtime',
-    ], '#RouteStop .component-example:nth-of-type(1) .component svg.realtime');
-  });
-
-  test('DepartureRow', (suite) => {
-    testVariation(suite, 'normal', 1, [
-      '#DepartureRow .component-example:nth-of-type(1) .component .realtime-icon',
-    ]);
-
-    testVariation(suite, 'with-cancelation', 2);
-  });
-
-  test('BicycleRentalStationRow', (suite) => {
-    testVariation(suite, 'plenty-of-bikes');
-    testVariation(suite, 'few-bikes', 2);
-    testVariation(suite, 'no-bikes', 3);
-  });
-
-  basicTest('StopPageHeader');
-  basicTest('StopCardHeader');
-  basicTest('SplitBars');
-  basicTest('Labeled');
-  basicTest('Centered');
-  basicTest('InfoIcon');
-  basicTest('DepartureListHeader');
-  basicTest('NextDeparturesListHeader');
-  basicTest('SelectedStopPopupContent');
-  basicTest('PageFooter');
-
-  test('FooterItem', (suite) => {
-    testVariation(suite, 'basic');
-    testVariation(suite, 'with-icon', 2);
-  });
-
-  test('SummaryRow', (suite) => {
-    testVariation(suite, 'passive');
-    testVariation(suite, 'active', 2);
-    testVariation(suite, 'open', 3);
-  });
-
-  test('CurrentPositionSuggestionItem', (suite) => {
-    testVariation(suite, 'with-position');
-    testVariation(suite, 'no-position', 2);
-  });
-
-  test('SuggestionItem', (suite) => {
-    testVariation(suite, 'Favourite');
-    testVariation(suite, 'Address', 2);
-    testVariation(suite, 'Route', 3);
-    testVariation(suite, 'Stop', 4);
-  });
 });
+
+testVariation(undefined, 'TimeSelectors');
+
+testVariation(undefined, 'RightOffcanvasToggle', 'default');
+testVariation(undefined, 'RightOffcanvasToggle', 'adjusted', 2);
+
+testVariation(undefined, 'MarkerSelectPopup');
+testVariation(undefined, 'SelectStopRow');
+testVariation(undefined, 'SelectTerminalRow');
+testVariation(undefined, 'SelectCityBikeRow');
+testVariation(undefined, 'SelectParkAndRideRow');
+testVariation(undefined, 'TicketInformation');
+testVariation(undefined, 'RouteScheduleDateSelect');
+testVariation(undefined, 'RouteScheduleHeader');
+testVariation(undefined, 'RouteScheduleStopSelect');
+testVariation(undefined, 'RouteScheduleTripRow');
+
+testVariation(undefined, 'AppBarSmall', 'with-back-arrow');
+testVariation(undefined, 'AppBarSmall', 'without-back-arrow', 2);
+testVariation(undefined, 'AppBarSmall', 'with-logo', 3);
+
+testVariation(undefined, 'AppBarLarge');
+
+testVariation(undefined, 'FakeSearchWithButton', 'basic');
+testVariation(undefined, 'FakeSearchWithButton', 'large', 2);
+
+
+testVariation(undefined, 'FrontPagePanelLarge');
+testVariation(undefined, 'FrontPagePanelSmall');
+testVariation(undefined, 'ExternalLink');
+testVariation(undefined, 'LangSelect');
+
+testVariation(undefined, 'ModeFilter', 'grey-buttons');
+testVariation(undefined, 'ModeFilter', 'white-buttons', [
+  '.nearby-routes .component-example:nth-of-type(1) .component',
+]);
+
+
+testVariation('ie11', 'RouteStop', 'normal', [
+  '#RouteStop .component-example:nth-of-type(1) .component',
+], '#RouteStop .component-example:nth-of-type(1) .component svg.realtime');
+
+
+testVariation('ie11', 'DepartureRow', 'normal', 1, [
+  '#DepartureRow .component-example:nth-of-type(1) .component .realtime-icon',
+]);
+
+testVariation(undefined, 'DepartureRow', 'with-cancelation', 2);
+
+
+testVariation(undefined, 'BicycleRentalStationRow', 'plenty-of-bikes');
+testVariation(undefined, 'BicycleRentalStationRow', 'few-bikes', 2);
+testVariation(undefined, 'BicycleRentalStationRow', 'no-bikes', 3);
+
+testVariation(undefined, 'StopPageHeader');
+testVariation(undefined, 'StopCardHeader');
+testVariation(undefined, 'SplitBars');
+testVariation(undefined, 'Labeled');
+testVariation(undefined, 'Centered');
+testVariation(undefined, 'InfoIcon');
+testVariation(undefined, 'DepartureListHeader');
+testVariation(undefined, 'NextDeparturesListHeader');
+testVariation(undefined, 'SelectedStopPopupContent');
+testVariation(undefined, 'PageFooter');
+
+testVariation(undefined, 'FooterItem', 'basic');
+testVariation(undefined, 'FooterItem', 'with-icon', 2);
+
+
+testVariation(undefined, 'SummaryRow', 'passive');
+testVariation(undefined, 'SummaryRow', 'active', 2);
+testVariation(undefined, 'SummaryRow', 'open', 3);
+
+testVariation(undefined, 'CurrentPositionSuggestionItem', 'with-position');
+testVariation(undefined, 'CurrentPositionSuggestionItem', 'no-position', 2);
+
+testVariation(undefined, 'SuggestionItem', 'Favourite');
+testVariation(undefined, 'SuggestionItem', 'Address', 2);
+testVariation(undefined, 'SuggestionItem', 'Route', 3);
+testVariation(undefined, 'SuggestionItem', 'Stop', 4);
