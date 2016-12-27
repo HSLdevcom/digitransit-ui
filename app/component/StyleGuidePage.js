8ad917a5412ef07b5da8c7a10357c22c1e7d9801
Sami Siren <siren@foofactory.fi>
2016-12-27 15:07:38 +0200

    fix lint

--- app/component/StyleGuidePage.js
+++ app/component/StyleGuidePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import sortBy from 'lodash/sortBy';
-import { Link } from 'react-router';
 
 import { FakeSearchWithButton } from './FakeSearchWithButtonContainer';
 import Icon from './Icon';
