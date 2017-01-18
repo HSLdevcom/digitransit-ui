1035d04cf4f8a8900176fa91f81ed732bf5a743d
Hannes Junnila <hannes.junnila@gmail.com>
2017-01-18 11:12:26 +0200

    move rules to logical groups

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,6 @@ module.exports = {
     'react/jsx-filename-extension': ['error', { "extensions": [".js"] }],
     'react/no-string-refs': 'warn',
     'react/no-find-dom-node': 'warn',
-    'graphql/template-strings': ['error', {
-      'env': 'relay',
-      'schemaJson': require('./build/schema.json'),
-    }],
     // TODO: https://github.com/yannickcr/eslint-plugin-react/issues/819
     // TODO: https://github.com/yannickcr/eslint-plugin-react/issues/811
     'react/no-unused-prop-types': ['off', { skipShapeProps: true }],
@@ -21,6 +17,11 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'warn',
     'no-plusplus': ['error', { "allowForLoopAfterthoughts": true }],
     'compat/compat': 'error',
+    // Enable GraphQL linting
+    'graphql/template-strings': ['error', {
+      'env': 'relay',
+      'schemaJson': require('./build/schema.json'),
+    }],
   },
   'env': {
     'browser': true,
