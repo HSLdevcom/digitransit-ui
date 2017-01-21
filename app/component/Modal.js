bd63c55908ca3020fa08c0ec41cad2bb90c6efda
Hannes Junnila <hannes.junnila@gmail.com>
2017-01-21 23:42:36 +0200

    convert modal to Material-ui

--- app/component/Modal.js
+++ app/component/Modal.js
@@ -1,66 +1,46 @@
 import React from 'react';
-import cx from 'classnames';
+import Dialog from 'material-ui/Dialog';
+
 import Icon from './Icon';
 
-class Modal extends React.Component {
-  static propTypes = {
-    allowClicks: React.PropTypes.bool,
-    children: React.PropTypes.node,
-    open: React.PropTypes.bool,
-    title: React.PropTypes.node,
-    toggleVisibility: React.PropTypes.func.isRequired,
-  };
-
-  stopClickPropagation = (e) => {
-    if (this.props.allowClicks !== true) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  }
-
-  render() {
-    const isActive = {
-      'is-active': this.props.open,
-    };
-
-    const modalClasses = {
-      modal: true,
-      'small-11': true,
-      column: true,
-    };
-
-    const overlayStyle = {
-      zIndex: 1400,
-    };
-
-    return (
-      <div
-        className={cx('modal-overlay', 'cursor-pointer', isActive)}
-        style={overlayStyle}
-        onClick={this.props.toggleVisibility}
-      >
-        <div
-          data-closable
-          className={cx(modalClasses, isActive)}
-          onClick={this.stopClickPropagation}
-        >
-          <div className="row">
-            <h2 className="left">{this.props.title}
-            </h2>
-            <div className="small-1 columns right text-right modal-top-nav">
-              <a onClick={this.props.toggleVisibility} className="close-button cursor-pointer">
-                <Icon img="icon-icon_close" />
-              </a>
-            </div>
-          </div>
-          <div className="modal-wrapper">
-            <div className="modal-content momentum-scroll">
-              {this.props.children}
-            </div>
-          </div>
+export default function Modal(props) {
+  return (
+    <Dialog
+      open={props.open}
+      onRequestClose={props.toggleVisibility}
+      autoScrollBodyContent
+      contentClassName="modal"
+    >
+      <div className="row">
+        <h2 className="left">{props.title}</h2>
+        <div className="right text-right modal-top-nav">
+          <a
+            onClick={props.toggleVisibility}
+            className="close-button cursor-pointer"
+            tabIndex={0}
+          >
+            <Icon img="icon-icon_close" />
+          </a>
         </div>
-      </div>);
-  }
+      </div>
+      <div className="modal-wrapper">
+        <div className="modal-content momentum-scroll">
+          {props.children}
+        </div>
+      </div>
+    </Dialog>
+  );
 }
 
-export default Modal;
+Modal.propTypes = {
+  children: React.PropTypes.node,
+  open: React.PropTypes.bool,
+  title: React.PropTypes.node,
+  toggleVisibility: React.PropTypes.func.isRequired,
+};
+
+Modal.defaultProps = {
+  open: false,
+  title: null,
+  children: null,
+};
