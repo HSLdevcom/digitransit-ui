6eb5c844d1cb54f98b0899337701c002937ccc6b
Vesa Meskanen <vesa.meskanen@logica.com>
2017-01-26 14:12:57 +0200

    Add-theme script run for Oulu, created config data added to git

---
+++ app/configurations/config.oulu.js
@@ -0,0 +1,79 @@
+/* eslint-disable */
+import mergeWith from 'lodash/mergeWith';
+
+const CONFIG = process.env.CONFIG || 'oulu';
+const APP_DESCRIPTION =
+  'Reittiopas uudistuu. Tule mukaan! Ota uuden uuden sukupolven matkaopas käyttöösi.';
+
+const walttiConfig = require('./waltti').default;
+
+export default mergeWith({}, walttiConfig, {
+  CONFIG,
+
+  appBarLink: { name: 'Oulu', href: 'http://www.oulunkaupunki.fi/' },
+
+  colors: {
+    primary: '#c40065',
+  },
+
+  socialMedia: {
+    title: 'Uusi Reittiopas - Oulu',
+    description: APP_DESCRIPTION,
+  },
+
+  title: 'Oulu Digitransit',
+
+  searchParams: {
+    'boundary.rect.min_lat': 64.71,
+    'boundary.rect.max_lat': 65.38,
+    'boundary.rect.min_lon': 24.37,
+    'boundary.rect.max_lon': 26.61,
+  },
+
+  areaPolygon: [[24.37, 64.71], [24.37, 65.38], [26.61, 65.38], [26.61, 64.71] ],
+
+  initialLocation: {
+    lat: 65.012367,
+    lon: 25.471350,
+  },
+
+  defaultOrigins: [
+    { icon: 'icon-icon_bus', label: 'Kauppatori, Oulu', lat: 65.013559, lon: 25.465032 },
+    { icon: 'icon-icon_rail', label: 'Rautatieasema, Oulu', lat: 65.011523, lon: 25.483571 },
+    { icon: 'icon-icon_airplane', label: 'Lentoasema, Oulu', lat: 64.928808, lon: 25.373296 },
+  ],
+
+  footer: {
+    content: [
+      { label: (function () { return `© Oulu ${(1900 + new Date().getYear())}`; }()) },
+      {},
+      { name: 'footer-feedback', nameEn: 'Send feedback', type: 'feedback', icon: 'icon-icon_speech-bubble' },
+      { name: 'about-this-service', nameEn: 'About this service', route: '/tietoja-palvelusta', icon: 'icon-icon_info' },
+    ],
+  },
+
+  aboutThisService: {
+    fi: {
+      about: 'Tämä on Oulun uusi reittiopas. Palvelu kattaa joukkoliikenteen, kävelyn, pyöräilyn ja yksityisautoilun rajatuilta osin. Palvelu perustuu Digitransit palvelualustaan.',
+      digitransit: 'Digitransit palvelualusta on HSL:n ja Liikenneviraston kehittämä avoimen lähdekoodin reititystuote. Lähdekoodi tarjotaan EUPL v1.2 ja AGPLv3 lisensseillä.',
+      datasources: 'Kartat, kadut, rakennukset, pysäkkisijainnit ym. tiedot tarjoaa © OpenStreetMap contributors ja ne ladataan Geofabrik palvelusta. Osoitetiedot tuodaan VRK:n rakennustietorekisteristä ja ne ladataan OpenAddresses-palvelusta. Joukkoliikenteen reitit ja aikataulut ladataan HSL:n dev.hsl.fi/gtfs palvelimelta.',
+    },
+
+    sv: {
+      about: 'This is a new service for Oulu area route planning. The service covers public transport, walking, cycling, and some private car use. Service is built on Digitransit platform.',
+      digitransit: 'Digitransit service platform is created by HSL Finnish Transport Agency. The source code of the platform is dual-licensed under the EUPL v1.2 and AGPLv3 licenses.',
+      datasources: "Maps, streets, buildings, stop locations etc. from © OpenStreetMap contributors downloaded from Geofabrik. Additional address data from Finland's Population Register Centre downloaded from OpenAddresses Public transport routes and timetables from HSL downloaded from dev.hsl.fi/gtfs.",
+    },
+
+    en: {
+      about: 'This is a new service for Oulu area route planning. The service covers public transport, walking, cycling, and some private car use. Service is built on Digitransit platform. The service covers public transport, walking, cycling, and some private car use. Service is built on Digitransit platform.',
+      digitransit: 'Digitransit service platform is created by HSL Finnish Transport Agency. The source code of the platform is dual-licensed under the EUPL v1.2 and AGPLv3 licenses.',
+      datasources: "Maps, streets, buildings, stop locations etc. from © OpenStreetMap contributors downloaded from Geofabrik. Additional address data from Finland's Population Register Centre downloaded from OpenAddresses Public transport routes and timetables from HSL downloaded from dev.hsl.fi/gtfs.",
+    },
+  },
+
+}, (objValue, srcValue) => {
+  if (Array.isArray(srcValue)) { return srcValue; }
+  if (Array.isArray(objValue)) { return objValue; }
+  return undefined; // default merge
+});
