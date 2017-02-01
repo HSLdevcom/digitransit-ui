d7c610e51395e86119d4e1f6636ab5344cbf1a41
Samuli Heljo <samuli@samuliheljo.com>
2017-02-01 11:47:01 +0200

    improve 404

--- app/component/404.js
+++ app/component/404.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router';
+import Icon from './Icon';
 
 export default function Page() {
   return (
-    <div>
+    <div className="page-not-found">
+      <Icon img="icon-icon_error_page_not_found" />
       <p>
-        <FormattedMessage id="page-not-found" defaultMessage="Bummer! page is not found" />
+        <FormattedMessage id="page-not-found" defaultMessage="Page was not found." />
+      </p>
+      <p>
+        <Link to="/">
+          <FormattedMessage id="back-to-front-page" defaultMessage="Back to front page ›" />
+        </Link>
       </p>
     </div>
   );
 }
+/*
+
+*/
