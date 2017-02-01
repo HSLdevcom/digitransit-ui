31c60f23f696d87cd68f3dfb98954cca7ab388d1
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-02-01 09:52:12 +0200

    Some more icons moved from static to sass

--- app/component/IntroMatka.js
+++ app/component/IntroMatka.js
@@ -12,7 +12,7 @@ export default [
       sv: 'Din position är nyckeln till nya egenskaper. Tillåt gärna att dela din plats!',
     },
   }, {
-    image: require('../../static/img/matka-origin.png'), // eslint-disable-line global-require
+    image: require('../../sass/themes/matka/matka-origin.png'), // eslint-disable-line global-require
     header: {
       fi: 'Missä oletkin',
       en: 'Wherever you are',
@@ -24,7 +24,7 @@ export default [
       sv: 'Din position fungerar som avfärdsplats. Resan planerar du genom att berätta vart du är på väg.',
     },
   }, {
-    image: require('../../static/img/matka-nearyou.png'), // eslint-disable-line global-require
+    image: require('../../sass/themes/matka/matka-nearyou.png'), // eslint-disable-line global-require
     header: {
       fi: 'Milloin lähdet?',
       en: 'When will I go?',
