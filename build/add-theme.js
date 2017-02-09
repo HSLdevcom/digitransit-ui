70e6914eb6ebdaca06f22caee93dea76a16dc138
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-02-09 13:22:29 +0200

    Make navbar logo configuration more automatic
    - Never assign default digitransit logo
    - Change textLogo setting automatically if theme creator gets a logo param
    - Move some waltti scss settings to the new waltti base theme

--- build/add-theme.js
+++ build/add-theme.js
@@ -7,8 +7,7 @@ const path = require('path');
 
 const theme = process.argv[2];
 const color = process.argv[3] || '$livi-blue';
-const logoPath = process.argv[4] || 'sass/themes/default/digitransit-logo.png'; // source icon
-const logo = path.basename(logoPath);
+const logoPath = process.argv[4];
 
 if (!theme || theme === '?') {
   console.log('Usage: npm run add-theme <name> <optional hex color (with quotes!)> <optional logo image> \n');
@@ -28,8 +27,8 @@ fs.writeFileSync(dir + '/main.scss',
 `
 );
 
-fs.writeFileSync(dir + '/_theme.scss',
-`@import '../default/theme';
+let themeContent =
+`@import '../../base/waltti';
 
 /* main theme colors */
 $primary-color: ${color};
@@ -40,23 +39,36 @@ $bus-color: $primary-color;
 $viewpoint-marker-color: $primary-color;
 
 $standalone-btn-color: $primary-color;
-$standalone-btn-hover-color: $gray;
-$standalone-btn-active-color: $gray;
 
-/* Navbar logo */
-$nav-logo: url(${logo});
-`);
+`;
 
-// copy logo
-fs.createReadStream(logoPath).pipe(fs.createWriteStream(dir + '/' + logo));
+let textLogo;
+
+if (logoPath) {
+  const logo = path.basename(logoPath);
+  // copy logo
+  fs.createReadStream(logoPath).pipe(fs.createWriteStream(dir + '/' + logo));
+
+  // add logo definition to theme
+  themeContent = themeContent + `$nav-logo: url('${logo}'); /* Navbar logo */`;
+  textLogo = 'false';
+} else {
+  textLogo = 'true';
+}
+
+// theme is ready, write it
+fs.writeFileSync(dir + '/_theme.scss', themeContent);
 
 // generate app configuration
 const regexColor = new RegExp('__color__', 'g');
 const regexTheme = new RegExp('__theme__', 'g');
 const regexName = new RegExp('__Theme__', 'g');
+const regexLogo = new RegExp('__textlogo__', 'g');
 
 let conf = fs.readFileSync('build/template.waltti.js', "utf-8");
-conf = conf.replace(regexColor, color).replace(regexTheme, theme).replace(regexName, name);
+conf = conf.replace(regexColor, color).replace(regexTheme, theme)
+           .replace(regexName, name).replace(regexLogo, textLogo);
+
 fs.writeFileSync('app/configurations/config.' + theme + '.js', conf);
 
 console.log('Theme for ' + theme + ' created! Apply "git add" to join new components to the project. \n');
