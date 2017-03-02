c1a7ba9c8b31c1aa640d7849709a7387fffc4937
Sami Siren <siren@foofactory.fi>
2017-03-02 21:22:42 +0200

    handle piwikId here

--- app/util/Raven.js
+++ app/util/Raven.js
@@ -1,21 +1,21 @@
 import { COMMIT_ID } from '../buildInfo';
 
-function getRaven(SENTRY_DSN) {
-  if (process.env.NODE_ENV === 'production') {
+export default function getRaven(sentryDsn, piwikId) {
+  if (sentryDsn) {
     /* eslint-disable global-require */
     const Raven = require('raven-js');
     Raven.addPlugin(require('raven-js/plugins/console'));
-    /* eslint-enable global-require */
 
-    Raven.config(SENTRY_DSN, {
+    Raven.config(sentryDsn, {
       release: COMMIT_ID,
       stacktrace: true,
     }).install();
+
+    if (piwikId) {
+      Raven.setUserContext({ piwik: piwikId });
+    }
+
     return Raven;
   }
-  return {
-    captureMessage: console.error, // eslint-disable-line no-console
-  };
+  return undefined;
 }
-
-export default getRaven;
