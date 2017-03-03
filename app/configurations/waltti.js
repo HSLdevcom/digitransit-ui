4b18f3d96430414c6bcb1e8ffd4075bf64110ecb
Hannes Junnila <hannes.junnila@gmail.com>
2017-03-03 15:19:46 +0200

    Use CDN for vector tiles

--- app/configurations/waltti.js
+++ app/configurations/waltti.js
@@ -1,10 +1,11 @@
 const API_URL = process.env.API_URL || 'https://dev-api.digitransit.fi';
+const MAP_URL = process.env.MAP_URL || 'https://digitransit-dev-cdn-origin.azureedge.net';
 const APP_DESCRIPTION = 'Uusi Reittiopas';
 
 export default {
   URL: {
     OTP: `${API_URL}/routing/v1/routers/waltti/`,
-    STOP_MAP: `${API_URL}/map/v1/waltti-stop-map/`,
+    STOP_MAP: `${MAP_URL}/map/v1/waltti-stop-map/`,
   },
 
   contactName: {
