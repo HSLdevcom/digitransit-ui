13fdb3303b21e0ade2bae8c7d6ee4c002a5c6259
Laura Pimiä <laura.pimia@netlight.com>
2017-03-13 12:52:03 +0200

    Added instructional image

--- app/component/NoFavouritesPanel.js
+++ app/component/NoFavouritesPanel.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+//const noFavouriteImg = require('../../sass/themes/default/favourites_empty_tip'); // eslint-disable-line global-require
+
 const NoFavouritesPanel = () => (
   <div className="row">
     <div className="small-12 columns">
-      <p className="gray text-center"><FormattedMessage
-        id="no-favourites"
+      <p className="gray text-center">
+      <div className="nofavs-img"></div>
+      <FormattedMessage
+        id="no-favourites" 
         defaultMessage="Use the star buttons to add routes or stops to your favorites. Your favorites are shown on this page."
       /></p></div></div>);
 
