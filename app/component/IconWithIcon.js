f973f6bc09c66728723266850ece6d1d33ca748b
Sami Siren <siren@foofactory.fi>
2017-03-23 11:08:23 +0200

    refactor iconwithicon

--- app/component/IconWithIcon.js
+++ app/component/IconWithIcon.js
@@ -1,29 +1,28 @@
 import React, { PropTypes } from 'react';
 
+import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 
-const IconWithIcon = ({ id, className, img, subIcon, iconTransform,
-  subIconTransform, subIconStyle }) => (
-    <svg id={id} viewBox="0 0 40 40" className={`icon ${className}`}>
-      <use
-        xlinkHref={`#${img}`}
-        transform={iconTransform}
-      />
-      <use
-        xlinkHref={`#${subIcon}`}
-        transform={subIconTransform}
-        style={subIconStyle}
-      />
-    </svg>
-);
+const subIconTemplate = { fontSize: '75%', position: 'absolute', bottom: '-5px', left: '-5px' };
+const IconWithIcon = ({ id, className, img, subIcon, subIconClassName }) => (
+  <span style={{ position: 'relative' }} id={id} className={className}>
+    <span ><Icon img={img} /></span>
+    <span className={subIconClassName} style={subIconTemplate}><Icon img={subIcon} /></span>
+  </span>
+  );
 
 IconWithIcon.description = () =>
   <div>
     <ComponentUsageExample description="Bus with caution">
-      <IconWithIcon className="bus" img={'icon-icon_bus'} subIconStyle={{ color: 'white', fill: 'red' }} />
+      <IconWithIcon
+        className="bus"
+        img="icon-icon_bus"
+        subIcon="icon-icon_caution"
+        subIconClassName="subicon-caution"
+      />
     </ComponentUsageExample>
     <ComponentUsageExample description="Bus with call agency caution">
-      <IconWithIcon className="bus" img={'icon-icon_bus'} subIcon="icon-icon_call" />
+      <IconWithIcon className="bus" img="icon-icon_bus" subIcon="icon-icon_call" />
     </ComponentUsageExample>
   </div>;
 
@@ -34,16 +33,13 @@ IconWithIcon.propTypes = {
   className: PropTypes.string,
   img: PropTypes.string.isRequired,
   subIcon: PropTypes.string,
-  iconTransform: PropTypes.string,
-  subIconTransform: PropTypes.string,
-  subIconStyle: PropTypes.string,
+  subIconClassName: PropTypes.string,
 };
 
 IconWithIcon.defaultProps = {
-  subIcon: 'icon-icon_caution',
+  subIcon: '',
   className: '',
-  iconTransform: 'scale(0.9) translate(5)',
-  subIconTransform: 'scale(0.7) translate(0,20) ',
+  subIconClassName: '',
   subIconStyle: {},
 };
 
