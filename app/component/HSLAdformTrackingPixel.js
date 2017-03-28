69ad97c5cd57f8dc817f8953380241314c97a07e
Sami Siren <siren@foofactory.fi>
2017-03-28 10:52:33 +0300

    add tracking pixel to hsl reittiopas

---
+++ app/component/HSLAdformTrackingPixel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const HSLAdformTrackingPixel = () => (
+  <img src="https://track.adform.net/Serving/TrackPoint/?pm=785700&ADFPageName=K%C3%A4yneet%20sivustolla&ADFdivider=|" width="1" height="1" alt="" />);
+
+HSLAdformTrackingPixel.displayName = 'HSLAdformTrackingPixel';
+
+export default HSLAdformTrackingPixel;
