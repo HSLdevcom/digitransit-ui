3807ce904ee3d5b15542bfc72ece20683e6bff1d
Sami Siren <siren@foofactory.fi>
2017-03-28 14:05:39 +0300

    make trackingpixel to not occupy space

--- app/component/HSLAdformTrackingPixel.js
+++ app/component/HSLAdformTrackingPixel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HSLAdformTrackingPixel = () => (
-  <img src="https://track.adform.net/Serving/TrackPoint/?pm=785700&ADFPageName=K%C3%A4yneet%20sivustolla&ADFdivider=|" width="1" height="1" alt="" />);
+  <div style={{ width: 0, height: 0 }} ><img id=""src="https://track.adform.net/Serving/TrackPoint/?pm=785700&ADFPageName=K%C3%A4yneet%20sivustolla&ADFdivider=|" width="1" height="1" alt="" /></div>);
 
 HSLAdformTrackingPixel.displayName = 'HSLAdformTrackingPixel';
 
