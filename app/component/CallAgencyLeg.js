32e1459092f09b87d54e5a321b071e28d1c30855
Sami Siren <siren@foofactory.fi>
2017-03-28 10:53:45 +0300

    remove cruft

--- app/component/CallAgencyLeg.js
+++ app/component/CallAgencyLeg.js
@@ -13,84 +13,6 @@ class CallAgencyLeg extends React.Component {
 
   stopCode = stopCode => stopCode && <StopCode code={stopCode} />;
 
-  renderMain = () => {
-    const originalTime = (
-      this.props.leg.realTime &&
-      this.props.leg.departureDelay >= this.context.config.itinerary.delayThreshold) &&
-      [<br key="br" />, <span key="time" className="original-time">
-        {moment(this.props.leg.startTime).subtract(this.props.leg.departureDelay, 's')
-          .format('HH:mm')
-        }
-      </span>];
-
-    const firstLegClassName = this.props.index === 0 ? ' start' : '';
-    const modeClassName = 'call';
-
-    return (
-      <div key={this.props.index}>
-        <div
-          className="row itinerary-row"
-        >
-          <div className="itinerary-call-agency-warning" />
-          <Link
-            onClick={e => e.stopPropagation()}
-            to={
-          `/linjat/${this.props.leg.route.gtfsId}/pysakit/${
-          this.props.leg.trip.pattern.code}/${this.props.leg.trip.gtfsId}`
-          // TODO: Create a helper function for generationg links
-        }
-          >
-            <div className="small-2 columns itinerary-time-column call">
-              <div className="itinerary-time-column-time">
-                <span className={this.props.leg.realTime ? 'realtime' : ''}>
-                  {this.props.leg.realTime &&
-                  <Icon img="icon-icon_realtime" className="realtime-icon realtime" />}
-                  {moment(this.props.leg.startTime).format('HH:mm')}
-                </span>{originalTime}
-              </div>
-              <RouteNumber
-                mode="call"
-                className="call"
-                realtime={false}
-                vertical
-                fadeLong
-              />
-            </div>
-          </Link>
-          <div
-            onClick={this.props.focusAction}
-            className={`small-10 columns itinerary-instruction-column ${firstLegClassName} ${modeClassName}`}
-          >
-            <div className="itinerary-leg-first-row">
-              <div>{this.props.leg.from.name}{this.stopCode(
-            this.props.leg.from.stop && this.props.leg.from.stop.code)}
-                <Icon
-                  img="icon-icon_arrow-collapse--right"
-                  className="itinerary-leg-first-row__arrow"
-                />
-              </div>
-              <Icon img="icon-icon_search-plus" className="itinerary-search-icon" />
-            </div>
-            <div className="itinerary-transit-leg-route call">
-              <span className="warning-message">
-                <FormattedMessage
-                  id="warning-call-agency"
-                  values={{
-                    routeName: <span className="route-name">{this.props.leg.route.longName}</span>,
-                  }}
-                  defaultMessage={'Only on demand: {routeName}, which needs to be booked in advance.'}
-                />
-                <div className="itinerary-warning-agency-container"><LegAgencyInfo leg={this.props.leg} /></div>
-                {this.props.leg.route.agency.phone ? (<div className="call-button"><Link href={`tel:${this.props.leg.route.agency.phone}`}><FormattedMessage
-                  id="call"
-                  defaultMessage="Call"
-                /> {this.props.leg.route.agency.phone}</Link></div>) : ''}
-              </span>
-            </div>
-          </div>
-        </div></div>);
-  }
-
   render() {
     const originalTime = (
       this.props.leg.realTime &&
@@ -105,68 +27,67 @@ class CallAgencyLeg extends React.Component {
     const modeClassName = 'call';
 
     return (
-      <div><div key={this.props.index}>
-        <div
-          className="row itinerary-row"
-        >
-          <div className="itinerary-call-agency-warning" />
-          <Link
-            onClick={e => e.stopPropagation()}
-            to={
+      <div
+        className="row itinerary-row"
+      >
+        <div className="itinerary-call-agency-warning" />
+        <Link
+          onClick={e => e.stopPropagation()}
+          to={
           `/linjat/${this.props.leg.route.gtfsId}/pysakit/${
           this.props.leg.trip.pattern.code}/${this.props.leg.trip.gtfsId}`
           // TODO: Create a helper function for generationg links
         }
-          >
-            <div className="small-2 columns itinerary-time-column call">
-              <div className="itinerary-time-column-time">
-                <span className={this.props.leg.realTime ? 'realtime' : ''}>
-                  {this.props.leg.realTime &&
+        >
+          <div className="small-2 columns itinerary-time-column call">
+            <div className="itinerary-time-column-time">
+              <span className={this.props.leg.realTime ? 'realtime' : ''}>
+                {this.props.leg.realTime &&
                   <Icon img="icon-icon_realtime" className="realtime-icon realtime" />}
-                  {moment(this.props.leg.startTime).format('HH:mm')}
-                </span>{originalTime}
-              </div>
-              <RouteNumber
-                mode="call"
-                className="call"
-                realtime={false}
-                vertical
-                fadeLong
+                {moment(this.props.leg.startTime).format('HH:mm')}
+              </span>{originalTime}
+            </div>
+            <RouteNumber
+              mode="call"
+              className="call"
+              realtime={false}
+              vertical
+              fadeLong
+            />
+          </div>
+        </Link>
+        <div
+          onClick={this.props.focusAction}
+          className={`small-10 columns itinerary-instruction-column ${firstLegClassName} ${modeClassName}`}
+        >
+          <div className="itinerary-leg-first-row">
+            <div>{this.props.leg.from.name}{this.stopCode(
+            this.props.leg.from.stop && this.props.leg.from.stop.code)}
+              <Icon
+                img="icon-icon_arrow-collapse--right"
+                className="itinerary-leg-first-row__arrow"
               />
             </div>
-          </Link>
-          <div
-            onClick={this.props.focusAction}
-            className={`small-10 columns itinerary-instruction-column ${firstLegClassName} ${modeClassName}`}
-          >
-            <div className="itinerary-leg-first-row">
-              <div>{this.props.leg.from.name}{this.stopCode(
-            this.props.leg.from.stop && this.props.leg.from.stop.code)}
-                <Icon
-                  img="icon-icon_arrow-collapse--right"
-                  className="itinerary-leg-first-row__arrow"
-                />
-              </div>
-              <Icon img="icon-icon_search-plus" className="itinerary-search-icon" />
-            </div>
-            <div className="itinerary-transit-leg-route call">
-              <span className="warning-message">
-                <FormattedMessage
-                  id="warning-call-agency"
-                  values={{
-                    routeName: <span className="route-name">{this.props.leg.route.longName}</span>,
-                  }}
-                  defaultMessage={'Only on demand: {routeName}, which needs to be booked in advance.'}
-                />
-                <div className="itinerary-warning-agency-container"><LegAgencyInfo leg={this.props.leg} /></div>
-                {this.props.leg.route.agency.phone ? (<div className="call-button"><Link href={`tel:${this.props.leg.route.agency.phone}`}><FormattedMessage
-                  id="call"
-                  defaultMessage="Call"
-                /> {this.props.leg.route.agency.phone}</Link></div>) : ''}
-              </span>
-            </div>
+            <Icon img="icon-icon_search-plus" className="itinerary-search-icon" />
           </div>
-        </div></div></div>);
+          <div className="itinerary-transit-leg-route call">
+            <span className="warning-message">
+              <FormattedMessage
+                id="warning-call-agency"
+                values={{
+                  routeName: <span className="route-name">{this.props.leg.route.longName}</span>,
+                }}
+                defaultMessage={'Only on demand: {routeName}, which needs to be booked in advance.'}
+              />
+              <div className="itinerary-warning-agency-container"><LegAgencyInfo leg={this.props.leg} /></div>
+              {this.props.leg.route.agency.phone ? (<div className="call-button"><Link href={`tel:${this.props.leg.route.agency.phone}`}><FormattedMessage
+                id="call"
+                defaultMessage="Call"
+              /> {this.props.leg.route.agency.phone}</Link></div>) : ''}
+            </span>
+          </div>
+        </div>
+      </div>);
   }
 }
 
