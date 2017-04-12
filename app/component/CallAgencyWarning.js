446bac088cf54373c81bd6c69b451d7be321539a
Sami Siren <siren@foofactory.fi>
2017-04-12 12:33:08 +0300

    add warning component

---
+++ app/component/CallAgencyWarning.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { FormattedMessage } from 'react-intl';
+import ComponentUsageExample from './ComponentUsageExample';
+import Icon from './Icon';
+
+const CallAgencyWarning = () => (
+  <div className="warning-message padding-small">
+    <Icon className="warning-message-icon" img="icon-icon_call" />
+    <FormattedMessage id="warning-call-agency-no-route" defaultMessage="Only on demand. Needs to be booked in advance." />
+  </div>);
+
+
+CallAgencyWarning.description = () => (
+  <div>
+    <p>Displays a warning message.</p>
+    <ComponentUsageExample description="normal">
+      <CallAgencyWarning />
+    </ComponentUsageExample>
+  </div>
+  );
+
+CallAgencyWarning.contextTypes = {
+  config: React.PropTypes.object.isRequired,
+};
+
+export default CallAgencyWarning;
