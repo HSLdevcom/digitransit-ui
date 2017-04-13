f15a716a71b8ea4796df54f8403248c3f1eafbf4
Sami Siren <siren@foofactory.fi>
2017-04-13 10:58:16 +0300

    tweak padding

--- app/component/CallAgencyWarning.js
+++ app/component/CallAgencyWarning.js
@@ -5,7 +5,7 @@ import ComponentUsageExample from './ComponentUsageExample';
 import Icon from './Icon';
 
 const CallAgencyWarning = () => (
-  <div className="warning-message padding-small">
+  <div className="warning-message padding-normal">
     <Icon className="warning-message-icon" img="icon-icon_call" />
     <FormattedMessage id="warning-call-agency-no-route" defaultMessage="Only on demand. Needs to be booked in advance." />
   </div>);
