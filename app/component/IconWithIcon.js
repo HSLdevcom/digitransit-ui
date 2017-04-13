952bbbfd94416845272c2a6a65821b164be09b2c
Sami Siren <siren@foofactory.fi>
2017-04-13 10:54:34 +0300

    tweak proportion

--- app/component/IconWithIcon.js
+++ app/component/IconWithIcon.js
@@ -3,11 +3,12 @@ import React, { PropTypes } from 'react';
 import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 
-const subIconTemplate = { fontSize: '75%', position: 'absolute', bottom: '-5px', left: '-5px' };
+const subIconTemplate = { fontSize: '65%', position: 'absolute', bottom: '-0.3em', left: '-0.3em' };
 const IconWithIcon = ({ id, className, img, subIcon, subIconClassName }) => (
   <span style={{ position: 'relative' }} id={id} className={className}>
     <span ><Icon img={img} /></span>
-    <span className={subIconClassName} style={subIconTemplate}><Icon img={subIcon} /></span>
+    {subIcon && <span className={subIconClassName} style={subIconTemplate}>
+      <Icon img={subIcon} /></span>}
   </span>
   );
 
@@ -25,6 +26,11 @@ IconWithIcon.description = () =>
     </ComponentUsageExample>
     <ComponentUsageExample description="Bus with call agency caution">
       <div style={{ paddingLeft: '5px' }}>
+        <IconWithIcon className="bus" img="icon-icon_bus" subIcon="icon-icon_call" />
+      </div>
+    </ComponentUsageExample>
+    <ComponentUsageExample description="Bus with call agency caution, with 5em base font size">
+      <div style={{ fontSize: '5em', paddingLeft: '5px' }}>
         <IconWithIcon className="bus" img="icon-icon_bus" subIcon="icon-icon_call" />
       </div>
     </ComponentUsageExample>
