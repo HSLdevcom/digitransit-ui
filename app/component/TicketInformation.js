0dcf34f81202cdee4fcf623c0621a029799989f9
Sami Siren <siren@foofactory.fi>
2017-04-25 12:29:38 +0300

    review fixes

--- app/component/TicketInformation.js
+++ app/component/TicketInformation.js
@@ -32,20 +32,18 @@ export default function TicketInformation({ fares }, { config }) {
 
   return (
     <div className="row itinerary-row itinerary-ticket-information">
-      <div className="itinerary-ticket-layout-left"><Icon img="icon-icon_ticket" /></div>
-      <div className="itinerary-ticket-layout-right">
-        <div className="itinerary-information-container">
-          <div className="itinerary-ticket-information-class">
-            <FormattedMessage id="ticket-single-adult" defaultMessage="Adult" />
+      <div className="columns small-2 itinerary-ticket-layout-left"><Icon img="icon-icon_ticket" /></div>
+      <div className="columns small-10 itinerary-ticket-layout-right">
+        <div className="itinerary-ticket-type">
+          <FormattedMessage id="ticket-single-adult" defaultMessage="Adult" />
             , <FormattedMessage id={`ticket-type-${mappedFareId}`} />, {`${(regularFare.cents / 100).toFixed(2)} ${currency}`}
-          </div>
-          <ExternalLink className="itinerary-ticket-external-link" href="https://www.hsl.fi/liput-ja-hinnat" >
-            <FormattedMessage
-              id="buy-ticket"
-              defaultMessage="How to buy a ticket (HSL.fi)"
-            />
-          </ExternalLink>
         </div>
+        <ExternalLink className="itinerary-ticket-external-link" href="https://www.hsl.fi/liput-ja-hinnat" >
+          <FormattedMessage
+            id="buy-ticket"
+            defaultMessage="How to buy a ticket (HSL.fi)"
+          />
+        </ExternalLink>
       </div>
     </div>
   );
