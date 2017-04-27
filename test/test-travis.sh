8dd172b87a31274dc75258e6b03c3b4cb450236b
Sami Siren <siren@foofactory.fi>
2017-04-27 21:32:07 +0300

    test

--- test/test-travis.sh
+++ test/test-travis.sh
@@ -2,11 +2,15 @@
 yarn install
 
 if [ -n "$VISUAL" ]; then
+  docker build -t hsldevcom/digitransit-ui:$TRAVIS_COMMIT .
+  docker run -d -e CONFIG=hsl -p 127.0.0.1:8080:8080 hsldevcom/digitransit-ui:$TRAVIS_COMMIT
   yarn run test-visual -- --browser $VISUAL
   exit $?
 fi
 
 if [ -n "$NWENV" ]; then
+  docker build -t hsldevcom/digitransit-ui:$TRAVIS_COMMIT .
+  docker run -d -e CONFIG=hsl -p 127.0.0.1:8080:8080 hsldevcom/digitransit-ui:$TRAVIS_COMMIT
   wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip
   unzip chromedriver_linux64.zip
   NOSERVER=1 CHROMEDRIVER=./chromedriver test/flow/script/run-snap-tests.sh
