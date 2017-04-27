537b5189da9204337cfed2b3248f0d5221a09560
Sami Siren <siren@foofactory.fi>
2017-04-27 18:19:43 +0300

    test

--- test/test-travis.sh
+++ test/test-travis.sh
@@ -9,7 +9,7 @@ fi
 if [ -n "$NWENV" ]; then
   wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip
   unzip chromedriver_linux64.zip
-  CHROMEDRIVER=./chromedriver test/flow/script/run-snap-tests.sh
+  NOSERVER=1 CHROMEDRIVER=./chromedriver test/flow/script/run-snap-tests.sh
   exit $?
 fi
 
