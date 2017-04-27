6bd7aa94de7f21f85846dd4d2394476ce81891db
Sami Siren <siren@foofactory.fi>
2017-04-27 16:47:46 +0300

    test

--- test/test-travis.sh
+++ test/test-travis.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 
 if [ -n "$VISUAL" ]; then
+  npm install --silent
   npm run test-visual -- --browser $VISUAL
   exit $?
 fi
 
 if [ -n "$NWENV" ]; then
-  test/flow/script/run-snap-tests.sh
+  wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip
+  unzip chromedriver_linux64.zip
+  CHROMEDRIVER=./chromedriver test/flow/script/run-snap-tests.sh
   exit $?
 fi
 
