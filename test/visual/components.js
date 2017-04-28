f952e56cebc24ecb5aa66879d95145fe5ccae5a3
Sami Siren <siren@foofactory.fi>
2017-04-28 10:03:01 +0300

    test

--- test/visual/components.js
+++ test/visual/components.js
@@ -23,6 +23,7 @@ function testVariation(componentName, variationName = 'normal', captureOrExample
           }
           suite
             .setUrl(`/styleguide/component/${componentName}?en`)
+            .waitForElementToShow(capture, 5000)
             .setCaptureElements(capture)
             .ignoreElements(ignoreElements || [])
             .capture(variationName, {}, fn);
