e2a4e035afe43ce2baab6d0696f5104559d3c701
Sami Siren <siren@foofactory.fi>
2017-05-02 10:57:02 +0300

    test

--- test/test-travis.sh
+++ test/test-travis.sh
@@ -2,16 +2,21 @@
 yarn install
 
 if [ -n "$VISUAL" ]; then
-  docker build -t hsldevcom/digitransit-ui:$TRAVIS_COMMIT .
-  docker run -d -e CONFIG=hsl -p 127.0.0.1:8080:8080 hsldevcom/digitransit-ui:$TRAVIS_COMMIT
-  IDENTIFIER=$TRAVIS_COMMIT_$VISUAL yarn run test-visual -- --browser $VISUAL
-  RESULT=$?
-  if [ $RESULT -ne 0 ]; then
-    echo "Uploading test images to https://www.dropbox.com/sh/emh3x8h38egy2k1/AAAq_eLYDxJ0AJAwFffoZqH9a?dl=0"
-    tar czf gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz gemini-report
-    ./dropbox_uploader.sh gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz /gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz
+  if [ -n "$BS_ACCESS_KEY" ]; then
+    docker build -t hsldevcom/digitransit-ui:$TRAVIS_COMMIT .
+    docker run -d -e CONFIG=hsl -p 127.0.0.1:8080:8080 hsldevcom/digitransit-ui:$TRAVIS_COMMIT
+    IDENTIFIER=$TRAVIS_COMMIT_$VISUAL yarn run test-visual -- --browser $VISUAL
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+      echo "Uploading test images to https://www.dropbox.com/sh/emh3x8h38egy2k1/AAAq_eLYDxJ0AJAwFffoZqH9a?dl=0"
+      tar czf gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz gemini-report
+      ./dropbox_uploader.sh gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz /gemini-report-$TRAVIS_COMMIT-$VISUAL.tar.gz
+    fi
+    exit $RESULT
+  else
+    echo "BS_ACCESS_KEY not set, failing build on purpose"
+    exit 1
   fi
-  exit $RESULT
 fi
 
 if [ -n "$NWENV" ]; then
