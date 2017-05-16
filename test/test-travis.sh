22ed6a08c8a98edc864067d53e6811bf848678d8
Sami Siren <siren@foofactory.fi>
2017-05-16 10:17:17 +0300

    Update test-travis.sh
    try fixing identifier

--- test/test-travis.sh
+++ test/test-travis.sh
@@ -5,7 +5,7 @@ if [ -n "$VISUAL" ]; then
   if [ -n "$BS_ACCESS_KEY" ]; then
     docker build -t hsldevcom/digitransit-ui:$TRAVIS_COMMIT .
     docker run -d -e CONFIG=hsl -p 127.0.0.1:8080:8080 hsldevcom/digitransit-ui:$TRAVIS_COMMIT
-    IDENTIFIER=$TRAVIS_COMMIT_$VISUAL yarn run test-visual -- --browser $VISUAL
+    IDENTIFIER=${TRAVIS_COMMIT}_${VISUAL} yarn run test-visual -- --browser $VISUAL
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
       echo "Uploading test images to https://www.dropbox.com/sh/emh3x8h38egy2k1/AAAq_eLYDxJ0AJAwFffoZqH9a?dl=0"
