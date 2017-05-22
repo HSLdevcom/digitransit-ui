f9928328edcebf94f25fbb1728d6b23f14eecba8
Laura Pimiä <laura.pimia@netlight.com>
2017-05-22 11:26:01 +0300

    mobile styles

--- app/component/StopPageTabContainer.js
+++ app/component/StopPageTabContainer.js
@@ -8,38 +8,42 @@ class StopPageTabContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: null,
+      active: 'right-now', //show departures as the default
     };
   }
 
   render() {
     return (<div className="stop-tab-container">
       <div
-        className={`stop-tab-singletab ${this.state.active === 'departures' && 'active'}`}
-        onClick={() => { this.setState({ active: 'departures' }); }}
+        className={`stop-tab-singletab ${this.state.active === 'right-now' ? 'active' : 'inactive'}`}
+        onClick={() => { this.setState({ active: 'right-now' }); }}
       >
         <div className="stop-tab-singletab-container">
-          <Icon img="icon-icon_realtime" className="stop-page-tab_icon" />
+          <div><Icon img="icon-icon_realtime" className="stop-page-tab_icon" /></div>
           <span>
             <FormattedMessage
-              id="departures"
-              defaultMessage="Departures"
+              id="right-now"
+              defaultMessage="right now"
             /></span>
         </div>
       </div>
       <div
-        className={`stop-tab-singletab ${this.state.active === 'schedule' && 'active'}`}
-        onClick={() => { this.setState({ active: 'schedule' }); }}
+        className={`stop-tab-singletab ${this.state.active === 'timetable' ? 'active' : 'inactive'}`}
+        onClick={() => { this.setState({ active: 'timetable' }); }}
       >
         <div className="stop-tab-singletab-container">
-          <Icon img="icon-icon_schedule" className="stop-page-tab_icon" />
+          <div><Icon img="icon-icon_schedule" className="stop-page-tab_icon" /></div>
           <span>
             <FormattedMessage
-              id="schedule"
-              defaultMessage="schedule"
+              id="timetable"
+              defaultMessage="timetable"
             /></span>
         </div>
       </div>
+      <div
+        className={`stop-tab-singletab ${this.state.active === 'add-info' ? 'add-info' : 'inactive'}`}
+        // onClick={() => { this.setState({ active: 'add-info' }); }}
+      />
     </div>);
   }
 }
