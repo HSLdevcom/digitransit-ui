daa22db83bb2d603c79df14af6a7bd697081f635
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-24 09:54:35 +0300

    Balance brackets

--- test/deploy.sh
+++ test/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 ORG=${ORG:-hsldevcom}
 
-if [[ [ -n "$TRAVIS_TAG" || ( "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false") ]]; then
+if [[ -n "$TRAVIS_TAG" || ( "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false") ]]; then
   docker pull $ORG/digitransit-ui:ci-$TRAVIS_COMMIT
   docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
   if [ -n "$TRAVIS_TAG" ]; then
