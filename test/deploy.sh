b5cfa035598c93c5a7529fc9f4c3577b29c286c9
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-24 09:07:09 +0300

    Use correct test to catch a prod release

--- test/deploy.sh
+++ test/deploy.sh
@@ -2,16 +2,16 @@
 set -e
 ORG=${ORG:-hsldevcom}
 
-if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+if [[ [ -n "$TRAVIS_TAG" || ( "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false") ]]; then
   docker pull $ORG/digitransit-ui:ci-$TRAVIS_COMMIT
   docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
-  if [ -z "$TRAVIS_TAG" ]; then
-      echo "Pushing dev release to docker"
-      docker tag $ORG/digitransit-ui:ci-$TRAVIS_COMMIT $ORG/digitransit-ui:latest
-      docker push $ORG/digitransit-ui:latest
-  else
+  if [ -n "$TRAVIS_TAG" ]; then
       echo "Pushing prod release to docker"
       docker tag $ORG/digitransit-ui:ci-$TRAVIS_COMMIT $ORG/digitransit-ui:prod
       docker push $ORG/digitransit-ui:prod
+  else
+      echo "Pushing dev release to docker"
+      docker tag $ORG/digitransit-ui:ci-$TRAVIS_COMMIT $ORG/digitransit-ui:latest
+      docker push $ORG/digitransit-ui:latest
   fi
 fi
