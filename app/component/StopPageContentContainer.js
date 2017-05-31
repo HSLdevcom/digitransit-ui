5ef5da6a4ebb8e681500f2118f90f1609c02c0dd
rrudd <raoul.udd@netlight.com>
2017-05-31 10:29:18 +0300

    only show scrollbar if content overflows

--- app/component/StopPageContentContainer.js
+++ app/component/StopPageContentContainer.js
@@ -41,14 +41,14 @@ class StopPageContentOptions extends React.Component {
         {this.state.showTab === 'right-now' && <DepartureListHeader />}
       </div>
       {this.state.showTab === 'right-now' &&
-        <div style={{ height: '100%', overflowY: 'scroll' }}>
+        <div style={{ height: '100%', overflowY: 'auto' }}>
           <div className="stop-scroll-container">
             <DepartureListContainerWithProps {...this.props.departureProps} />
           </div>
         </div>
       }
       {this.state.showTab === 'timetable' &&
-      <div style={{ height: '100%', overflowY: 'scroll' }}>
+      <div style={{ height: '100%', overflowY: 'auto' }}>
         <StopPageActionBar breakpoint={this.props.breakPoint} printUrl={this.props.printUrl} />
         <TimetableContainer stop={this.props.departureProps.stop} />
       </div>
