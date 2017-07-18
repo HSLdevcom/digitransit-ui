a9afd57113cfaa77bacae25c3892139ed6c79f27
Hannes Junnila <hannes.junnila@gmail.com>
2017-07-18 17:27:08 +0300

    fix react/prefer-stateless-function

--- app/component/DisruptionInfoButton.js
+++ app/component/DisruptionInfoButton.js
@@ -4,39 +4,34 @@ import Relay from 'react-relay';
 import { FormattedMessage } from 'react-intl';
 import Icon from './Icon';
 
-class DisruptionInfoButton extends React.Component {
-  static propTypes = {
-    toggleDisruptionInfo: PropTypes.func,
-    root: PropTypes.shape({
-      alerts: PropTypes.array,
-    }).isRequired,
-  };
-
-  static contextTypes = {
-    config: PropTypes.object.isRequired,
-  };
-
-  render() {
-    if (
-      !this.context.config.disruption ||
-      this.context.config.disruption.showInfoButton
-    ) {
-      return (
-        <div
-          className={'cursor-pointer disruption-info'}
-          onClick={this.props.toggleDisruptionInfo}
-        >
-          <FormattedMessage id="disruptions" defaultMessage="Disruptions" />
-          {this.props.root &&
-            this.props.root.alerts &&
-            this.props.root.alerts.length > 0 &&
-            <Icon img={'icon-icon_caution'} className={'disruption-info'} />}
-        </div>
-      );
-    }
-    return null;
+function DisruptionInfoButton(props, { config }) {
+  if (!config.disruption || config.disruption.showInfoButton) {
+    return (
+      <div
+        className={'cursor-pointer disruption-info'}
+        onClick={props.toggleDisruptionInfo}
+      >
+        <FormattedMessage id="disruptions" defaultMessage="Disruptions" />
+        {props.root &&
+          props.root.alerts &&
+          props.root.alerts.length > 0 &&
+          <Icon img={'icon-icon_caution'} className={'disruption-info'} />}
+      </div>
+    );
   }
+  return null;
 }
+
+DisruptionInfoButton.propTypes = {
+  toggleDisruptionInfo: PropTypes.func.isRequired,
+  root: PropTypes.shape({
+    alerts: PropTypes.array,
+  }).isRequired,
+};
+
+DisruptionInfoButton.contextTypes = {
+  config: PropTypes.object.isRequired,
+};
 
 export default Relay.createContainer(DisruptionInfoButton, {
   fragments: {
--- app/component/StopCard.js
+++ app/component/StopCard.js
@@ -4,34 +4,25 @@ import { Link } from 'react-router';
 import StopCardHeaderContainer from './StopCardHeaderContainer';
 import Card from './Card';
 
-class StopCard extends React.Component {
-  render() {
-    if (
-      !this.props.stop ||
-      !this.props.children ||
-      this.props.children.length === 0
-    ) {
-      return false;
-    }
-    const prefix = this.props.isTerminal ? 'terminaalit' : 'pysakit';
-
-    return (
-      <Link
-        to={`/${prefix}/${this.props.stop.gtfsId}`}
-        className="no-decoration"
-      >
-        <Card className={this.props.className}>
-          <StopCardHeaderContainer
-            stop={this.props.stop}
-            icons={this.props.icons}
-            distance={this.props.distance}
-            headingStyle="header-primary"
-          />
-          {this.props.children}
-        </Card>
-      </Link>
-    );
+function StopCard(props) {
+  if (!props.stop || !props.children || props.children.length === 0) {
+    return false;
   }
+  const prefix = props.isTerminal ? 'terminaalit' : 'pysakit';
+
+  return (
+    <Link to={`/${prefix}/${props.stop.gtfsId}`} className="no-decoration">
+      <Card className={props.className}>
+        <StopCardHeaderContainer
+          stop={props.stop}
+          icons={props.icons}
+          distance={props.distance}
+          headingStyle="header-primary"
+        />
+        {props.children}
+      </Card>
+    </Link>
+  );
 }
 
 StopCard.propTypes = {
