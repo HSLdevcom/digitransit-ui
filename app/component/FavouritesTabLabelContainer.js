197aa450352a2529565253dff09d5b58a872c85d
Hannes Junnila <hannes.junnila@gmail.com>
2017-07-20 15:55:38 +0300

    convert FavouritesTabLabelContainer

--- app/component/FavouritesTabLabelContainer.js
+++ app/component/FavouritesTabLabelContainer.js
@@ -1,56 +1,41 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { createFragmentContainer, graphql } from 'react-relay/compat';
-import { Renderer, Store } from 'react-relay/classic';
+import { QueryRenderer, graphql } from 'react-relay/compat';
+import { Store } from 'react-relay/classic';
 import connectToStores from 'fluxible-addons-react/connectToStores';
-import mapProps from 'recompose/mapProps';
 import some from 'lodash/some';
 import flatten from 'lodash/flatten';
-import RoutesRoute from '../route/RoutesRoute';
 import FavouritesTabLabel from './FavouritesTabLabel';
-import { isBrowser } from '../util/browser';
 
 const hasDisruption = routes =>
   some(flatten(routes.map(route => route.alerts.length > 0)));
 
-const alertReducer = mapProps(({ routes, ...rest }) => ({
-  hasDisruption: hasDisruption(routes),
-  ...rest,
-}));
-
-const FavouritesTabLabelRelayConnector = createFragmentContainer(
-  alertReducer(FavouritesTabLabel),
-  {
-    routes: graphql`
-      fragment FavouritesTabLabelContainer_routes on Route
-        @relay(plural: true) {
-        alerts {
-          id
-        }
-      }
-    `,
-  },
-);
-
 function FavouritesTabLabelContainer({ routes, ...rest }) {
-  if (isBrowser) {
-    return (
-      <Renderer
-        Container={FavouritesTabLabelRelayConnector}
-        queryConfig={new RoutesRoute({ ids: routes })}
-        environment={Store}
-        render={({ done, props }) =>
-          done
-            ? <FavouritesTabLabelRelayConnector {...props} {...rest} />
-            : <FavouritesTabLabel {...rest} />}
-      />
-    );
-  }
-  return <div />;
+  return (
+    <QueryRenderer
+      cacheConfig={{ force: true, poll: 30 * 1000 }}
+      query={graphql`
+        query FavouritesTabLabelContainerQuery($ids: [String]) {
+          routes(ids: $ids) {
+            alerts {
+              id
+            }
+          }
+        }
+      `}
+      variables={{ ids: routes }}
+      environment={Store}
+      render={({ props }) =>
+        <FavouritesTabLabel
+          hasDisruption={props && hasDisruption(props.routes)}
+          {...rest}
+        />}
+    />
+  );
 }
 
 FavouritesTabLabelContainer.propTypes = {
-  routes: PropTypes.array.isRequired,
+  routes: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 export default connectToStores(
