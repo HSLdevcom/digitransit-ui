9af1ad54445af2c81ac17418549f57477b801880
Laura Pimiä <laura.pimia@netlight.com>
2017-09-21 12:59:50 +0300

    Refactored SecondaryButton component

--- app/component/SecondaryButton.js
+++ app/component/SecondaryButton.js
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import cx from 'classnames';
 
 import { FormattedMessage, intlShape } from 'react-intl';
 import Icon from './Icon';
 
-const secondaryButton = (props, context) =>
-  <button
-    className={`secondary-button ${props.buttonName} ${props.smallSize &&
-      `small`}`}
-    aria-label={context.intl.formatMessage({
-      id: props.ariaLabel,
-      defaultMessage: props.ariaLabel,
-    })}
-    onClick={e => props.buttonClickAction(e)}
-  >
-    {props.buttonIcon && <Icon img={props.buttonIcon} />}
-    <FormattedMessage id={props.buttonName} defaultMessage={props.buttonName} />
-  </button>;
+const secondaryButton = (props, context) => {
+  const className = cx([
+    'secondary-button',
+    props.buttonName,
+    { small: props.smallSize },
+  ]);
+  return (
+    <button
+      className={className}
+      aria-label={context.intl.formatMessage({
+        id: props.ariaLabel,
+        defaultMessage: props.ariaLabel,
+      })}
+      onClick={e => props.buttonClickAction(e)}
+    >
+      {props.buttonIcon && <Icon img={props.buttonIcon} />}
+      <FormattedMessage
+        id={props.buttonName}
+        defaultMessage={props.buttonName}
+      />
+    </button>
+  );
+};
 
 secondaryButton.propTypes = {
   ariaLabel: PropTypes.string.isRequired,
