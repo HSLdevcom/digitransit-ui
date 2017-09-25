f4f87351a0d26c1525e72180ac914a94d0390551
Sami Siren <siren@foofactory.fi>
2017-09-25 16:08:07 +0300

    add todo

--- app/component/AppBarContainer.js
+++ app/component/AppBarContainer.js
@@ -6,6 +6,7 @@ import AppBarSmall from './AppBarSmall';
 import AppBarLarge from './AppBarLarge';
 
 const AppBarContainer = ({ breakpoint, router, location, ...args }) =>
+  // TODO fix show logo
   (breakpoint !== 'large' &&
     <AppBarSmall {...args} showLogo={router.isActive('/')} />) ||
   <AppBarLarge {...args} titleClicked={() => router.push('/lahellasi')} />;
