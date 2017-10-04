923820c19e1454ea09b7e6b5fbdef751ede7dbce
Sami Siren <siren@foofactory.fi>
2017-10-04 12:42:50 +0300

    not used

--- app/util/shape.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-import PropTypes from 'prop-types';
-
-export const routerShape = PropTypes.shape({
-  replace: PropTypes.func.isRequired,
-  push: PropTypes.func.isRequired,
-});
