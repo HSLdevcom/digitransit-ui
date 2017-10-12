123ec648d894cfba159e2d96128eac3d884e42d7
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-10-12 11:45:04 +0300

    Use actions to modify stores as defined by fluxible ideology

--- app/component/MessageBar.js
+++ app/component/MessageBar.js
@@ -4,6 +4,7 @@ import connectToStores from 'fluxible-addons-react/connectToStores';
 import SwipeableViews from 'react-swipeable-views';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import { intlShape } from 'react-intl';
+import { markMessageAsRead } from '../action/messageActions';
 
 import Icon from './Icon';
 import MessageBarMessage from './MessageBarMessage';
@@ -17,6 +18,7 @@ class MessageBar extends Component {
   static contextTypes = {
     getStore: PropTypes.func.isRequired,
     intl: intlShape.isRequired,
+    executeAction: PropTypes.func.isRequired,
   };
 
   static propTypes = {
@@ -97,9 +99,7 @@ class MessageBar extends Component {
 
   /* Find the id of nth unread (we don't show read messages) and mark it as read */
   markRead = value => {
-    this.context
-      .getStore('MessageStore')
-      .markMessageAsRead(this.validMessages()[value].id);
+    this.context.executeAction(markMessageAsRead, this.validMessages()[value].id);
   };
 
   handleChange = value => {
