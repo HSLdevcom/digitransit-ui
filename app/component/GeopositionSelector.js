508630a86710b01ab156e5bd50bd9c666e5c4c68
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-11-17 09:52:56 +0200

    Show geopositionselector unless we know it won't work

--- app/component/GeopositionSelector.js
+++ app/component/GeopositionSelector.js
@@ -1,25 +1,53 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import connectToStores from 'fluxible-addons-react/connectToStores';
+import { intlShape } from 'react-intl';
+import { routerShape, locationShape } from 'react-router';
+
 import { startLocationWatch } from '../action/PositionActions';
+import PositionStore from '../store/PositionStore';
 import OriginSelectorRow from './OriginSelectorRow';
 
-const GeopositionSelector = (props, context) => (
-  <OriginSelectorRow
-    key={`panel-locationing-button`}
-    icon="icon-icon_position"
-    onClick={() => context.executeAction(startLocationWatch)}
-    label={
-      <FormattedMessage
-        id="use-own-position"
-        defaultMessage="Use current location"
+const GeopositionSelector = ({ status }, context) => {
+  /* States:
+   * - locationing hasn't been started
+   * . locationing in progress
+   * . locationing denied
+   * . locationing failed
+   * - locationing succeeded
+   */
+
+  if (status !== PositionStore.STATUS_GEOLOCATION_NOT_SUPPORTED) {
+    return (
+      <OriginSelectorRow
+        key={`panel-locationing-button`}
+        icon="icon-icon_position"
+        onClick={() => context.executeAction(startLocationWatch)}
+        label={context.intl.formatMessage({
+          id: 'use-own-position',
+          defaultMessage: 'Use current location',
+        })}
       />
-    }
-  />
-);
+    );
+  }
+  return null;
+};
+
+GeopositionSelector.propTypes = {
+  status: PropTypes.string.isRequired,
+};
 
 GeopositionSelector.contextTypes = {
   executeAction: PropTypes.func.isRequired,
+  router: routerShape.isRequired,
+  location: locationShape.isRequired,
+  intl: intlShape.isRequired,
 };
 
-export default GeopositionSelector;
+export default connectToStores(
+  GeopositionSelector,
+  ['PositionStore'],
+  context => ({
+    status: context.getStore('PositionStore').getLocationState().status,
+  }),
+);
