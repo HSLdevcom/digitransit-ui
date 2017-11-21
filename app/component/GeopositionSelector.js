43e381ae35f36546857bddfdead75c3dc4243ba9
Sami Siren <siren@foofactory.fi>
2017-11-21 08:42:21 +0200

    fix lint

--- app/component/GeopositionSelector.js
+++ app/component/GeopositionSelector.js
@@ -5,18 +5,9 @@ import { intlShape } from 'react-intl';
 import { routerShape, locationShape } from 'react-router';
 
 import { startLocationWatch } from '../action/PositionActions';
-import PositionStore from '../store/PositionStore';
 import OriginSelectorRow from './OriginSelectorRow';
 
-const GeopositionSelector = ({ status }, context) => (
-  /* States:
-   * - locationing hasn't been started
-   * . locationing in progress
-   * . locationing denied
-   * . locationing failed
-   * - locationing succeeded
-   */
-
+const GeopositionSelector = (props, context) => (
   <OriginSelectorRow
     key={`panel-locationing-button`}
     icon="icon-icon_position"
@@ -27,10 +18,6 @@ const GeopositionSelector = ({ status }, context) => (
     })}
   />
 );
-
-GeopositionSelector.propTypes = {
-  status: PropTypes.string.isRequired,
-};
 
 GeopositionSelector.contextTypes = {
   executeAction: PropTypes.func.isRequired,
