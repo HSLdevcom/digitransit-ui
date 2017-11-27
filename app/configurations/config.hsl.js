1eb22d25f6b459cfc5a77abe7244f0b15b91f6d9
Sami Siren <siren@foofactory.fi>
2017-11-27 14:20:17 +0200

    two weeks

--- app/configurations/config.hsl.js
+++ app/configurations/config.hsl.js
@@ -220,7 +220,7 @@ export default {
   shouldShowIntro: false,
 
   redirectReittiopasParams: true,
-  queryMaxAgeDays: 30, // to drop too old route request times from entry url
+  queryMaxAgeDays: 14, // to drop too old route request times from entry url
 
   aboutThisService: {
     fi: [
