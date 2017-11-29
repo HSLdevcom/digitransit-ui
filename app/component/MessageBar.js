41d3fef5c395c929e6d4e56033266e96ef022798
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-11-29 11:11:20 +0200

    Revert messagebar transition attempt
    Height can't be animated as content disappears before transition even starts,
    and height adapts to content. The bar should use fixed height if animated height is desired.

--- app/component/MessageBar.js
+++ app/component/MessageBar.js
@@ -109,80 +109,79 @@ class MessageBar extends Component {
   handleClose = () => {
     const ids = [];
     this.validMessages().forEach(msg => ids.push(msg.id));
-    this.context.executeAction(markMessageAsRead, ids);
+    setTimeout(() => this.context.executeAction(markMessageAsRead, ids), 600);
   };
 
   render = () => {
     const messages = this.validMessages();
 
-    const index = Math.min(this.state.slideIndex, messages.length - 1);
-    let className = 'message-bar flex-horizontal';
-    let msg;
-
     if (messages.length > 0) {
-      className = 'message-bar flex-horizontal show';
-      msg = messages[index];
-    } else {
-      msg = {};
-    }
-    const type = msg.type || 'info';
-    const icon = msg.icon || 'info';
-    const iconName = `icon-icon_${icon}`;
+      const index = Math.min(this.state.slideIndex, messages.length - 1);
+      const msg = messages[index];
+      const type = msg.type || 'info';
+      const icon = msg.icon || 'info';
+      const iconName = `icon-icon_${icon}`;
 
-    return (
-      <section id="messageBar" role="banner" className={className}>
-        <div className="banner-container">
-          <Icon img={iconName} className="message-icon" />
-          <div className={`flex-grow message-bar-${type}`}>
-            <SwipeableViews
-              index={index}
-              onChangeIndex={this.handleChange}
-              className={!this.state.maximized ? 'message-bar-fade' : ''}
-              containerStyle={{
-                maxHeight: this.state.maximized ? '400px' : '100px',
-                transition: 'max-height 300ms',
-              }}
-              slideStyle={{
-                maxHeight: this.state.maximized ? '400px' : '100px',
-                transition: 'max-height 300ms',
-                padding: '10px 10px 0px 10px',
-                overflow: 'hidden',
-                background: '#fff',
-              }}
-            >
-              {this.getTabContent()}
-            </SwipeableViews>
-            <Tabs
-              onChange={this.handleChange}
-              value={index}
-              tabItemContainerStyle={{
-                backgroundColor: '#fff',
-                height: '18px',
-                width: '60px',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-              }}
-              inkBarStyle={{ display: 'none' }}
-            >
-              {this.getTabs()}
-            </Tabs>
+      return (
+        <section
+          id="messageBar"
+          role="banner"
+          className="message-bar flex-horizontal"
+        >
+          <div className="banner-container">
+            <Icon img={iconName} className="message-icon" />
+            <div className={`flex-grow message-bar-${type}`}>
+              <SwipeableViews
+                index={index}
+                onChangeIndex={this.handleChange}
+                className={!this.state.maximized ? 'message-bar-fade' : ''}
+                containerStyle={{
+                  maxHeight: this.state.maximized ? '400px' : '100px',
+                  transition: 'max-height 300ms',
+                }}
+                slideStyle={{
+                  maxHeight: this.state.maximized ? '400px' : '100px',
+                  transition: 'max-height 300ms',
+                  padding: '10px 10px 0px 10px',
+                  overflow: 'hidden',
+                  background: '#fff',
+                }}
+              >
+                {this.getTabContent()}
+              </SwipeableViews>
+              <Tabs
+                onChange={this.handleChange}
+                value={index}
+                tabItemContainerStyle={{
+                  backgroundColor: '#fff',
+                  height: '18px',
+                  width: '60px',
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+                inkBarStyle={{ display: 'none' }}
+              >
+                {this.getTabs()}
+              </Tabs>
+            </div>
+            <div>
+              <button
+                id="close-message-bar"
+                title={this.context.intl.formatMessage({
+                  id: 'messagebar-label-close-message-bar',
+                  defaultMessage: 'Close banner',
+                })}
+                onClick={this.handleClose}
+                className="noborder close-button cursor-pointer"
+              >
+                <Icon img="icon-icon_close" className="close" />
+              </button>
+            </div>
           </div>
-          <div>
-            <button
-              id="close-message-bar"
-              title={this.context.intl.formatMessage({
-                id: 'messagebar-label-close-message-bar',
-                defaultMessage: 'Close banner',
-              })}
-              onClick={this.handleClose}
-              className="noborder close-button cursor-pointer"
-            >
-              <Icon img="icon-icon_close" className="close" />
-            </button>
-          </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
+    return null;
   };
 }
 
