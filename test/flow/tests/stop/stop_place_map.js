6dd22a47c022b6ea73d1e04e06c71fde10a08b53
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-05 14:05:43 +0200

    Fix the stop place map test
    Use the new well working click command

--- test/flow/tests/stop/stop_place_map.js
+++ test/flow/tests/stop/stop_place_map.js
@@ -1,14 +1,12 @@
 module.exports = {
   tags: ['stops', 'map', 'geolocation'],
   'Open Ruoholahden villat cluster, select stop to show its departures': browser => {
-    browser.url(browser.launch_url);
-    browser.setGeolocation(60.1659488, 24.92693);
+    browser.url('http://127.0.0.1:8000/testipaikka::60.1659488,24.92693/-');
 
     const marker = browser.page.marker();
     browser.debug('Waiting a while for tiles to load');
     marker.waitForVectorLayerLoaded();
-    browser.pause(1000);
-    marker.clickSouthOfCurrentLocation();
+    marker.clickSouthOfPlaceMarker();
     marker.waitForPopupPaneHeaderVisible();
 
     const stop = browser.page.stopCard();
