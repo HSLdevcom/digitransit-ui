efb10e078904fc31a3393dc1b5262062ab829206
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-05 14:33:57 +0200

    Update data expectations for stop timetable search & click test

--- test/flow/tests/stop/stop_place_search.js
+++ test/flow/tests/stop/stop_place_search.js
@@ -3,9 +3,9 @@ module.exports = {
   'Search for 1240 (Kamppi) and verify that the title is correct': browser => {
     browser.url(browser.launch_url);
 
-    browser.page.searchFields().selectTimetableForFirstResult('1240');
+    browser.page.searchFields().selectTimetableForFirstResult('1249');
 
-    browser.page.stopCard().expectCardHeaderDescription('Fredrikinkatu 65');
+    browser.page.stopCard().expectCardHeaderDescription('Kamppi');
     browser.page.stopCard().waitForDepartureVisible();
 
     browser.end();
