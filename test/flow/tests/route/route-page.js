b00770a2a3a96b6731e367e60a0cfb82df526de8
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-08 08:13:34 +0200

    Route page test was plain nonsense, changed to a meaningful test

--- test/flow/tests/route/route-page.js
+++ test/flow/tests/route/route-page.js
@@ -1,12 +1,11 @@
 module.exports = {
   '@tags': ['route', 'smoke'],
-  'Route page shows distance to nearest stop': browser => {
+  'Route page shows stops with stop codes': browser => {
     browser.url(browser.launch_url);
-    browser.setGeolocation(60.2, 24.95);
     browser.page.searchFields().setOrigin('6');
     browser.page.searchFields().enterKeyOrigin();
 
-    browser.page.route().waitForWalkDistance();
+    browser.page.route().waitForStopCode();
     browser.end();
   },
 };
