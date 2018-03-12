d0046c0e70653510d1d7eb15b79ea194bf5cc59f
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-12 13:05:25 +0200

    Utility function to parse and validate latlon
    Accepts proper numeric lat,lon pairs

--- app/util/otpStrings.js
+++ app/util/otpStrings.js
@@ -1,13 +1,27 @@
 // Convert between location objects (address, lat, lon)
 // and string format OpenTripPlanner uses in many places
 
+export const parseLatLon = coords => {
+  const latlon = coords.split(',');
+  if (latlon.length === 2) {
+    const lat = parseFloat(latlon[0]);
+    const lon = parseFloat(latlon[1]);
+    if (!Number.isNaN(lat) && !Number.isNaN(lon)) {
+      return {
+        lat,
+        lon,
+      };
+    }
+  }
+  return undefined;
+};
+
 export const otpToLocation = otpString => {
   const [address, coords] = otpString.split('::');
   if (coords) {
     return {
+        ...parseLatLon(coords),
       address,
-      lat: parseFloat(coords.split(',')[0]),
-      lon: parseFloat(coords.split(',')[1]),
     };
   }
   return { address };
