73c03b55b7bb2af188bd6cbd5bbd1ce1991b5daf
Joel Lappalainen <joel.lappalainen@cgi.com>
2018-03-14 15:50:28 +0200

    Add tampere to piwik

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -572,6 +572,7 @@ export default {
     { id: '21', expr: 'oulu' },
     { id: '29', expr: 'kotka' },
     { id: '31', expr: 'mikkeli' },
+    { id: '35', expr: 'tampere' },
     // put generic expressions last so that they do not match waltti cities
     // e.g. reittiopas.hameenlinna.fi or turku.digitransit.fi
     { id: '5', expr: 'dev.reittiopas' },
