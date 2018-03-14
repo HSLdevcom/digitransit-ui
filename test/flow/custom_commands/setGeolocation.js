3e90bc04f5144fef40cc17a8219dc5f2475d321e
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-14 12:08:21 +0200

    Fix typo

--- test/flow/custom_commands/setGeolocation.js
+++ test/flow/custom_commands/setGeolocation.js
@@ -3,7 +3,7 @@ exports.command = function setGeolocation(latitude, longitude) {
   this.execute(
     function fakeGeo(latitude2, longitude2) {
       // Requires using digitransit-ui with the following in the URL: ?mock
-      indow.mock.geolocation.setCurrentPosition(
+      window.mock.geolocation.setCurrentPosition(
         latitude2,
         longitude2,
       );
