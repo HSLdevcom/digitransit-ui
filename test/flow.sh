b0e8283f40518233d8fd95df6ad6f9d70acce0ae
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-15 15:43:39 +0200

    test ... again

--- test/flow.sh
+++ test/flow.sh
@@ -10,4 +10,7 @@ yarn lint
 
 sudo apt-get install -y libappindicator1 fonts-liberation
 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.20.0-linux64.tar.gz
+
 yarn test-local
