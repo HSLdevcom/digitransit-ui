53b2f3fcca3be3d89c5c6aed80532b327df14ac5
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-15 10:52:05 +0200

    Don't start xvfb again

--- test/flow.sh
+++ test/flow.sh
@@ -10,7 +10,4 @@ yarn lint
 
 sudo apt-get install -y libappindicator1 fonts-liberation
 
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-export DISPLAY=:99
-
 yarn test-local
