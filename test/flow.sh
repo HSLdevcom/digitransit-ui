5d3d7391ed9e30ebf7130b9da5c02632734a94c9
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-15 16:03:53 +0200

    test, what else.....

--- test/flow.sh
+++ test/flow.sh
@@ -13,4 +13,8 @@ sudo apt-get install -y libappindicator1 fonts-liberation
 wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
 tar -xvzf geckodriver-v0.20.0-linux64.tar.gz
 
+Xvfb :40 &
+export DISPLAY=:40
+sleep 1
+
 yarn test-local
