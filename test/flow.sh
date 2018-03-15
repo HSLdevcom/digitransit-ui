b9f9494422c4c5e076e2c931f7ed43db1f6e72d2
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-15 08:50:41 +0200

    Try running the flow tests with local firefox provided by travis
    For some reason, chromedriver fails all the time. Try ff.

--- test/flow.sh
+++ test/flow.sh
@@ -4,22 +4,10 @@
 if [ -n "$TRAVIS_TAG" ]; then exit 0; fi
 
 set -e
+
+Xvfb :99 &
+export DISPLAY=:99.0
+
 yarn install
 yarn lint
-yarn build
-
-export CHROME_BIN=/usr/bin/google-chrome
-export DISPLAY=:99.0
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-sudo apt-get install -y libappindicator1 fonts-liberation
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-
-yarn install
-yarn build
-
-CONFIG=hsl yarn start &
-
-wget -N http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-test/flow/script/run-flow-tests.sh local
+yarn test-local
