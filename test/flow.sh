d6618698bf6f9bfac422f5ab5ab50feb8faac249
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-16 10:03:57 +0200

    Back to chrome
    - use chrome not firefox
    - Fail faster
    - Use newest chromediver. NightWatch's .setValue() is broken with older ones.

--- test/flow.sh
+++ test/flow.sh
@@ -2,19 +2,23 @@
 
 # do nothing if the build is for tagging a prod release
 if [ -n "$TRAVIS_TAG" ]; then exit 0; fi
+openssl aes-256-cbc -K $encrypted_59b1a6418079_key -iv $encrypted_59b1a6418079_iv -in test/.dropbox_uploader.enc -out test/.dropbox_uploader -d
 
 set -e
 
-yarn install
-yarn lint
-
+export CHROME_BIN=/usr/bin/google-chrome
+export DISPLAY=:99.0
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 sudo apt-get install -y libappindicator1 fonts-liberation
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
-tar -xvzf geckodriver-v0.20.0-linux64.tar.gz
+ORG=${ORG:-hsldevcom}
+yarn install
+yarn build
 
-Xvfb :40 &
-export DISPLAY=:40
-sleep 1
+CONFIG=hsl yarn start &
 
-yarn test-local
+wget -N http://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+CHROMEDRIVER=./chromedriver test/flow/script/run-flow-tests.sh
