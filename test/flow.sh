1c2741f2ac2ed122d9248d6fd78fa11fa21147d2
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-03-16 10:50:52 +0200

    back to port 8080

--- test/flow.sh
+++ test/flow.sh
@@ -13,7 +13,6 @@ sudo apt-get install -y libappindicator1 fonts-liberation
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
-ORG=${ORG:-hsldevcom}
 yarn install
 yarn build
 
