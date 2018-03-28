ca5b037b9e002c5caa3554d6ec0f7f6bb91f30c9
Laura Pimiä <laura.pimia@netlight.com>
2018-03-28 16:07:30 +0300

    Remodeled relay query, wip

--- app/component/RoutePatternSelect.js
+++ app/component/RoutePatternSelect.js
@@ -1,39 +1,55 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import Relay from 'react-relay/classic';
 import cx from 'classnames';
+import connectToStores from 'fluxible-addons-react/connectToStores';
 import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 import { routePatterns as exampleRoutePatterns } from './ExampleData';
 
-function RoutePatternSelect(props) {
-  const options =
-    props.route &&
-    props.route.patterns.map(pattern => (
-      <option key={pattern.code} value={pattern.code}>
-        {pattern.stops[0].name} ➔ {pattern.headsign}
-      </option>
-    ));
+const DATE_FORMAT = 'YYYYMMDD';
 
-  return (
-    <div className={cx('route-pattern-select', props.className)}>
-      <Icon img="icon-icon_arrow-dropdown" />
-      <select
-        onChange={props.onSelectChange}
-        value={props.params && props.params.patternId}
-      >
-        {options}
-      </select>
-    </div>
-  );
+class RoutePatternSelect extends Component {
+
+  static propTypes = {
+    params: PropTypes.object,
+    route: PropTypes.object,
+    className: PropTypes.string,
+    onSelectChange: PropTypes.func.isRequired,
+    patterns: PropTypes.object.isRequired,
+    serviceDay: PropTypes.string.isRequired,
+    relay: PropTypes.object.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.props.relay.setVariables({ serviceDay: this.props.serviceDay });
+  }
+  render() {
+    const options =
+      this.props.route &&
+      this.props.route.patterns.map(pattern => (
+        <option key={pattern.code} value={pattern.code}>
+          {pattern.stops[0].name} ➔ {pattern.headsign}
+        </option>
+      ));
+    console.log(this.props.route.patterns);
+    console.log(this.props.route);
+    console.log(this.options);
+    console.log(this.props.serviceDay);
+    return (
+      <div className={cx('route-pattern-select', this.props.className)}>
+        <Icon img="icon-icon_arrow-dropdown" />
+        <select
+          onChange={this.props.onSelectChange}
+          value={this.props.params && this.props.params.patternId}
+        >
+          {options}
+        </select>
+      </div>
+    );
+  }
 }
-
-RoutePatternSelect.propTypes = {
-  params: PropTypes.object,
-  route: PropTypes.object,
-  className: PropTypes.string,
-  onSelectChange: PropTypes.func.isRequired,
-};
 
 RoutePatternSelect.description = () => (
   <div>
@@ -47,10 +63,13 @@ RoutePatternSelect.description = () => (
   </div>
 );
 
-export default Relay.createContainer(RoutePatternSelect, {
-  fragments: {
-    route: () =>
-      Relay.QL`
+export default connectToStores(
+  Relay.createContainer(RoutePatternSelect, {
+    initialVariables: {
+      serviceDay: '19700101',
+    },
+    fragments: {
+      route: () => Relay.QL`
       fragment on Route {
         patterns {
           code
@@ -58,8 +77,27 @@ export default Relay.createContainer(RoutePatternSelect, {
           stops {
             name
           }
+          tripsForDate(serviceDay: $serviceDay) {
+            id
+            stoptimes: stoptimesForDate(serviceDay: $serviceDay) {
+              scheduledArrival
+              scheduledDeparture
+              serviceDay
+              stop {
+                id
+              }
+            }
+          }
         }
       }
-    `,
-  },
-});
+      `,
+    },
+  }),
+  [],
+  context => ({
+    serviceDay: context
+      .getStore('TimeStore')
+      .getCurrentTime()
+      .format(DATE_FORMAT),
+  }),
+);
