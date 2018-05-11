c11dfe069f79cba17531e2ab2e6e39a8f88fdd90
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-05-11 08:57:51 +0300

    Do not use parallel uglify workers at all

--- webpack.config.js
+++ webpack.config.js
@@ -256,7 +256,7 @@ function getPluginsConfig(env) {
     new ManifestPlugin({ fileName: '../manifest.json' }),
     new UglifyJsPlugin({
       sourceMap: true,
-      parallel: 2,
+      parallel: false,
     }),
     ...getAllFaviconPlugins(),
     new ExtractTextPlugin({
