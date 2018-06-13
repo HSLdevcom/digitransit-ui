0e070679249792d3c45104d1e5ecc2c553bab3ab
Vesa Meskanen <vesa@realsoft.com>
2018-06-13 16:55:58 +0300

    DT-2682: fix changing the locale (#2348)
    * Do not render on server
    * Do not attach to PreferencesStore directly
    IndexPage now updates only via app/util/StoreListeningIntlProvider.js.
    Direct event monitoring did render the page before context.intl had updated.
    * Fixed an old unique key warning.
    * Cleanup and linter fix.

--- app/component/MessageBarMessage.js
+++ app/component/MessageBarMessage.js
@@ -1,23 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const heading = e => {
+const heading = (e, key) => {
   if (e.type === 'heading') {
-    return <h2>{e.content}</h2>;
+    return <h2 key={`${key}-heading`}>{e.content}</h2>;
   }
   return null;
 };
 
-const span = e => {
+// eslint-disable-next-line no-unused-vars
+const span = (e, key) => {
   if (e.type === 'text') {
     return e.content;
   }
   return null;
 };
 
-const a = e => {
+const a = (e, key) => {
   if (e.type === 'a') {
-    return <a href={e.href}>{e.content}</a>;
+    return (
+      <a key={`${key}-link`} href={e.href}>
+        {e.content}
+      </a>
+    );
   }
   return null;
 };
@@ -25,21 +30,20 @@ const a = e => {
 const elements = [heading, span, a];
 
 const renderContent = content =>
-  content.map(fragment => elements.map(t => t(fragment)));
+  content.map((fragment, i) => elements.map(t => t(fragment, i)));
 
 /*
  * Renders message
  */
-const MessageBarMessage = ({ id, content, onMaximize }) => (
+const MessageBarMessage = ({ content, onMaximize }) => (
   // TOOD: find out how this should be accessible
   // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-  <div tabIndex={0} role="button" key={id} onClick={onMaximize}>
+  <div tabIndex={0} role="button" onClick={onMaximize}>
     {renderContent(content)}
   </div>
 );
 
 MessageBarMessage.propTypes = {
-  id: PropTypes.string.isRequired,
   content: PropTypes.array,
   onMaximize: PropTypes.func.isRequired,
 };
