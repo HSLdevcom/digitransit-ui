aeca14aa4a3d73f8cee15112dd70d211ba343cfd
Joel Lappalainen <joel.lappalainen@cgi.com>
2018-07-06 16:07:24 +0300

    Refactor no favourites image

--- app/component/NoFavouritesPanel.js
+++ app/component/NoFavouritesPanel.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { isBrowser } from '../util/browser';
 
 const NoFavouritesPanel = () => (
   <div className="nofavs row">
     <div className="small-12 columns">
       <div className="nofavs-p black text-center">
-        <div className="nofavs-img" />
+        <div className="nofavs-img">
+          {isBrowser && (
+            <img src={require('../../sass/themes/default/favourites_empty_tip.png')} />
+          )}
+        </div>
         <FormattedMessage
           id="no-favourites"
           defaultMessage="Use the star buttons to add routes or stops to your favorites. Your favorites are shown on this page."
