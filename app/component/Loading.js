254e1ee344e43fdcf100a54af14148a37a35d09f
pailakka <P3lt1pailakka>
2018-07-13 09:43:20 +0300

    Revert to master version

--- app/component/Loading.js
+++ app/component/Loading.js
@@ -1,46 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Loading extends React.Component {
-  static propTypes = {
+const Loading = props => (
+    <div className="spinner-loader">{(props && props.children) || null}</div>
+);
+
+Loading.displayName = 'Loading';
+Loading.propTypes = {
     children: PropTypes.node,
-  };
-
-  static contextTypes = {
-    config: PropTypes.object.isRequired,
-  };
-
-  static displayName = 'Loading';
-
-  constructor() {
-    super();
-    this.state = {
-      spinner: null,
-    };
-  }
-
-  componentDidMount() {
-    import(/* webpackChunkName: "main" */ `../configurations/images/${
-      this.context.config.spinner
-    }`).then(spinner => {
-      this.setState({ spinner: spinner.default });
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.spinner && (
-          <div
-            className="spinner-loader"
-            style={{ backgroundImage: `url(${this.state.spinner})` }}
-          >
-            {(this.props && this.props.children) || null}
-          </div>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+};
 
 export default Loading;
+
