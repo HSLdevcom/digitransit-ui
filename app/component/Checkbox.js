da28cfe767f7230c0f3ed38df92a883cfe47aa08
Joona Olkkola <joona.olkkola@octo3.fi>
2018-07-26 11:16:07 +0300

    Included some simple tests for the Checkbox component.
    Changed the Checkbox component to use auto-generated ids for its elements. Made the labelId field optional.

--- app/component/Checkbox.js
+++ app/component/Checkbox.js
@@ -1,4 +1,5 @@
 import cx from 'classnames';
+import uniqueId from 'lodash/uniqueId';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { intlShape, FormattedMessage } from 'react-intl';
@@ -8,45 +9,53 @@ import { isKeyboardSelectionEvent } from '../util/browser';
 const Checkbox = (
   { checked, disabled, onChange, labelId, defaultMessage, showLabel },
   { intl },
-) => (
-  <div className="option-checkbox-container">
-    <div
-      aria-checked={checked}
-      className="option-checkbox"
-      onKeyPress={e =>
-        !disabled &&
-        isKeyboardSelectionEvent(e) &&
-        onChange({ target: { checked: !checked } })
-      }
-      role="checkbox"
-      tabIndex={disabled ? -1 : 0}
-    >
-      <label className={cx({ checked, disabled })} htmlFor={`input-${labelId}`}>
-        <input
-          aria-label={intl.formatMessage({
-            id: labelId,
-            defaultMessage,
-          })}
-          checked={checked}
-          disabled={disabled}
-          id={`input-${labelId}`}
-          onChange={onChange}
-          type="checkbox"
-        />
-      </label>
+) => {
+  const id = uniqueId('input-');
+  return (
+    <div className="option-checkbox-container">
+      <div
+        aria-checked={checked}
+        className="option-checkbox"
+        onKeyPress={e =>
+          !disabled &&
+          isKeyboardSelectionEvent(e) &&
+          onChange({ target: { checked: !checked } })
+        }
+        role="checkbox"
+        tabIndex={disabled ? -1 : 0}
+      >
+        <label className={cx({ checked, disabled })} htmlFor={id}>
+          <input
+            aria-label={
+              labelId
+                ? intl.formatMessage({
+                    id: labelId,
+                    defaultMessage,
+                  })
+                : undefined
+            }
+            checked={checked}
+            disabled={disabled}
+            id={id}
+            onChange={e => !disabled && onChange(e)}
+            type="checkbox"
+          />
+        </label>
+      </div>
+      {showLabel &&
+        labelId && (
+          <FormattedMessage id={labelId} defaultMessage={defaultMessage} />
+        )}
     </div>
-    {showLabel && (
-      <FormattedMessage id={labelId} defaultMessage={defaultMessage} />
-    )}
-  </div>
-);
+  );
+};
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
   defaultMessage: PropTypes.string,
   disabled: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
-  labelId: PropTypes.string.isRequired,
+  labelId: PropTypes.string,
   showLabel: PropTypes.bool,
 };
 
@@ -54,6 +63,7 @@ Checkbox.defaultProps = {
   checked: false,
   defaultMessage: '',
   disabled: false,
+  labelId: undefined,
   showLabel: true,
 };
 
