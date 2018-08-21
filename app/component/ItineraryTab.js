a63a2b0d45ace1f32c7d2772a90046ccc55e4ff7
Juha-Matti Viertola <juha-matti.viertola@cgi.com>
2018-08-21 10:54:23 +0300

    Show route description together with call agency warning

--- app/component/ItineraryTab.js
+++ app/component/ItineraryTab.js
@@ -205,6 +205,7 @@ export default Relay.createContainer(ItineraryTab, {
             color
             gtfsId
             longName
+            desc
             agency {
               phone
             }
