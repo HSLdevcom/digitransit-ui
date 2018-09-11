53d90fc6febb823dd4605e77a5970317d78f76f0
Juha-Matti Viertola <juha-matti.viertola@cgi.com>
2018-09-11 10:04:12 +0300

    Use real config data in reittiopasParameterMiddleware test instead of test data

--- test/unit/reittiopasParameterMiddleware.test.js
+++ test/unit/reittiopasParameterMiddleware.test.js
@@ -2,7 +2,18 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { validateParams } from '../../server/reittiopasParameterMiddleware';
 
-import data from './test-data/dt2730';
+import config from '../../app/configurations/config.default';
+
+const req = {
+  query: {
+    minTransferTime: '60',
+    modes: 'BUS,TRAM,RAIL,SUBWAY,FERRY,WALK,CITYBIKE',
+    transferPenalty: '0',
+    walkBoardCost: '540',
+    walkReluctance: '1.5',
+    walkSpeed: '1.5',
+  },
+};
 
 // validateParams returns an url if it is modified and it removes invalid
 // parameteres from req.query => two ways to check if it did what it should
@@ -10,20 +21,24 @@ import data from './test-data/dt2730';
 describe('reittiopasParameterMiddleware', () => {
   describe('validateParams', () => {
     it('should not modify valid url', () => {
-      const url = validateParams(data.req, data.config);
+      const url = validateParams(req, config);
       expect(url).to.be.a('undefined');
     });
 
     it('should remove invalid time parameter', () => {
-      data.req.query.time = 'test';
-      validateParams(data.req, data.config);
-      expect(data.req.query.time).to.be.an('undefined');
+      req.query.time = 'test';
+      validateParams(req, config);
+      expect(req.query.time).to.be.an('undefined');
     });
 
     it('should remove invalid modes parameter', () => {
-      data.req.query.modes = 'test1,test2';
-      validateParams(data.req, data.config);
-      expect(data.req.query.modes).to.be.an('undefined');
+      const properModes = req.query.modes;
+      validateParams(req, config);
+      expect(req.query.modes).to.equal(properModes);
+
+      req.query.modes = 'test1,test2';
+      validateParams(req, config);
+      expect(req.query.modes).to.be.an('undefined');
     });
   });
 });
