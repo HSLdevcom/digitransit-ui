95b786f7a264936d8f7c20b94b981a8f810de4c6
Laura Pimiä <laura.pimia@netlight.com>
2018-10-04 15:08:32 +0300

    Fixed vertical center positioning

--- app/component/NetworkError.js
+++ app/component/NetworkError.js
@@ -6,19 +6,21 @@ import { Link, locationShape } from 'react-router';
 import Icon from './Icon';
 
 const NetworkError = ({ retry }, { location }) => (
-  <div className="page-not-found">
-    <Icon img="icon-icon_error_page_not_found" />
-    <p>
-      <FormattedMessage
-        id="network-error"
-        defaultMessage="There was a network error"
-      />
-    </p>
-    <p>
-      <Link to={location} onClick={retry}>
-        <FormattedMessage id="try-again" defaultMessage="Try again ›" />
-      </Link>
-    </p>
+  <div className="page-not-found-container">
+    <div className="page-not-found">
+      <Icon img="icon-icon_error_page_not_found" />
+      <p>
+        <FormattedMessage
+          id="network-error"
+          defaultMessage="There was a network error"
+        />
+      </p>
+      <p>
+        <Link to={location} onClick={retry}>
+          <FormattedMessage id="try-again" defaultMessage="Try again ›" />
+        </Link>
+      </p>
+    </div>
   </div>
 );
 
