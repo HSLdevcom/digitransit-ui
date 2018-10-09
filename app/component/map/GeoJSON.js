3819b9adf14ce60fad940e60da38d00a2c24f9e6
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-10-09 11:24:03 +0300

    Cache icons and allow reference by icon id

--- app/component/map/GeoJSON.js
+++ app/component/map/GeoJSON.js
@@ -19,55 +19,72 @@ const GeoJsonIcon = L.Icon.extend({
   },
 });
 
-const pointToLayer = (feature, latlng) => {
-  // add some custom rendering control by feature props
-  const props = feature.properties || {};
-  let marker;
+class GeoJSON extends React.Component {
+  static propTypes = { data: PropTypes.object.isRequired };
+  static contextTypes = { config: PropTypes.object.isRequired };
 
-  if (props.textOnly) {
-    marker = L.marker(latlng);
-    marker.bindTooltip(props.name, {
-      permanent: true,
-      className: 'geoJsonText',
-      direction: 'center',
-      offset: [0, -25],
+  constructor(args) {
+    super(args);
+
+    // cache dynamic icons in advance to allow references by id without data duplication
+    const icons = {};
+
+    this.props.data.features.forEach(feature => {
+      const p = feature.properties;
+      if (p && p.icon && p.icon.id && p.icon.data) {
+        /*
+          For data URI SVG support in Firefox & IE it's necessary to URI encode the string
+          & replace the '#' character with '%23'. `encodeURI()` won't do this.
+        */
+        const url = encodeURI(`data:image/svg+xml, ${p.icon.data}`).replace(
+          '#',
+          '%23',
+        );
+        icons[p.icon.id] = new GeoJsonIcon({ iconUrl: url });
+      }
     });
-  } else if (props.icon) {
-    /*
-    For data URI SVG support in Firefox & IE it's necessary to URI encode the string
-    & replace the '#' character with '%23'. `encodeURI()` won't do this.
-    */
-    const url = encodeURI(`data:image/svg+xml, ${props.icon.data}`).replace(
-      '#',
-      '%23',
-    );
-    const icon = new GeoJsonIcon({ iconUrl: url });
-    marker = L.marker(latlng, { icon });
-  } else {
-    marker = L.circleMarker(latlng);
+    this.state = { icons };
   }
-  if (props.popupContent) {
-    marker.bindPopup(props.popupContent, { className: 'geoJsonPopup' });
-  }
-  return marker;
-};
 
-const styler = config => {
-  const defaultLineStyle = {
-    color: config.colors.primary,
-    weight: 3,
-    opacity: 0.8,
-  };
-  const defaultMarkerStyle = {
-    color: config.colors.primary,
-    fillColor: 'white',
-    radius: 6,
-    opacity: 1,
-    fillOpacity: 1,
-    weight: 2,
+  pointToLayer = (feature, latlng) => {
+    // add some custom rendering control by feature props
+    const props = feature.properties || {};
+    let marker;
+
+    if (props.textOnly) {
+      marker = L.marker(latlng);
+      marker.bindTooltip(props.name, {
+        permanent: true,
+        className: 'geoJsonText',
+        direction: 'center',
+        offset: [0, -25],
+      });
+    } else if (props.icon) {
+      marker = L.marker(latlng, { icon: this.state.icons[props.icon.id] });
+    } else {
+      marker = L.circleMarker(latlng);
+    }
+    if (props.popupContent) {
+      marker.bindPopup(props.popupContent, { className: 'geoJsonPopup' });
+    }
+    return marker;
   };
 
-  return feature => {
+  styler = feature => {
+    const defaultLineStyle = {
+      color: this.context.config.colors.primary,
+      weight: 3,
+      opacity: 0.8,
+    };
+    const defaultMarkerStyle = {
+      color: this.context.config.colors.primary,
+      fillColor: 'white',
+      radius: 6,
+      opacity: 1,
+      fillOpacity: 1,
+      weight: 2,
+    };
+
     if (feature.geometry && feature.geometry.type === 'Point') {
       return feature.style
         ? { ...defaultMarkerStyle, ...feature.style }
@@ -77,13 +94,16 @@ const styler = config => {
       ? { ...defaultLineStyle, ...feature.style }
       : defaultLineStyle;
   };
-};
 
-const GeoJSON = ({ data }, { config }) => (
-  <Geojson data={data} style={styler(config)} pointToLayer={pointToLayer} />
-);
-
-GeoJSON.propTypes = { data: PropTypes.object.isRequired };
-GeoJSON.contextTypes = { config: PropTypes.object.isRequired };
+  render() {
+    return (
+      <Geojson
+        data={this.props.data}
+        style={this.styler}
+        pointToLayer={this.pointToLayer}
+      />
+    );
+  }
+}
 
 export default GeoJSON;
