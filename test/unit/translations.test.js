865fdbdf692e9c8e5d57ca9b0d45cd8407aaa521
Joona Olkkola <joona.olkkola@octo3.fi>
2018-10-17 12:42:57 +0300

    Added a unit test to assert that no relevant translations are missing.

---
+++ test/unit/translations.test.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import translations from '../../app/translations';
+
+/**
+ * This array contains language terms that are defined (and translated) but
+ * do not seem to have any use in any part of the code. Consider removing these
+ * from the translations file altogether.
+ */
+const ignoredTerms = ['destination-label-change'];
+
+describe('translations', () => {
+  it('English translations should have all the Finnish terms', () => {
+    const missing = Object.keys(translations.fi)
+      .filter(key => !ignoredTerms.includes(key))
+      .filter(key => translations.en[key] === undefined)
+      .map(key => ({ [key]: translations.fi[key] }));
+    expect(missing).to.deep.equal(
+      [],
+      'the English translations are missing for these Finnish terms',
+    );
+  });
+
+  it('Swedish translations should have all the Finnish terms', () => {
+    const missing = Object.keys(translations.fi)
+      .filter(key => !ignoredTerms.includes(key))
+      .filter(key => translations.sv[key] === undefined)
+      .map(key => ({ [key]: translations.fi[key] }));
+    expect(missing).to.deep.equal(
+      [],
+      'the Swedish translations are missing for these Finnish terms',
+    );
+  });
+});
