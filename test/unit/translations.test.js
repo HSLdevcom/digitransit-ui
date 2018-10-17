52e737917558b2e40c74c8f7d65750cc30d03a68
Joona Olkkola <joona.olkkola@octo3.fi>
2018-10-17 12:55:21 +0300

    Improved the unit test legibility.
    Changed the map layer translations to plural form in Finnish.

--- test/unit/translations.test.js
+++ test/unit/translations.test.js
@@ -8,27 +8,33 @@ import translations from '../../app/translations';
  * do not seem to have any use in any part of the code. Consider removing these
  * from the translations file altogether.
  */
-const ignoredTerms = ['destination-label-change'];
+const ignoredTerms = ['destination-label-change', 'splash-welcome'];
 
 describe('translations', () => {
   it('English translations should have all the Finnish terms', () => {
-    const missing = Object.keys(translations.fi)
+    const missing = {};
+    Object.keys(translations.fi)
       .filter(key => !ignoredTerms.includes(key))
       .filter(key => translations.en[key] === undefined)
-      .map(key => ({ [key]: translations.fi[key] }));
+      .forEach(key => {
+        missing[key] = translations.fi[key];
+      });
     expect(missing).to.deep.equal(
-      [],
+      {},
       'the English translations are missing for these Finnish terms',
     );
   });
 
   it('Swedish translations should have all the Finnish terms', () => {
-    const missing = Object.keys(translations.fi)
+    const missing = {};
+    Object.keys(translations.fi)
       .filter(key => !ignoredTerms.includes(key))
       .filter(key => translations.sv[key] === undefined)
-      .map(key => ({ [key]: translations.fi[key] }));
+      .forEach(key => {
+        missing[key] = translations.fi[key];
+      });
     expect(missing).to.deep.equal(
-      [],
+      {},
       'the Swedish translations are missing for these Finnish terms',
     );
   });
