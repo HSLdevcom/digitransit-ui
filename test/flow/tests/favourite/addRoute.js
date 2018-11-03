9d7c9254dc1034321c5d3028510c6feba2b1d52e
Eemeli Aro <eemeli@gmail.com>
2018-11-03 18:10:42 +0200

    Fix (hopefully) the add-route flow test
    In test/flow/page_object/searchFields.js, selectFirstRouteSuggestion()
    calls setOrigin() which clicks on clearOrigin, which now changes the URL,
    which meant that the browser.back() in the test was leading to a location
    too far from line 615.

--- test/flow/tests/favourite/addRoute.js
+++ test/flow/tests/favourite/addRoute.js
@@ -1,15 +1,15 @@
 module.exports = {
   tags: ['favourite'],
   'Add route 615 as favourite': browser => {
-    browser.url(
-      'http://127.0.0.1:8080/Opastinsilta%206,%20Helsinki::60.199437,24.940472/-',
-    );
+    const homeUrl =
+      'http://127.0.0.1:8080/Opastinsilta%206,%20Helsinki::60.199437,24.940472/-';
+    browser.url(homeUrl);
 
     browser.page.searchFields().selectFirstRouteSuggestion('615');
 
     const route = browser.page.route();
     route.addRouteAsFavourite();
-    browser.back();
+    browser.url(homeUrl);
 
     const myFavourites = browser.page.myFavourites();
     myFavourites.clickFavourites();
