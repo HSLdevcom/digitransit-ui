d2d655f21d8ddc38113ad463cbbefd232c8afd78
Joona Olkkola <joona.olkkola@octo3.fi>
2018-11-07 09:37:10 +0200

    Fix lint.

--- test/unit/PrintableItineraryHeader.test.js
+++ test/unit/PrintableItineraryHeader.test.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { shallowWithIntl } from './helpers/mock-intl-enzyme';
-import { mockContext, mockChildContextTypes } from './helpers/mock-context';
-import {
-  getRelayContextMock,
-  mockRelayChildContextTypes,
-} from './helpers/mock-relay';
 
 import dt2772a from './test-data/dt2772a';
 import dt2772b from './test-data/dt2772b';
