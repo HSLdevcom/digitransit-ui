ba07786a956ec7df227134c792355cd937c12d79
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-11-16 09:47:32 +0200

    Fix route sorting
    Route seaches were primarily sorted by operator name which is not
    visible at all in the usual front page search context. New sort
    implemented.

--- app/util/searchUtils.js
+++ app/util/searchUtils.js
@@ -10,7 +10,6 @@ import merge from 'lodash/merge';
 import uniqWith from 'lodash/uniqWith';
 
 import { getJson } from './xhrPromise';
-import routeCompare from './route-compare';
 import { distance } from './geo-utils';
 import { uniqByLabel, isStop } from './suggestionUtils';
 import mapPeliasModality from './pelias-to-modality-mapper';
@@ -71,6 +70,19 @@ const mapRoute = item => {
     },
   };
 };
+
+export function routeNameCompare(a, b) {
+  const aName =
+    a.shortName ||
+    a.longName ||
+    (a.agency && a.agency.name ? a.agency.name : '');
+  const bName =
+    b.shortName ||
+    b.longName ||
+    (b.agency && b.agency.name ? b.agency.name : '');
+
+  return aName.localeCompare(bName);
+}
 
 function truEq(val1, val2) {
   // accept equality of non nullish values
@@ -296,7 +308,7 @@ function getFavouriteRoutes(favourites, input) {
       ]),
     )
     .then(routes =>
-      routes.sort((x, y) => routeCompare(x.properties, y.properties)),
+      routes.sort((x, y) => routeNameCompare(x.properties, y.properties)),
     );
 }
 
@@ -406,7 +418,7 @@ function getRoutes(input, config) {
             config.feedIds.indexOf(item.gtfsId.split(':')[0]) > -1,
         )
         .map(mapRoute)
-        .sort((x, y) => routeCompare(x.properties, y.properties)),
+        .sort((x, y) => routeNameCompare(x.properties, y.properties)),
     )
     .then(suggestions => take(suggestions, 10));
 }
