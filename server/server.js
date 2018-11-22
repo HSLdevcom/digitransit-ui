38f92f046c2710250f206777ba286a437608280e
Hannes Junnila <hannes.junnila@gmail.com>
2018-11-22 17:27:18 +0200

    Make it posible to transpile code in node_modules

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@
 const path = require('path');
 const fs = require('fs');
 
-require('@babel/register')();
+require('@babel/register')({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: [/node_modules\/(?!react-leaflet|@babel\/runtime\/helpers\/esm)/],
+});
 
 global.fetch = require('node-fetch');
 const proxy = require('express-http-proxy');
---
+++ test/unit/helpers/babel-register.js
@@ -0,0 +1,5 @@
+require('@babel/register')({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: [/node_modules\/(?!react-leaflet|@babel\/runtime\/helpers\/esm)/],
+});
