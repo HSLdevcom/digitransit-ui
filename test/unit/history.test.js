4d4e56d906aca3d2801ecd4ebc3edd7aab126513
Joona Olkkola <joona.olkkola@octo3.fi>
2018-12-11 16:44:20 +0200

    Allow the software to work without localStorage ans sessionStorage.
    This should also fix rendering problems when using a direct link in iOS or a non-browser environment. Relevant unit tests are included.

--- test/unit/history.test.js
+++ test/unit/history.test.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { createHistory, createMemoryHistory } from 'history';
+import sinon from 'sinon';
 
-import { getCreateHistoryFunction } from '../../app/history';
+import createRouter, { getCreateHistoryFunction } from '../../app/history';
 import createLocalStorageHistory from '../../app/localStorageHistory';
 
 describe('history', () => {
@@ -12,10 +13,18 @@ describe('history', () => {
       expect(result).to.equal(createMemoryHistory);
     });
 
-    it('should use createHistory if in browser', () => {
+    it('should use createHistory if in browser and sessionStorage exists', () => {
       const path = '/';
       const result = getCreateHistoryFunction(path, true);
       expect(result).to.equal(createHistory);
+    });
+
+    it('should use createMemoryHistory if in browser and sessionStorage does not exist', () => {
+      const stub = sinon.stub(window, 'sessionStorage').get(() => null);
+      const path = '/';
+      const result = getCreateHistoryFunction(path, true);
+      expect(result).to.equal(createMemoryHistory);
+      stub.restore();
     });
 
     it('should use localStorageHistory if in iOSApp and path is root', () => {
@@ -28,6 +37,17 @@ describe('history', () => {
       const path = '/reitti/foo/bar';
       const result = getCreateHistoryFunction(path, true, true);
       expect(result).to.equal(createHistory);
+    });
+  });
+
+  describe('default export', () => {
+    it('should apply the given path if not using createHistory', () => {
+      const config = {
+        APP_PATH: 'foobar',
+      };
+      const path = '/foo/bar';
+      const router = createRouter(config, path);
+      expect(router.getCurrentLocation().pathname).to.equal(path);
     });
   });
 });
