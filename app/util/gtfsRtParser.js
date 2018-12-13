2111172870a591f049412660183ca25841389ee9
Joona Olkkola <joona.olkkola@octo3.fi>
2018-12-13 14:33:31 +0200

    Included some tests for the gtfsRtParser class.

--- app/util/gtfsRtParser.js
+++ app/util/gtfsRtParser.js
@@ -1,14 +1,41 @@
 import ceil from 'lodash/ceil';
 import Pbf from 'pbf';
 
-function findTracked(trip, trackedRoutes) {
-  for (let i = 0; i < trackedRoutes.length; i++) {
-    if (trackedRoutes[i].route === trip.route_id) {
-      return trackedRoutes[i];
-    }
+export const parseFeed = (feed, agency, trackedRoutes) => {
+  if (trackedRoutes.length === 0) {
+    return null;
   }
-  return null;
-}
+
+  const messages = [];
+  feed.entity.forEach(entity => {
+    const vehiclePos = entity.vehicle;
+    if (vehiclePos) {
+      const { trip, position, vehicle } = vehiclePos;
+      if (trip && position && vehicle) {
+        const trackedRoute = trackedRoutes.find(
+          item => item.route === trip.route_id,
+        );
+        if (trackedRoute) {
+          const message = {
+            id: `${agency}:${vehicle.id}`,
+            route: `${agency}:${trackedRoute.gtfsId}`,
+            direction: trip.direction_id || 0,
+            tripStartTime: trip.start_time.replace(/:/g, ''),
+            operatingDay: trip.start_date,
+            mode: trackedRoute.mode || 'bus',
+            next_stop: vehiclePos.stop_id,
+            timestamp: vehiclePos.timestamp || feed.header.timestamp,
+            lat: ceil(position.latitude, 5),
+            long: ceil(position.longitude, 5),
+            heading: position.bearing ? Math.floor(position.bearing) : 0,
+          };
+          messages.push(message);
+        }
+      }
+    }
+  });
+  return messages.length > 0 ? messages : null;
+};
 
 export default class GtfsRtParser {
   constructor(agency, bindings) {
@@ -17,41 +44,8 @@ export default class GtfsRtParser {
   }
 
   parse(data, trackedRoutes) {
-    let found = false;
-    const messages = [];
-
     const pbf = new Pbf(data);
     const feed = this.feedParser(pbf);
-
-    feed.entity.forEach(entity => {
-      const vehiclePos = entity.vehicle;
-      if (vehiclePos) {
-        const { trip, position, vehicle } = vehiclePos;
-        if (trip && position && vehicle) {
-          const trackedRoute = findTracked(trip, trackedRoutes);
-          if (trackedRoute) {
-            found = true;
-            const message = {
-              id: `${this.agency}:${vehicle.id}`,
-              route: `${this.agency}:${trackedRoute.gtfsId}`,
-              direction: trip.direction_id || 0,
-              tripStartTime: trip.start_time.replace(/:/g, ''),
-              operatingDay: trip.start_date,
-              mode: trackedRoute.mode || 'bus',
-              next_stop: vehiclePos.stop_id,
-              timestamp: vehiclePos.timestamp || feed.header.timestamp,
-              lat: ceil(position.latitude, 5),
-              long: ceil(position.longitude, 5),
-              heading: position.bearing ? Math.floor(position.bearing) : 0,
-            };
-            messages.push(message);
-          }
-        }
-      }
-    });
-    if (found) {
-      return messages;
-    }
-    return null;
+    return parseFeed(feed, this.agency, trackedRoutes);
   }
 }
