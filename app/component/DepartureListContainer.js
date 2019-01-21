d3b280de6fc1f238dc4e3158c5ae9c79528d2caf
Laura Pimiä <laura.pimia@netlight.com>
2019-01-21 11:42:32 +0200

    Lint fix

--- app/component/DepartureListContainer.js
+++ app/component/DepartureListContainer.js
@@ -96,7 +96,7 @@ class DepartureListContainer extends Component {
       .filter(departure => currentTime < departure.stoptime)
       .slice(0, this.props.limit);
 
-    departures.forEach((departure, i) => {
+    departures.forEach(departure => {
       if (departure.stoptime >= tomorrow) {
         departureObjs.push(
           <div
