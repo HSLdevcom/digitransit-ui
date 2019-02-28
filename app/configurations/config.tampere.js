e13fd9ab7337b252bf27f9fbacfa37fbc74af5b2
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-02-28 10:17:39 +0200

    Fix tre conf (no string interpolation, it breaks ie11)

--- app/configurations/config.tampere.js
+++ app/configurations/config.tampere.js
@@ -104,7 +104,7 @@ export default configMerger(walttiConfig, {
       } else {
         zoneCount = 4;
       }
-      return `${ticketType[lang]}, ${count[lang][zoneCount]} ${zone[lang]}`;
+      return ticketType[lang] + ', ' + count[lang][zoneCount] + ' ' +  zone[lang];
     }
     return '';
   },
