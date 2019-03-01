09657cb85cc8fbeafb6a7c4fbd106eab534ef805
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-03-01 08:26:11 +0200

    Fetch chromedriver

--- test/smoke.sh
+++ test/smoke.sh
@@ -3,5 +3,8 @@
 # do nothing if the build is for tagging a prod release
 if [ -n "$TRAVIS_TAG" ]; then exit 0; fi
 
+wget -N http://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+
 yarn install
-yarn test-smoke BS_USERNAME BS_ACCESS_KEY
+CHROMEDRIVER=./chromedriver yarn test-smoke BS_USERNAME BS_ACCESS_KEY
