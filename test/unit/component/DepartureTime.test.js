ac18d36f8de5544fe874f174ce57cbda7408eba4
Joona Olkkola <joona.olkkola@visma.com>
2019-03-06 10:57:00 +0200

    Included unit tests for showing the cancelation icon.

---
+++ test/unit/component/DepartureTime.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import DepartureTime, {
+  fromStopTime,
+} from '../../../app/component/DepartureTime';
+import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
+import { RealtimeStateType } from '../../../app/constants';
+
+describe('<DepartureTime />', () => {
+  describe('fromStopTime', () => {
+    it('should generate a canceled DepartureTime', () => {
+      const stoptime = {
+        realtimeState: RealtimeStateType.Canceled,
+      };
+      const currentTime = 0;
+      const component = fromStopTime(stoptime, currentTime);
+      expect(component.props.canceled).to.equal(true);
+      expect(component.props.showCancelationIcon).to.equal(true);
+    });
+  });
+
+  it('should show the cancelation icon', () => {
+    const props = {
+      canceled: true,
+      currentTime: 0,
+      departureTime: 180,
+      showCancelationIcon: true,
+    };
+    const wrapper = shallowWithIntl(<DepartureTime {...props} />, {
+      context: { config: { minutesToDepartureLimit: 2 } },
+    });
+    expect(wrapper.find('.caution')).to.have.lengthOf(1);
+  });
+});
