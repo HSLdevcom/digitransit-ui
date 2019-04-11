bd87ee02bbfafc3d207e6d2c94132f3f731e7c64
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-04-11 10:17:23 +0300

    Disable troublesome tests in edge

--- test/visual/components.js
+++ test/visual/components.js
@@ -159,7 +159,10 @@ testVariation('SelectStopRow');
 testVariation('SelectTerminalRow');
 testVariation('SelectCityBikeRow');
 testVariation('SelectParkAndRideRow');
-testVariation('TicketInformation');
+testVariation('TicketInformation').then(
+  skip('edge17'),
+);
+
 testVariation('DateSelect');
 testVariation('RoutePatternSelect');
 testVariation('RouteScheduleHeader');
@@ -227,7 +230,7 @@ testVariation('StopMarkerPopup', 'basic', 2, [
   'div.departure-list > p:nth-child(2) > span.realtime svg.realtime',
   'p:nth-child(2) > span.route-destination > span > span.last-stop-icon',
   'p:nth-child(4) > span.route-destination > span > span.last-stop-icon',
-]).then(skip('ie11'));
+]).then(skip(['ie11','edge17']));
 
 testVariation('SelectStreetModeDialog');
 testVariation('SelectMapLayersDialog');
