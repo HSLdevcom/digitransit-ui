994c9dbe33624cd2242bcababd000d63b79aa643
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-05-21 12:35:08 +0300

    Do not show ticket info in Kuopio

--- app/configurations/config.kuopio.js
+++ app/configurations/config.kuopio.js
@@ -73,7 +73,7 @@ export default configMerger(walttiConfig, {
     },
   ],
 
-  showTicketInformation: true,
+  showTicketInformation: false,
   ticketInformation: {
     primaryAgencyName: 'Kuopion seudun joukkoliikenne',
   },
