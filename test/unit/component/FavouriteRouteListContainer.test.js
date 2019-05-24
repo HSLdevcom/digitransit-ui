3ccfe5b4dcf0fefd1c8db533c78214cacf74e222
Joona Olkkola <joona.olkkola@visma.com>
2019-05-24 16:40:36 +0300

    Added handling for alert severity level.

--- test/unit/component/FavouriteRouteListContainer.test.js
+++ test/unit/component/FavouriteRouteListContainer.test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
 import { getNextDepartures } from '../../../app/component/FavouriteRouteListContainer';
+import { AlertSeverityLevelType } from '../../../app/constants';
 
 describe('<FavouriteRouteListContainer />', () => {
   describe('getNextDepartures', () => {
@@ -9,8 +10,51 @@ describe('<FavouriteRouteListContainer />', () => {
       const routes = [null];
       const lat = 60.219235;
       const lon = 24.81329;
-      const result = getNextDepartures(routes, lat, lon);
+      const currentTime = 1558703670;
+      const result = getNextDepartures(routes, lat, lon, currentTime);
       expect(result).to.deep.equal([]);
+    });
+
+    it('should map the active alertSeverityLevel if available', () => {
+      const lat = 60.219235;
+      const lon = 24.81329;
+      const currentTime = 1558703670;
+      const routes = [
+        {
+          alerts: [
+            {
+              alertSeverityLevel: AlertSeverityLevelType.Info,
+              effectiveStartDate: currentTime,
+            },
+            {
+              alertSeverityLevel: AlertSeverityLevelType.Warning,
+              effectiveStartDate: currentTime + 1, // in the future
+            },
+          ],
+          patterns: [
+            {
+              stops: [
+                {
+                  lat: 60,
+                  lon: 25,
+                  stoptimes: [
+                    {
+                      pattern: {
+                        route: {},
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      const result = getNextDepartures(routes, lat, lon, currentTime);
+      expect(result[0].alertSeverityLevel).to.equal(
+        AlertSeverityLevelType.Info,
+      );
     });
   });
 });
