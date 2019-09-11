80bb7a785105414417739746812a4fd46538ac78
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-09-11 10:16:05 +0300

    Try alternative fix for travis issues

--- test/flow.sh
+++ test/flow.sh
@@ -10,7 +10,7 @@ set -e
 export CHROME_BIN=/usr/bin/google-chrome
 export DISPLAY=:99.0
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-sudo apt-get install -y libappindicator1 fonts-liberation
+sudo apt-get install -y libappindicator1 fonts-liberation dpkg
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
