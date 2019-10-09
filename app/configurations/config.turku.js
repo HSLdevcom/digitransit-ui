cac195f6cfedf55a3ca6284aa8cc98248ce7aa68
Huang Zhiguan <huangzhiguan@Huangs-MacBook-Pro-2.local>
2019-10-09 11:10:07 +0300

    enable real time bus location in the map for kuopio and turku DT-3203

--- app/configurations/config.turku.js
+++ app/configurations/config.turku.js
@@ -182,7 +182,8 @@ export default configMerger(walttiConfig, {
   },
 
   staticMessages: [],
-
+  showAllBusses: true,
+  showVehiclesOnStopPage: true,
   geoJson: {
     layerConfigUrl: 'https://data.foli.fi/geojson/reittiopas',
   },
