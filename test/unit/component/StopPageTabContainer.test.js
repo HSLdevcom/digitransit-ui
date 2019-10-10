c9de605f2187d7521ed3b793070ce5799c64ca60
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-10-10 13:54:31 +0300

    Add some tests for StopPageTabContainer

--- test/unit/component/StopPageTabContainer.test.js
+++ test/unit/component/StopPageTabContainer.test.js
@@ -128,6 +128,131 @@ describe('<StopPageTabContainer />', () => {
     expect(wrapper.find('.alert-active')).to.have.lengthOf(1);
   });
 
+  it('should mark the disruptions tab as having an active disruption alert due to a stop SEVERE level service alert', () => {
+    const props = {
+      breakpoint: 'large',
+      children: <div />,
+      location: {
+        pathname: 'foobar',
+      },
+      params: {
+        terminalId: 'HSL:2211275',
+      },
+      routes: [],
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Severe,
+          },
+        ],
+        stoptimesForServiceDate: [],
+      },
+    };
+    const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+    expect(wrapper.find('.active-disruption-alert')).to.have.lengthOf(2);
+  });
+
+  it('should mark the disruptions tab as having an active disruption alert due to a stop WARNING level service alert', () => {
+    const props = {
+      breakpoint: 'large',
+      children: <div />,
+      location: {
+        pathname: 'foobar',
+      },
+      params: {
+        terminalId: 'HSL:2211275',
+      },
+      routes: [],
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Warning,
+          },
+        ],
+        stoptimesForServiceDate: [],
+      },
+    };
+    const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+    expect(wrapper.find('.active-disruption-alert')).to.have.lengthOf(2);
+  });
+
+  it('should mark the disruptions tab as having an active info alert due to a stop UNKNOWN_SEVERITY level service alert', () => {
+    const props = {
+      breakpoint: 'large',
+      children: <div />,
+      location: {
+        pathname: 'foobar',
+      },
+      params: {
+        terminalId: 'HSL:2211275',
+      },
+      routes: [],
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Unkown,
+          },
+        ],
+        stoptimesForServiceDate: [],
+      },
+    };
+    const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+    expect(wrapper.find('.active-disruption-alert')).to.have.lengthOf(2);
+  });
+
+  it('should mark the disruptions tab as having an active info alert due to a stop INFO level service alert', () => {
+    const props = {
+      breakpoint: 'large',
+      children: <div />,
+      location: {
+        pathname: 'foobar',
+      },
+      params: {
+        terminalId: 'HSL:2211275',
+      },
+      routes: [],
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Info,
+          },
+        ],
+        stoptimesForServiceDate: [],
+      },
+    };
+    const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+    expect(wrapper.find('.active-service-alert')).to.have.lengthOf(2);
+  });
+
+  it('should mark the disruptions tab as having an active disruption alert due to a route SEVERE level service alert', () => {
+    const props = {
+      breakpoint: 'large',
+      children: <div />,
+      location: {
+        pathname: 'foobar',
+      },
+      params: {
+        terminalId: 'HSL:2211275',
+      },
+      routes: [
+        {
+          alerts: [
+            {
+              alertSeverityLevel: AlertSeverityLevelType.Severe,
+              effectiveStartDate: Date.now() - 100000,
+              effectiveEndDate: Date.now() + 100000,
+            },
+          ],
+        },
+      ],
+      stop: {
+        stoptimesForServiceDate: [],
+      },
+    };
+    const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+    expect(wrapper.find('.active-disruption-alert')).to.have.lengthOf(2);
+  });
+
   it('should render empty if stop information is missing', () => {
     const props = {
       breakpoint: 'large',
