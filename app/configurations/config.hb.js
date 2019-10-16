9a3a196dcc1a451c35ca7f4f6c0085097c4e3fa2
Mérő Mihály <meromisi88@gmail.com>
2019-10-16 13:43:56 +0200

    fixed typo

--- app/configurations/config.hb.js
+++ app/configurations/config.hb.js
@@ -322,7 +322,7 @@ export default configMerger(walttiConfig, {
         name: {
           fi: '',
           en: 'Car charging stations',
-          de: 'Elktroauto-Ladestationen',
+          de: 'Elektroauto-Ladestationen',
         },
         url: '/assets/geojson/hb-layers/carchargingstation.geojson',
       }
