e18862dadd5a917b4e59d1b21dda914f1e9048f5
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-10-30 08:03:27 +0200

    Disable test whose content changes by season

--- test/visual/components.js
+++ test/visual/components.js
@@ -91,7 +91,7 @@ testVariation('PlatformNumber');
 testVariation('CardHeader');
 testVariation('Card');
 testVariation('CityBikeCard');
-testVariation('CityBikeContent');
+//testVariation('CityBikeContent');
 testVariation('CityBikeAvailability');
 testVariation('CityBikeUse');
 
