2e168c2b7a770718d07a76e8a8e1c2c3fe87d299
Leonard Ehrenfried <mail@leonard.io>
2019-12-13 10:53:14 +0100

    Fix CancelationInfo test

--- test/unit/component/DepartureCancelationInfo.test.js
+++ test/unit/component/DepartureCancelationInfo.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import React from 'react';
+import LocalTime from '../../../app/component/LocalTime';
 
 import { mountWithIntl } from '../helpers/mock-intl-enzyme';
 import DepartureCancelationInfo from '../../../app/component/DepartureCancelationInfo';
@@ -14,14 +15,22 @@ describe('<DepartureCancelationInfo />', () => {
     scheduledDepartureTime: 1547630218,
   };
 
+  // because the local time of the epoch second is different depending on your timezone, we have to localise it too
+  // before running the asserts
+  const localTimeInCurrentTimezone = LocalTime({
+    forceUtc: false,
+    time: defaultProps.scheduledDepartureTime,
+  });
+
   it('should render in English', () => {
     const wrapper = mountWithIntl(
       <DepartureCancelationInfo {...defaultProps} />,
       {},
       'en',
     );
+    expect(localTimeInCurrentTimezone).to.contain('16');
     expect(wrapper.text()).to.equal(
-      'Bus 52 Arabianranta–Munkkiniemi at 11:16 is cancelled',
+      `Bus 52 Arabianranta–Munkkiniemi at ${localTimeInCurrentTimezone} is cancelled`,
     );
   });
 
@@ -32,7 +41,7 @@ describe('<DepartureCancelationInfo />', () => {
       'fi',
     );
     expect(wrapper.text()).to.equal(
-      'Bussin 52 lähtö Arabianranta–Munkkiniemi kello 11:16 on peruttu',
+      `Bussin 52 lähtö Arabianranta–Munkkiniemi kello ${localTimeInCurrentTimezone} on peruttu`,
     );
   });
 
@@ -43,7 +52,7 @@ describe('<DepartureCancelationInfo />', () => {
       'sv',
     );
     expect(wrapper.text()).to.equal(
-      'Avgång på linje 52 Arabianranta–Munkkiniemi kl. 11:16 är inställd',
+      `Avgång på linje 52 Arabianranta–Munkkiniemi kl. ${localTimeInCurrentTimezone} är inställd`,
     );
   });
 });
