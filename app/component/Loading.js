d104678e90e605bb7b584a6d0cecd14173008021
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2019-12-16 13:31:15 +0200

    Loading spinners are now announced with screen readers

--- app/component/Loading.js
+++ app/component/Loading.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+const defaultMessage = (
+  <span className="sr-only">
+    <FormattedMessage id="loading" defaultMessage="Loading" />
+  </span>
+);
 
 const Loading = props => (
-  <div className="spinner-loader">{(props && props.children) || null}</div>
+  <div className="spinner-loader">
+    {(props && props.children) || defaultMessage}
+  </div>
 );
 
 Loading.displayName = 'Loading';
