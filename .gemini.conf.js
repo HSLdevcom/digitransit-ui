26480fdc43746eaa36e9e80f6803e16917a7b570
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-12-20 09:09:42 +0200

    test: small retry count to allow build to finish

--- .gemini.conf.js
+++ .gemini.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   sessionRequestTimeout: 120000,
   sessionsPerBrowser: 1,
   suitesPerSession: 50,
-  retry: 10,
+  retry: 2,
   tolerance: 10,
   antialiasingTolerance: 10,
   compositeImage: true,
