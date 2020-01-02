f039084fdbadd59ff24dcb88863e8d0b5d662e98
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-01-02 12:16:51 +0200

    fix tests

--- test/unit/component/UserInfo.test.js
+++ test/unit/component/UserInfo.test.js
@@ -2,39 +2,39 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
+import {  mountWithIntl } from '../helpers/mock-intl-enzyme';
 import UserInfo from '../../../app/component/UserInfo';
 
 describe('<UserInfo />', () => {
   const defaultProps = {
     user: { name: 'Matti Meikäläinen' },
     list: [
-      { key: '1', messageId: 'dropdown-item-1' },
-      { key: '2', messageId: 'dropdown-item-2' },
+      { key: '1', messageId: 'userinfo-item-1' },
+      { key: '2', messageId: 'userinfo-item-2' },
     ],
   };
 
-  it('should open dropdown menu on click', () => {
-    const wrapper = shallowWithIntl(<UserInfo {...defaultProps} />);
-    expect(wrapper.find('.dropdown-list-item')).to.have.lengthOf(0);
-    wrapper.find('.noborder').simulate('click');
-    expect(wrapper.find('.dropdown-list-item')).to.have.lengthOf(2);
+  it('should open userinfo menu on click', () => {
+    const wrapper = mountWithIntl(<UserInfo {...defaultProps} />);
+    expect(wrapper.find('.userinfo-list-item')).to.have.lengthOf(0);
+    wrapper.find('.noborder').at(0).simulate('click');
+    expect(wrapper.find('.userinfo-list-item')).to.have.lengthOf(2);
   });
 
   it('should render correct text', () => {
-    const wrapper = shallowWithIntl(<UserInfo {...defaultProps} />);
-    wrapper.find('.noborder').simulate('click');
+    const wrapper = mountWithIntl(<UserInfo {...defaultProps} />);
+    wrapper.find('.noborder').at(0).simulate('click');
     expect(
       wrapper
         .find(FormattedMessage)
         .at(0)
         .props().id,
-    ).to.equal('dropdown-item-1');
+    ).to.equal('userinfo-item-1');
     expect(
       wrapper
         .find(FormattedMessage)
         .at(1)
         .props().id,
-    ).to.equal('dropdown-item-2');
+    ).to.equal('userinfo-item-2');
   });
 });
