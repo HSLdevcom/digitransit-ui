f0f86d83b7cd47298922764b25800d01f171144c
Kimmo Hujanen <kimmo.hujanen@luukku.com>
2020-01-23 08:59:51 +0200

    fix: date ranges

--- app/util/routePatternSelectUtil.js
+++ app/util/routePatternSelectUtil.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
 /* eslint no-bitwise: ["error", { "allow": [">>"] }] */
+/* eslint-disable prefer-destructuring */
 
 import cloneDeep from 'lodash/cloneDeep';
 import moment from 'moment';
@@ -10,73 +11,131 @@ export const DATE_FORMAT = 'YYYYMMDD';
 export const DATE_FORMAT2 = 'D.M.';
 export const DATE_FORMAT3 = 'D.';
 
-export const getWeekdayText = (str, arr) => {
+export const getWeekdayInfo = (str, arr, dayNo) => {
   let weekdayText = '-';
+  let allowedDiff = 1;
   switch (Array.from(new Set(arr)).length) {
     case 1:
       switch (arr[0]) {
         case '1':
           weekdayText = 'ma-ma';
+          allowedDiff = 7 - dayNo + 1;
           break;
         case '2':
           weekdayText = 'ti-ti';
+          if (dayNo < 2) {
+            allowedDiff = 2 - dayNo;
+          } else {
+            allowedDiff = 7 - dayNo + 2;
+          }
           break;
         case '3':
           weekdayText = 'ke-ke';
+          if (dayNo < 3) {
+            allowedDiff = 3 - dayNo;
+          } else {
+            allowedDiff = 7 - dayNo + 3;
+          }
           break;
         case '4':
           weekdayText = 'to-to';
+          if (dayNo < 4) {
+            allowedDiff = 4 - dayNo;
+          } else {
+            allowedDiff = 7 - dayNo + 4;
+          }
           break;
         case '5':
           weekdayText = 'pe-pe';
+          if (dayNo < 5) {
+            allowedDiff = 5 - dayNo;
+          } else {
+            allowedDiff = 7 - dayNo + 5;
+          }
           break;
         case '6':
           weekdayText = 'la-la';
+          if (dayNo < 6) {
+            allowedDiff = 6 - dayNo;
+          } else {
+            allowedDiff = 7 - dayNo + 6;
+          }
           break;
         case '7':
           weekdayText = 'su-su';
+          if (dayNo < 7) {
+            allowedDiff = 7 - dayNo;
+          } else {
+            allowedDiff = 7;
+          }
           break;
         default:
-          weekdayText = '';
+          weekdayText = '-';
       }
       break;
     case 2:
       if (str.indexOf('56') !== -1) {
         weekdayText = 'pe-la';
+        if (dayNo < 5) {
+          allowedDiff = 5 - dayNo;
+        } else {
+          allowedDiff = 7 - dayNo + 5;
+        }
         break;
       } else if (str.indexOf('67') !== -1) {
         weekdayText = 'la-su';
+        if (dayNo < 6) {
+          allowedDiff = 6 - dayNo;
+        } else {
+          allowedDiff = 7 - dayNo + 6;
+        }
         break;
       }
       break;
     case 3:
       if (str.indexOf('567') !== -1) {
         weekdayText = 'pe-su';
+        if (dayNo < 5) {
+          allowedDiff = 5 - dayNo;
+        } else {
+          allowedDiff = 7 - dayNo + 5;
+        }
         break;
       }
       break;
     case 5:
       if (str.indexOf('12345') !== -1) {
         weekdayText = 'ma-pe';
+        if (dayNo < 5) {
+          allowedDiff = 1;
+        } else {
+          allowedDiff = 7 - dayNo + 1;
+        }
         break;
       }
       break;
     case 6:
       if (str.indexOf('123456') !== -1) {
         weekdayText = 'ma-la';
+        allowedDiff = dayNo === 6 ? 2 : 1;
+        break;
+      } else if (str.indexOf('712345') !== -1 || str.indexOf('123457')) {
+        weekdayText = 'su-pe';
+        allowedDiff = dayNo === 5 ? 2 : 1;
         break;
       }
       break;
     case 7:
       if (str.indexOf('1234567') !== -1) {
         weekdayText = 'ma-su';
+        allowedDiff = 1;
         break;
       }
       break;
     default:
-      weekdayText = '';
+      weekdayText = '-';
   }
-  return weekdayText;
+  return [weekdayText, allowedDiff];
 };
 
 export const enrichPatterns = (patterns, onlyInFuture, itineraryFutureDays) => {
@@ -121,7 +180,17 @@ export const enrichPatterns = (patterns, onlyInFuture, itineraryFutureDays) => {
       x.activeDates = [];
     }
     x.activeDates = Array.from(new Set(uniqueDates.sort()));
+    if (
+      x.activeDates.length > 0 &&
+      moment(x.activeDates[0]).isAfter(lastRangeDate)
+    ) {
+      x.activeDates = [];
+    }
   });
+
+  futureTrips = futureTrips.filter(
+    f => f.tripsForDate.length > 0 || f.activeDates.length > 0,
+  );
 
   for (let y = 0; y < futureTrips.length; y++) {
     const actDates = [];
@@ -162,24 +231,65 @@ export const enrichPatterns = (patterns, onlyInFuture, itineraryFutureDays) => {
         rangeFollowingDays[rangeFollowingDays.length - 1][1] = arr[index];
       }
     });
+
+    const rangeDays = [];
+    for (let z = 0; z < rangeFollowingDays.length; z++) {
+      if (moment(rangeFollowingDays[z][0]).isSameOrBefore(lastRangeDate)) {
+        rangeDays.push(rangeFollowingDays[z]);
+      }
+    }
+
     futureTrips[y].lastRangeDay = lastRangeDate.format(DATE_FORMAT);
-    futureTrips[y].rangeFollowingDays = rangeFollowingDays;
+    futureTrips[y].rangeFollowingDays = rangeDays;
     futureTrips[y].dayDiff = dayDiff;
     futureTrips[y].dayNumbers = dayNumbers;
     futureTrips[y].activeDates = Array.from(new Set(actDates));
     futureTrips[y].dayJoin = dayNumbers.join('');
-    futureTrips[y].dayString = getWeekdayText(
+    const dayInfo = getWeekdayInfo(
       futureTrips[y].dayJoin,
       Array.from(new Set(futureTrips[y].dayNumbers.sort())),
+      Number(currentDate.format('E')),
     );
-    futureTrips[y].fromDate =
-      moment(minAndMaxDate[0]).isAfter(currentDate) &&
-      moment(minAndMaxDate[0]).diff(currentDate, 'days') >= 6
+    futureTrips[y].dayString = dayInfo[0];
+    futureTrips[y].allowedDiff = dayInfo[1];
+
+    if (
+      futureTrips[y].rangeFollowingDays.length === 1 &&
+      futureTrips[y].rangeFollowingDays[0][0] ===
+        futureTrips[y].rangeFollowingDays[0][1]
+    ) {
+      futureTrips[y].fromDate = futureTrips[y].rangeFollowingDays[0][0];
+      futureTrips[y].untilDate = futureTrips[y].rangeFollowingDays[0][1];
+      futureTrips[y].rangeFollowingDays[0][1] = 0;
+    } else if (
+      futureTrips[y].rangeFollowingDays.length === 1 &&
+      futureTrips[y].rangeFollowingDays[0][0] !==
+        futureTrips[y].rangeFollowingDays[0][1]
+    ) {
+      if (moment(minAndMaxDate[0]).isAfter(currentDate)) {
+        futureTrips[y].fromDate = futureTrips[y].rangeFollowingDays[0][0];
+      } else {
+        futureTrips[y].fromDate = '-';
+      }
+      if (
+        moment(futureTrips[y].rangeFollowingDays[0][1]).isBefore(lastRangeDate)
+      ) {
+        futureTrips[y].untilDate = futureTrips[y].rangeFollowingDays[0][1];
+      } else {
+        futureTrips[y].untilDate = '-';
+      }
+    } else {
+      futureTrips[y].fromDate = moment(minAndMaxDate[0]).isAfter(
+        currentDate.add(futureTrips[y].allowedDiff, 'days'),
+      )
         ? `${minAndMaxDate[0]}`
         : '-';
-    futureTrips[y].untilDate = moment(minAndMaxDate[1]).isBefore(lastRangeDate)
-      ? `${minAndMaxDate[1]}`
-      : '-';
+      futureTrips[y].untilDate = moment(minAndMaxDate[1]).isBefore(
+        lastRangeDate,
+      )
+        ? `${minAndMaxDate[1]}`
+        : '-';
+    }
   }
 
   futureTrips = futureTrips.filter(
@@ -207,16 +317,17 @@ export const getOptionText = (context, pattern, isTogglable) => {
   }
   // Add opt #1: (vain 31.12.) or (19.-23.1.)
   if (
-    pattern.tripsForDate.length === 0 &&
+    // pattern.tripsForDate.length === 0 &&
     pattern.activeDates.length > 0 &&
     pattern.rangeFollowingDays.length === 1 &&
+    pattern.fromDate !== '-' &&
     moment(pattern.lastRangeDay).isAfter(
       moment(pattern.rangeFollowingDays[0][1]),
     )
   ) {
     retValue += ' (';
     if (
-      pattern.activeDates.length === 1 &&
+      pattern.activeDates.length === 1 ||
       pattern.rangeFollowingDays[0][1] === 0
     ) {
       retValue += context.intl.formatMessage({
@@ -328,7 +439,7 @@ export const getOptionText = (context, pattern, isTogglable) => {
     return retValue;
   }
 
-  // Add opt #3b: (arkisin), (viikonloppuisin) or (ma-la)
+  // Add opt #3b: (ma-pe), (la-su) or (ma-la)
   if (
     pattern.untilDate === '-' &&
     pattern.fromDate === '-' &&
@@ -360,11 +471,11 @@ export const getOptionText = (context, pattern, isTogglable) => {
           day: moment(pattern.untilDate, DATE_FORMAT).format(DATE_FORMAT2),
         },
       )
-      .replace(/\( /gi, '(');
+      .replace(/\( {2}|\( |\(/gi, '(');
     return retValue;
   }
 
-  // Add opt #4b: (arkisin 31.1. saakka) or (viikonloppuisin 31.1. saakka)
+  // Add opt #4b: (ma-pe 31.1. saakka) or (la-su 31.1. saakka)
   if (
     pattern.untilDate !== '-' &&
     pattern.untilDate !== undefined &&
@@ -384,7 +495,7 @@ export const getOptionText = (context, pattern, isTogglable) => {
           day: moment(pattern.untilDate, DATE_FORMAT).format(DATE_FORMAT2),
         },
       )
-      .replace(/\( /gi, '(');
+      .replace(/\( {2}|\( |\(/gi, '(');
     return retValue;
   }
 
@@ -408,11 +519,11 @@ export const getOptionText = (context, pattern, isTogglable) => {
           day: moment(pattern.fromDate, DATE_FORMAT).format(DATE_FORMAT2),
         },
       )
-      .replace(/\( /gi, '(');
+      .replace(/\( {2}|\( |\(/gi, '(');
     return retValue;
   }
 
-  // Add opt #5b: (arkisin 1.1. lähtien) or (viikonloppuisin 31.1. lähtien)
+  // Add opt #5b: (ma-pe 1.1. lähtien) or (la-su 31.1. lähtien)
   if (
     pattern.fromDate !== '-' &&
     pattern.fromDate !== undefined &&
@@ -432,7 +543,8 @@ export const getOptionText = (context, pattern, isTogglable) => {
           day: moment(pattern.fromDate, DATE_FORMAT).format(DATE_FORMAT2),
         },
       )
-      .replace(/\( /gi, '(');
+      .replace(/\( {2}|\( |\(/gi, '(');
+    return retValue;
   }
   return retValue;
 };
