8f1f6f1fc6e22c2245cb37b27afe538d88b8131d
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-01-27 12:21:24 +0200

    chore: set saveUninitialize flag to false and destroy session on logout

--- server/server.js
+++ server/server.js
@@ -84,7 +84,7 @@ function setUpOIDC() {
       secret: process.env.SESSION_SECRET || 'reittiopas_secret',
       store: new FileStore(),
       resave: false,
-      saveUninitialized: true,
+      saveUninitialized: false,
       cookie: {
         secure: true,
         httpOnly: true,
@@ -120,7 +120,9 @@ function setUpOIDC() {
 
   app.get('/logout', function(req, res) {
     req.logout();
-    res.redirect('/');
+    req.session.destroy(function() {
+      res.redirect('/');
+    });
   });
 
   app.use('/api', function(req, res, next) {
