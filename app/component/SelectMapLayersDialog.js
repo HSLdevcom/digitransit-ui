a39f890f25591f066f0f01c2a3d620ea09dfcbcd
Beck-berry <trebeka98@gmail.com>
2020-02-18 14:11:57 +0100

    add carpool to map layer selector

--- app/component/SelectMapLayersDialog.js
+++ app/component/SelectMapLayersDialog.js
@@ -176,6 +176,16 @@ class SelectMapLayersDialog extends React.Component {
               }}
             />
           )}
+          {isTransportModeEnabled(transportModes.carpool) && (
+            <Checkbox
+              checked={terminal.carpool}
+              defaultMessage="Carpool stops"
+              labelId="map-layer-carpool"
+              onChange={e =>
+                this.updateStopAndTerminalSetting({ carpool: e.target.checked })
+              }
+            />
+          )}
           {config.cityBike &&
             config.cityBike.showCityBikes && (
               <Checkbox
