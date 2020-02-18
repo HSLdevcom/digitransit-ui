c9b3cb500696e943a2af9ec7f50c26f010aba9de
Juha-Matti Viertola <juha-matti.viertola@cgi.com>
2020-02-18 10:05:58 +0200

    fix: No favourites images was missing

--- app/component/NoFavouritesPanel.js
+++ app/component/NoFavouritesPanel.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { isBrowser } from '../util/browser';
 
-const NoFavouritesPanel = () => (
-  <div className="nofavs row">
-    <div className="small-12 columns">
-      <div className="nofavs-p black text-center">
-        <div className="nofavs-img">
-          {isBrowser && (
-            <img
-              // eslint-disable-next-line global-require
-              src={require('../configurations/images/default/favourites_empty_tip.png')}
-              alt="No favourites tip icon"
-            />
-          )}
+const NoFavouritesPanel = () => {
+  const bgStyle = {};
+  if (isBrowser) {
+    // eslint-disable-next-line global-require
+    const bg = require('../configurations/images/default/favourites_empty_tip.png');
+    bgStyle.backgroundImage = `url(${bg.default})`;
+    bgStyle.backgroundSize = '160px 160px';
+  }
+
+  return (
+    <div className="nofavs row">
+      <div className="small-12 columns">
+        <div className="nofavs-p black text-center">
+          <div className="nofavs-img" style={bgStyle} />
+          <FormattedMessage
+            id="no-favourites"
+            defaultMessage="Use the star buttons to add routes or stops to your favorites. Your favorites are shown on this page."
+          />
         </div>
-        <FormattedMessage
-          id="no-favourites"
-          defaultMessage="Use the star buttons to add routes or stops to your favorites. Your favorites are shown on this page."
-        />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NoFavouritesPanel;
