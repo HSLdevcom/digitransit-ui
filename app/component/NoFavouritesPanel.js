cac0b6548ee13a9351d90aebf4030de60707e55e
Juha-Matti Viertola <juha-matti.viertola@cgi.com>
2020-02-19 10:11:09 +0200

    style: Replaced empty favourites image with star icon

--- app/component/NoFavouritesPanel.js
+++ app/component/NoFavouritesPanel.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { isBrowser } from '../util/browser';
+import Icon from './Icon';
 
-const NoFavouritesPanel = () => {
-  const bgStyle = {};
-  if (isBrowser) {
-    // eslint-disable-next-line global-require
-    const bg = require('../configurations/images/default/favourites_empty_tip.png');
-    bgStyle.backgroundImage = `url(${bg.default})`;
-    bgStyle.backgroundSize = '160px 160px';
-  }
-
-  return (
-    <div className="nofavs row">
-      <div className="small-12 columns">
-        <div className="nofavs-p black text-center">
-          <div className="nofavs-img" style={bgStyle} />
-          <FormattedMessage
-            id="no-favourites"
-            defaultMessage="Use star button to add routes to your favorites. Your favorite routes are shown on this page."
-          />
+const NoFavouritesPanel = () => (
+  <div className="nofavs row">
+    <div className="small-12 columns">
+      <div className="nofavs-p black text-center">
+        <div className="nofavs-img">
+          {isBrowser && (
+            <Icon className="no-favourites-icon" img="icon-icon_star" />
+          )}
         </div>
+        <FormattedMessage
+          id="no-favourites"
+          defaultMessage="Use star button to add routes to your favorites. Your favorite routes are shown on this page."
+        />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default NoFavouritesPanel;
