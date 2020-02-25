716fb8c90500ba2a74fd34dfcb61bb5411d92a6b
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2020-02-25 10:50:06 +0200

    fix: search fields are properly announced with browse mode in NVDA

--- app/component/DTSearchAutosuggest.js
+++ app/component/DTSearchAutosuggest.js
@@ -342,10 +342,7 @@ class DTAutosuggest extends React.Component {
           shouldRenderSuggestions={() => this.state.editing}
           highlightFirstSuggestion
           renderInputComponent={p => (
-            <div id={`${this.props.id}-container`} style={{ display: 'flex' }}>
-              <span className="sr-only" role="alert">
-                {ariaSuggestionLen}
-              </span>
+            <>
               <input
                 aria-label={SearchBarId.concat(' ').concat(ariaLabelText)}
                 id={this.props.id}
@@ -353,8 +350,11 @@ class DTAutosuggest extends React.Component {
                 onKeyDown={this.keyDown}
                 {...p}
               />
+              <span className="sr-only" role="alert">
+                {ariaSuggestionLen}
+              </span>
               {this.clearButton()}
-            </div>
+            </>
           )}
           onSuggestionSelected={this.onSelected}
           ref={this.storeInputReference}
