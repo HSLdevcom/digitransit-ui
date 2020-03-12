73733e749804f58ae17418ad5264d9a9a2d17064
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-03-12 13:55:06 +0200

    feat: disable clicking on element when favourites are updating

--- app/component/Favourite.js
+++ app/component/Favourite.js
@@ -1,22 +1,43 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cx from 'classnames';
 
 import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 
 /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
-const Favourite = ({ addFavourite, deleteFavourite, favourite, className }) => (
-  <span
-    className={cx('cursor-pointer favourite-icon', className)}
-    onClick={favourite ? deleteFavourite : addFavourite}
-  >
-    <Icon
-      className={cx('favourite', { selected: favourite })}
-      img="icon-icon_star"
-    />
-  </span>
-);
+const Favourite = ({ addFavourite, deleteFavourite, favourite, className }) => {
+  const [disable, handleDisable] = useState(false);
+
+  useEffect(
+    () => {
+      handleDisable(false);
+    },
+    [favourite],
+  );
+
+  const onClick = () => {
+    if (!disable) {
+      handleDisable(true);
+      if (favourite) {
+        deleteFavourite();
+      } else {
+        addFavourite();
+      }
+    }
+  };
+  return (
+    <span
+      className={cx('cursor-pointer favourite-icon', className)}
+      onClick={onClick}
+    >
+      <Icon
+        className={cx('favourite', { selected: favourite })}
+        img="icon-icon_star"
+      />
+    </span>
+  );
+};
 
 Favourite.propTypes = {
   addFavourite: PropTypes.func.isRequired,
