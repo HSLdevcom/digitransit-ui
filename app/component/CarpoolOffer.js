46ba4bf1dcf89625b4567cd4316f1922b0a7c79d
Mérő Mihály <meromisi88@gmail.com>
2020-03-16 11:17:40 +0100

    fix checkboxes

--- app/component/CarpoolOffer.js
+++ app/component/CarpoolOffer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { intlShape } from 'react-intl';
+import { intlShape, FormattedMessage } from 'react-intl';
 import { routerShape } from 'react-router';
 import Icon from './Icon';
 import Checkbox from './Checkbox';
@@ -49,8 +49,7 @@ export default class CarpoolOffer extends React.Component {
 
   isFinished = false;
 
-  setFrequency = e => {
-    e.preventDefault();
+  setFrequency = () => {
     this.isRegularly = document.getElementById('regularly').checked;
     this.forceUpdate();
   };
@@ -112,7 +111,8 @@ export default class CarpoolOffer extends React.Component {
             <h2>Vielen Dank</h2>
             <p>
               Ihr {this.isRegularly ? 'regelmäßiges' : ''} Inserat von{' '}
-              {this.props.from} nach {this.props.to} wurde eingestellt.<br />
+              {this.props.from} nach {this.props.to} wurde eingestellt.
+              <br />
               Sie haben die folgende Zeit und Tagen inseriert: <br />
               {offeredTimes}
             </p>
@@ -132,127 +132,132 @@ export default class CarpoolOffer extends React.Component {
           <div className="sidePanelText">
             <h2>Ihr Inserat</h2>
             <p>
-              Abfahrt: {this.props.from} um {this.props.start}<br />
-              Ankunft: {this.props.to} um {this.props.start+this.props.duration}
+              Abfahrt: {this.props.from} um {this.props.start}
+              <br />
+              Ankunft: {this.props.to} um{' '}
+              {this.props.start + this.props.duration}
             </p>
             <p>Wie oft bieten Sie diese Fahrt an?</p>
-            <form onSubmit={this.setFrequency}>
-              <div>
-                <input
-                  type="radio"
-                  id="once"
-                  value="once"
-                  name="times"
-                  defaultChecked
-                />
-                <label className="radio-label" htmlFor="once">
-                  einmal
-                </label>
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  id="regularly"
-                  value="regularly"
-                  name="times"
-                />
-                <label className="radio-label" htmlFor="regularly">
-                  regelmäßig
-                </label>
-              </div>
+            <div>
               <input
-                className="sidePanel-btn"
-                type="submit"
-                value={this.isRegularly ? 'Update' : 'Next'}
+                onChange={this.setFrequency}
+                type="radio"
+                id="once"
+                value="once"
+                name="times"
+                defaultChecked
               />
-            </form>
-            {this.isRegularly ? (
-              <form onSubmit={this.finishForm}>
-                <Checkbox
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.mon = !this.days.mon;
-                    this.forceUpdate();
-                  }}
-                  checked={this.days.mon}
-                  labelId="monday"
-                  title="mon"
-                />
-                <Checkbox
-                  checked={this.days.tue}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.tue = !this.days.tue;
-                    this.forceUpdate();
-                  }}
-                  labelId="tuesday"
-                />
-                <Checkbox
-                  checked={this.days.wed}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.wed = !this.days.wed;
-                    this.forceUpdate();
-                  }}
-                  labelId="wednesday"
-                />
-                <Checkbox
-                  checked={this.days.thu}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.thu = !this.days.thu;
-                    this.forceUpdate();
-                  }}
-                  labelId="thursday"
-                />
-                <Checkbox
-                  checked={this.days.fri}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.fri = !this.days.fri;
-                    this.forceUpdate();
-                  }}
-                  labelId="friday"
-                />
-                <Checkbox
-                  checked={this.days.sat}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.sat = !this.days.sat;
-                    this.forceUpdate();
-                  }}
-                  labelId="saturday"
-                />
-                <Checkbox
-                  checked={this.days.sun}
-                  onChange={e => {
-                    this.updateSelectedDays(
-                      e.currentTarget.getAttribute('aria-label'),
-                    );
-                    this.days.sun = !this.days.sun;
-                    this.forceUpdate();
-                  }}
-                  labelId="sunday"
-                />
-                <div>
-                  <input className="sidePanel-btn" type="submit" value="Next" />
-                </div>
-              </form>
-            ) : (
-              ''
-            )}
+              <label className="radio-label" htmlFor="once">
+                einmal
+              </label>
+            </div>
+            <div>
+              <input
+                onChange={this.setFrequency}
+                type="radio"
+                id="regularly"
+                value="regularly"
+                name="times"
+              />
+              <label className="radio-label" htmlFor="regularly">
+                regelmäßig
+              </label>
+            </div>
+            <Checkbox
+              disabled={!this.isRegularly}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.mon = !this.days.mon;
+                this.forceUpdate();
+              }}
+              checked={this.isRegularly && this.days.mon}
+              labelId="monday"
+              title="mon"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.tue}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.tue = !this.days.tue;
+                this.forceUpdate();
+              }}
+              labelId="tuesday"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.wed}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.wed = !this.days.wed;
+                this.forceUpdate();
+              }}
+              labelId="wednesday"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.thu}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.thu = !this.days.thu;
+                this.forceUpdate();
+              }}
+              labelId="thursday"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.fri}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.fri = !this.days.fri;
+                this.forceUpdate();
+              }}
+              labelId="friday"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.sat}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.sat = !this.days.sat;
+                this.forceUpdate();
+              }}
+              labelId="saturday"
+            />
+            <Checkbox
+              disabled={!this.isRegularly}
+              checked={this.isRegularly && this.days.sun}
+              onChange={e => {
+                this.updateSelectedDays(
+                  e.currentTarget.getAttribute('aria-label'),
+                );
+                this.days.sun = !this.days.sun;
+                this.forceUpdate();
+              }}
+              labelId="sunday"
+            />
+            <button
+              className="standalone-btn"
+              type="submit"
+              onClick={this.finishForm}
+            >
+              <FormattedMessage
+                id="offer-ride"
+                defaultMessage="Offer carpool"
+              />
+            </button>
           </div>
         )}
       </div>
