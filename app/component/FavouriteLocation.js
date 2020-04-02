2bcc2cd7d383431c86f545b12363cdbb1d403d98
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-04-02 16:09:20 +0300

    feat: update favourite location styles

--- app/component/FavouriteLocation.js
+++ app/component/FavouriteLocation.js
@@ -1,18 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import cx from 'classnames';
-import Link from 'found/lib/Link';
-
 import Icon from './Icon';
 
 const FavouriteLocation = ({ favourite, className, clickFavourite, key }) => {
-  const { name, favouriteId, selectedIconId, address } = favourite;
+  const { name, selectedIconId, address } = favourite;
 
   /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
   return (
     <div
       key={key}
-      data-swipeable="true"
       className={cx('new-favourite-location-content', className)}
       onClick={() => clickFavourite(favourite)}
     >
@@ -21,14 +18,6 @@ const FavouriteLocation = ({ favourite, className, clickFavourite, key }) => {
         <div className="favourite-location-name">{name}</div>
         <div className="favourite-location-address">{address}</div>
       </div>
-      <Link
-        to={`/suosikki/muokkaa/sijainti/${favouriteId}`}
-        className="cursor-pointer no-decoration"
-      >
-        <div className="favourite-edit-icon-click-area">
-          <Icon className="favourite-edit-icon" img="icon-icon_edit" />
-        </div>
-      </Link>
     </div>
   );
 };
