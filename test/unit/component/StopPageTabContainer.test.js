7e3212b716987a5841ba84909b272606cbddf9d1
Kimmo Hujanen <Kimmo Hujanen>
2020-04-06 07:52:51 +0300

    fix: added stop.vehicleMode to mock data

--- test/unit/component/StopPageTabContainer.test.js
+++ test/unit/component/StopPageTabContainer.test.js
@@ -313,7 +313,7 @@ describe('<StopPageTabContainer />', () => {
 });
 
 // DT-3387
-it('should render "Routes, platforms" when vehicleMode is rail or subway but route´s mode is bus', () => {
+it('should render "Routes, platforms" when vehicleMode is rail but route´s mode is bus', () => {
   const props = {
     breakpoint: 'large',
     children: <div />,
@@ -325,6 +325,41 @@ it('should render "Routes, platforms" when vehicleMode is rail or subway but rou
     },
     routes: [],
     stop: {
+      vehicleMode: 'RAIL',
+      routes: [
+        {
+          mode: 'BUS',
+          patterns: [
+            {
+              code: 'HSL:9665A:0:02',
+            },
+          ],
+        },
+      ],
+    },
+  };
+  const wrapper = shallowWithIntl(<StopPageTabContainer {...props} />);
+  expect(
+    wrapper
+      .find(FormattedMessage)
+      .at(2)
+      .props().id,
+  ).to.equal('routes-platforms');
+});
+
+it('should render "Routes, platforms" when vehicleMode is subway but route´s mode is bus', () => {
+  const props = {
+    breakpoint: 'large',
+    children: <div />,
+    location: {
+      pathname: 'foobar',
+    },
+    params: {
+      stopId: 'HSL:2211275',
+    },
+    routes: [],
+    stop: {
+      vehicleMode: 'SUBWAY',
       routes: [
         {
           mode: 'BUS',
