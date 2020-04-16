14fb3fda8c93e3e1692d01109cb7ef3c73656b40
Janne Antikainen <antiik91@gmail.com>
2020-04-16 15:23:44 +0300

    feat: started working on map-pelias-modality module

---
+++ digitransit-search-util/packages/digitransit-search-util-map-pelias-modality/test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import mapPeliasModality from '.';
+
+describe('Testing @digitransit-search-util/digitransit-search-util-map-pelias-modality module', () => {
+  it('Checking that true is true', () => {
+    // const retValue = mapPeliasModality(param1, param2);
+    expect(true).to.be.equal(true);
+  });
+});
