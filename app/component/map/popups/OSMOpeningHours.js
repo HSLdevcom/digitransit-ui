25b415677f3dd2373bba7e79538ce62f9e69d5c6
Leonard Ehrenfried <mail@leonard.io>
2020-04-20 13:50:28 +0200

    Use correct import

--- app/component/map/popups/OSMOpeningHours.js
+++ app/component/map/popups/OSMOpeningHours.js
@@ -1,4 +1,4 @@
-import SimpleOpeningHours from 'lehrenfried-simple-opening-hours';
+import SimpleOpeningHours from 'simple-opening-hours';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, intlShape } from 'react-intl';
