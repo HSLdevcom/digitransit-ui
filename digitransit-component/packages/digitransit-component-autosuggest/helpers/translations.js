6c21504faf6ebc1c3030c9c0302cb5b425523589
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-04-21 11:39:19 +0300

    feat: add digitransit-autosuggest

---
+++ digitransit-component/packages/digitransit-component-autosuggest/helpers/translations.js
@@ -0,0 +1,96 @@
+const translations = {
+  en: {
+    'add-via-button-label': 'Add via point',
+    'add-via-duration-button-label': 'Set stop duration at via point {{index}}',
+    'add-via-duration-button-label-close':
+      'Close stop duration selector at via point {{index}}',
+    'add-via-duration-button-label-open':
+      'Open stop duration selector at via point {{index}}',
+    'clear-button-label': 'Clear',
+    destination: 'Destination',
+    origin: 'Start',
+    preferred: 'Prefer routes',
+    loading: 'Loading...',
+    'minute-short': 'm',
+    search: 'Search',
+    'search-button-label': 'Search',
+    'search-destination': 'Search destination, route or stop',
+    'search-destination-index': 'Enter destination',
+    'search-destination-results-but-no-search':
+      'View results in the adjacent “Destination” tab',
+    'search-no-results': 'No location',
+    'search-origin': 'Search origin, route or stop',
+    'search-origin-index': 'Enter origin',
+    'search-position': 'Detect location',
+    'search-autosuggest-label':
+      'Venue, place and stopsearch. Navigate list with arrow keys and select with enter key',
+    'search-autosuggest-len': 'There is {{count}} suggestion available',
+    'search-autosuggest-len_plural':
+      'There are {{count}} suggestions available',
+    // 'There {len, plural, =1 {is 1 suggestion} other {are {len} suggestions}} available',
+    'search-current-suggestion': 'Current selection: {{selection}}',
+    'use-own-position': 'Use current location',
+    where: 'Where to?',
+  },
+  fi: {
+    'add-via-button-label': 'Lisää välipiste',
+    'add-via-duration-button-label':
+      'Aseta välipisteelle {{index}} pysähdyksen kesto',
+    'add-via-duration-button-label-close':
+      'Avaa välipisteen {{index}} pysähdyksen kesto valitsin',
+    'add-via-duration-button-label-open':
+      'Sulje välipisteen {{index}} pysähdyksen kesto valitsin',
+    destination: 'Määränpää',
+    origin: 'Lähtöpaikka',
+    preferred: 'Suosi linjoja',
+    loading: 'Ladataan...',
+    'minute-short': 'min',
+    search: 'Etsi',
+    'search-button-label': 'Hae',
+    'search-destination': 'Hae määränpäätä, linjaa tai pysäkkiä',
+    'search-destination-index': 'Syötä määränpää',
+    'search-destination-results-but-no-search':
+      'Katso tulokset viereiseltä Määränpää-välilehdeltä',
+    'search-no-results': 'Ei tuloksia',
+    'search-origin': 'Hae lähtöpaikkaa, linjaa tai pysäkkiä',
+    'search-origin-index': 'Syötä lähtöpaikka',
+    'search-autosuggest-label':
+      'Paikka, linja ja pysäkkihaku. Navigoi listassa nuolinäppäimillä ja valitse enterillä',
+    'search-autosuggest-len': ' Löydettiin {{count}} ehdotus',
+    'search-autosuggest-len_plural': ' Löydettiin {{count}} ehdotusta',
+    'search-current-suggestion': 'Tämänhetkinen valinta: {{selection}}',
+    'search-position': 'Paikanna',
+    where: 'Minne mennään?',
+  },
+  sv: {
+    'add-via-button-label': 'Lägg till viapunkt',
+    'add-via-duration-button-label':
+      'Ställ in varaktighet för viapunkten {{index}}',
+    'add-via-duration-button-label-close':
+      'Stäng väljaren för varaktighet vid via-punkten {{index}}',
+    'add-via-duration-button-label-open':
+      'Öppna väljaren för varaktighet vid via-punkten {{index}}',
+    destination: 'Destination',
+    origin: 'Avfärdsplats',
+    preferred: 'Föredra rutter',
+    loading: 'Laddar...',
+    'minute-short': 'min',
+    search: 'Sök',
+    'search-button-label': 'Sök',
+    'search-destination': 'Sök destination, linje eller hållplats',
+    'search-destination-index': 'Skriv destination',
+    'search-destination-results-but-no-search':
+      'Se resultaten under fliken Destination',
+    'search-no-results': 'Inga sökresultat',
+    'search-origin': 'Sök avfärdsplats, linje eller hållplats',
+    'search-origin-index': 'Skriv avfärdsplats',
+    'search-position': 'Sök position',
+    'search-autosuggest-label':
+      'Plats, linje och hållplatssökning. Navigera listan med piltangenterna och välj med Enter-tangeten',
+    'search-autosuggest-len': 'Hittade {{count}} förslag',
+    'search-current-suggestion': 'Nuvarande val: {{selection}}',
+    where: 'Var till?',
+  },
+};
+
+export default translations;
