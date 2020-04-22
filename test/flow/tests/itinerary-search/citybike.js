0e527fa4c9f155a16f73c71677ccef33629a0398
Vesa Meskanen <vesa.meskanen@cgi.com>
2020-04-22 12:42:34 +0300

    fix: reformulate tests to survive without problematic imports

--- test/flow/tests/itinerary-search/citybike.js
+++ test/flow/tests/itinerary-search/citybike.js
@@ -1,7 +1,5 @@
-import { PREFIX_ITINERARY_SUMMARY } from '../../../../app/util/path';
-
 module.exports = {
-  '@disabled': true, // TODO: change this whenever citybike period starts/ends
+  '@disabled': false, // TODO: change this whenever citybike period starts/ends
   tags: ['citybike'],
   'Citybike tests should be disabled during winter period': browser => {
     const d = new Date();
@@ -13,18 +11,20 @@ module.exports = {
   },
 
   "Citybikes are used when it's the only modality": browser => {
-    browser.url(
-      `http://localhost:8080/${PREFIX_ITINERARY_SUMMARY}/Katajanokka%3A%3A60.16716%2C24.97992/Kauppatori%3A%3A60.16736%2C24.95171?modes=WALK%2CCITYBIKE`,
-    );
+    browser.url('http://localhost:8080/?modes=WALK%2CCITYBIKE');
+
+    browser.page.searchFields().itinerarySearch('Katajanokka', 'Kauppatori');
+
     browser.page.itinerarySummary().waitForItineraryRowOfType('citybike');
 
     browser.end();
   },
 
   'Citybikes are not used when disabled': browser => {
-    browser.url(
-      `http://localhost:8080/${PREFIX_ITINERARY_SUMMARY}/Katajanokka%3A%3A60.16716%2C24.97992/Kauppatori%3A%3A60.16736%2C24.95171?modes=WALK`,
-    );
+    browser.url('http://localhost:8080/?modes=WALK');
+
+    browser.page.searchFields().itinerarySearch('Katajanokka', 'Kauppatori');
+
     browser.page.itinerarySummary().waitForFirstItineraryRow();
 
     browser.page
