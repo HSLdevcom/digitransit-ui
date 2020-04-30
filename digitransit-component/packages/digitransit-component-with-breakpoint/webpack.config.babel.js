90a61741c72a42b44ba2c45c75b88f125e61d855
Kimmo Hujanen <Kimmo Hujanen>
2020-04-30 08:13:31 +0300

    fix: details

---
+++ digitransit-component/packages/digitransit-component-with-breakpoint/webpack.config.babel.js
@@ -0,0 +1,36 @@
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    globalObject: "typeof self !== 'undefined' ? self : this",
+    filename: 'index.generated',
+    path: __dirname,
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          configFile: false,
+          presets: [['@babel/preset-react', { useBuiltIns: true }]],
+          plugins: [
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+          ],
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  externals: [
+    {
+      react: 'umd react',
+      'react-dom': 'umd react-dom',
+    },
+  ],
+};
--- digitransit-component/packages/digitransit-component/index.mjs
+++ digitransit-component/packages/digitransit-component/index.mjs
@@ -4,4 +4,8 @@
  * @module digitransit-component
  * @summary Component library for Digitransit-ui
  */
+export { default as DTAutosuggest } from '@digitransit-component/digitransit-component-autosuggest';
+export { default as DTAutosuggestPanel } from '@digitransit-component/digitransit-component-autosuggest-panel';
 export { default as CtrlPanel } from '@digitransit-component/digitransit-component-control-panel';
+export { default as SuggestionItem } from '@digitransit-component/digitransit-suggestion-item';
+export { default as withBreakpoint } from '@digitransit-component/digitransit-with-breakpoint';
