2bd60fbebc6099f5ea297e35d7e77b7fc7e6b875
Beck-berry <trebeka98@gmail.com>
2020-04-30 10:40:10 +0200

    add roadworks to marker select popup

---
+++ app/component/map/tile-layer/RoadworksRow.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '../../Icon';
+
+export default function RoadworksRow(props) {
+  const { selectRow, properties } = props;
+
+  return (
+    <div className="no-margin">
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+      <div className="cursor-pointer select-row" onClick={selectRow}>
+        <div className="padding-vertical-normal select-row-icon">
+          <Icon img="icon-icon_roadworks" viewBox="0 0 847.15857 743.62268" />
+        </div>
+        <div className="padding-vertical-normal select-row-text">
+          <span className="header-primary no-margin link-color">
+            {properties.Baustellenbeschreibung}
+          </span>
+        </div>
+        <div className="clear" />
+      </div>
+      <hr className="no-margin gray" />
+    </div>
+  );
+}
+
+RoadworksRow.propTypes = {
+  selectRow: PropTypes.func.isRequired,
+  properties: PropTypes.object.isRequired,
+};
