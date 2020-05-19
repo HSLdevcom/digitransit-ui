44a2df17fbec60aa4b92624f8bc11849dbf41319
Joel Lappalainen <joel.lappalainen@cgi.com>
2020-05-19 16:18:59 +0300

    test: fix TripLink tests

--- test/unit/component/FuzzyTripLink.test.js
+++ test/unit/component/FuzzyTripLink.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { Component as FuzzyTripLink } from '../../../app/component/FuzzyTripLink';
-import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
+import IconWithTail from '../../../app/component/IconWithTail';
+import { mountWithIntl } from '../helpers/mock-intl-enzyme';
 
 describe('<FuzzyTripLink />', () => {
   it('should render content and icon', () => {
     const props = {
-      mode: 'bus',
       trip: {
         trip: {
           route: {
@@ -18,10 +18,18 @@ describe('<FuzzyTripLink />', () => {
           gtfsId: 'OULU:12345',
         },
       },
+      vehicle: {
+        direction: 0,
+        mode: 'bus',
+        operatingDay: '2020-05-09',
+        route: 'HSL:2550',
+        tripStartTime: '2143',
+      },
+      relayEnvironment: {},
     };
 
-    const wrapper = shallowWithIntl(<FuzzyTripLink {...props} />);
+    const wrapper = mountWithIntl(<FuzzyTripLink {...props} />);
     expect(wrapper.find('.route-now-content')).to.have.lengthOf(1);
-    expect(wrapper.find('.tail-icon')).to.have.lengthOf(1);
+    expect(wrapper.find(IconWithTail)).to.have.lengthOf(1);
   });
 });
