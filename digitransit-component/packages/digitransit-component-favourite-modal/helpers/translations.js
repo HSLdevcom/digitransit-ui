fae411e3548a3efa4e0c38bb96cdebe3f9a02597
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-07-08 15:12:53 +0300

    feat: add cancel and edit place translations

--- digitransit-component/packages/digitransit-component-favourite-modal/helpers/translations.js
+++ digitransit-component/packages/digitransit-component-favourite-modal/helpers/translations.js
@@ -1,21 +1,27 @@
 const translations = {
   en: {
+    cancel: 'Cancel',
     'choose-icon': "Select the place's icon",
     'close-favourite-modal': 'Close the favourite modal',
+    'edit-place': 'Edit place',
     'input-placeholder': "Enter place's name (voluntary)",
     save: 'Save',
     'save-place': 'Save place',
   },
   fi: {
+    cancel: 'Peruuta',
     'choose-icon': 'Valitse paikan kuvake',
     'close-favourite-modal': 'Sulje suosikkimodaali',
+    'edit-place': 'Muokkaa paikkaa',
     'input-placeholder': 'Anna paikalle nimi (vapaaehtoinen)',
     save: 'Tallenna',
     'save-place': 'Tallenna paikka',
   },
   sv: {
+    cancel: 'Avbryt',
     'choose-icon': 'Välj platsens ikon',
     'close-favourite-modal': 'Stäng favoritmodalen',
+    'edit-place': 'Redigera plats',
     'input-placeholder': 'Ange platsens namn (frivilligt)',
     save: 'Spara',
     'save-place': 'Spara plats',
