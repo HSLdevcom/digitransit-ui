b1345fba2595d744b2a654e29f6975fa23d4e174
MikkoPuustinen <mikkowr@gmail.com>
2020-07-16 17:22:58 +0300

    chore: remove redundant comment

--- test/unit/component/SummaryRow.test.js
+++ test/unit/component/SummaryRow.test.js
@@ -39,7 +39,7 @@ describe('<SummaryRow />', () => {
     });
 
     expect(wrapper.find('.itinerary-legs').children()).to.have.lengthOf(3);
-    expect(wrapper.find(ModeLeg)).to.have.lengthOf(2); // two walk legs and a wait leg
+    expect(wrapper.find(ModeLeg)).to.have.lengthOf(2);
     expect(wrapper.find(ViaLeg)).to.have.lengthOf(1);
   });
 
