d4f51b3c5cabb19b8b318ac11a26fa060b4e41e9
Leonard Ehrenfried <mail@leonard.io>
2020-07-22 11:54:14 +0200

    Override tracking URL

--- app/util/analyticsUtils.js
+++ app/util/analyticsUtils.js
@@ -34,6 +34,7 @@ export function getAnalyticsInitCode(GTMid, MATOMO_URL) {
       <script type="text/javascript">
       var _mtm = _mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+      _mtm.push({'setCustomUrl': 'https://herrenberg.stadtnavi.de'});
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.type='text/javascript'; g.async=true; g.defer=true; g.src='${MATOMO_URL}'; s.parentNode.insertBefore(g,s);
       </script>
