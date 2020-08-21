81bf5dcc936f97f6f420a9c2a7c131bcc088bef6
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-08-21 10:10:15 +0300

    fix: import path

--- digitransit-component/packages/digitransit-component-autosuggest/test.js
+++ digitransit-component/packages/digitransit-component-autosuggest/test.js
@@ -4,7 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { shallow, configure } from 'enzyme';
-import Autosuggest from '.';
+import Autosuggest from './src';
 
 configure({ adapter: new Adapter() });
 
