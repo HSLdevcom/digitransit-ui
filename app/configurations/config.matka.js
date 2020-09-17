10f2d722905a0819b624ea1f56c6e3c289241c30
Janne Antikainen <antiik91@gmail.com>
2020-09-17 14:17:18 +0300

    feat: accessibility statement

--- app/configurations/config.matka.js
+++ app/configurations/config.matka.js
@@ -90,6 +90,11 @@ export default {
         href: 'https://www.traficom.fi/fi/joukkoliikenteen-informaatiopalvelut',
         icon: 'icon-icon_info',
       },
+      {
+        name: 'accessibility-statement',
+        nameEn: 'Accessibility statement',
+        // href: 'todo',
+      },
     ],
   },
 
