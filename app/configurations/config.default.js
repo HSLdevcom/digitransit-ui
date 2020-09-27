6fb17ac6ce6131ad3846a00ea19cfbb031619ddd
Joel Lappalainen <lappalj8@gmail.com>
2020-09-27 21:31:37 +0300

    feat: matka-opas config

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -748,6 +748,7 @@ export default {
     oulu: 'oulu',
     hameenlinna: 'hameenlinna',
     matka: 'matka',
+    'waltti-opas': 'waltti',
     salo: 'salo',
     rovaniemi: 'rovaniemi',
     kouvola: 'kouvola',
