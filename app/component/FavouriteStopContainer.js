31d506d8711c036ac4e0e843f671ffb6775e9cb0
Joel Lappalainen <lappalj8@gmail.com>
2020-10-12 23:17:36 +0300

    feat: change what information is stored for favourite stops/stations

--- app/component/FavouriteStopContainer.js
+++ app/component/FavouriteStopContainer.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import connectToStores from 'fluxible-addons-react/connectToStores';
+import getJson from '@digitransit-search-util/digitransit-search-util-get-json';
+import { getStopName } from '@digitransit-search-util/digitransit-search-util-helpers';
 import Favourite from './Favourite';
 import { saveFavourite, deleteFavourite } from '../action/FavouriteActions';
 import { addAnalyticsEvent } from '../util/analyticsUtils';
@@ -12,18 +14,37 @@ const FavouriteStopContainer = connectToStores(
       .getStore('FavouriteStore')
       .isFavourite(stop.gtfsId, isTerminal ? 'station' : 'stop'),
     addFavourite: () => {
-      context.executeAction(saveFavourite, {
-        ...stop,
-        type: isTerminal ? 'station' : 'stop',
-        layer: isTerminal ? '' : 'stop',
-        address: stop.desc || '',
-      });
-      addAnalyticsEvent({
-        category: 'Stop',
-        action: 'MarkStopAsFavourite',
-        name: !context
-          .getStore('FavouriteStore')
-          .isFavourite(stop.gtfsId, isTerminal ? 'station' : 'stop'),
+      const favouriteType = isTerminal ? 'station' : 'stop';
+      let gid = `gtfs${stop.gtfsId
+        .split(':')[0]
+        .toLowerCase()}:${favouriteType}:GTFS:${stop.gtfsId}`;
+      if (stop.code) {
+        gid += `#${stop.code}`;
+      }
+      getJson(context.config.URL.PELIAS_PLACE, {
+        ids: gid,
+        lang: context.getStore('PreferencesStore').getLanguage(),
+      }).then(res => {
+        if (Array.isArray(res.features) && res.features.length > 0) {
+          const stopOrStation = res.features[0];
+          const { label, name } = stopOrStation.properties;
+          context.executeAction(saveFavourite, {
+            address: label,
+            code: stop.code,
+            gtfsId: stop.gtfsId,
+            lat: stop.lat,
+            lon: stop.lon,
+            name: getStopName(name, stop.code),
+            type: favouriteType,
+          });
+          addAnalyticsEvent({
+            category: 'Stop',
+            action: 'MarkStopAsFavourite',
+            name: !context
+              .getStore('FavouriteStore')
+              .isFavourite(stop.gtfsId, favouriteType),
+          });
+        }
       });
     },
     deleteFavourite: () => {
@@ -45,6 +66,7 @@ const FavouriteStopContainer = connectToStores(
 FavouriteStopContainer.contextTypes = {
   getStore: PropTypes.func.isRequired,
   executeAction: PropTypes.func.isRequired,
+  config: PropTypes.object.isRequired,
 };
 
 export default FavouriteStopContainer;
