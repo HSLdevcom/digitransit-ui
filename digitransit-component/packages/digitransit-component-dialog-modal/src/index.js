71b37908e408801673c3bfb3f9edbacf333590ad
Leonard Ehrenfried <mail@leonard.io>
2020-11-10 14:20:12 +0100

    Automatically apply all translations to i18n

--- digitransit-component/packages/digitransit-component-dialog-modal/src/index.js
+++ digitransit-component/packages/digitransit-component-dialog-modal/src/index.js
@@ -10,10 +10,9 @@ import translations from './helpers/translations';
 
 i18next.init({ lng: 'fi', resources: {} });
 
-i18next.addResourceBundle('de', 'translation', translations.de);
-i18next.addResourceBundle('en', 'translation', translations.en);
-i18next.addResourceBundle('fi', 'translation', translations.fi);
-i18next.addResourceBundle('sv', 'translation', translations.sv);
+Object.keys(translations).forEach(lang => {
+  i18next.addResourceBundle(lang, 'translation', translations[lang]);
+});
 
 /**
  * General component description in JSDoc format. Markdown is *supported*.
