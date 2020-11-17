fbc5e79cde2397aa4a84209de700cafcff86f389
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-11-17 10:20:04 +0200

    feat: use same webpack for all digitransit-components

---
+++ digitransit-component/packages/webpack.config.babel.js
@@ -0,0 +1,97 @@
+const path = require('path');
+
+const mode = process.env.ENV;
+const name = process.env.NAME;
+const isProduction = mode === 'production';
+
+module.exports = {
+  mode,
+  devtool: isProduction ? 'source-map' : 'eval',
+  output: {
+    globalObject: "typeof self !== 'undefined' ? self : this",
+    filename: 'index.js',
+    path: path.join(__dirname, name, 'lib'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          configFile: false,
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: 'auto',
+              },
+            ],
+            ['@babel/preset-react', { useBuiltIns: true }],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                helpers: true,
+                regenerator: true,
+                useESModules: true,
+              },
+            ],
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            ['@babel/plugin-proposal-numeric-separator', { loose: true }],
+            ['inline-react-svg'],
+          ],
+        },
+      },
+      {
+        test: /.s(a|c)ss$/,
+        use: [
+          'iso-morphic-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    namedModules: true,
+    namedChunks: true,
+    removeAvailableModules: true,
+    flagIncludedChunks: true,
+    occurrenceOrder: false,
+    usedExports: true,
+    concatenateModules: true,
+    sideEffects: false,
+    splitChunks: {
+      cacheGroups: {
+        hslFi: {
+          test: /[\\/]node_modules[\\/]@hsl-fi[\\/]/,
+          name: 'hsl-fi',
+          chunks: isProduction ? 'all' : 'async',
+          reuseExistingChunk: false,
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.scss'],
+  },
+  externals: [
+    {
+      react: 'umd react',
+      'react-dom': 'umd react-dom',
+    },
+  ],
+  target: 'node',
+};
