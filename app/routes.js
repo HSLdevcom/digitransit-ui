12b4a57f135da33746266e692bc5ecf27cb8ae3a
Joel Lappalainen <lappalj8@gmail.com>
2020-11-23 19:09:23 +0200

    feat: redirect from /from/to with selected locations to itinerary page

--- app/routes.js
+++ app/routes.js
@@ -39,6 +39,46 @@ export const historyMiddlewares = [queryMiddleware];
 export const render = createRender({});
 
 export default config => {
+  const indexPageComponents = {
+    title: (
+      <Route
+        getComponent={() =>
+          import(/* webpackChunkName: "itinerary" */ './component/Title').then(
+            getDefault,
+          )
+        }
+      />
+    ),
+    content: (
+      <Route
+        getComponent={() =>
+          import(
+            /* webpackChunkName: "itinerary" */ './component/IndexPage'
+          ).then(getDefault)
+        }
+      />
+    ),
+    meta: (
+      <Route
+        getComponent={() =>
+          import(
+            /* webpackChunkName: "itinerary" */ './component/IndexPageMeta'
+          ).then(getDefault)
+        }
+      />
+    ),
+    map: (
+      <Route
+        // TODO: Must be decided how we will handle selecting from map!
+        disableMapOnMobile
+        getComponent={() =>
+          import(
+            /* webpackChunkName: "itinerary" */ './component/map/IndexPageMap.js'
+          ).then(getDefault)
+        }
+      />
+    ),
+  };
   return (
     <Route Component={TopLevel}>
       {getStopRoutes()}
@@ -155,6 +195,10 @@ export default config => {
           ),
         }}
       </Route>
+      <Redirect
+        from={`/${PREFIX_ITINERARY_SUMMARY}/:from`}
+        to={`${config.indexPath === '' ? '' : `/${config.indexPath}`}/:from`}
+      />
       <Route
         path={`/${PREFIX_ITINERARY_SUMMARY}/POS/:to`}
         getComponent={() =>
@@ -393,52 +437,41 @@ export default config => {
         }}
       </Route>
       <Route
-        path={`${
-          config.indexPath === '' ? '' : `/${config.indexPath}`
-        }/:from?/:to?`}
+        path={config.indexPath === '' ? '/' : `/${config.indexPath}`}
         topBarOptions={{ disableBackButton: true }}
       >
-        {{
-          title: (
-            <Route
-              getComponent={() =>
-                import(
-                  /* webpackChunkName: "itinerary" */ './component/Title'
-                ).then(getDefault)
-              }
-            />
-          ),
-          content: (
-            <Route
-              getComponent={() =>
-                import(
-                  /* webpackChunkName: "itinerary" */ './component/IndexPage'
-                ).then(getDefault)
-              }
-            />
-          ),
-          meta: (
-            <Route
-              getComponent={() =>
-                import(
-                  /* webpackChunkName: "itinerary" */ './component/IndexPageMeta'
-                ).then(getDefault)
-              }
-            />
-          ),
-          map: (
-            <Route
-              // TODO: Must be decided how we will handle selecting from map!
-              disableMapOnMobile
-              getComponent={() =>
-                import(
-                  /* webpackChunkName: "itinerary" */ './component/map/IndexPageMap.js'
-                ).then(getDefault)
-              }
-            />
-          ),
-        }}
+        {indexPageComponents}
       </Route>
+      <Route
+        path={`${config.indexPath === '' ? '' : `/${config.indexPath}`}/:from`}
+        topBarOptions={{ disableBackButton: true }}
+      >
+        {indexPageComponents}
+      </Route>
+      <Route
+        path={`${
+          config.indexPath === '' ? '' : `/${config.indexPath}`
+        }/:from/-`}
+        topBarOptions={{ disableBackButton: true }}
+      >
+        {indexPageComponents}
+      </Route>
+      <Route
+        path={`${config.indexPath === '' ? '' : `/${config.indexPath}`}/-/:to`}
+        topBarOptions={{ disableBackButton: true }}
+      >
+        {indexPageComponents}
+      </Route>
+      <Redirect
+        from="/:from/:to"
+        to={`/${PREFIX_ITINERARY_SUMMARY}/:from/:to`}
+      />
+      {config.indexPath !== '' && (
+        <Redirect
+          from={`/${config.indexPath}/:from/:to`}
+          to={`/${PREFIX_ITINERARY_SUMMARY}/:from/:to`}
+        />
+      )}
       <Route path="/?mock" topBarOptions={{ disableBackButton: true }}>
         {{
           title: (
