ac3207d03cca411fb18d4851126876ab3b31296a
Vesa Meskanen <vesa.meskanen@cgi.com>
2020-11-27 17:46:56 +0200

    fix: restore buildinfo

--- app/buildInfo.js
+++ app/buildInfo.js
@@ -1,2 +1,2 @@
-export const COMMIT_ID = "ba4738918a899fe64c2cb764ede8b0ceda0af82d";
-export const BUILD_TIME = "2020-11-27T14:38+00:00";
+export const COMMIT_ID = 'unset';
+export const BUILD_TIME = 'unset';
