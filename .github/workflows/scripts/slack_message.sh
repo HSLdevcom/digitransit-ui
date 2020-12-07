3da794ec6450ebd73cb8ce7579a051af8378617d
Janne Antikainen <antiik91@gmail.com>
2020-12-07 08:40:01 +0200

    ci: fix typo on script

--- .github/workflows/scripts/slack_message.sh
+++ .github/workflows/scripts/slack_message.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -X POST -H 'Content-type: application/json' --data '{"username":"npm publisher","text":"'$PUBLISHED_PACKAGES'\n", "channel": "reittiopas-git"}' ${SLACK_WEBHOOK_URL}
+curl -X POST -H 'Content-type: application/json' --data '{"username":"npm publisher","text":"'${PUBLISHED_PACKAGES}'\n", "channel": "reittiopas-git"}' ${SLACK_WEBHOOK_URL}
