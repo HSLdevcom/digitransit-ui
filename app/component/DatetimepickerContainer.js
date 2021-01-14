816d068647677febc39ac5041c6801a2275864f6
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2021-01-14 10:26:44 +0200

    feat: placeholder code for AB testing

--- app/component/DatetimepickerContainer.js
+++ app/component/DatetimepickerContainer.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { matchShape, routerShape } from 'found';
 import debounce from 'lodash/debounce';
 import { connectToStores } from 'fluxible-addons-react';
-import Datetimepicker from '@digitransit-component/digitransit-component-datetimepicker';
+// TODO use this again once AB testing is done
+// import Datetimepicker from '@digitransit-component/digitransit-component-datetimepicker';
+import DatetimepickerA from 'datetimepicker-A';
+import DatetimepickerB from 'datetimepicker-B';
 import { replaceQueryParams } from '../util/queryUtils';
 import { addAnalyticsEvent } from '../util/analyticsUtils';
+
+// placeholder for AB testing
+
+const testVariant = 'A';
+const Datetimepicker = testVariant === 'A' ? DatetimepickerA : DatetimepickerB;
 
 function DatetimepickerContainer(
   { realtime, embedWhenClosed, lang, color },
