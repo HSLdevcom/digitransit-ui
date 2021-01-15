78283b8705983f0269d5d17b2a9a0f70e4d831cc
Joel Lappalainen <lappalj8@gmail.com>
2021-01-15 20:09:50 +0200

    feat: use route color for links in top menu disruptions listing

--- app/component/RouteAlertsRow.js
+++ app/component/RouteAlertsRow.js
@@ -70,6 +70,7 @@ export default function RouteAlertsRow(
             key={gtfsIdList[i]}
             to={`/${PREFIX_ROUTES}/${gtfsIdList[i]}/${PREFIX_STOPS}/${gtfsIdList[i]}:0:01`}
             className="route-alert-row-link"
+            style={{ color }}
           >
             {' '}
             {identifier}{' '}
