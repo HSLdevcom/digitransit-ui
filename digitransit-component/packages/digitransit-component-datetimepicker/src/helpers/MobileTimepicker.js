32c7d1043c86616e9c2f15bb7c62cb1904caa3c1
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2021-02-09 15:24:23 +0200

    feat: don't select input text onfocus, instead clear input when typing from start

--- digitransit-component/packages/digitransit-component-datetimepicker/src/helpers/MobileTimepicker.js
+++ digitransit-component/packages/digitransit-component-datetimepicker/src/helpers/MobileTimepicker.js
@@ -39,11 +39,24 @@ function MobileTimepicker({
           id={inputId}
           inputMode="numeric"
           type="text"
-          maxLength="5"
+          maxLength="6"
           className={styles['time-input-mobile']}
           value={inputValue}
+          onFocus={e => {
+            e.target.setSelectionRange(0, 0); // set caret to start of input
+          }}
           onChange={event => {
-            const newValue = event.target.value;
+            let newValue = event.target.value;
+            if (
+              // number typed as first char => clear rest of the input
+              newValue.match(/[0-9]{3}:[0-9]{2}/) &&
+              newValue.slice(1) === inputValue
+            ) {
+              newValue = newValue.charAt(0);
+            }
+            if (newValue.length > 5) {
+              return;
+            }
             const actual = utils.parseTypedTime(newValue);
             changeInputValue(actual);
             const timestamp = validate(actual, value);
