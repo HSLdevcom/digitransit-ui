e6decc1c16d1d09ffe1e34e2cf62c7af8440da93
Aleksi Siitari <aleksi.siitari@gmail.com>
2021-02-18 10:38:06 +0200

    feat: scroll into tracked vehicle marker

--- app/component/PatternLink.js
+++ app/component/PatternLink.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import Link from 'found/Link';
 import IconWithTail from './IconWithTail';
 import SelectedIconWithTail from './SelectedIconWithTail';
@@ -12,13 +12,27 @@ function PatternLink({
   vehicleNumber,
   selected = false,
 }) {
+  const trackedVehicleRef = useRef();
+
+  useEffect(() => {
+    if (selected) {
+      trackedVehicleRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'center',
+      });
+    }
+  }, []);
+
   const imgName = `icon-icon_${mode}-live`;
   const icon = (selected && (
-    <SelectedIconWithTail
-      img={imgName}
-      mode={mode}
-      vehicleNumber={vehicleNumber}
-    />
+    <div ref={trackedVehicleRef}>
+      <SelectedIconWithTail
+        img={imgName}
+        mode={mode}
+        vehicleNumber={vehicleNumber}
+      />
+    </div>
   )) || (
     <IconWithTail
       desaturate
