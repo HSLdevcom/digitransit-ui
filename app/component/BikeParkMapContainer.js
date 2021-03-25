f88b31de826ff4612c75575986765616adba541b
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-03-25 15:53:45 +0200

    feat: add BikeParkMapContainer

---
+++ app/component/BikeParkMapContainer.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { createFragmentContainer, graphql } from 'react-relay';
+import StopPageMap from './StopPageMap';
+
+const BikeParkPageMapContainer = ({ bikePark }) => {
+  if (!bikePark) {
+    return false;
+  }
+  return <StopPageMap stop={bikePark} />;
+};
+
+BikeParkPageMapContainer.contextTypes = {
+  config: PropTypes.object.isRequired,
+};
+
+BikeParkPageMapContainer.propTypes = {
+  bikePark: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lon: PropTypes.number.isRequired,
+    name: PropTypes.string,
+  }),
+};
+
+BikeParkPageMapContainer.defaultProps = {
+  bikePark: undefined,
+};
+
+const containerComponent = createFragmentContainer(BikeParkPageMapContainer, {
+  bikePark: graphql`
+    fragment BikeParkMapContainer_bikePark on BikePark {
+      lat
+      lon
+      name
+    }
+  `,
+});
+
+export { containerComponent as default, BikeParkPageMapContainer as Component };
