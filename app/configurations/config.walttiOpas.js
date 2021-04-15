f05aa908f9923fcbd666ca94f22f649b98439c96
Joel Lappalainen <lappalj8@gmail.com>
2021-04-15 16:09:34 +0300

    chore: footer -> menu in config

--- app/configurations/config.walttiOpas.js
+++ app/configurations/config.walttiOpas.js
@@ -63,15 +63,20 @@ export default configMerger(walttiConfig, {
     },
   ],
 
-  footer: {
+  menu: {
+    copyright: { label: `© TVV lippu- ja maksujärjestelmä Oy ${walttiConfig.YEAR}` },
     content: [
-      { label: `© TVV lippu- ja maksujärjestelmä Oy ${walttiConfig.YEAR}` },
-      {},
       {
         name: 'about-this-service',
         nameEn: 'About this service',
         route: '/tietoja-palvelusta',
         icon: 'icon-icon_info',
+      },
+      {
+        name: 'accessibility-statement',
+        nameEn: 'Accessibility statement',
+        href:
+          'https://kauppa.waltti.fi/media/authority/154/files/Saavutettavuusseloste_Waltti-reittiopas_JyQfJhC.htm',
       },
     ],
   },
