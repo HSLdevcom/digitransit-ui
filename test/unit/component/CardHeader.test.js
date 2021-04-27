a8cbf319173a3d48c08b5e45037c90c513c0d0b1
Anton <anton.jyrkiainen@helsinki.fi>
2021-04-27 18:54:14 +0300

    fix: fixed test

--- test/unit/component/CardHeader.test.js
+++ test/unit/component/CardHeader.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
 import CardHeader from '../../../app/component/CardHeader';
 import ExternalLink from '../../../app/component/ExternalLink';
+import AddressRow from '../../../app/component/AddressRow';
 
 describe('<CardHeader />', () => {
   it('should render the header icon', () => {
@@ -15,15 +16,16 @@ describe('<CardHeader />', () => {
     expect(wrapper.find('.header-icon')).to.have.lengthOf(1);
   });
   it('should render the station code', () => {
-    // const props = {
-    //   description: 'Ratapihantie',
-    //   headerIcon: <div className="header-icon" />,
-    //   name: 'Pasilan asema',
-    //   code: '7528',
-    //   network: 'citybike',
-    // };
-    // const wrapper = shallowWithIntl(<CardHeader {...props} />);
-    // expect(wrapper.find('.card-code').text()).to.equal('7528');
+    const props = {
+      description: 'Ratapihantie',
+      headerIcon: <div className="header-icon" />,
+      name: 'Pasilan asema',
+      code: '7528',
+      network: 'citybike',
+    };
+    const wrapper = shallowWithIntl(<CardHeader {...props} />);
+    const addressRow = wrapper.find(AddressRow);
+    expect(addressRow.length).to.equal(1);
   });
   it('should  render the virtual monitor if so configured', () => {
     const props = {
