855b78caf5be65650bf36ad9379e585829fbcbc3
Patrick Beck <patrick.beck@coyoapp.com>
2021-05-08 11:08:37 +0200

    Fix errors in console

--- app/component/BikeRentalStationHeader.js
+++ app/component/BikeRentalStationHeader.js
@@ -29,7 +29,7 @@ const BikeRentalStationHeader = (
       if (data.features != null && data.features.length > 0) {
         const match = data.features[0].properties;
         const id = getZoneId(config, match.zones, data.zones);
-        setZoneId(id.toString().toLowerCase());
+        setZoneId(id?.toString().toLowerCase());
       }
     });
   }, []);
