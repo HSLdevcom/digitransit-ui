799cc9b8146b77647d3b253f6d90b3bc2f3064a5
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-06-02 15:39:25 +0300

    test: mobile visual test

--- test/e2e/helpers/image-snapshot-config.js
+++ test/e2e/helpers/image-snapshot-config.js
@@ -5,7 +5,6 @@ const getConfig = (
 ) => {
   return {
     diffDirection: 'vertical',
-    // useful on CI (no need to retrieve the diff image, copy/paste image content from logs)
     dumpDiffToConsole: false,
     comparisonMethod: 'pixelmatch',
     failureThreshold: 0.01,
--- test/e2e/visual.test.js
+++ test/e2e/visual.test.js
@@ -4,9 +4,13 @@ import getConfig from './helpers/image-snapshot-config';
 const customSnapshotsDir = `test/e2e/__image_snapshots__`;
 const customDiffDir = `test/e2e/__image_snapshots__/__diff_output__`;
 
-describe('Frontpage', () => {
-  it(`Desktop visual regression`, async () => {
-    const snapshotName = 'front-page';
+describe('Front page', () => {
+  test(`on desktop`, async () => {
+    context = await browser.newContext({
+      viewport: { width: 1360, height: 768 },
+    });
+    page = await context.newPage();
+    const snapshotName = 'front-page-desktop';
     const response = await page.goto('http://localhost:8080/etusivu');
     expect(response.status()).toBe(200);
 
@@ -15,6 +19,24 @@ describe('Frontpage', () => {
     const mainContent = await page.$('.main-content');
     const image = await mainContent.screenshot({ fullPage: true });
 
+    const config = getConfig(
+      snapshotName,
+      `${customSnapshotsDir}/${browserName}/`,
+      `${customDiffDir}/${browserName}/`,
+    );
+    expect(image).toMatchImageSnapshot(config);
+  });
+
+  test('on mobile', async () => {
+    context = await browser.newContext({
+      viewport: { width: 414, height: 715 }, // iPhone 11 viewport sizes
+    });
+    page = await context.newPage();
+    const snapshotName = `front-page-mobile`;
+    const response = await page.goto('http://localhost:8080/etusivu');
+    expect(response.status()).toBe(200);
+    await expect(page.title()).resolves.toMatch('Reittiopas');
+    const image = await page.screenshot({ fullPage: true });
     const config = getConfig(
       snapshotName,
       `${customSnapshotsDir}/${browserName}/`,
