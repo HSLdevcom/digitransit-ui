dbc96a901d0a5f3794d056065af857b6e78ca028
Joel Lappalainen <lappalj8@gmail.com>
2021-06-08 11:05:29 +0300

    feat: add vehicles for walttiOpas (Salo)

--- app/configurations/realtimeUtils.js
+++ app/configurations/realtimeUtils.js
@@ -282,4 +282,17 @@ export default {
 
     active: true,
   },
+  Salo: {
+    mqttTopicResolver: walttiTopicResolver,
+
+    mqtt: 'wss://mqtt.waltti.fi/mqtt',
+
+    credentials: { username: 'user', password: 'userpass' },
+
+    gtfsrt: true,
+
+    routeSelector: defaultRouteSelector,
+
+    active: true,
+  },
 };
