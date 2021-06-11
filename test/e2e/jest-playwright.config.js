1e4a5fe29ba918556041ac7e1c6cfb7cf8fd7611
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-06-11 14:50:56 +0300

    chore: run dev server on no watch

--- test/e2e/jest-playwright.config.js
+++ test/e2e/jest-playwright.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   serverOptions: {
-    command: `CONFIG=hsl yarn dev`,
+    command: `CONFIG=hsl yarn dev-nowatch`,
     port: 8080,
     launchTimeout: 200000,
     debug: true,
