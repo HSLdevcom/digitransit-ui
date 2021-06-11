3426623aab28918352c682783c46d0413575f228
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-06-11 14:34:46 +0300

    chore: increase launch timeout

--- test/e2e/jest-playwright.config.js
+++ test/e2e/jest-playwright.config.js
@@ -2,8 +2,7 @@ module.exports = {
   serverOptions: {
     command: `CONFIG=hsl yarn dev`,
     port: 8080,
-    protocol: 'http',
-    launchTimeout: 30000,
+    launchTimeout: 200000,
     debug: true,
     usedPortAction: 'ignore',
   },
