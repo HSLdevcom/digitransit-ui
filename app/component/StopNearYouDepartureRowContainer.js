eb60c028e0b0f9dc34c532707932cf65febb3862
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2021-06-22 17:43:16 +0300

    feat: mark departure list as table for stop, terminal and near you pages

--- app/component/StopNearYouDepartureRowContainer.js
+++ app/component/StopNearYouDepartureRowContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import DepartureRow from './DepartureRow';
 
-const StopNearYouDepartureRowContainer = ({ stopTimes, ...props }) => {
+const StopNearYouDepartureRowContainer = ({ stopTimes, mode, ...props }) => {
   const sortedStopTimes = stopTimes
     .slice()
     .sort(
@@ -13,19 +14,39 @@ const StopNearYouDepartureRowContainer = ({ stopTimes, ...props }) => {
   const departures = sortedStopTimes.map(row => {
     const departureTime = row.serviceDay + row.realtimeArrival;
     return (
-      <DepartureRow
-        key={uuid()}
-        departure={row}
-        departureTime={departureTime}
-        currentTime={props.currentTime}
-        showPlatformCode={props.isStation}
-      />
+      <tr key={uuid()}>
+        <DepartureRow
+          departure={row}
+          departureTime={departureTime}
+          currentTime={props.currentTime}
+          showPlatformCode={props.isStation}
+        />
+      </tr>
     );
   });
   return (
-    <div role="list" className="near-departures-container">
-      {departures}
-    </div>
+    <table className="near-departures-container">
+      <thead className="sr-only">
+        <tr>
+          <th>
+            <FormattedMessage id="route" defaultMessage="Route" />
+          </th>
+          <th>
+            <FormattedMessage id="destination" defaultMessage="Destination" />
+          </th>
+          <th>
+            <FormattedMessage id="leaving-at" defaultMessage="Leaves" />
+          </th>
+          <th>
+            <FormattedMessage
+              id={mode === 'BUS' ? 'platform' : 'track'}
+              defaultMessage={mode === 'BUS' ? 'Platform' : 'Track'}
+            />
+          </th>
+        </tr>
+      </thead>
+      <tbody>{departures}</tbody>
+    </table>
   );
 };
 
@@ -35,6 +56,7 @@ StopNearYouDepartureRowContainer.propTypes = {
       distance: PropTypes.number,
     }),
   ),
+  mode: PropTypes.string.isRequired,
   isStation: PropTypes.bool.isRequired,
   currentTime: PropTypes.number.isRequired,
 };
