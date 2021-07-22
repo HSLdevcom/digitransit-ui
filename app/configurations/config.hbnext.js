5cecadeef9bf1ec94950369e7ceb61072520fa0b
Leonard Ehrenfried <mail@leonard.io>
2021-07-22 17:13:36 +0200

    Use proxied charging layer URL

--- app/configurations/config.hbnext.js
+++ app/configurations/config.hbnext.js
@@ -41,7 +41,7 @@ export default configMerger(walttiConfig, {
         CITYBIKE_MAP: `${API_URL}/routing/v1/router/vectorTiles/citybikes/`,
         BIKE_PARKS_MAP: `${API_URL}/routing/v1/router/vectorTiles/parking/`,
         WEATHER_STATIONS_MAP: `${API_URL}/map/v1/weather-stations/`,
-        CHARGING_STATIONS_MAP: `https://ochp.next-site.de/tiles/`,
+        CHARGING_STATIONS_MAP: `${API_URL}/tiles/charging-stations/`,
         PELIAS: `${process.env.GEOCODING_BASE_URL || GEOCODING_BASE_URL}/search`,
         PELIAS_REVERSE_GEOCODER: `${
             process.env.GEOCODING_BASE_URL || GEOCODING_BASE_URL
