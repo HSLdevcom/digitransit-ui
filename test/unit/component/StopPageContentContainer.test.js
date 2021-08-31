fb91a98fa34b9dde93b119dd2d0ec8cf0d7eb7b4
Joel Lappalainen <lappalj8@gmail.com>
2021-08-31 19:14:29 +0300

    test: update test

--- test/unit/component/StopPageContentContainer.test.js
+++ test/unit/component/StopPageContentContainer.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
+import { mockRouter } from '../helpers/mock-router';
 import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
 import { Component as StopPageContentContainer } from '../../../app/component/StopPageContentContainer';
 
@@ -16,6 +17,7 @@ describe('<StopPageContentContainer />', () => {
         refetch: () => {},
       },
       stop: {},
+      router: mockRouter,
     };
     const wrapper = shallowWithIntl(<StopPageContentContainer {...props} />);
     expect(wrapper.find('.stop-no-departures-container')).to.have.lengthOf(1);
