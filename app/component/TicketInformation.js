d4a71249a2ba33433b66a35f37f626dad3fc9786
Aleksi <aleksi.siitari@gmail.com>
2021-09-09 16:01:37 +0300

    fix: hiding logic for ticketinfo button

--- app/component/TicketInformation.js
+++ app/component/TicketInformation.js
@@ -101,28 +101,50 @@ export default function TicketInformation(
             )}
           </div>
         </div>
-        {fare.agency &&
-          fare.agency.fareUrl &&
-          !config.hideExternalOperator(fare.agency) && (
-            <div
-              className="ticket-type-agency-link"
-              key={i} // eslint-disable-line react/no-array-index-key
-            >
-              <ExternalLink
-                className="itinerary-ticket-external-link"
-                href={fare.agency.fareUrl}
-                onClick={() => {
-                  addAnalyticsEvent({
-                    category: 'Itinerary',
-                    action: 'OpenHowToBuyTicket',
-                    name: null,
-                  });
-                }}
+        {fare.isUnknown
+          ? fare.agency &&
+            fare.agency.fareUrl &&
+            !config.hideExternalOperator(fare.agency) && (
+              <div
+                className="ticket-type-agency-link"
+                key={i} // eslint-disable-line react/no-array-index-key
               >
-                {intl.formatMessage({ id: 'extra-info' })}
-              </ExternalLink>
-            </div>
-          )}
+                <ExternalLink
+                  className="itinerary-ticket-external-link"
+                  href={fare.agency.fareUrl}
+                  onClick={() => {
+                    addAnalyticsEvent({
+                      category: 'Itinerary',
+                      action: 'OpenHowToBuyTicket',
+                      name: null,
+                    });
+                  }}
+                >
+                  {intl.formatMessage({ id: 'extra-info' })}
+                </ExternalLink>
+              </div>
+            )
+          : fare.agency &&
+            fare.agency.fareUrl && (
+              <div
+                className="ticket-type-agency-link"
+                key={i} // eslint-disable-line react/no-array-index-key
+              >
+                <ExternalLink
+                  className="itinerary-ticket-external-link"
+                  href={fare.agency.fareUrl}
+                  onClick={() => {
+                    addAnalyticsEvent({
+                      category: 'Itinerary',
+                      action: 'OpenHowToBuyTicket',
+                      name: null,
+                    });
+                  }}
+                >
+                  {intl.formatMessage({ id: 'extra-info' })}
+                </ExternalLink>
+              </div>
+            )}
         {config.ticketLink && (
           <ExternalLink
             className="itinerary-ticket-external-link"
