fe8376655f3289ff7426331b7f92c691f26dac05
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-09-13 15:39:16 +0300

    chore: mock current time for tests

--- app/store/TimeStore.js
+++ app/store/TimeStore.js
@@ -6,15 +6,22 @@ class TimeStore extends Store {
 
   static TWICE_PER_MINUTE = 30 * 1000;
 
+  config = {};
+
   constructor(dispatcher) {
     super(dispatcher);
+    this.config = dispatcher.getContext().config;
     this.updateCurrentTime();
     setInterval(this.updateCurrentTime, TimeStore.TWICE_PER_MINUTE);
   }
 
   updateCurrentTime = () => {
-    this.currentTime = moment();
-
+    if (this.config.NODE_ENV === 'test') {
+      // Set current time to Fri Sep 10 2021 for E2E-tests
+      this.currentTime = moment(1631221200);
+    } else {
+      this.currentTime = moment();
+    }
     this.emitChange({
       currentTime: this.currentTime,
     });
