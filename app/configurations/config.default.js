a6da10449d7d48e9542c758b89438bb51f426168
Marco Metz <mm@tpwd.de>
2021-09-24 10:59:55 +0200

    Disabling Weather data
    api call to https://opendata.fmi.fi disabled

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -53,8 +53,7 @@ export default {
     STOP_TIMETABLES: {
       HSL: `${API_URL}/timetables/v1/hsl/stops/`,
     },
-    WEATHER_DATA:
-      'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&timestep=5&parameters=temperature,WindSpeedMS,WeatherSymbol3',
+    // WEATHER_DATA: 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&timestep=5&parameters=temperature,WindSpeedMS,WeatherSymbol3',
   },
 
   APP_PATH: `${APP_PATH}`,
