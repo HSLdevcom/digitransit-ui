cfe11ca6598cef76f583d2cf878531284b3786f2
Janne Antikainen <antiik91@gmail.com>
2021-10-19 11:54:41 +0300

    fix: crash problem

--- digitransit-component/packages/digitransit-component-autosuggest/src/index.js
+++ digitransit-component/packages/digitransit-component-autosuggest/src/index.js
@@ -77,7 +77,8 @@ function getSuggestionContent(item) {
       (item.properties.layer === 'stop' || item.properties.layer === 'station')
     ) {
       const stopCode = getStopCode(item.properties);
-      const mode = item.properties.addendum.GTFS.modes;
+
+      const mode = item.properties.addendum?.GTFS.modes;
       return [
         suggestionType,
         getStopName(name, stopCode),
