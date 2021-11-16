67ac881610fd3293a322c11e38673092b46d7979
Aleksi <aleksi.siitari@gmail.com>
2021-11-16 11:35:22 +0200

    fix: exclude routeNotifications in e2e tests

--- app/component/TransitLeg.js
+++ app/component/TransitLeg.js
@@ -280,7 +280,11 @@ class TransitLeg extends React.Component {
     }
 
     const routeNotifications = [];
-    if (config.routeNotifications && config.routeNotifications.length > 0) {
+    if (
+      config.NODE_ENV !== 'test' &&
+      config.routeNotifications &&
+      config.routeNotifications.length > 0
+    ) {
       for (let i = 0; i < config.routeNotifications.length; i++) {
         const notification = config.routeNotifications[i];
         if (notification.showForRoute(leg.route.gtfsId)) {
