7a7563e928ac7197970366c6152718bea4be1a87
MikkoPuustinen <mikkowr@gmail.com>
2022-02-01 01:42:15 +0200

    feat: waltti language menu aria descriptions

--- app/component/LangSelect.js
+++ app/component/LangSelect.js
@@ -1,26 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { intlShape } from 'react-intl';
 import { matchShape, routerShape } from 'found';
 import connectToStores from 'fluxible-addons-react/connectToStores';
 import { isBrowser } from '../util/browser';
 
-const language = (lang, highlight, match) => (
-  <a
-    id={`lang-${lang}`}
-    key={lang}
-    href={`/${lang}${match.location.pathname}${match.location.search}`}
-    className={`${(highlight && 'selected') || ''} noborder lang`}
-  >
-    {lang}
-  </a>
-);
+const language = (lang, highlight, match, intl) => {
+  const aria = highlight
+    ? intl.formatMessage(
+        { id: 'search-current-suggestion' },
+        { selection: lang },
+      )
+    : intl.formatMessage({ id: 'language-selection' }, { language: lang });
+  return (
+    <a
+      id={`lang-${lang}`}
+      aria-label={aria}
+      key={lang}
+      href={`/${lang}${match.location.pathname}${match.location.search}`}
+      className={`${(highlight && 'selected') || ''} noborder lang`}
+    >
+      {lang}
+    </a>
+  );
+};
 
-const LangSelect = ({ currentLanguage }, { config, match }) => {
+const LangSelect = ({ currentLanguage }, { config, match, intl }) => {
   if (isBrowser) {
     return (
       <div key="lang-select" id="lang-select">
         {config.availableLanguages.map(lang =>
-          language(lang, lang === currentLanguage, match),
+          language(lang, lang === currentLanguage, match, intl),
         )}
       </div>
     );
@@ -39,6 +49,7 @@ LangSelect.contextTypes = {
   config: PropTypes.object.isRequired,
   router: routerShape.isRequired,
   match: matchShape.isRequired,
+  intl: intlShape.isRequired,
 };
 
 const connected = connectToStores(
