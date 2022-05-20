72a4f1e41b1df2f0e82d0ada66500da66e895fd1
MikkoPuustinen <mikkowr@gmail.com>
2022-05-20 12:46:41 +0300

    chore: translations and stop page alerts

--- app/component/StopAlertsContainer.js
+++ app/component/StopAlertsContainer.js
@@ -52,6 +52,7 @@ const containerComponent = createFragmentContainer(StopAlertsContainer, {
         shortName
         longName
         mode
+        type
         color
         alerts {
           id
