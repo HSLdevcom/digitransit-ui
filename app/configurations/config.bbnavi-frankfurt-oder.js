5fff99e4bde0d91019701ae7b976add2c9d3b5a0
Jannis R <mail@jannisr.de>
2022-07-07 12:28:50 +0200

    add new frankfurt-oder.bbnavi.de instance

---
+++ app/configurations/config.bbnavi-frankfurt-oder.js
@@ -0,0 +1,37 @@
+import configMerger from '../util/configMerger';
+import bbnaviConfig from './config.bbnavi';
+
+const CONFIG = 'bbnavi-frankfurt-oder';
+const APP_TITLE = 'bbnavi Frankfurt Oder';
+const HEADER_TITLE = 'Frankfurt Oder';
+
+export default configMerger(bbnaviConfig, {
+  CONFIG,
+
+  URL: {
+    HEADER_TITLE,
+  },
+
+  map: {
+    zoom: 18,
+  },
+
+  socialMedia: {
+    title: APP_TITLE,
+  },
+
+  title: APP_TITLE,
+  appBarTitle: HEADER_TITLE,
+
+  searchParams: {
+    'focus.point.lat': 52.3424526,
+    'focus.point.lon': 14.5524818,
+  },
+
+  defaultEndpoint: {
+    lat: 52.3424526,
+    lon: 14.5524818,
+  },
+
+  defaultOrigins: [],
+});
