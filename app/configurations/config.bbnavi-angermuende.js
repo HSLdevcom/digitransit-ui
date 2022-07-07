6fd46376daefe22aae2274efc9c6b7f71f54dc65
Philipp Wilimzig <p.wilimzig@ikusei.de>
2022-07-07 15:27:07 +0200

    New Matomo-Container-ID for Angermuende

--- app/configurations/config.bbnavi-angermuende.js
+++ app/configurations/config.bbnavi-angermuende.js
@@ -4,7 +4,7 @@ import bbnaviConfig from './config.bbnavi';
 const CONFIG = 'bbnavi-angermuende';
 const APP_TITLE = 'bbnavi Angermünde';
 const HEADER_TITLE = 'Angermünde';
-const MATOMO_URL = 'https://nutzung.bbnavi.de/js/container_zrAgG1hE.js';
+const MATOMO_URL = 'https://nutzung.bbnavi.de/js/container_4BRoKipH.js';
 
 export default configMerger(bbnaviConfig, {
   CONFIG,
