83c3b4bfcba3d4bda4e91082e7d0be7315adee6e
Jannis R <mail@jannisr.de>
2022-11-16 00:53:15 +0100

    bbnavi config: add config.URL.CHARGING_STATION_DETAILS_API

--- app/configurations/config.bbnavi.js
+++ app/configurations/config.bbnavi.js
@@ -47,6 +47,7 @@ export default configMerger(walttiConfig, {
         CITYBIKE_MAP: `${API_URL}/otp/routers/default/vectorTiles/citybikes/`,
         WEATHER_STATIONS_MAP: '', // `${API_URL}/map/v1/weather-stations/`,
         CHARGING_STATIONS_MAP: `https://api.ocpdb.de/tiles/`,
+        CHARGING_STATION_DETAILS_API: 'https://ocpdb.bbnavi.de/api/ocpi/2.2/location/',
         PELIAS: `${process.env.GEOCODING_BASE_URL || GEOCODING_BASE_URL}/search`,
         PELIAS_REVERSE_GEOCODER: `${
             process.env.GEOCODING_BASE_URL || GEOCODING_BASE_URL
