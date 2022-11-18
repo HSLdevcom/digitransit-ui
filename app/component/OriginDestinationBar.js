f18cbf338eff7869dd8e3560c9f3ebc2d7cf586a
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-11-18 08:49:07 +0200

    fix: remove bad prop which sneaked in from v3

--- app/component/OriginDestinationBar.js
+++ app/component/OriginDestinationBar.js
@@ -148,7 +148,7 @@ class OriginDestinationBar extends React.Component {
           refPoint={refPoint}
           originPlaceHolder="search-origin-index"
           destinationPlaceHolder="search-destination-index"
-          showMultiPointControls
+          showMultiPointControls={this.context.config.viaPointsEnabled}
           viaPoints={this.props.viaPoints}
           updateViaPoints={this.updateViaPoints}
           addAnalyticsEvent={addAnalyticsEvent}
@@ -171,7 +171,6 @@ class OriginDestinationBar extends React.Component {
           }
           modeSet={this.props.modeSet}
           onFocusChange={() => {}}
-          showViaPointEdit={this.context.config.viaPointsEnabled}
           filterResults={filter}
         />{' '}
       </div>
