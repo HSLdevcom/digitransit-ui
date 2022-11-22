cd1907180a65a1518cf8c1d297339f8008e97405
Viljami Nurminen <viljami.nurminen@cgi.com>
2022-11-22 15:04:51 +0200

    feat: create new components for suggested and alternative legs

---
+++ app/component/LegInfo.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import cx from 'classnames';
+import Link from 'found/Link';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import { intlShape } from 'react-intl';
+import RouteNumber from './RouteNumber';
+import { PREFIX_ROUTES, PREFIX_STOPS } from '../util/path';
+
+const LegInfo = (
+  { leg, hasNoShortName, mode, headsign, alertSeverityLevel, isAlternativeLeg },
+  { intl },
+) => {
+  return (
+    <div
+      className={cx('itinerary-transit-leg-route', {
+        'long-name': hasNoShortName,
+        'alternative-leg-suggestion': isAlternativeLeg,
+      })}
+    >
+      <Link
+        onClick={e => {
+          e.stopPropagation();
+        }}
+        to={
+          `/${PREFIX_ROUTES}/${leg.route.gtfsId}/${PREFIX_STOPS}/${leg.trip.pattern.code}/${leg.trip.gtfsId}`
+          // TODO: Create a helper function for generationg links
+        }
+        aria-label={`${intl.formatMessage({
+          id: mode.toLowerCase(),
+          defaultMessage: 'Vehicle',
+        })} ${leg.route && leg.route.shortName}`}
+      >
+        <span aria-hidden="true">
+          <RouteNumber
+            mode={mode.toLowerCase()}
+            alertSeverityLevel={alertSeverityLevel}
+            color={leg.route ? `#${leg.route.color}` : 'currentColor'}
+            text={leg.route && leg.route.shortName}
+            realtime={false}
+            withBar
+            fadeLong
+          />
+        </span>
+      </Link>
+      <div className="headsign">{headsign}</div>
+      <span className="realtime">{moment(leg.startTime).format('HH:mm')}</span>
+    </div>
+  );
+};
+
+LegInfo.propTypes = {
+  leg: PropTypes.object.isRequired,
+  hasNoShortName: PropTypes.bool.isRequired,
+  mode: PropTypes.string.isRequired,
+  headsign: PropTypes.string.isRequired,
+  alertSeverityLevel: PropTypes.object.isRequired,
+  isAlternativeLeg: PropTypes.bool.isRequired,
+};
+
+LegInfo.contextTypes = {
+  intl: intlShape.isRequired,
+};
+
+export default LegInfo;
