ff86bece69953dd413a9371927102ba91612560f
Jannis R <mail@jannisr.de>
2023-01-25 16:32:24 +0100

    remove .github.disabled directory

--- .github.disabled/workflows/scripts/slack_message.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -X POST -H 'Content-type: application/json' --data '{"username":"npm publisher","text":"'"${PUBLISHED_PACKAGES}"'\n", "channel": "reittiopas-git"}' ${SLACK_WEBHOOK_URL}
