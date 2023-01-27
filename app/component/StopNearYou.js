85d1cbc0ffe521371f752476b10947a9cfda72c2
Topi Koivunen <topik@debian-BULLSEYE-live-builder-AMD64>
2023-01-27 11:17:58 +0200

    fix: encode near you stop id DT-5678

--- app/component/StopNearYou.js
+++ app/component/StopNearYou.js
@@ -33,9 +33,10 @@ const StopNearYou = ({
   const isStation = !!stop.parentStation || !!stopId;
   const gtfsId =
     (stop.parentStation && stop.parentStation.gtfsId) || stop.gtfsId;
+  const urlEncodedGtfsId = gtfsId.replace('/', '%2F');
   const linkAddress = isStation
-    ? `/${PREFIX_TERMINALS}/${gtfsId}`
-    : `/${PREFIX_STOPS}/${gtfsId}`;
+    ? `/${PREFIX_TERMINALS}/${urlEncodedGtfsId}`
+    : `/${PREFIX_STOPS}/${urlEncodedGtfsId}`;
   return (
     <span role="listitem">
       <div className="stop-near-you-container">
