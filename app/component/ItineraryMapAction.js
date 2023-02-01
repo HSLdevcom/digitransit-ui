698d5adac4a85ad949aa4f3b62833274c95c38df
Holger Bruch <hb@mfdz.de>
2023-02-01 23:43:47 +0100

    feat: add support to disable itinary-map-actions via config setting

--- app/component/ItineraryMapAction.js
+++ app/component/ItineraryMapAction.js
@@ -4,19 +4,24 @@ import { intlShape } from 'react-intl';
 import { isKeyboardSelectionEvent } from '../util/browser';
 import Icon from './Icon';
 
-function ItineraryMapAction({ target, focusAction, ariaLabelId }, { intl }) {
+function ItineraryMapAction(
+  { target, focusAction, ariaLabelId },
+  { intl, config },
+) {
   /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
   return (
-    <div
-      className="itinerary-map-action"
-      onClick={focusAction}
-      onKeyPress={e => isKeyboardSelectionEvent(e) && focusAction(e)}
-      role="button"
-      tabIndex="0"
-      aria-label={intl.formatMessage({ id: ariaLabelId }, { target })}
-    >
-      <Icon img="icon-icon_show-on-map" className="itinerary-search-icon" />
-    </div>
+    config.showItineraryMapActions !== false && (
+      <div
+        className="itinerary-map-action"
+        onClick={focusAction}
+        onKeyPress={e => isKeyboardSelectionEvent(e) && focusAction(e)}
+        role="button"
+        tabIndex="0"
+        aria-label={intl.formatMessage({ id: ariaLabelId }, { target })}
+      >
+        <Icon img="icon-icon_show-on-map" className="itinerary-search-icon" />
+      </div>
+    )
   );
 }
 
@@ -31,6 +36,7 @@ ItineraryMapAction.defaultProps = {
 };
 
 ItineraryMapAction.contextTypes = {
+  config: PropTypes.object.isRequired,
   intl: intlShape.isRequired,
 };
 
