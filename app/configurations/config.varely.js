99c397dfdb79f94994877746862eee938ced2278
Joel Lappalainen <lappalj8@gmail.com>
2023-02-27 14:55:34 +0200

    refactor: use same env for varely OTP as others

--- app/configurations/config.varely.js
+++ app/configurations/config.varely.js
@@ -5,8 +5,7 @@ const CONFIG = 'varely';
 const API_URL = process.env.API_URL || 'https://dev-api.digitransit.fi';
 const APP_TITLE = 'Seutu+ reittiopas';
 const APP_DESCRIPTION = 'Varsinais-Suomen ELY-keskuksen reittiopas';
-const OTP_URL =
-  process.env.DEV_OTP_URL || `${API_URL}/routing/v2/routers/varely/`;
+const OTP_URL = process.env.OTP_URL || `${API_URL}/routing/v2/routers/varely/`;
 const MAP_URL =
   process.env.MAP_URL || 'https://digitransit-dev-cdn-origin.azureedge.net';
 const POI_MAP_PREFIX = `${MAP_URL}/map/v3/varely`;
