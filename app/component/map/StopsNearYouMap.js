4343851596fb634a825d0ca2cf9802abc3e7140f
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-03-16 08:50:10 +0200

    Revert "fix: remove cookiesettings button from map"
    This reverts commit b9705aeaa1e9a4af12dc8c205ba913f0c222fa76.

--- app/component/map/StopsNearYouMap.js
+++ app/component/map/StopsNearYouMap.js
@@ -30,6 +30,7 @@ import Loading from '../Loading';
 import LazilyLoad, { importLazy } from '../LazilyLoad';
 import { getDefaultNetworks } from '../../util/citybikes';
 import { getRouteMode } from '../../util/modeUtils';
+import CookieSettingsButton from '../CookieSettingsButton';
 
 const locationMarkerModules = {
   LocationMarker: () =>
@@ -462,6 +463,7 @@ function StopsNearYouMap(
   if (breakpoint === 'large') {
     return (
       <>
+        {context.config.useCookiesPrompt && <CookieSettingsButton />}
         <MapWithTracking {...mapProps} />
       </>
     );
