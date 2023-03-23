542d0448d67acb81cfd4b1e4321d16af242e4d67
Viljami Nurminen <viljami.nurminen@cgi.com>
2023-03-23 08:50:13 +0200

    chore: move styles from inline to itinerary.scss

--- app/component/RoutingFeedbackPrompt.js
+++ app/component/RoutingFeedbackPrompt.js
@@ -4,30 +4,13 @@ import PropTypes from 'prop-types';
 
 const RoutingFeedbackPrompt = (props, { config }) => {
   return config.useRoutingFeedbackPrompt ? (
-    <div
-      style={{
-        backgroundColor: '#f4f4f4',
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
+    <div className="routing-feedback-container">
       <div style={{ padding: '15px' }}>
         <FormattedMessage id="routing-feedback-header" />
       </div>
       <a
+        className="routing-feedback-link"
         href="https://link.webropol.com/s/otp2"
-        style={{
-          background: 'white',
-          width: '60%',
-          borderRadius: '50vh',
-          padding: '5px',
-          color: '#007ac9',
-          border: '1px solid #888',
-          textAlign: 'center',
-          textDecoration: 'none',
-          marginBottom: '30px',
-        }}
       >
         <FormattedMessage id="routing-feedback-link" />
       </a>
