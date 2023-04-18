a56d5580b3213c88d15023a54a8e6d16c5ff2423
Viljami Nurminen <viljami.nurminen@cgi.com>
2023-04-18 11:54:00 +0300

    chore: add new configurations and sass for Kela

---
+++ app/configurations/config.kela.js
@@ -0,0 +1,25 @@
+import configMerger from '../util/configMerger';
+
+const defaultConfig = require('./config.default').default;
+
+const APP_TITLE = 'Kelan matkalaskuri';
+const APP_DESCRIPTION = 'Kelan matkalaskuri';
+
+export default configMerger(defaultConfig, {
+  title: APP_TITLE,
+
+  meta: {
+    description: APP_DESCRIPTION,
+  },
+
+  transportModes: {
+    citybike: {
+      availableForSelection: false,
+      default: false,
+    },
+    airplane: {
+      availableForSelection: false,
+      default: true,
+    },
+  },
+});
