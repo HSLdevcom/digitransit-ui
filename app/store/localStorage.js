a22b193e0717acddeca840bfb302b6dacf17ea24
Janne Antikainen <antiik91@gmail.com>
2023-07-19 14:39:42 +0300

    chore: update localStorage

--- app/store/localStorage.js
+++ app/store/localStorage.js
@@ -132,6 +132,14 @@ export function setCustomizedSettings(data) {
       data.showBikeAndParkItineraries,
       oldSettings.showBikeAndParkItineraries,
     ),
+    forceCarRouting: getValueOrDefault(
+      data.forceCarRouting,
+      oldSettings.forceCarRouting,
+    ),
+    showCO2InItinerarySummary: getValueOrDefault(
+      data.showCO2InItinerarySummary,
+      oldSettings.showCO2InItinerarySummary,
+    ),
   };
 
   setItem('customizedSettings', newSettings);
