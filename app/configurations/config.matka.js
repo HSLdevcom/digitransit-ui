f5b9d9f67ea8ba22f1f6f960b2c1ebc27640449c
Joel Lappalainen <lappalj8@gmail.com>
2023-12-04 16:19:44 +0200

    chore: temporarily disable co2

--- app/configurations/config.matka.js
+++ app/configurations/config.matka.js
@@ -525,5 +525,5 @@ export default {
     },
   },
   // Notice! Turning on this setting forces the search for car routes (for the CO2 comparison only).
-  showCO2InItinerarySummary: true,
+  showCO2InItinerarySummary: false,
 };
