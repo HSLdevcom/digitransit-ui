5c1e56e14391884404f265477b6fdb67e118b346
Viljami Nurminen <viljami.nurminen@cgi.com>
2023-12-19 09:36:54 +0200

    chore: read (M) in stop names in lower case

--- app/component/TransitLeg.js
+++ app/component/TransitLeg.js
@@ -409,6 +409,7 @@ class TransitLeg extends React.Component {
           >
             <div className="itinerary-leg-row">
               <Link
+                aria-label={leg.from.name.toLowerCase()}
                 onClick={e => {
                   e.stopPropagation();
                   addAnalyticsEvent({
