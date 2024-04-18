679a078e571b2637d3211af9f220b5f36fa0e37d
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-04-18 13:08:55 +0300

    chore: remove vague test

--- test/unit/component/ItineraryLegs.test.js
+++ test/unit/component/ItineraryLegs.test.js
@@ -2,11 +2,9 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import React from 'react';
 
-import { mockContext, mockChildContextTypes } from '../helpers/mock-context';
-import { mountWithIntl, shallowWithIntl } from '../helpers/mock-intl-enzyme';
+import { mockContext } from '../helpers/mock-context';
+import { shallowWithIntl } from '../helpers/mock-intl-enzyme';
 import ItineraryLegs from '../../../app/component/ItineraryLegs';
-
-import data from '../test-data/dcw12';
 
 const context = {
   ...mockContext,
@@ -21,24 +19,6 @@ const context = {
 };
 
 describe('<ItineraryLegs />', () => {
-  it('should not fail to render even if the first leg is an intermediate place', () => {
-    const props = {
-      itinerary: {
-        endTime: data.firstLegIsAnIntermediatePlace[3].endTime,
-        legs: data.firstLegIsAnIntermediatePlace,
-      },
-      toggleCanceledLegsBanner: () => {},
-      focusToPoint: () => {},
-      focusToLeg: () => {},
-    };
-    const wrapper = mountWithIntl(<ItineraryLegs {...props} />, {
-      context,
-      childContextTypes: mockChildContextTypes,
-    });
-
-    expect(wrapper).to.have.lengthOf(1);
-  });
-
   it("should not fail to render even if the itinerary's legs array is empty", () => {
     const props = {
       itinerary: {
