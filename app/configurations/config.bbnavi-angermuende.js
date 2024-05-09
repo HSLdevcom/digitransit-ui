024fb288c75e6b883fb03c9786e8506dfe9e8fb0
Volker Kerkhoff <143070075+epam-vkerkhoff@users.noreply.github.com>
2024-05-09 12:38:21 +0200

    Matomo Tracking Angermünde

--- app/configurations/config.bbnavi-angermuende.js
+++ app/configurations/config.bbnavi-angermuende.js
@@ -4,7 +4,7 @@ import bbnaviConfig from './config.bbnavi';
 const CONFIG = 'bbnavi-angermuende';
 const APP_TITLE = 'bbnavi Angermünde';
 const HEADER_TITLE = 'Angermünde';
-const MATOMO_URL = 'https://nutzung.bbnavi.de/js/container_4BRoKipH.js';
+const MATOMO_URL = 'https://nutzung.bbnavi.de/js/container_e185O0Vx.js';
 
 export default configMerger(bbnaviConfig, {
   CONFIG,
