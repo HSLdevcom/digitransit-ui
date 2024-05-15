2badda4247e5b485e77ff8cd0f80869d3b663b1b
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-05-15 06:58:52 +0300

    Revert "chore: remove bike + transit from tampere before release"
    This reverts commit b56d0dc98260a9eac78646fb7c087ba18202ba5a.

--- app/configurations/config.tampere.js
+++ app/configurations/config.tampere.js
@@ -269,6 +269,7 @@ export default configMerger(walttiConfig, {
     },
   },
 
+  // enable train routing for Tampere
   transportModes: {
     rail: {
       availableForSelection: true,
@@ -282,11 +283,11 @@ export default configMerger(walttiConfig, {
       availableForSelection: true,
     },
   },
-  /*
+
   bikeBoardingModes: {
     RAIL: { showNotification: true },
     TRAM: { showNotification: true },
   },
-  */
+
   showTenWeeksOnRouteSchedule: true,
 });
