2d7e497393a0717e15cfa7918967681e3030855a
Janne Antikainen <antiik91@gmail.com>
2024-09-24 12:16:01 +0300

    refactor: content from NaviInfo to NaviStack

--- app/component/itinerary/NaviStack.js
+++ app/component/itinerary/NaviStack.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { intlShape } from 'react-intl';
+import { FormattedMessage, intlShape } from 'react-intl';
 import cx from 'classnames';
-import NaviInfo from './NaviInfo';
 import { legShape } from '../../util/shapes';
+import { timeStr } from '../../util/timeUtils';
+import Icon from '../Icon';
 
-const getInfo = nextLeg => {
-  const { realTimeState, start, realTime, to } = nextLeg;
+const getInfo = (nextLeg, canceled, transferProblem, intl) => {
+  const { start, realTime, to, mode } = nextLeg;
   const { estimatedTime, scheduledTime } = start;
+  const { parentStation, name } = to.stop;
 
-  if (realTimeState === 'CANCELED') {
+  const localizedMode = intl.formatMessage({
+    id: `${mode.toLowerCase()}`,
+    defaultMessage: `${mode}`,
+  });
+
+  if (canceled) {
     // Todo: No current design
     return {
+      content: <div className="notifiler">Osa matkan lähdöistä on peruttu</div>,
       backgroundColor: '#DC0451',
       iconColor: '#888',
       iconId: 'icon-icon_caution_white_exclamation',
     };
   }
+
+  if (transferProblem !== null) {
+    // todo no current design
+    return {
+      content: (
+        <div className="notifiler">{`Vaihto ${transferProblem[0].route.shortName} - ${transferProblem[1].route.shortName} ei onnistu reittisuunnitelman mukaisesti`}</div>
+      ),
+      iconId: 'icon-icon_caution_white_exclamation',
+      iconColor: '#888',
+    };
+  }
   if (start.estimate?.delay > 0) {
     // Todo: No current design
     return {
+      content: <div className="notifiler">Kulkuneuvo on myöhässä</div>,
       backgroundColor: '#FDF3F6',
       iconColor: '#DC0451',
       iconId: 'icon-icon_delay',
@@ -28,42 +48,63 @@ const getInfo = nextLeg => {
 
   if (!realTime) {
     return {
+      content: (
+        <div className="navi-info-content">
+          <FormattedMessage id="navileg-mode-schedule" />
+          <FormattedMessage
+            id="navileg-start-schedule"
+            values={{
+              time: timeStr(scheduledTime),
+              mode: localizedMode,
+            }}
+          />{' '}
+        </div>
+      ),
       backgroundColor: '#FFF8E8',
       iconColor: '#FED100',
       iconId: 'icon-icon_info',
-      scheduledTime,
-      stopName: to.stop.name,
     };
   }
+  const stopOrStation = parentStation
+    ? intl.formatMessage({ id: 'from-station' })
+    : intl.formatMessage({ id: 'from-stop' });
 
   return {
+    content: (
+      <div className="navi-info-content">
+        <FormattedMessage id="navileg-mode-realtime" values={{ mode }} />
+        <FormattedMessage
+          id="navileg-start-realtime"
+          values={{
+            time: timeStr(estimatedTime),
+            stopOrStation,
+            stopName: name,
+          }}
+        />
+      </div>
+    ),
     backgroundColor: '##0074BF',
     iconColor: '#FFF',
     iconId: 'icon-icon_info',
-    estimatedTime,
-    parentStation: to.stop.parentStation,
   };
 };
 
 const NaviStack = ({ transferProblem, nextLeg, canceled, show }, { intl }) => {
-  const info = getInfo(nextLeg);
-  const mode = nextLeg.mode.toLowerCase();
-  const localizedMode = intl.formatMessage({
-    id: `${mode}`,
-    defaultMessage: `${mode}`,
-  });
+  const info = getInfo(nextLeg, canceled, transferProblem, intl);
+
   return (
     <div
       style={{ backgroundColor: info.backgroundColor }}
       className={cx('info-stack', show ? 'slide-in' : 'slide-out')}
     >
-      <NaviInfo
-        transferProblem={transferProblem}
-        canceled={canceled}
-        nextLeg={nextLeg}
-        mode={localizedMode}
-        {...info}
+      <Icon
+        img={info.iconId}
+        height={1}
+        width={1}
+        className="info-icon"
+        color={info.iconColor}
       />
+      {info.content}
     </div>
   );
 };
