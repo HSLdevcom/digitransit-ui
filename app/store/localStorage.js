feaf1da5d4a2f8079288a5748677c36b3858d214
Vesa Meskanen <vesa@realsoft.com>
2024-10-08 16:55:50 +0200

    fix: update old search filtering
    hsl.fi no longer pushes current location to local store, but it does save
    SelectFromMap selections. Filter them away as early as possible i.e. when
    reading local storage, until they fix the bug.

--- app/store/localStorage.js
+++ app/store/localStorage.js
@@ -202,7 +202,13 @@ export function getFavouriteRoutesStorage() {
 }
 
 export function getOldSearchesStorage() {
-  return getItemAsJson('saved-searches', '{"items": []}');
+  const storage = getItemAsJson('saved-searches', '{"items": []}');
+  return {
+    ...storage,
+    items: storage.items.filter(
+      search => search.item.address !== 'SelectFromMap',
+    ),
+  };
 }
 
 export function setOldSearchesStorage(data) {
