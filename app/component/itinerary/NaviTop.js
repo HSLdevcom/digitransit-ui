2b421708317237fe5f71d0983db7c09f4659daf3
Janne Antikainen <antiik91@gmail.com>
2024-10-08 09:10:08 +0300

    merge

--- app/component/itinerary/NaviTop.js
+++ app/component/itinerary/NaviTop.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, intlShape } from 'react-intl';
-import { itineraryShape, legShape, configShape } from '../../util/shapes';
+import { legShape, configShape } from '../../util/shapes';
 import { legTime, legTimeStr } from '../../util/legUtils';
 import NaviLeg from './NaviLeg';
 import Icon from '../Icon';
@@ -9,6 +9,12 @@ import NaviStack from './NaviStack';
 import { timeStr } from '../../util/timeUtils';
 
 const TRANSFER_SLACK = 60000;
+
+function getFirstLastLegs(legs) {
+  const first = legs[0];
+  const last = legs[legs.length - 1];
+  return { first, last };
+}
 
 function findTransferProblem(legs) {
   for (let i = 1; i < legs.length - 1; i++) {
@@ -185,16 +191,14 @@ const getAlerts = (realTimeLegs, intl) => {
   return alerts;
 };
 
-function NaviTop(
-  { itinerary, focusToLeg, time, realTimeLegs },
-  { intl, config },
-) {
+function NaviTop({ focusToLeg, time, realTimeLegs }, { intl, config }) {
   const [currentLeg, setCurrentLeg] = useState(null);
   const [show, setShow] = useState(true);
   // All notifications including those user has dismissed.
   const [notifications, setNotifications] = useState([]);
   // notifications that are shown to the user.
   const [displayNotifs, setdisplayNotifs] = useState([]);
+  const focusRef = useRef(false);
 
   const handleClick = () => {
     setShow(!show);
@@ -219,7 +223,7 @@ function NaviTop(
       : currentLeg?.mode === newLeg?.mode;
     const l = currentLeg || newLeg;
     if (l) {
-      nextLeg = itinerary.legs.find(
+      nextLeg = realTimeLegs.legs.find(
         leg => legTime(leg.start) > legTime(l.start),
       );
       if (nextLeg) {
@@ -236,7 +240,7 @@ function NaviTop(
         // remove Old main notification when new leg is started.
         currentNots = currentNots.filter(n => n.type !== 'main');
         if (newLeg) {
-          focusToLeg(newLeg, false);
+          focusToLeg?.(newLeg);
           setCurrentLeg(newLeg);
         }
         const problems = getAlerts(realTimeLegs, intl);
@@ -247,6 +251,21 @@ function NaviTop(
           notifs.push(newAlerts);
         }
       }
+
+      if (!focusRef.current && focusToLeg) {
+        // handle initial focus when not tracking
+        if (newLeg) {
+          focusToLeg(newLeg);
+        } else {
+          const { first, last } = getFirstLastLegs(realTimeLegs);
+          if (time < legTime(first.start)) {
+            focusToLeg(first);
+          } else {
+            focusToLeg(last);
+          }
+        }
+        focusRef.current = true;
+      }
       if (notifs.length > 0 || currentNots.length < displayNotifs.length) {
         const combined = currentNots.concat(...notifs);
         setdisplayNotifs(combined);
@@ -256,8 +275,7 @@ function NaviTop(
     }
   }, [time]);
 
-  const first = realTimeLegs[0];
-  const last = realTimeLegs[realTimeLegs.length - 1];
+  const { first, last } = getFirstLastLegs(realTimeLegs);
 
   let info;
   if (time < legTime(first.start)) {
@@ -269,7 +287,7 @@ function NaviTop(
     );
   } else if (currentLeg) {
     if (!currentLeg.transitLeg) {
-      nextLeg = itinerary.legs.find(
+      nextLeg = realTimeLegs.legs.find(
         leg => legTime(leg.start) > legTime(currentLeg.start),
       );
       info = <NaviLeg leg={currentLeg} nextLeg={nextLeg} />;
@@ -312,7 +330,6 @@ function NaviTop(
 }
 
 NaviTop.propTypes = {
-  itinerary: itineraryShape.isRequired,
   focusToLeg: PropTypes.func.isRequired,
   time: PropTypes.number.isRequired,
   realTimeLegs: PropTypes.arrayOf(legShape).isRequired,
