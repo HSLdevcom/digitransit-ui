bd0b7a27778c3e1d321a7729839e5e86bffe8d7e
sharhio <sade.harhio@cgi.com>
2025-06-04 12:40:54 +0300

    DT-6929 replacement bus infos

--- app/configurations/config.waltti.js
+++ app/configurations/config.waltti.js
@@ -330,8 +330,8 @@ export default {
       id: 'replacementBusNotification',
       header: {
         fi: 'Korvaava bussi',
-        en: 'Korvaava bussi',
-        sv: 'Korvaava bussi',
+        en: 'Replacement bus',
+        sv: 'Ersättande buss',
       },
       content: {
         fi: [
@@ -340,25 +340,15 @@ export default {
           'Linja käyttää valikoituja pysäkkejä, eli bussi ei pysähdy kaikilla pysäkeillä.',
         ],
         en: [
-          'Voit nousta kyytiin myös bussin keskiovista.',
-          'Pysäkit on merkitty punaisilla tunnuksilla.',
-          'Linja käyttää valikoituja pysäkkejä, eli bussi ei pysähdy kaikilla pysäkeillä.',
+          'You can also board the bus through the middle doors.',
+          'The stops are marked with red signs.',
+          'The bus stops only at designated stops and does not serve all stops.',
         ],
         sv: [
-          'Voit nousta kyytiin myös bussin keskiovista.',
-          'Pysäkit on merkitty punaisilla tunnuksilla.',
-          'Linja käyttää valikoituja pysäkkejä, eli bussi ei pysähdy kaikilla pysäkeillä.',
+          'Du kan också stiga på bussen genom mittdörren.',
+          'Hållplatserna är markerade med röda punkter.',
+          'Linjen stannar endast vid vissa hållplatser, dvs. bussen stannar inte vid alla hållplatser.',
         ],
-      },
-      closeButtonLabel: {
-        fi: '',
-        en: '',
-        sv: '',
-      },
-      link: {
-        fi: '',
-        en: '',
-        sv: '',
       },
     },
   ],
