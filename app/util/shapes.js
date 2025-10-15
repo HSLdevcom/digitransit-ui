0a18ef74916d3f5c853b505a6ae3d9875ae842e2
Simo Partinen <simo.partinen@twoday.com>
2025-10-15 16:15:26 +0300

    chore: additional shapes

--- app/util/shapes.js
+++ app/util/shapes.js
@@ -15,11 +15,8 @@ export const alertShape = PropTypes.shape({
   alertSeverityLevel: PropTypes.string,
   alertUrl: PropTypes.string,
   id: PropTypes.string,
-  entities: PropTypes.arrayOf(
-    PropTypes.shape({
-      __typename: PropTypes.string.isRequired,
-    }),
-  ),
+  // eslint-disable-next-line no-use-before-define
+  entities: PropTypes.arrayOf(() => entityShape),
 });
 
 export const childrenShape = PropTypes.oneOfType([
@@ -432,3 +429,29 @@ export const pickupBookingInfoShape = PropTypes.shape({
   }),
   message: PropTypes.string,
 });
+
+export const stopOnRouteShape = PropTypes.shape({
+  route: routeShape,
+});
+
+export const entityShape = (props, propName, componentName, ...rest) => {
+  const entity = props[propName];
+  if (!entity || typeof entity !== 'object') {
+    return null;
+  }
+  /* eslint-disable no-underscore-dangle */
+  switch (entity.__typename) {
+    case 'Route':
+      return routeShape(props, propName, componentName, ...rest);
+    case 'Stop':
+      return stopShape(props, propName, componentName, ...rest);
+    case 'StopOnRoute':
+      return stopOnRouteShape(props, propName, componentName, ...rest);
+    case 'Unknown':
+      return null;
+    default:
+      return new Error(
+        `Invalid entity type '${entity.__typename}' in ${componentName}`,
+      );
+  }
+};
