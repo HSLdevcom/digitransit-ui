1c25cf47c18f34dd4c6cea880673d2b7cae6c517
Henrik Sundell <henrik.sundell@gmail.com>
2025-10-25 16:59:02 +0300

    Increase default transfer time for tampere

--- app/configurations/config.tampere.js
+++ app/configurations/config.tampere.js
@@ -371,4 +371,9 @@ export default configMerger(walttiConfig, {
     showParkAndRide: true,
     showParkAndRideForBikes: true,
   },
+
+  defaultSettings: {
+    ...walttiConfig.defaultSettings,
+    minTransferTime: 180,
+  },
 });
