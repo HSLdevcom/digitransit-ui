761f96fd3577b9e866bcc6538bceb37b5c5aef6f
Vesa Meskanen <vesa@realsoft.com>
2025-12-03 08:36:35 +0200

    chore: survey freq to 1/500

--- app/util/analyticsUtils.js
+++ app/util/analyticsUtils.js
@@ -63,7 +63,7 @@ export function getAnalyticsInitCode(config, req) {
       );
     }
     if (config.crazyEgg) {
-      const surveyShare = process.env.SURVEY_SHARE || 2;
+      const surveyShare = process.env.SURVEY_SHARE || 500;
 
       const lang = cookies.get('lang');
       let id;
