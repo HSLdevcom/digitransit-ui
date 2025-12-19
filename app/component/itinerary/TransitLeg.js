f5da0d45103574ec58a8c75f44ccfa47a95e234b
sharhio <sade.harhio@cgi.com>
2025-12-19 12:06:33 +0200

    fix separate time and text

--- app/component/itinerary/TransitLeg.js
+++ app/component/itinerary/TransitLeg.js
@@ -481,7 +481,7 @@ class TransitLeg extends React.Component {
           </span>
           <span aria-hidden="true">
             <div className="itinerary-time-column-time">
-              {isCallAgencyLeg(leg) && <FormattedMessage id="estimate" />}
+              {isCallAgencyLeg(leg) && <FormattedMessage id="estimate" />}{' '}
               <span className={cx({ realtime: leg.realTime })}>
                 <span className={cx({ canceled: legHasCancelation(leg) })}>
                   {time}
