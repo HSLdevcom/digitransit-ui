5d43c2988a23be20a69c3a73f2253b7682eabbd6
Heikki Vuorinen <hjvuor@icloud.com>
2026-01-15 10:40:16 +0200

    turku new default transfer time

--- app/configurations/config.turku.js
+++ app/configurations/config.turku.js
@@ -104,6 +104,11 @@ export default configMerger(walttiConfig, {
     },
   },
 
+  defaultSettings: {
+    ...walttiConfig.defaultSettings,
+    minTransferTime: 5 * 60,
+  },
+
   transportModes: {
     bus: {
       color: '#e8aa27',
