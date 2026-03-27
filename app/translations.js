bdfa273133cd9537d670f4a6807b63da7f63846e
sharhio <sade.harhio@cgi.com>
2026-03-27 09:31:01 +0200

    add missing translation

--- app/translations.js
+++ app/translations.js
@@ -5094,6 +5094,7 @@ const translations = {
     'biking-speed': 'Cykling hastighet',
     'book-a-lift':
       'Tilaa kyyti sovelluksella ja jää odottamaan sen saapumista:',
+    'booking-method': 'Beställningssätt',
     bus: 'Buss',
     'bus-express': 'Stombuss',
     'bus-express-with-route-number': 'Stombuss {routeNumber} {headSign}',
