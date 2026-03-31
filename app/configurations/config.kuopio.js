dc130a988544228d5237d72656bc066ae1dae987
Heikki Vuorinen <hjvuor@icloud.com>
2026-03-31 11:13:28 +0300

    change kuopio appName to vilkkuapp

--- app/configurations/config.kuopio.js
+++ app/configurations/config.kuopio.js
@@ -41,6 +41,7 @@ export default configMerger(walttiConfig, {
   showTicketPrice: true,
   showTicketInformation: true,
   primaryAgencyName: 'Kuopion seudun joukkoliikenne',
+  appName: 'vilkkuapp',
 
   ticketLink: {
     fi: 'https://vilkku.kuopio.fi/lipputyypit-hinnat/lippujen-hinnat',
