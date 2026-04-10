ea095a4b4cb5b5616965b497d28fc6f22be98875
Vesa Meskanen <vesa@realsoft.com>
2026-04-10 10:36:10 +0300

    Revert "Merge pull request #5723 from HSLdevcom/new-hsl-header"
    This reverts commit 5f507faa12ef4795b068a26824ff1493751d2134, reversing
    changes made to aff84bdf8706455b8c3e8cd83e29a543d61c8dbf.

--- app/configurations/config.hsl.js
+++ app/configurations/config.hsl.js
@@ -17,8 +17,7 @@ const BANNER_URL = process.env.CONTENT_DOMAIN
 const SUGGESTION_URL = process.env.CONTENT_DOMAIN
   ? `${process.env.CONTENT_DOMAIN}/api/v1/search/suggestions`
   : 'https://content.hsl.fi/api/v1/search/suggestions'; // old url
-const travelersAccountUrl = process.env.TRAVELERS_ACCOUNT_URL;
-const staticAssetsUrl = process.env.STATIC_ASSETS_URL;
+
 const virtualMonitorBaseUrl = IS_DEV()
   ? 'https://dev-hslmonitori.digitransit.fi'
   : 'https://omatnaytot.hsl.fi';
@@ -68,8 +67,6 @@ export default {
     FONT: 'https://www.hsl.fi/fonts/784131/6C5FB8083F348CFBB.css',
     FONTCOUNTER: 'https://cloud.typography.com/6364294/7432412/css/fonts.css',
     ROOTLINK: rootLink,
-    TRAVELERS_ACCOUNT: travelersAccountUrl,
-    STATIC_ASSETS: staticAssetsUrl,
     BANNERS: BANNER_URL,
     HSL_FI_SUGGESTIONS: SUGGESTION_URL,
     EMBEDDED_SEARCH_GENERATION: '/reittiopas-elementti',
