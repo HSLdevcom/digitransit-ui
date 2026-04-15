f09188edd3461738fd664d25ce1dc6c4833270b8
Janne Antikainen <antiik91@gmail.com>
2026-04-15 08:13:03 +0300

    fix test

--- test/unit/helpers/babel-register.js
+++ test/unit/helpers/babel-register.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 require('@babel/register')({
   // This will override `node_modules` ignoring - you can alternatively pass
   // an array of strings to be explicitly matched or a regex / glob
@@ -5,3 +6,37 @@ require('@babel/register')({
     /node_modules\/(?!react-leaflet|@babel\/runtime\/helpers\/esm|lodash-es|@digitransit-util|@digitransit-component)/,
   ],
 });
+
+// Prevent Node.js from trying to parse CSS files as JavaScript
+require.extensions['.css'] = () => {};
+
+// Stub out @hsl-fi packages that are ESM-only — they can't be require()'d by
+// Node's CJS loader (ERR_REQUIRE_ESM). Unit tests don't need the real
+// implementations; stubs are sufficient for shallow rendering.
+const Module = require('module');
+
+const originalLoad = Module._load;
+Module._load = function hslFiStub(...args) {
+  const [request] = args;
+  if (request.startsWith('@hsl-fi/')) {
+    return new Proxy(
+      function StubComponent() {
+        return null;
+      },
+      {
+        get(target, prop) {
+          if (prop === '__esModule') {
+            return true;
+          }
+          if (prop === 'default') {
+            return target;
+          }
+          return function StubComponent() {
+            return null;
+          };
+        },
+      },
+    );
+  }
+  return originalLoad.apply(this, args);
+};
