afe92a9e73f16b7368432310cf713c43cf009aee
Heikki Vuorinen <hjvuor@icloud.com>
2026-05-13 15:24:07 +0300

    add transit group relax function to PlanConnection

--- app/util/planParamUtil.js
+++ app/util/planParamUtil.js
@@ -516,6 +516,9 @@ export function getPlanParams(
   const transferPenalty = relaxSettings
     ? defaultSettings.transferPenalty
     : settings.transferPenalty;
+
+  const { transitGroupRelaxFunction } = defaultSettings;
+
   const timeStr = (time ? DateTime.fromSeconds(+time) : DateTime.now()).toISO({
     suppressMilliseconds: true,
   });
@@ -539,6 +542,7 @@ export function getPlanParams(
     walkReluctance,
     walkBoardCost,
     transferPenalty,
+    transitGroupRelaxFunction,
     modes,
     planType,
     noIterationsForShortTrips,
