268f4a67a2eb532b01e185368e2f6e8e8d9db92e
Heikki Vuorinen <hjvuor@icloud.com>
2026-05-13 15:24:07 +0300

    add transit group relax function to PlanConnection

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -194,6 +194,10 @@ export default {
     includeCarSuggestions: false,
     showBikeAndParkItineraries: false,
     includeTaxiSuggestions: false,
+    transitGroupRelaxFunction: {
+      constant: 300,
+      coefficient: 1.2,
+    },
   },
 
   /**
