081f98f13d03d67a8aeffb38a1eb2c8ca66cc4e6
Ville Pihlava <ville.pihlava@cgi.com>
2026-07-03 14:28:50 +0300

    Remove hsl config changes.

--- app/configurations/config.hsl.js
+++ app/configurations/config.hsl.js
@@ -112,7 +112,7 @@ export default {
   useRoutingFeedbackPrompt: true,
 
   feedIds: ['HSL', 'HSLlautta', 'Sipoo'],
-  externalFeedIds: ['HSLlautta', 'Uber', '02Taksi'],
+  externalFeedIds: ['HSLlautta', 'Uber'],
   externalFerryByStopCode: true, // no stop code means external ferry
 
   allowLogin: true,
@@ -760,12 +760,6 @@ export default {
     }, */
     infoLanguage: 'fi',
     settingLabelOverride: 'demand-responsive-traffic',
-  },
-
-  carPickupZone: {
-    enabled: true,
-    direct: false,
-    transit: true,
   },
 
   showRouteDescNotification: IS_DEV,
