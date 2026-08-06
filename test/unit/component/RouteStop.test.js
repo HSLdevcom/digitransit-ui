8dab42adf3800c616d53717ed3eea130da86c548
sharhio <sade.harhio@cgi.com>
2026-08-06 14:38:02 +0300

    add stop status tests for route and trip pages

--- test/unit/component/RouteStop.test.js
+++ test/unit/component/RouteStop.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { mountWithIntl } from '../helpers/mock-intl-enzyme';
 import RouteStop from '../../../app/component/routepage/RouteStop';
+import Icon from '../../../app/component/Icon';
 import ServiceAlertIcon from '../../../app/component/ServiceAlertIcon';
 import { AlertSeverityLevelType } from '../../../app/constants';
 
@@ -72,5 +73,56 @@ describe('<RouteStop />', () => {
     };
     const wrapper = mountWithIntl(<RouteStop {...props} />, { context });
     expect(wrapper.find(ServiceAlertIcon).isEmptyRender()).to.equal(true);
+  });
+
+  it('should not render a service alert icon when showStopStatusMarkers is enabled', () => {
+    const props = {
+      currentTime: 1471515614,
+      color: '',
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Warning,
+          },
+        ],
+      },
+    };
+    const context = {
+      config: {
+        CONFIG: 'default',
+        minutesToDepartureLimit: 0,
+        zones: { stops: true },
+        showStopStatusMarkers: true,
+      },
+    };
+    const wrapper = mountWithIntl(<RouteStop {...props} />, { context });
+    expect(wrapper.find(ServiceAlertIcon)).to.have.lengthOf(0);
+  });
+
+  it('should render the status badge icon instead of the SVG circle when the stop has no service', () => {
+    const props = {
+      currentTime: 1471515614,
+      color: '',
+      stop: {
+        alerts: [],
+        serviceToday: [],
+        stoptimesWithoutPatterns: [],
+      },
+    };
+    const context = {
+      config: {
+        CONFIG: 'default',
+        minutesToDepartureLimit: 0,
+        zones: { stops: true },
+        showStopStatusMarkers: true,
+      },
+    };
+    const wrapper = mountWithIntl(<RouteStop {...props} />, { context });
+    expect(
+      wrapper.find(Icon).filter('.route-stop-status-badge'),
+    ).to.have.lengthOf(1);
+    expect(wrapper.find('.route-stop-now_circleline circle')).to.have.lengthOf(
+      0,
+    );
   });
 });
--- test/unit/component/TripRouteStop.test.js
+++ test/unit/component/TripRouteStop.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { mountWithIntl } from '../helpers/mock-intl-enzyme';
 import TripRouteStop from '../../../app/component/routepage/TripRouteStop';
+import Icon from '../../../app/component/Icon';
 import ServiceAlertIcon from '../../../app/component/ServiceAlertIcon';
 import { AlertSeverityLevelType } from '../../../app/constants';
 
@@ -33,5 +34,77 @@ describe('<TripRouteStop />', () => {
       context: { config: { CONFIG: 'default', zones: { stops: true } } },
     });
     expect(wrapper.find(ServiceAlertIcon).isEmptyRender()).to.equal(true);
+  });
+
+  it('should not render a service alert icon when showStopStatusMarkers is enabled', () => {
+    const props = {
+      currentTime: 1471515614,
+      shortName: '',
+      distance: false,
+      mode: 'bus',
+      pattern: 'HSL:4444T:0:02',
+      route: 'HSL:4444T',
+      selectedVehicle: {},
+      color: '#000000',
+      stop: {
+        alerts: [
+          {
+            alertSeverityLevel: AlertSeverityLevelType.Warning,
+            effectiveStartDate: 1471515600,
+          },
+        ],
+      },
+      stoptime: {},
+      stopPassed: false,
+      vehicles: [],
+      setHumanScrolling: () => {},
+    };
+    const wrapper = mountWithIntl(<TripRouteStop {...props} />, {
+      context: {
+        config: {
+          CONFIG: 'default',
+          zones: { stops: true },
+          showStopStatusMarkers: true,
+        },
+      },
+    });
+    expect(wrapper.find(ServiceAlertIcon)).to.have.lengthOf(0);
+  });
+
+  it('should render the status badge icon instead of the SVG circle when the stop has no service', () => {
+    const props = {
+      currentTime: 1471515614,
+      shortName: '',
+      distance: false,
+      mode: 'bus',
+      pattern: 'HSL:4444T:0:02',
+      route: 'HSL:4444T',
+      selectedVehicle: {},
+      color: '#000000',
+      stop: {
+        alerts: [],
+        serviceToday: [],
+        stoptimesWithoutPatterns: [],
+      },
+      stoptime: {},
+      stopPassed: false,
+      vehicles: [],
+      setHumanScrolling: () => {},
+    };
+    const wrapper = mountWithIntl(<TripRouteStop {...props} />, {
+      context: {
+        config: {
+          CONFIG: 'default',
+          zones: { stops: true },
+          showStopStatusMarkers: true,
+        },
+      },
+    });
+    expect(
+      wrapper.find(Icon).filter('.route-stop-status-badge'),
+    ).to.have.lengthOf(1);
+    expect(wrapper.find('.route-stop-now_circleline circle')).to.have.lengthOf(
+      0,
+    );
   });
 });
