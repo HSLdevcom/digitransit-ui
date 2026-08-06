d5c76ed63e6ad651b31bb652d58e7282cbbbe463
sharhio <sade.harhio@cgi.com>
2026-08-06 14:17:24 +0300

    remove alert from the top of route page

--- app/component/routepage/RoutePage.js
+++ app/component/routepage/RoutePage.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useRef } from 'react';
-import connectToStores from 'fluxible-addons-react/connectToStores';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { FormattedMessage, useIntl } from 'react-intl';
 import cx from 'classnames';
@@ -10,11 +9,7 @@ import Icon from '../Icon';
 import RouteAgencyInfo from './RouteAgencyInfo';
 import RouteNumber from '../RouteNumber';
 import RouteControlPanel from './RouteControlPanel';
-import {
-  PREFIX_ROUTES,
-  PREFIX_DISRUPTION,
-  routePagePath,
-} from '../../util/path';
+import { PREFIX_ROUTES, PREFIX_DISRUPTION } from '../../util/path';
 import withBreakpoint from '../../util/withBreakpoint';
 import BackButton from '../BackButton';
 import { getRouteMode } from '../../util/modeUtils';
@@ -22,13 +17,6 @@ import {
   getModeIconColor,
   ensureColorAccessibleOnWhite,
 } from '../../util/colorUtils';
-import AlertBanner from '../AlertBanner';
-import {
-  hasEntitiesOfType,
-  hasMeaningfulData,
-  isAlertValid,
-} from '../../util/alertUtils';
-import { AlertEntityType } from '../../constants';
 import FavouriteRouteContainer from './FavouriteRouteContainer';
 import RouteNotificationButton from './RouteNotificationButton';
 import { useConfigContext } from '../../configurations/ConfigContext';
@@ -45,13 +33,7 @@ function resolveHeadsign(pattern) {
   return pattern.stops[pattern.stops.length - 1].name;
 }
 
-function RoutePage({
-  route,
-  match,
-  breakpoint,
-  error = undefined,
-  currentTime,
-}) {
+function RoutePage({ route, match, breakpoint, error = undefined }) {
   const intl = useIntl();
   const config = useConfigContext();
 
@@ -65,7 +47,7 @@ function RoutePage({
     throw error.message;
   }
 
-  const { tripId, patternId, routeId } = match.params;
+  const { tripId, patternId } = match.params;
 
   if (route == null) {
     /* In this case there is little we can do
@@ -83,9 +65,6 @@ function RoutePage({
   const selectedPattern =
     patternId && route.patterns.find(p => p.code === patternId);
   const headsign = resolveHeadsign(selectedPattern || null);
-  const filteredAlerts = selectedPattern?.alerts
-    ?.filter(alert => hasEntitiesOfType(alert, AlertEntityType.Route))
-    .filter(alert => isAlertValid(alert, currentTime));
   const matchingNotification = config.routeNotifications?.find(n =>
     n.showForRoute?.(route),
   );
@@ -158,14 +137,6 @@ function RoutePage({
             </div>
           )}
         </div>
-        {tripId && hasMeaningfulData(filteredAlerts) && (
-          <div className="trip-page-alert-container">
-            <AlertBanner
-              alerts={filteredAlerts}
-              linkAddress={routePagePath(routeId, PREFIX_DISRUPTION, patternId)}
-            />
-          </div>
-        )}
         <RouteAgencyInfo route={route} />
       </div>
       {route.patterns && match.params.type === PREFIX_DISRUPTION && (
@@ -184,78 +155,72 @@ RoutePage.propTypes = {
   match: matchShape.isRequired,
   breakpoint: PropTypes.string.isRequired,
   error: errorShape,
-  currentTime: PropTypes.number.isRequired,
 };
 
-const containerComponent = createFragmentContainer(
-  connectToStores(withBreakpoint(RoutePage), ['TimeStore'], context => ({
-    currentTime: context.getStore('TimeStore').getCurrentTime(),
-  })),
-  {
-    route: graphql`
-      fragment RoutePage_route on Route
-      @argumentDefinitions(date: { type: "String" }) {
+const containerComponent = createFragmentContainer(withBreakpoint(RoutePage), {
+  route: graphql`
+    fragment RoutePage_route on Route
+    @argumentDefinitions(date: { type: "String" }) {
+      gtfsId
+      color
+      shortName
+      longName
+      mode
+      type
+      ...RouteAgencyInfo_route
+      ...RoutePatternSelectContainer_route @arguments(date: $date)
+      agency {
+        name
+        phone
         gtfsId
-        color
-        shortName
-        longName
-        mode
-        type
-        ...RouteAgencyInfo_route
-        ...RoutePatternSelectContainer_route @arguments(date: $date)
-        agency {
-          name
-          phone
-          gtfsId
+      }
+      patterns {
+        alerts(types: [ROUTE, STOPS_ON_PATTERN]) {
+          id
+          alertDescriptionText
+          alertHash
+          alertHeaderText
+          alertSeverityLevel
+          alertUrl
+          effectiveEndDate
+          effectiveStartDate
+          entities {
+            __typename
+            ... on Route {
+              color
+              type
+              mode
+              shortName
+              gtfsId
+            }
+            ... on Stop {
+              name
+              code
+              vehicleMode
+              gtfsId
+            }
+          }
         }
-        patterns {
-          alerts(types: [ROUTE, STOPS_ON_PATTERN]) {
-            id
-            alertDescriptionText
-            alertHash
-            alertHeaderText
-            alertSeverityLevel
-            alertUrl
-            effectiveEndDate
-            effectiveStartDate
-            entities {
-              __typename
-              ... on Route {
-                color
-                type
-                mode
-                shortName
-                gtfsId
-              }
-              ... on Stop {
-                name
-                code
-                vehicleMode
-                gtfsId
-              }
-            }
+        headsign
+        code
+        stops {
+          name
+        }
+        trips: tripsForDate(serviceDate: $date) {
+          stoptimes: stoptimesForDate(serviceDate: $date) {
+            realtimeState
+            scheduledArrival
+            scheduledDeparture
+            serviceDay
           }
-          headsign
-          code
-          stops {
-            name
-          }
-          trips: tripsForDate(serviceDate: $date) {
-            stoptimes: stoptimesForDate(serviceDate: $date) {
-              realtimeState
-              scheduledArrival
-              scheduledDeparture
-              serviceDay
-            }
-          }
-          activeDates: trips {
-            serviceId
-            day: activeDates
-          }
+        }
+        activeDates: trips {
+          serviceId
+          day: activeDates
         }
       }
-    `,
-  },
-);
+    }
+  `,
+});
 
 export { containerComponent as default, RoutePage as Component };
