41bd19d0108e47d4a8c792a7735fc525b36ffd4c
Heikki Vuorinen <hjvuor@icloud.com>
2026-09-03 13:43:54 +0300

    filter out canceled trips from irrelevant feeds

--- app/component/trafficnow/CanceledTripsContainer.js
+++ app/component/trafficnow/CanceledTripsContainer.js
@@ -7,6 +7,9 @@ import CanceledTrips from './CanceledTrips';
 import CanceledTripsForModeQuery from './queries/CanceledTripsForModeQuery';
 import { favouriteShape } from '../../util/shapes';
 import { sortRoutes } from './utils';
+import { useFilterContext } from './filters/FiltersContext';
+import { splitGtfsId } from '../../util/gtfs';
+import { useConfigContext } from '../../configurations/ConfigContext';
 
 const CanceledTripsContainer = ({ mode, isMobile, favourites = [] }) => {
   const { canceledTripsSummary } = useLazyLoadQuery(CanceledTripsForModeQuery, {
@@ -14,10 +17,21 @@ const CanceledTripsContainer = ({ mode, isMobile, favourites = [] }) => {
     serviceDateRanges: [{ start: DateTime.now().toISODate(), end: null }],
   });
   const favRoutes = favourites.map(({ gtfsId }) => gtfsId);
+  const {
+    selectedFilters: { selectedFeeds },
+  } = useFilterContext();
+  const { feedIds } = useConfigContext();
+
+  const filteredRoutes = canceledTripsSummary.routes.filter(
+    route =>
+      (!selectedFeeds?.length &&
+        feedIds.includes(splitGtfsId(route.route.gtfsId)?.feedId)) ||
+      selectedFeeds?.includes(splitGtfsId(route.route.gtfsId)?.feedId),
+  );
 
   return (
     <CanceledTrips
-      canceledRoutes={sortRoutes(canceledTripsSummary.routes, favRoutes)}
+      canceledRoutes={sortRoutes(filteredRoutes, favRoutes)}
       mode={mode}
       isMobile={isMobile}
     />
