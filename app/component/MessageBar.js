bd40e36f035c639963fe26081bdc821409589d4f
Vesa Meskanen <vesa@realsoft.com>
2026-09-14 12:07:07 +0300

    fix: undefined default prop

--- app/component/MessageBar.js
+++ app/component/MessageBar.js
@@ -123,7 +123,7 @@ function MessageBar({
   messages,
   relayEnvironment,
   duplicateMessageCounter,
-  breakpoint = undefined,
+  breakpoint,
   markMessageAsRead = () => {},
 }) {
   const intl = useIntl();
